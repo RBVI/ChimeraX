4a624749c1c4d149ea8941008afdff71d8a33b27
Tom Goddard <goddard@sonic.net>
2013-10-11 14:18:52 -0700

    Made fetch code for individual databases save the database id with the data fetched for use in session files.

--- src/hydra/file_io/fetch.py
+++ src/hydra/file_io/fetch.py
@@ -5,8 +5,6 @@ def fetch_from_database(id, dbname):
     global databases
     if dbn in databases:
         mlist = databases[dbn].fetch_id(id)
-        for m in mlist:
-            m.database_fetch = (id, dbn)
     else:
         from ..ui.gui import show_status
         show_status('Unknown database %s' % dbname)
