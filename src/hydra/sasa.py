d972e8247129f9a777b70478387ad397264d1c5d
Tom Goddard <goddard@sonic.net>
2013-11-08 19:09:53 -0800

    Added calculation of analytic solvent accessible area and comparison to numerical estimate for testing.

--- src/hydra/sasa.py
+++ src/hydra/sasa.py
@@ -2,19 +2,22 @@
 
 def spheres_surface_area(spheres):
 
-    surf = sphere_model(spheres)
-    for p in surf.surface_pieces()[1:]:
-        p.color = (.7,.7,.7,.5)
-        p.display = False
+    s0 = spheres[0]
+    surf0 = sphere_model([s0])
+    surfn = sphere_model(spheres[1:])
+    for p in surfn.surface_pieces():
+        p.color = (.7,.7,.9,.7)
+#        p.display = False
 
     # Compute intersections with first sphere.
-    s0 = spheres[0]
     circles = []
     for s1 in spheres[1:]:
         c = sphere_intersection(s0, s1)
         if not c is None:
             circles.append(c)
-    draw_circles(circles, s0, surf, width = 0.01, offset = 0.01)
+    from .surface import Surface
+    surfc = Surface('circles')
+    draw_circles(circles, s0, surfc, width = 0.01, offset = 0.01)
 
     # Compute intersections of circles on sphere.
     cint = []
@@ -24,28 +27,56 @@ def spheres_surface_area(spheres):
             if pts:
                 cint.append(Circle_Intersection(c0,c1,pts[0]))
                 cint.append(Circle_Intersection(c1,c0,pts[1]))
-    draw_sphere_points([ci.point for ci in cint], s0, surf, (.8,.2,0,1))
+    surfi = Surface('intersections')
+    draw_sphere_points([ci.point for ci in cint], s0, surfi, (.8,.2,0,1), offset = 0.02)
 
     # Count disjoint regions in union of circles
     reg = {}
-#    for ci in cint:
-#        ci.circle1
+    for ci in cint:
+        c1,c2 = ci.circle1, ci.circle2
+        c1r, c2r = reg.get(c1, (c1,)), reg.get(c2, (c2,))
+        if not c1r is c2r:
+            r = c1r + c2r
+            for c in c1r:
+                reg[c] = r
+            for c in c2r:
+                reg[c] = r
+    nreg = len(set(reg.values()))
+    print(nreg, 'disjoint multicircle regions')
 
-    # Find circles with no intercepts.
+    # Find circles with no intercepts and not contained within other circles.
     lc = set(circles)
     for ci in cint:
         lc.discard(ci.circle1)
         lc.discard(ci.circle2)
+    for c in tuple(lc):
+        if circle_in_circles(c, circles):
+            lc.discard(c)
+    print(len(lc), 'lone circles')
 
     # Remove circle intersections that are inside another circle.
     bndry = [ci for ci in cint if not in_circles(ci, circles)]
-    draw_sphere_points([ci.point for ci in bndry], s0, surf, (.2,.8,0,1), 0.03)
+    surfbp = Surface('bndry pts')
+    draw_sphere_points([ci.point for ci in bndry], s0, surfbp, (.2,.8,0,1), offset = 0.03)
 
     # Connect arcs to form boundary paths.
     paths = boundary_paths(bndry)
+    surfb = Surface('boundary')
     for bp in paths:
-        draw_boundary(bp, s0, surf, color = (.8,.5,.5,1), width = 0.01, offset = 0.02)
-    draw_circles(tuple(lc), s0, surf, color = (.8,.5,.5,1), width = 0.01, offset = 0.02)
+        draw_boundary(bp, s0, surfb, color = (.8,.5,.5,1), width = 0.01, offset = 0.02)
+    draw_circles(tuple(lc), s0, surfb, color = (.8,.5,.5,1), width = 0.01, offset = 0.02)
+    print(len(paths) + len(lc), 'boundaries for', nreg + len(lc), 'regions')
+
+    area = bounded_area(paths, nreg, lc)
+    print('area =', area)
+
+#    for i in range(5):
+    for i in range(1):
+        ea = estimate_area(spheres[1:])
+        print('est area =', ea)
+
+    from .ui.gui import main_window
+    main_window.view.add_models((surf0, surfn, surfc, surfi, surfbp, surfb))
 
 def draw_boundary(bp, sphere, surf, color, width, offset):
 
@@ -155,6 +186,16 @@ def circle_intercepts(c0, c1):
     
     return (a*c0.center + b*c1.center - d*x01,
             a*c0.center + b*c1.center + d*x01)
+
+def circle_in_circles(c, circles):
+    from .geometry.vector import inner_product
+    from math import cos
+    for c2 in circles:
+        if (not c is c2 and
+            c2.angle >= c.angle and
+            inner_product(c.center, c2.center) >= cos(c2.angle-c.angle)):
+            return True
+    return False
 
 def draw_circles(circles, sphere, s, offset, width, color = (0,.2,.9,1)):
     cs, r = sphere
@@ -269,8 +310,6 @@ def sphere_model(spheres, ntri = 2000):
         p = s.newPiece()
         p.geometry = va, ta
         p.normals = na
-    from .ui.gui import main_window
-    main_window.view.add_model(s)
 
     return s
 
@@ -282,7 +321,8 @@ def random_spheres(n):
     from random import uniform, gauss
     from .geometry.vector import distance
     while len(spheres) < n:
-        r = uniform(0.5,1)
+#        r = uniform(0.5,1)
+        r = uniform(0.25,.5)
         center = array((gauss(0,1), gauss(0,1), gauss(0,1)), float64)
         d = distance(center, c0)
         if d > r+1 or d+r < 1 or r-d > 1:
@@ -290,6 +330,84 @@ def random_spheres(n):
         spheres.append((center, r))
     return spheres
 
-def test_sasa(n = 5):
+def bounded_area(paths, nreg, lone_circles):
+
+    from math import cos, pi
+    la = sum((2*pi*(1-cos(c.angle)) for c in lone_circles), 0)
+    pa = 0
+    for path in paths:
+        n = len(path)
+        ba = 0
+        for i in range(n):
+            bp1 = path[i]
+            p, c1, c2 = bp1.point, bp1.circle1, bp1.circle2
+            ia = circle_intercept_angle(c1.center, p, c2.center)
+            ba += ia - 2*pi
+            bp2 = path[(i+1)%n]
+            a = circle_arc_angle(p, bp2.point, c2.center)
+            print('seg', i, 'kink', (ia - 2*pi)*180/pi, 'arc', a*180/pi)
+            ba += a*cos(bp1.circle2.angle) # circular segment bend angle
+        print('path length', n, 'area', 2*pi-ba)
+        pa += 2*pi - ba
+    if len(paths) > nreg:
+        pa -= 4*pi*(len(paths)-nreg)
+    area = la + pa
+    return area
+
+def circle_arc_angle(p1, p2, center):
+    from .geometry.place import orthonormal_frame
+    a = polar_angle(orthonormal_frame(center, xdir = p1).transpose()*p2)
+    return a
+
+def circle_intercept_angle(center1, pintersect, center2):
+    from .geometry.vector import cross_product
+    t1 = cross_product(center1, pintersect)
+    t2 = cross_product(center2, pintersect)
+    from .geometry.place import orthonormal_frame
+    a = polar_angle(orthonormal_frame(center1, xdir = t1).transpose()*t2)
+    return a
+
+def estimate_area(spheres, npts = 100000):
+#    from .molecule.molecule import sphere_geometry
+#    va, na, ta = sphere_geometry(2*npts)
+    # TODO: oops icosahedral subdivision does not give uniform density subdivision
+# Gaussian distribution is not giving correct area, 20% error, not sure why.
+#    from numpy import random, sqrt
+#    va = random.normal(size = (npts,3))
+#    vlen = sqrt((va*va).sum(axis=1))
+#    for a in (0,1,2):
+#        va[:,a] /= vlen
+    va, w = sphere_points_and_weights(2*npts)
+    from numpy import zeros, int32, logical_or
+    n = len(va)
+    inside = zeros((n,), int32)
+    for center, radius in spheres:
+        d = va - center
+        logical_or(inside, (d*d).sum(axis = 1) <= radius*radius, inside)
+    from math import pi
+#    a = 4*pi*float(sum(inside)) / n
+    a = 4*pi*sum(inside*w)/w.sum()
+    return a
+
+def sphere_points_and_weights(ntri):
+  from .geometry import icosahedron
+  va, ta = icosahedron.icosahedron_geometry()
+  from numpy import int32, sqrt
+  ta = ta.astype(int32)
+  from . import _image3d
+  while 4*len(ta) <= ntri:
+    va, ta = _image3d.subdivide_triangles(va, ta)
+  vn = sqrt((va*va).sum(axis = 1))
+  for a in (0,1,2):
+    va[:,a] /= vn
+  w = 1.0 / vn*vn
+  return va, w
+        
+def test_sasa(n = 30):
     spheres = random_spheres(n)
+#    from numpy import array
+#    spheres = [(array((0.0,0,0)), 1), (array((1.0,0,0)),.5), (array((1.0,0,0)),.25)]       # Nested circle test
+    from numpy import array
+#    spheres = [(array((0.0,0,0)), 1), (array((1.0,0,0)),1.0)] # area test, pi
+    spheres = [(array((0.0,0,0)), 1), (array((1.0,0,0)),1.0), (array((0,1.0,0)),1.0)] # area test
     spheres_surface_area(spheres)
