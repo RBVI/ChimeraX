8043d7231edc88fb5fc4dea88b0cf506fd062538
Tom Goddard <goddard@sonic.net>
2013-11-11 01:10:47 -0800

    Optimizing and simplifying for eventual port to C++.

--- src/hydra/sasa.py
+++ src/hydra/sasa.py
@@ -45,11 +45,40 @@ def buried_sphere_area(i, centers, radii, draw = False):
     area *= r*r
 
     # Compute numerical estimate of buried area.
-    ea = estimate_buried_area(i, centers, radii)
+#    ea = estimate_buried_area(i, centers, radii)
 
-    print('area =', area, 'est area =', ea)
+#    print('area =', area, 'est area =', ea)
 
     return area
+
+def sphere_in_another_sphere(i, centers, radii):
+    c, r = centers[i], radii[i]
+    d = centers - c
+    from numpy import sqrt
+    inside = (sqrt((d*d).sum(axis=1)) + r <= radii)
+    return inside[:i].any() or inside[i+1:].any()
+
+def sphere_intersection(c0, r0, c1, r1):
+
+    from .geometry.vector import distance
+    d = distance(c0,c1)
+    if d > r0+r1 or r1+d < r0 or r0+d < r1:
+        return None
+    ca = (r0*r0 + d*d - r1*r1) / (2*r0*d)
+    if ca < -1 or ca > 1:
+        return None
+    import math
+    a = math.acos(ca)
+    c = (c1-c0)/d
+    return Circle(c, a)
+
+# Circle on a unit sphere specified by center point on sphere and angular radius.
+class Circle:
+    def __init__(self, center, angle):
+        self.center = center
+        self.angle = angle
+        from math import cos
+        self.cos_angle = cos(angle)
 
 def area_in_circles_on_unit_sphere(circles, draw = False):
 
@@ -59,51 +88,22 @@ def area_in_circles_on_unit_sphere(circles, draw = False):
         s0 = unit_sphere()
         draw_circles(circles, s0, surfc, width = 0.01, offset = 0.01)
 
-    # Compute intersections of circles on sphere.
-    cint = []
-    for i, c0 in enumerate(circles):
-        for c1 in circles[i+1:]:
-            pts = circle_intercepts(c0, c1)
-            if pts:
-                cint.append(Circle_Intersection(c0,c1,pts[0]))
-                cint.append(Circle_Intersection(c1,c0,pts[1]))
+    cint, lc, nreg = intersect_circles(circles)
+
     if draw:
-        surfi = Surface('intersections')
+        surfi = Surface('boundary points')
         draw_sphere_points([ci.point for ci in cint], s0, surfi, (.8,.2,0,1), offset = 0.02)
 
-    # Count disjoint regions in union of circles
-    reg = {}
-    for ci in cint:
-        c1,c2 = ci.circle1, ci.circle2
-        c1r, c2r = reg.get(c1, (c1,)), reg.get(c2, (c2,))
-        if not c1r is c2r:
-            r = c1r + c2r
-            for c in c1r:
-                reg[c] = r
-            for c in c2r:
-                reg[c] = r
-    nreg = len(set(reg.values()))
+    # Check if circles cover the sphere
+    if len(cint) == 0 and len(lc) == 0:
+        from math import pi
+        return 4*pi
 
-    # Find circles with no intercepts and not contained within other circles.
-    lc = set(circles)
-    for ci in cint:
-        lc.discard(ci.circle1)
-        lc.discard(ci.circle2)
-    for c in tuple(lc):
-        if circle_in_circles(c, circles):
-            lc.discard(c)
+    # Connect circle arcs to form boundary paths.
+    paths = boundary_paths(cint)
 
-    # Remove circle intersections that are inside another circle.
-    bndry = [ci for ci in cint if not in_circles(ci, circles)]
-    if draw:
-        surfbp = Surface('bndry pts')
-        draw_sphere_points([ci.point for ci in bndry], s0, surfbp, (.2,.8,0,1), offset = 0.03)
-
-    # Connect arcs to form boundary paths.
-    paths = boundary_paths(bndry)
-
-    print(len(lc), 'lone circles,', nreg, 'multicircle regions,',
-          len(paths) + len(lc), 'boundaries for', nreg + len(lc), 'regions')
+#    print('boundary lengths', ','.join(str(nr) for nr in [1]*len(lc) + [len(p) for p in paths]),
+#          (('for %d regions' % (nreg + len(lc))) if nreg < len(paths) else ''))
 
     if draw:
         surfb = Surface('boundary')
@@ -111,7 +111,9 @@ def area_in_circles_on_unit_sphere(circles, draw = False):
             draw_boundary(bp, s0, surfb, color = (.8,.5,.5,1), width = 0.01, offset = 0.02)
         draw_circles(tuple(lc), s0, surfb, color = (.8,.5,.5,1), width = 0.01, offset = 0.02)
 
-    area = bounded_area(paths, nreg, lc)
+    la = lone_circles_area(lc)
+    ba = bounded_area(paths, nreg)
+    area = la + ba
 
     if draw:
         from .ui.gui import main_window
@@ -119,14 +121,209 @@ def area_in_circles_on_unit_sphere(circles, draw = False):
 
     return area
 
+def intersect_circles(circles):
+
+    # Remove circles contained in other circles.
+    circles2 = [c for i,c in enumerate(circles) if not circle_in_circles(i, circles)]
+
+    from numpy import array
+    ce2, ca2 = array(tuple(c.center for c in circles2)), array(tuple(c.cos_angle for c in circles2))
+
+    # Compute intersection points of circles that are not contained in other circles.
+    cint = []
+    rc = Region_Count(len(circles2))
+    for i, c0 in enumerate(circles2):
+        for j, c1 in enumerate(circles2[i+1:], i+1):
+            p0,p1 = circle_intercepts(c0, c1)
+            if not p0 is None:
+                rc.join(i,j)
+#                if not point_in_circles(p0, circles2, (i,j)):
+                if not point_in_circles2(p0, ce2, ca2, (i,j)):
+                    cint.append(Circle_Intersection(c0,c1,p0))
+#                if not point_in_circles(p1, circles2, (i,j)):
+                if not point_in_circles2(p1, ce2, ca2, (i,j)):
+                    cint.append(Circle_Intersection(c1,c0,p1))
+    sz = rc.region_sizes()
+    lc = [circles2[i] for i,s in enumerate(sz) if s == 1]       # Lone circles
+    nreg = rc.number_of_regions() - len(lc)     # Number of multicircle regions
+
+    return cint, lc, nreg
+
+def circle_in_circles(i, circles):
+    from .geometry.vector import inner_product
+    from math import cos
+    p,a = circles[i].center, circles[i].angle
+    for j,c in enumerate(circles):
+        if c.angle >= a and inner_product(p, c.center) >= cos(c.angle-a) and j != i:
+            return True
+    return False
+
+def circle_intercepts(c0, c1):
+
+    from .geometry.vector import inner_product, cross_product, norm
+    ca01 = inner_product(c0.center,c1.center)
+    x01 = cross_product(c0.center,c1.center)
+    sa01 = norm(x01)
+    s2 = sa01*sa01
+    if s2 == 0:
+        return None,None
+    
+    ca0 = c0.cos_angle
+    ca1 = c1.cos_angle
+    a = (ca0 - ca01*ca1) / s2
+    b = (ca1 - ca01*ca0) / s2
+    d2 = (sa01*sa01 - ca0*ca0 - ca1*ca1 + 2*ca01*ca0*ca1)
+    if d2 < 0:
+        return None,None
+    from math import sqrt
+    d = sqrt(d2) / s2
+    
+    return (a*c0.center + b*c1.center - d*x01,
+            a*c0.center + b*c1.center + d*x01)
+
+def point_in_circles(p, circles, exclude):
+    from .geometry.vector import inner_product
+    for i,c in enumerate(circles):
+        if inner_product(p,c.center) >= c.cos_angle and not i in exclude:
+            return True
+    return False
+
+def point_in_circles2(p, centers, cos_angles, exclude):
+    pin = ((centers*p).sum(axis=1) >= cos_angles)
+    return pin.sum() > sum(pin[i] for i in exclude)
+
+class Circle_Intersection:
+    def __init__(self, circle1, circle2, point):
+        self.circle1 = circle1
+        self.circle2 = circle2
+        self.point = point
+
+class Region_Count:
+    def __init__(self, n):
+        from numpy import arange
+        self.c = arange(n)
+    def join(self, i, j):
+        mi = self.min_connected(i)
+        mj = self.min_connected(j)
+        if mi < mj:
+            self.c[mj] = mi
+        elif mj < mi:
+            self.c[mi] = mj
+    def min_connected(self, i):
+        c = self.c
+        while c[i] != i:
+            i = c[i]
+        return i
+    def number_of_regions(self):
+        nr = 0
+        for i,ci in enumerate(self.c):
+            if ci == i:
+                nr += 1
+        return nr
+    def region_sizes(self):
+        n = len(self.c)
+        from numpy import zeros
+        s = zeros((n,), self.c.dtype)
+        for i in range(n):
+            s[self.min_connected(i)] += 1
+        return s
+
+def boundary_paths(cint):
+
+    ciset = set(cint)
+    bpaths = [boundary_path(ci, ciset) for ci in cint if ci in ciset]
+    return bpaths
+
+def boundary_path(bpoint, bset):
+
+    bpath = [bpoint]
+    bp = bpoint
+    while True:
+        c1points = [bpn for bpn in bset if bpn.circle1 is bp.circle2]
+        if len(c1points) == 0:
+            raise RuntimeError('Could not follow boundary. Probably due to 3 or more circles intersecting at one point.')
+        bpn = min(c1points, key=lambda cp: polar_angle(bp.circle2.center, bp.point, cp.point))
+        if bpn is bpoint:
+            break
+        bpath.append(bpn)
+        bset.remove(bpn)
+        bp = bpn
+    bset.remove(bpoint)
+    return bpath
+
+def lone_circles_area(lone_circles):
+
+    from math import pi
+    area = sum((2*pi*(1-c.cos_angle) for c in lone_circles), 0)
+    return area
+
+def bounded_area(paths, nreg):
+
+    area = 0
+    from math import pi
+    for path in paths:
+        n = len(path)
+        ba = 0
+        for i in range(n):
+            bp1 = path[i]
+            p, c1, c2 = bp1.point, bp1.circle1, bp1.circle2
+            ia = circle_intercept_angle(c1.center, p, c2.center)
+            ba += ia - 2*pi
+            bp2 = path[(i+1)%n]
+            a = polar_angle(c2.center, p, bp2.point)  # Circular arc angle
+#            print('seg', i, 'kink', (ia - 2*pi)*180/pi, 'arc', a*180/pi)
+            ba += a * bp1.circle2.cos_angle  # circular segment bend angle
+#        print('path length', n, 'area', 2*pi-ba)
+        area += 2*pi - ba
+    if len(paths) > nreg:
+        area -= 4*pi*(len(paths)-nreg)
+    return area
+
+def circle_intercept_angle(center1, pintersect, center2):
+    # Angle made by tangent vectors t1 = c1 x p and t2 = c2 x p is same as
+    # polar angle of c1 and c2 about p.
+    a = polar_angle(pintersect, center1, center2)
+    return a
+
+# Angle from plane defined by z and v1 rotated to plane defined by z and v2
+# range 0 to 2*pi.
+def polar_angle(zaxis, v1, v2):
+    from .geometry.place import orthonormal_frame
+    f = orthonormal_frame(zaxis, xdir = v1)
+    x,y,z = f.transpose()*v2
+    from math import atan2, pi
+    a = atan2(y,x)
+    if a < 0:
+        a += 2*pi
+    return a
+
+def estimate_buried_area(i, centers, radii, npts = 100000):
+    from .molecule.molecule import sphere_geometry
+    va, na, ta = sphere_geometry(2*npts)
+    # Weight vertices by area since distribution is not uniform.
+    from ._image3d import vertex_areas
+    weights = vertex_areas(va, ta)
+    c,r = centers[i], radii[i]
+    va *= r
+    va += c
+    from numpy import zeros, int32, logical_or
+    n = len(va)
+    inside = zeros((n,), int32)
+    for j, radius in enumerate(radii):
+        if j != i:
+            d = va - centers[j]
+            d2 = (d*d).sum(axis = 1)
+            logical_or(inside, d2 <= radius*radius, inside)
+    from math import pi
+    a = 4*pi*r*r*sum(inside*weights)/weights.sum()
+    return a
+
 def draw_boundary(bp, sphere, surf, color, width, offset):
 
     n = len(bp)
     for i in range(n):
         bp1 = bp[i]
         bp2 = bp[(i+1)%n]
-        if not bp1.circle2 is bp2.circle1:
-            raise RuntimeError('boundary error')
         draw_arc(bp1.circle2, bp1.point, bp2.point, sphere, surf, color, width, offset)
 
 def draw_arc(circle, p1, p2, sphere, surf, color, width, offset):
@@ -145,82 +342,9 @@ def draw_arc(circle, p1, p2, sphere, surf, color, width, offset):
     p.normals = na
     p.color = color
 
-def boundary_paths(bndry):
-
-    bpaths = []
-    bset = set(bndry)
-    for bp in bndry:
-        if bp in bset:
-            bp = boundary_path(bp, bset)
-            bpaths.append(bp)
-    return bpaths
-
-def boundary_path(bpoint, bset):
-
-    bpath = [bpoint]
-    bp = bpoint
-    while True:
-        c1points = [bpn for bpn in bset if bpn.circle1 is bp.circle2]
-        bpn = min(c1points, key=lambda cp: polar_angle(bp.circle2.center, bp.point, cp.point))
-        if bpn is bpoint:
-            break
-        bpath.append(bpn)
-        bset.remove(bpn)
-        bp = bpn
-    bset.remove(bpoint)
-    return bpath
-
-def in_circles(ci, circles):
-    from .geometry.vector import inner_product
-    from math import cos
-    for c in circles:
-        if not c in (ci.circle1, ci.circle2):
-            if inner_product(ci.point,c.center) >= cos(c.angle):
-                return True
-    return False
-
 def draw_sphere_points(points, sphere, s, color, radius = 0.02, offset = 0.02):
     circles = tuple(Circle(p,radius) for p in points)
     draw_circles(circles, sphere, s, offset, 0.5*radius, color)
-
-class Circle_Intersection:
-    def __init__(self, circle1, circle2, point):
-        self.circle1 = circle1
-        self.circle2 = circle2
-        self.point = point
-
-def circle_intercepts(c0, c1):
-
-    from math import cos, sqrt
-    ca0 = cos(c0.angle)
-    ca1 = cos(c1.angle)
-    from .geometry.vector import inner_product, cross_product, norm
-    ca01 = inner_product(c0.center,c1.center)
-    x01 = cross_product(c0.center,c1.center)
-    sa01 = norm(x01)
-    s2 = sa01*sa01
-    if s2 == 0:
-        return []
-    
-    a = (ca0 - ca01*ca1) / s2
-    b = (ca1 - ca01*ca0) / s2
-    d2 = (sa01*sa01 - ca0*ca0 - ca1*ca1 + 2*ca01*ca0*ca1)
-    if d2 < 0:
-        return []
-    d = sqrt(d2) / s2
-    
-    return (a*c0.center + b*c1.center - d*x01,
-            a*c0.center + b*c1.center + d*x01)
-
-def circle_in_circles(c, circles):
-    from .geometry.vector import inner_product
-    from math import cos
-    for c2 in circles:
-        if (not c is c2 and
-            c2.angle >= c.angle and
-            inner_product(c.center, c2.center) >= cos(c2.angle-c.angle)):
-            return True
-    return False
 
 def draw_circles(circles, sphere, s, offset, width, color = (0,.2,.9,1)):
     cs, r = sphere
@@ -301,26 +425,6 @@ def arc_points(arc, n):
     p = transpose((cos(a), sin(a), zeros(n))).astype(float32)
     return p
 
-def sphere_intersection(c0, r0, c1, r1):
-
-    from .geometry.vector import distance
-    d = distance(c0,c1)
-    if d > r0+r1 or r1+d < r0 or r0+d < r1:
-        return None
-    ca = (r0*r0 + d*d - r1*r1) / (2*r0*d)
-    if ca < -1 or ca > 1:
-        return None
-    import math
-    a = math.acos(ca)
-    c = (c1-c0)/d
-    return Circle(c, a)
-
-# Circle on a unit sphere specified by center point on sphere and angular radius.
-class Circle:
-    def __init__(self, center, angle):
-        self.center = center
-        self.angle = angle
-
 def sphere_model(indices, centers, radii, ntri = 2000):
 
     from .surface import Surface
@@ -351,84 +455,14 @@ def random_spheres_intersecting_unit_sphere(n):
     radii[0] = 1
     return centers, radii
 
-def bounded_area(paths, nreg, lone_circles):
-
-    from math import cos, pi
-    la = sum((2*pi*(1-cos(c.angle)) for c in lone_circles), 0)
-    pa = 0
-    for path in paths:
-        n = len(path)
-        ba = 0
-        for i in range(n):
-            bp1 = path[i]
-            p, c1, c2 = bp1.point, bp1.circle1, bp1.circle2
-            ia = circle_intercept_angle(c1.center, p, c2.center)
-            ba += ia - 2*pi
-            bp2 = path[(i+1)%n]
-            a = polar_angle(c2.center, p, bp2.point)  # Circular arc angle
-#            print('seg', i, 'kink', (ia - 2*pi)*180/pi, 'arc', a*180/pi)
-            ba += a*cos(bp1.circle2.angle) # circular segment bend angle
-#        print('path length', n, 'area', 2*pi-ba)
-        pa += 2*pi - ba
-    if len(paths) > nreg:
-        pa -= 4*pi*(len(paths)-nreg)
-    area = la + pa
-    return area
-
-def circle_intercept_angle(center1, pintersect, center2):
-    # Angle made by tangent vectors t1 = c1 x p and t2 = c2 x p is same as
-    # polar angle of c1 and c2 about p.
-    a = polar_angle(pintersect, center1, center2)
-    return a
-
-def estimate_buried_area(i, centers, radii, npts = 100000):
-    from .molecule.molecule import sphere_geometry
-    va, na, ta = sphere_geometry(2*npts)
-    # Weight vertices by area since distribution is not uniform.
-    from ._image3d import vertex_areas
-    weights = vertex_areas(va, ta)
-    c,r = centers[i], radii[i]
-    va *= r
-    va += c
-    from numpy import zeros, int32, logical_or
-    n = len(va)
-    inside = zeros((n,), int32)
-    for j, radius in enumerate(radii):
-        if j != i:
-            d = va - centers[j]
-            d2 = (d*d).sum(axis = 1)
-            logical_or(inside, d2 <= radius*radius, inside)
-    from math import pi
-    a = 4*pi*r*r*sum(inside*weights)/weights.sum()
-    return a
-
-def sphere_in_another_sphere(i, centers, radii):
-    c, r = centers[i], radii[i]
-    d = centers - c
-    from numpy import sqrt
-    inside = (sqrt((d*d).sum(axis=1)) + r <= radii)
-    return inside[:i].any() or inside[i+1:].any()
-
 def unit_sphere():
     from numpy import array, float64
     center = array((0,0,0),float64)
     radius = 1
     return (center, radius)
 
-# Angle from plane defined by z and v1 rotated to plane defined by z and v2
-# range 0 to 2*pi.
-def polar_angle(zaxis, v1, v2):
-    from .geometry.place import orthonormal_frame
-    f = orthonormal_frame(zaxis, xdir = v1)
-    x,y,z = f.transpose()*v2
-    from math import atan2, pi
-    a = atan2(y,x)
-    if a < 0:
-        a += 2*pi
-    return a
-
 def test_sasa(n = 30):
-    centers, radii = random_spheres_intersecting_unit_sphere(n)
+#    centers, radii = random_spheres_intersecting_unit_sphere(n)
 #    from numpy import array
 #    centers, radii = array(((0.0,0,0), (1.0,0,0))), array((1.0, 1.0))     # area test, pi
 #    centers, radii = array(((0.0,0,0), (1.0,0,0), (0,1.0,0))), array((1.0, 1.0, 1.0)) # area test
@@ -438,7 +472,19 @@ def test_sasa(n = 30):
 #    centers, radii = array(((0.0,0,0), (1.0,0,0))), array((1.0, r))     # area test, 2*pi
 #    centers, radii = array(((0.0,0,0), (1.0,0,0), (0,1.0,0))), array((1.0, r, r))     # area test, 3*pi
 
-    buried_sphere_area(0, centers, radii, draw = True)
+#    buried_sphere_area(0, centers, radii, draw = True)
 
+    from .ui.gui import main_window
+    mlist = main_window.view.molecules()
+    from .molecule import Atom_Set
+    aset = Atom_Set()
+    aset.add_molecules(mlist)
+    aset = aset.exclude_water()
+    centers = aset.coordinates()
+    probe_radius = 1.4
+    radii = aset.radii() + probe_radius
+
+#    import cProfile
+#    cProfile.runctx('print("area =", spheres_surface_area(centers, radii))', globals(), locals())
     a = spheres_surface_area(centers, radii)
-    print('surface area =', a)
+    print('surface area', len(centers), 'atoms =', a)
