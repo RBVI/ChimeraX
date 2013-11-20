1c846d23cce3924601ffc7c4b2ae8c4a2e150ce1
Greg Couch <gregcouch@gmail.com>
2013-11-19 22:10:52 -0800

    Make work with webgl client data.

--- src/c++-app/demo.cpp
+++ src/c++-app/demo.cpp
@@ -10,94 +10,10 @@
 
 using namespace llgr;
 
+extern int width, height;
+extern void initialize();
+
 static bool initialized = false;
-
-static Id shader_id, obj_id, data_id, matrix_id;
-
-void
-initialize()
-{
-	initialized = true;
-	char vertex_shader[4096+1], fragment_shader[4096+1];
-
-	std::ifstream input;
-	input.open("../shaders/vertexShader.txt");
-	input.read(vertex_shader, 4096);
-	vertex_shader[input.gcount()] = '\0';
-	input.close();
-	input.clear();
-	input.open("../shaders/fragmentShader.txt");
-	input.read(fragment_shader, 4096);
-	fragment_shader[input.gcount()] = '\0';
-	input.close();
-
-	++shader_id;
-	create_program(shader_id, vertex_shader, fragment_shader, NULL);
-#if 1
-	glMatrixMode(GL_PROJECTION);
-	glLoadMatrixf(projection_matrix);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadMatrixf(modelview_matrix);
-#else
-	set_uniform_matrix(0, "ProjectionMatrix", false, llgr::Mat4x4, sizeof projection_matrix, projection_matrix);
-	set_uniform_matrix(0, "ModelViewMatrix", false, llgr::Mat4x4, sizeof modelview_matrix, modelview_matrix);
-	float normal_matrix[3][3];
-	for (int i = 0; i < 3; ++i)
-		for (int j = 0; j < 3; ++j)
-			normal_matrix[i][j] = modelview_matrix[i * 4 + j];
-	set_uniform_matrix(0, "NormalMatrix", false, llgr::Mat3x3, sizeof normal_matrix, normal_matrix);
-#endif
-
-	for (Sphere *s = &spheres[0]; s != &spheres[sphere_count]; ++s) {
-		++data_id;
-		create_singleton(data_id, sizeof s->color, s->color);
-
-		AttributeInfos ais;
-		AttributeInfo ai("gl_Color", data_id, 0, 0, 4, Float);
-		ais.push_back(ai);
-
-		++matrix_id;
-		static float mat[4][4] = {
-			{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}};
-#if 1
-		// default chimera layout
-		mat[0][3] = s->center[0];
-		mat[1][3] = s->center[1];
-		mat[2][3] = s->center[2];
-#else
-		// OpenGL layout
-		mat[3][0] = s->center[0];
-		mat[3][1] = s->center[1];
-		mat[3][2] = s->center[2];
-#endif
-		create_matrix(matrix_id, mat, false);
-
-		++obj_id;
-		add_sphere(obj_id, s->radius, shader_id, matrix_id, ais);
-	}
-
-	for (Cylinder *c = &cylinders[0]; c != &cylinders[cylinder_count]; ++c) {
-		++data_id;
-		create_singleton(data_id, sizeof c->color, c->color);
-
-		AttributeInfos ais;
-		AttributeInfo ai("gl_Color", data_id, 0, 0, 4, Float);
-		ais.push_back(ai);
-
-		++matrix_id;
-		static float mat[4][4] = {
-			{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}};
-		for (unsigned row = 0; row != 3; ++row) {
-			for (unsigned col = 0; col != 4; ++col) {
-				mat[row][col] = c->orient[row][col];
-			}
-		}
-		create_matrix(matrix_id, mat, false);
-
-		++obj_id;
-		add_cylinder(obj_id, c->radius, c->length, shader_id, matrix_id, ais);
-	}
-}
 
 void reshape(int w, int h)
 {
@@ -106,8 +22,10 @@ void reshape(int w, int h)
 
 void display()
 {
-	if (!initialized)
+	if (!initialized) {
 		initialize();
+		initialized = true;
+	}
 	// TODO: use shader uniforms to set lighting
 	static GLfloat zero[4] = { 0, 0, 0, 0 };
 	static GLfloat one[4] = { 1, 1, 1, 1 };
---
+++ src/c++-app/webgl2c++.py
@@ -0,0 +1,174 @@
+# convert webgl client data to llgr calls
+
+import json
+import llgr
+llgr.set_output('pyopengl')
+
+DataType = llgr.DataType
+PrimitiveType = llgr.PrimitiveType
+ShaderType = llgr.ShaderType
+
+class Symbolic(str):
+
+	def __repr__(self):
+		return self
+
+filename = 'one_webgl.json'
+
+def print_arg(arg):
+	if isinstance(arg, bool):
+		if arg:
+			return print('true', end='')
+		else:
+			return print('false', end='')
+	if isinstance(arg, (int, float)):
+		return print(arg, end='')
+	if isinstance(arg, str):
+		arg = repr(arg)
+		if arg[0] == "'":
+			arg = arg.replace('"', '\\"')
+			arg = '"%s"' % arg[1:-1]
+		return print(arg, end='')
+	if isinstance(arg, list) and len(arg) == 3 and isinstance(arg[0], bool) and isinstance(arg[1], int) and isinstance(arg[2], list) and ((arg[1] + 3) // 4) == len(arg[2]):
+		# it's a buffer
+		nbytes = arg[1]
+		import struct
+		fmt = 'I' * (nbytes // 4)
+		fmt = '<' + fmt + ['', 'B', 'H', 'HB'][nbytes % 4]
+		b = struct.pack(fmt, *arg[2])
+		b = ''.join('\\x%02x' % x for x in b)
+		return print('%d, "%s"' % (nbytes, b), end='')
+	return print(arg, end='')
+
+# Things to convert to symbolic ShaderType
+shader_types = {
+	# llgr function: nth argument
+	'set_uniform': 2,
+	'set_uniform_matrix': 3
+}
+
+# Things to convert to symbolic DataType
+data_types = {
+	'create_object': 8
+}
+
+# Things to convert to symbolic PrimitiveType
+primitive_types = {
+	'create_object': 4
+}
+
+def print_symbolic(value, type):
+	index = type.values.index(value)
+	print(type.labels[index], end='')
+
+# Things to convert to AttributeInfos
+attribute_infos = {
+	'create_object': 3,
+	'add_sphere': 4,
+	'add_cylinder': 5,
+}
+
+# Things to convert to 4x4 matrix
+matrices = {
+	'create_matrix': 1
+}
+
+# Things to convert to Objects
+objects = {
+	'hide_objects': 0,
+	'show_objects': 0,
+	'transparent': 0,
+	'opaque': 0,
+	'selection_add': 0,
+	'selection_remove': 0,
+	'create_group': 1,
+}
+
+num_attrinfos = 0
+num_matrices = 0
+num_objects = 0
+
+def print_call(call):
+	global num_attrinfos, num_matrices, num_objects
+	func = call[0]
+	if len(call) > 1:
+		args = call[1]
+	else:
+		args = []
+	if func in attribute_infos:
+		argnum = attribute_infos[func]
+		num_attrinfos += 1
+		argname = 'ais%s' % num_attrinfos
+		print('AttributeInfos %s;' % argname)
+		for ai in args[argnum]:
+			print('%s.push_back(AttributeInfo(' % argname, end='')
+			for i in range(len(ai)):
+				if i > 0:
+					print(', ', end='')
+				if i == 5:
+					print_symbolic(ai[i], DataType)
+					continue
+				print_arg(ai[i])
+			print('));')
+		args[argnum] = Symbolic(argname)
+	if func in matrices:
+		argnum = matrices[func]
+		num_matrices += 1
+		argnam = 'mat%d' % num_matrices
+		print('const float %s[4][4] = { %s };' % (argnam,
+				', '.join(str(x) for x in args[argnum])))
+		args[argnum] = Symbolic('%s' % argnam)
+	if func in objects:
+		argnum = objects[func]
+		num_objects += 1
+		argnam = 'objs%d' % num_objects
+		print('Objects %s;' % argnam)
+		for i in args[argnum]:
+			print("%s.push_back(%s);" % (argnam, i))
+		args[argnum] = Symbolic(argnam)
+	print('%s(' % func, end='')
+	for i in range(len(args)):
+		if i > 0:
+			print(', ', end='')
+		if func in data_types and data_types[func] == i:
+			print_symbolic(args[i], DataType)
+			continue
+		if func in primitive_types and primitive_types[func] == i:
+			print_symbolic(args[i], PrimitiveType)
+			continue
+		if func in shader_types and shader_types[func] == i:
+			print_symbolic(args[i], ShaderType)
+			continue
+		print_arg(args[i])
+	print(');')
+
+f = open(filename)
+json = json.load(f)
+
+print("""#include <llgr.h>
+
+using namespace llgr;
+""")
+
+width, height = 0, 0
+for line in json:
+	tag, data = line
+	if tag == 'scene':
+		wh = data['viewport']
+		width = wh[0]
+		height = wh[1]
+print('int width = %d;' % width)
+print('int height = %d;' % height)
+print("""
+void
+initialize()
+{""")
+
+for line in json:
+	tag, data = line
+	if tag == 'llgr':
+		for call in data:
+			print_call(call)
+		continue
+
+print("}")
