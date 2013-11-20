9bfdfa554fbdfac18db7f34f9a9921afcfae0249
Tom Goddard <goddard@sonic.net>
2013-11-20 11:32:32 -0800

    Made new surface module.  Moved bounds code to geometry module.

---
+++ src/hydra/surface/__init__.py
@@ -0,0 +1,5 @@
+from .surface import Surface
+from .surface import surface_image, rgba_surface_piece
+from .sasa import accessible_surface_area
+from .split import split_surfaces
+from .shapes import sphere_geometry, cylinder_geometry
