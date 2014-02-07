eadf31f02a0d3696cc7b3e512c37677383fcf8ee
Tom Goddard <goddard@sonic.net>
2014-02-06 17:12:15 -0800

    Added accept_touch_events() routine to enable trackpad event generation on Mac. Qt 5.2 has trackpad events disabled on Mac because it causes high scroll latency and it requires a Mac objective-C call to enable these events.

---
+++ src/hydra/_image3d/touchevents.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <Python.h>
+#include "touchevents_mac.h"
+
+extern "C" PyObject *accept_touch_events(PyObject *s, PyObject *args, PyObject *keywds)
+{
+  long nsview;
+  const char *kwlist[] = {"nsview", NULL};
+  if (!PyArg_ParseTupleAndKeywords(args, keywds, const_cast<char *>("l"),
+				   (char **)kwlist,
+				   &nsview))
+    return NULL;
+
+  mac_accept_touch_events((void *)nsview);
+
+  Py_INCREF(Py_None);
+  return Py_None;
+}
