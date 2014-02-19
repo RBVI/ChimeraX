6606974cbde55aa280fed0afda7c0e0e2712cb77
Eric Pettersen <pett@cgl.ucsf.edu>
2014-02-19 13:07:58 -0800

    preliminary commit in order to switch w/o stashing; consistent state that compiles

--- src/pdbio/molecule/Chain.h
+++ src/pdbio/molecule/Chain.h
@@ -1,24 +1,28 @@
+// vim: set expandtab ts=4 sw=4:
 #ifndef molecule_chain
 #define molecule_chain
 
 #include "Sequence.h"
 #include <string>
 #include <vector>
+#include "imex.h"
 
 class Residue;
 
-class Chain: public Sequence {
+class MOLECULE_IMEX Chain: public Sequence {
 public:
-	typedef std::vector<Residue *>  Residues;
+    typedef std::vector<Residue *>  Residues;
 private:
-	std::string  _chain_id;
-	Residues  _residues;
+    std::string  _chain_id;
+    Residues  _residues;
 
 public:
-	const std::string &  chain_id() const { return _chain_id; }
-	const Residues&  residues() const { return _residues; }
-	Residue *  get(unsigned i) const { return _residues[i]; }
-	void  set(unsigned i, Residue *r, char character = -1);
+    const std::string&  chain_id() const { return _chain_id; }
+    const Residues&  residues() const { return _residues; }
+    Residue*  get(unsigned i) const { return _residues[i]; }
+    void  set(unsigned i, Residue* r, char character = -1);
+    void  bulk_set(std::vector<Residue *> residues,
+            Sequence::Contents* chars = nullptr);
 };
 
 #endif  // molecule_chain
