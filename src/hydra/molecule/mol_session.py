0be1a65f6d6c7178e654e7c9b5c0d83d9068a714
Tom Goddard <goddard@sonic.net>
2014-05-07 15:42:31 -0700

    Save per-atom and per-ribbon custom colors in sessions and scenes. Changed numpy array saving to include array size and value type.

--- src/hydra/molecule/mol_session.py
+++ src/hydra/molecule/mol_session.py
@@ -1,5 +1,5 @@
 mol_attrs = ('path', 'id', 'database_fetch', 'display', 'atom_style',
-             'color_mode', 'ribbon_radius', 'ball_scale')
+             'ribbon_radius', 'ball_scale')
 
 def molecule_state(m):  
     ms = {'place':m.position.matrix}
@@ -11,7 +11,11 @@ def molecule_state(m):
     if not m.bonds is None:
         ms['has_bonds'] = True
     ms['atom_shown'] = array_to_string(m.atom_shown)
+    if not m.atom_colors is None:
+        ms['atom_colors'] = array_to_string(m.atom_colors)
     ms['ribbon_shown'] = array_to_string(m.ribbon_shown)
+    if not m.ribbon_colors is None:
+        ms['ribbon_colors'] = array_to_string(m.ribbon_colors)
     return ms
 
 def restore_molecules(mstate, session, file_paths, attributes_only = False):
@@ -65,12 +69,23 @@ def set_molecule_state(m, ms, session):
         if sac == m.atom_count():
             m.atom_shown = sa
             m.atom_shown_count = sac
+    if 'atom_colors' in ms:
+        m.atom_colors = string_to_array(ms['atom_colors'])
+    if 'color_mode' in ms:
+        # Old sessions.
+        cm = ms['color_mode']
+        if cm == 'by element':
+            m.color_by_element()
+        elif cm == 'by chain':
+            m.color_by_chain()
     if 'ribbon_shown' in ms:
         sr = string_to_array(ms['ribbon_shown'], bool)
         src = sr.sum()
         if src == m.atom_count():
             m.ribbon_shown = sr
             m.ribbon_shown_count = src
+    if 'ribbon_colors' in ms:
+        m.ribbon_colors = string_to_array(ms['ribbon_colors'])
 
     m.need_graphics_update = True
 
@@ -81,12 +96,22 @@ def array_to_string(a):
     cs = bz2.compress(s)
     import base64
     ecs = base64.b64encode(cs)
-    return ecs
+    aspec = (a.shape, a.dtype.str, ecs)
+    return aspec
 
 # convert a string to a numpy array
-def string_to_array(s, dtype):
-    import base64, bz2, numpy
-    ds = base64.b64decode(s)
-    dds = bz2.decompress(ds)
-    a = numpy.fromstring(dds, dtype)
+def string_to_array(s, dtype = None):
+    if isinstance(s,tuple):
+        shape, atype, astr = s
+        import base64, bz2, numpy
+        ds = base64.b64decode(astr)
+        dds = bz2.decompress(ds)
+        a1d = numpy.fromstring(dds, atype)
+        a = numpy.reshape(a1d, shape)
+    else:
+        # Old style only handled 1d arrays.
+        import base64, bz2, numpy
+        ds = base64.b64decode(s)
+        dds = bz2.decompress(ds)
+        a = numpy.fromstring(dds, dtype)
     return a
