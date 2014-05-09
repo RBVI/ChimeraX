5bf031321c20c0a66e994e272cf4d02f7054df18
Tom Goddard <goddard@sonic.net>
2014-05-09 12:35:37 -0700

    C++ general array operations.

---
+++ src/hydra/_image3d/arrayops.h
@@ -0,0 +1,14 @@
+#ifndef ARRAYOPS_HEADER_INCLUDED
+#define ARRAYOPS_HEADER_INCLUDED
+
+#include <Python.h>			// use PyObject
+
+extern "C" {
+
+PyObject *value_ranges(PyObject *s, PyObject *args, PyObject *keywds);
+PyObject *contiguous_intervals(PyObject *s, PyObject *args, PyObject *keywds);
+PyObject *mask_intervals(PyObject *s, PyObject *args, PyObject *keywds);
+
+}
+
+#endif
