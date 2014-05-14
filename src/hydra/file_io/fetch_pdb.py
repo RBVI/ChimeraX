28e6a7c72faef1219f900cca6944c64f91a7e043
Tom Goddard <goddard@sonic.net>
2014-05-14 11:03:46 -0700

    Added mmCIF fetch from PDB.

--- src/hydra/file_io/fetch_pdb.py
+++ src/hydra/file_io/fetch_pdb.py
@@ -1,14 +1,15 @@
-def fetch_pdb(id, session, ignore_cache=False):
+def fetch_pdb(id, session, ignore_cache = False, file_format = 'pdb'):
     '''
     Fetch molecular models from the Protein Data Bank, www.rcsb.org
     '''
 
     site = 'www.rcsb.org'
-    url_pattern = 'pdb/files/%s.pdb.gz'
+    url_pattern = 'pdb/files/%s.%s.gz'
     idu = id.upper()
-    url = "http://%s/%s" % (site, url_pattern % idu)
+    file_suffix = {'pdb':'pdb', 'mmcif':'cif'}[file_format]
+    url = "http://%s/%s" % (site, url_pattern % (idu, file_suffix))
     save_dir = 'PDB'
-    save_name = '%s.pdb' % idu
+    save_name = '%s.%s' % (idu, file_suffix)
     min_file_size = 80*20
     
     from .fetch import fetch_file
@@ -19,11 +20,23 @@ def fetch_pdb(id, session, ignore_cache=False):
     except IOError as e:
       raise
 
-    from .pdb import open_pdb_file
-    m = open_pdb_file(path, session)
-    m.database_fetch = (id, 'pdb')
+    if file_format == 'pdb':
+        from .pdb import open_pdb_file
+        m = open_pdb_file(path, session)
+    elif file_format == 'mmcif':
+        from .mmcif import open_mmcif_file
+        m = open_mmcif_file(path, session)
+    m.database_fetch = (id, file_format)
     from ..molecule import Molecule
     return [m] if isinstance(m, Molecule) else m
+
+# -----------------------------------------------------------------------------
+#
+def fetch_mmcif(id, session, ignore_cache = False):
+    '''
+    Fetch molecular models in mmCIF format from the Protein Data Bank, www.rcsb.org
+    '''
+    return fetch_pdb(id, session, ignore_cache, file_format = 'mmcif')
 
 # -----------------------------------------------------------------------------
 # Register to fetch PDB maps with open command.
@@ -32,4 +45,6 @@ def register_pdb_fetch(session):
 
   from .fetch import register_fetch_database as reg
   reg('PDB', fetch_pdb, '1a0m', 'www.rcsb.org/pdb/home',
+      'http://www.rcsb.org/pdb/explore/explore.do?structureId=%s', session)
+  reg('PDBmmCIF', fetch_mmcif, '1a0m', 'www.rcsb.org/pdb/home',
       'http://www.rcsb.org/pdb/explore/explore.do?structureId=%s', session)
