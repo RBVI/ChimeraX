c396b711f6b97e545e4a4c2245717d8e547e08e4
Greg Couch <gregcouch@gmail.com>
2014-05-29 00:49:33 -0700

    Undo accidental additional commit.

--- src/chimera2/io/formats/pdb.py
+++ src/chimera2/io/formats/pdb.py
@@ -3,7 +3,7 @@ from chimera2.cli import UserError
 
 _builtin_open = open
 
-def open_pdb(stream, *args, **kw):
+def open(stream, *args, **kw):
 
 	if hasattr(stream, 'read'):
 		input = stream
@@ -21,7 +21,7 @@ def open_pdb(stream, *args, **kw):
 
 	return [model], "Model %d: PDB data containing %d atoms and %d bonds" % (model.id, model.num_atoms, model.num_bonds)
 
-def fetch_pdb(pdb_id):
+def fetch(pdb_id):
 	if len(pdb_id) != 4:
 		raise UserError("PDB identifiers are 4 characters long")
 	import os
@@ -46,34 +46,16 @@ def fetch_pdb(pdb_id):
 	except URLError as e:
 		raise UserError(str(e))
 
-def open_mmcif(stream, *args, **kw):
-
-	if hasattr(stream, 'name'):
-		filename = stream.name
-	else:
-		# it's really a filename
-		filename = stream
-
-	import pdbio
-	mol_blob = pdbio.read_mmcif_file(filename)
-	model = molecule.Molecule()
-	model.mol_blob = mol_blob
-	model.make_graphics()
-
-	return [model], "Model %d: PDB data containing %d atoms and %d bonds" % (model.id, model.num_atoms, model.num_bonds)
-
 def register():
 	from chimera2 import io
 	io.register_format("PDB", molecule.CATEGORY,
 		(".pdb", ".pdb1", ".ent", ".pqr"), ("pdb",),
 		mime=("chemical/x-pdb", "chemical/x-spdbv"),
 		reference="http://wwpdb.org/docs.html#format",
-		open_func=open_pdb, fetch_func=fetch_pdb)
-	# Don't register CIF separately since mmCIF also uses .cif suffix
-	#io.register_format("CIF", molecule.CATEGORY, (".cif",), ("cif", "cifID"),
-	#	mime=("chemical/x-cif"),
-	#	reference="http://www.iucr.org/__data/iucr/cif/standard/cifstd1.html")
-	io.register_format("mmCIF", molecule.CATEGORY, (".mcif", ".cif"), ("mmcif",),
-		mime=("chemical/x-mmcif", "chemical/x-cif"),
-		reference="http://mmcif.rcsb.org/",
-		requires_seeking=True, open_func=open_mmcif)
+		open_func=open, fetch_func=fetch)
+	io.register_format("CIF", molecule.CATEGORY, (".cif"), ("cif", "cifID"),
+		mime=("chemical/x-cif"),
+		reference="http://www.iucr.org/__data/iucr/cif/standard/cifstd1.html")
+	io.register_format("mmCIF", molecule.CATEGORY, (".mcif"), ("mmcif",),
+		mime=("chemical/x-mmcif"),
+		reference="http://mmcif.rcsb.org/")
