38d9a16d65c20b544b632059e138df549831bd59
Tom Goddard <goddard@sonic.net>
2014-05-29 17:09:44 -0700

    Renamed drawing.py to opengl.py.

--- src/hydra/draw/__init__.py
+++ src/hydra/draw/__init__.py
@@ -1,1 +1,6 @@
-from .drawing import *
+from .opengl import Render, Framebuffer, Lighting, Bindings, Buffer, Texture
+
+# Specific buffer types
+from .opengl import VERTEX_BUFFER, NORMAL_BUFFER, VERTEX_COLOR_BUFFER, TEXTURE_COORDS_2D_BUFFER, ELEMENT_BUFFER
+from .opengl import INSTANCE_MATRIX_BUFFER, INSTANCE_SHIFT_AND_SCALE_BUFFER, INSTANCE_COLOR_BUFFER
+
