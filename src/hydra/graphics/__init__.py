6da0dc66488acc79c61434cf41c6499fbb62959e
Tom Goddard <goddard@sonic.net>
2014-06-05 17:41:54 -0700

    Removed Lighting from graphics interface since it is currently only used within Render class.

--- src/hydra/graphics/__init__.py
+++ src/hydra/graphics/__init__.py
@@ -3,4 +3,4 @@ from .drawing import image_drawing, rgba_drawing
 
 from .camera import Camera, camera_framing_models
 
-from .opengl import Render, Framebuffer, Lighting, Texture
+from .opengl import Render, Framebuffer, Texture
