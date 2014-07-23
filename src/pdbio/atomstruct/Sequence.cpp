9c4ea7e1cf7febe42d72ad2fb0d60df3ffdf4fd0
Eric Pettersen <pett@cgl.ucsf.edu>
2014-07-23 13:01:54 -0700

    stop using const char * as a map key, which won't work

--- src/pdbio/atomstruct/Sequence.cpp
+++ src/pdbio/atomstruct/Sequence.cpp
@@ -3,64 +3,63 @@
 
 namespace atomstruct {
 
-Sequence::_1Letter_Map Sequence::_rname3to1;
+Sequence::_1Letter_Map Sequence::_rname3to1 = {
+    { "A", 'A' },
+    { "+A", 'A' },
+    { "ADE", 'A' },
+    { "DA", 'A' },
+    { "C", 'C' },
+    { "+C", 'C' },
+    { "CYT", 'C' },
+    { "DC", 'C' },
+    { "G", 'G' },
+    { "+G", 'G' },
+    { "GUA", 'G' },
+    { "DG", 'G' },
+    { "T", 'T' },
+    { "+T", 'T' },
+    { "THY", 'T' },
+    { "DT", 'T' },
+    { "U", 'U' },
+    { "+U", 'U' },
+    { "URA", 'U' },
+    { "ALA", 'A' },
+    { "ARG", 'R' },
+    { "ASH", 'D' }, // Amber (protonated ASP)
+    { "ASN", 'N' },
+    { "ASP", 'D' },
+    { "ASX", 'B' }, // ambiguous ASP/ASN
+    { "CYS", 'C' },
+    { "CYX", 'C' }, // Amber (disulphide)
+    { "GLH", 'E' }, // Amber (protonated GLU)
+    { "GLU", 'E' },
+    { "GLN", 'Q' },
+    { "GLX", 'Z' }, // ambiguous GLU/GLN
+    { "GLY", 'G' },
+    { "HID", 'H' }, // Amber (delta protonated)
+    { "HIE", 'H' }, // Amber (epsilon protonated)
+    { "HIP", 'H' }, // Amber (doubly protonated)
+    { "HIS", 'H' },
+    { "HYP", 'P' }, // hydroxyproline, which in theory
+        // has its own letter ('O') but using that is problematic
+        // with similarity matrices
+    { "ILE", 'I' },
+    { "LEU", 'L' },
+    { "LYS", 'K' },
+    { "MET", 'M' },
+    { "MSE", 'M' }, // Selenomethionine
+    { "PHE", 'F' },
+    { "PRO", 'P' },
+    { "SER", 'S' },
+    { "THR", 'T' },
+    { "TRP", 'W' },
+    { "TYR", 'Y' },
+    { "VAL", 'V' }
+};
 
 unsigned char
-Sequence::rname3to1(const char *rn)
+Sequence::rname3to1(const std::string& rn)
 {
-    if (_rname3to1.size() == 0) {
-        _rname3to1["A"] = 'A';
-        _rname3to1["+A"] = 'A';
-        _rname3to1["ADE"] = 'A';
-        _rname3to1["DA"] = 'A';
-        _rname3to1["C"] = 'C';
-        _rname3to1["+C"] = 'C';
-        _rname3to1["CYT"] = 'C';
-        _rname3to1["DC"] = 'C';
-        _rname3to1["G"] = 'G';
-        _rname3to1["+G"] = 'G';
-        _rname3to1["GUA"] = 'G';
-        _rname3to1["DG"] = 'G';
-        _rname3to1["T"] = 'T';
-        _rname3to1["+T"] = 'T';
-        _rname3to1["THY"] = 'T';
-        _rname3to1["DT"] = 'T';
-        _rname3to1["U"] = 'U';
-        _rname3to1["+U"] = 'U';
-        _rname3to1["URA"] = 'U';
-        _rname3to1["ALA"] = 'A';
-        _rname3to1["ARG"] = 'R';
-        _rname3to1["ASH"] = 'D'; // Amber (protonated ASP)
-        _rname3to1["ASN"] = 'N';
-        _rname3to1["ASP"] = 'D';
-        _rname3to1["ASX"] = 'B'; // ambiguous ASP/ASN
-        _rname3to1["CYS"] = 'C';
-        _rname3to1["CYX"] = 'C'; // Amber (disulphide)
-        _rname3to1["GLH"] = 'E'; // Amber (protonated GLU)
-        _rname3to1["GLU"] = 'E';
-        _rname3to1["GLN"] = 'Q';
-        _rname3to1["GLX"] = 'Z'; // ambiguous GLU/GLN
-        _rname3to1["GLY"] = 'G';
-        _rname3to1["HID"] = 'H'; // Amber (delta protonated)
-        _rname3to1["HIE"] = 'H'; // Amber (epsilon protonated)
-        _rname3to1["HIP"] = 'H'; // Amber (doubly protonated)
-        _rname3to1["HIS"] = 'H';
-        _rname3to1["HYP"] = 'P'; // hydroxyproline, which in theory
-            // has its own letter ('O') but using that is problematic
-            // with similarity matrices
-        _rname3to1["ILE"] = 'I';
-        _rname3to1["LEU"] = 'L';
-        _rname3to1["LYS"] = 'K';
-        _rname3to1["MET"] = 'M';
-        _rname3to1["MSE"] = 'M'; // Selenomethionine
-        _rname3to1["PHE"] = 'F';
-        _rname3to1["PRO"] = 'P';
-        _rname3to1["SER"] = 'S';
-        _rname3to1["THR"] = 'T';
-        _rname3to1["TRP"] = 'W';
-        _rname3to1["TYR"] = 'Y';
-        _rname3to1["VAL"] = 'V';
-    }
     _1Letter_Map::const_iterator l1i = _rname3to1.find(rn);
     if (l1i == _rname3to1.end())
         return 'X';
