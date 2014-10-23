2d1bd49300638629f7b9f2f1e7653a149f680276
Eric Pettersen <pett@cgl.ucsf.edu>
2014-10-23 12:55:28 -0700

    send path separator character to C++ appdirs module, so that it can form paths

--- src/core/cpp_appdirs/AppDirs.cpp
+++ src/core/cpp_appdirs/AppDirs.cpp
@@ -7,15 +7,15 @@ namespace cpp_appdirs {
 AppDirs*  AppDirs::_app_dirs = nullptr;
 	
 void
-AppDirs::init_app_dirs(std::string user_data_dir,
-	std::string user_config_dir, std::string user_cache_dir,
-	std::string site_data_dir, std::string site_config_dir,
-	std::string user_log_dir)
+AppDirs::init_app_dirs(std::string path_sep,
+	std::string user_data_dir, std::string user_config_dir,
+	std::string user_cache_dir, std::string site_data_dir,
+	std::string site_config_dir, std::string user_log_dir)
 {
 	if (_app_dirs != nullptr)
 		throw std::logic_error("C++ appdirs already initialized!");
-	_app_dirs = new AppDirs(user_data_dir, user_config_dir, user_cache_dir,
-		site_data_dir, site_config_dir, user_log_dir);
+	_app_dirs = new AppDirs(path_sep, user_data_dir, user_config_dir,
+		user_cache_dir, site_data_dir, site_config_dir, user_log_dir);
 }
 
 }; // namespace cpp_appdirs
--- src/core/cpp_appdirs/AppDirs.h
+++ src/core/cpp_appdirs/AppDirs.h
@@ -12,12 +12,14 @@ namespace cpp_appdirs {
 class CPP_APPDIRS_IMEX AppDirs {
 private:
     static AppDirs*  _app_dirs;
-    AppDirs(std::string& user_data_dir, std::string& user_config_dir,
-        std::string& user_cache_dir, std::string& site_data_dir,
-        std::string& site_config_dir, std::string& user_log_dir):
+    AppDirs(std::string& path_sep, std::string& user_data_dir,
+        std::string& user_config_dir, std::string& user_cache_dir,
+        std::string& site_data_dir, std::string& site_config_dir,
+        std::string& user_log_dir): _path_sep(path_sep),
         site_config_dir(site_config_dir), site_data_dir(site_data_dir),
         user_cache_dir(user_cache_dir), user_config_dir(user_config_dir),
         user_data_dir(user_data_dir), user_log_dir(user_log_dir) {}
+    const std::string  _path_sep;
 
 public:
     static const AppDirs&  get() {
@@ -25,10 +27,10 @@ public:
             " initialized");
         return *_app_dirs;
     }
-    static void  init_app_dirs(std::string user_data_dir,
-        std::string user_config_dir, std::string user_cache_dir,
-        std::string site_data_dir, std::string site_config_dir,
-        std::string user_log_dir);
+    static void  init_app_dirs(std::string path_sep,
+        std::string user_data_dir, std::string user_config_dir,
+        std::string user_cache_dir, std::string site_data_dir,
+        std::string site_config_dir, std::string user_log_dir);
     const std::string  site_config_dir;
     const std::string  site_data_dir;
     const std::string  user_cache_dir;
