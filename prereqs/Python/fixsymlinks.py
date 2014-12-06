84b8b6a3b97d6775969317b28ed6393908fa39a8
Greg Couch <gregc@cgl.ucsf.edu>
2014-12-05 19:18:59 -0800

    Fix up Darwin shared library naming and references, part 1.

---
+++ prereqs/Python/fixsymlinks.py
@@ -0,0 +1,29 @@
+# vim: set expandtab shiftwidth=4 softtabstop=4:
+#
+# For each filename given on the command line
+#    if it is a symbolic link to an abolute location,
+#    then replace it with a relative one.
+from __future__ import print_function
+
+import os
+import sys
+
+for filename in sys.argv[1:]:
+    if not os.path.exists(filename):
+        print("warning:", filename, "is missing", file=sys.stderr)
+        continue
+    source = os.path.abspath(filename)
+    if not os.path.islink(source):
+        #print("warning:", filename, "is not a symbolic link", file=sys.stderr)
+        continue
+    link = os.readlink(source)
+    if link[0] != '/':
+        continue
+    rellink = os.path.relpath(link, os.path.dirname(source))
+    try:
+        os.remove(source)
+    except OSError:
+        print("error: unable to remove", filename, file=sys.stderr)
+        continue
+    print('symbolic linking', source, 'to', rellink)
+    os.symlink(rellink, source)
