2e1664760490178f99ab674b8955c4dda4d35ecf
Greg Couch <gregc@cgl.ucsf.edu>
2015-01-05 15:25:10 -0800

    All generic 3D models will save state similarly.

--- src/core/generic3d.py
+++ src/core/generic3d.py
@@ -1,8 +1,20 @@
 from . import io
 from . import models
+from .session import State
+
 CATEGORY = io.GENERIC3D
 
 
 class Generic3DModel(models.Model):
     """Commom base class for generic 3D data"""
-    pass
+
+    # TODO: just save/restore drawing state
+
+    def take_snapshot(self, session, flags):
+        State.take_snapshot(self, session, flags)
+       
+    def restore_snapshot(self, phase, session, version, data):
+        State.restore_snapshot(self, phase, session, version, data)
+
+    def reset_state(self):
+        State.reset_state(self)
