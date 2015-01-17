4a21d33786040e789e9c055f29539d0ecf7b1aae
Greg Couch <gregc@cgl.ucsf.edu>
2015-01-16 18:37:39 -0800

    Add missing structure.py.

---
+++ src/core/structure.py
@@ -0,0 +1,22 @@
+from . import io
+from . import models
+from .graphics.drawing import Drawing
+
+CATEGORY = io.STRUCTURE
+
+
+class StructureModel(models.Model):
+    """Commom base class for atomic structures"""
+
+    STRUCTURE_STATE_VERSION = 0
+
+    def take_snapshot(self, session, flags):
+        data = {}
+        return [self.STRUCTURE_STATE_VERSION, data]
+
+    def restore_snapshot(self, phase, session, version, data):
+        if version != self.STRUCTURE_STATE_VERSION or len(data) > 0:
+            raise RuntimeError("Unexpected version or data")
+
+    def reset_state(self):
+        pass
