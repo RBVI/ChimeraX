cfdeb70ca316fe2693a81c7ccca0fe3f3ccc0e7b
Tom Goddard <goddard@sonic.net>
2015-01-21 14:46:56 -0800

    Added initial code to draw atoms as spheres.

--- src/core/structure.py
+++ src/core/structure.py
@@ -27,3 +27,45 @@ class StructureModel(models.Model):
         element_numbers = atom_blob.element_numbers
 
         # TODO: fill in drawing
+        self.create_atom_spheres(coords, element_numbers)
+
+    def create_atom_spheres(self, coords, element_numbers, radius = 1.5):
+        if not hasattr(self, '_atoms_drawing'):
+            self._atoms_drawing = self.new_drawing('atoms')
+        p = self._atoms_drawing
+
+        # Set instanced sphere triangulation
+        from .geometry import icosahedron
+        va, ta = icosahedron.icosahedron_geometry()
+        from numpy import int32
+        p.geometry = va, ta.astype(int32)
+        p.normals = va
+
+        # Set instanced sphere center position and radius
+        n = len(coords)
+        from numpy import empty, float32, uint8
+        xyzr = empty((n,4), float32)
+        xyzr[:,:3] = coords
+        xyzr[:,3] = radius
+        from .geometry import place
+        p.positions = place.Places(shift_and_scale = xyzr)
+
+        # Set atom colors
+        p.colors = element_colors(element_numbers)
+
+element_rgba_256 = None
+def element_colors(element_numbers):
+    global element_rgba_256
+    if element_rgba_256 is None:
+        from numpy import empty, uint8
+        element_rgba_256 = ec = empty((256,4), uint8)
+        ec[:,:3] = 180
+        ec[:,3] = 255
+        ec[6,:] = (255,255,255,255)     # H
+        ec[6,:] = (144,144,144,255)     # C
+        ec[7,:] = (48,80,248,255)       # N
+        ec[8,:] = (255,13,13,255)       # O
+        ec[15,:] = (255,128,0,255)      # P
+        ec[16,:] = (255,255,48,255)      # S
+    colors = element_rgba_256[element_numbers]
+    return colors
