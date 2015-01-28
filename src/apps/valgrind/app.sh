882fb333deb3b83a8543c1d0c596c1a736990652
Greg Couch <gregc@cgl.ucsf.edu>
2015-01-28 11:43:35 -0800

    Add ability to use valgrind on application.

---
+++ src/apps/valgrind/app.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+bindir=`/usr/bin/dirname "$0"`
+app=`/usr/bin/basename "$0" .valgrind`
+datadir="$bindir/../share"
+
+exec valgrind --tool=memcheck --error-limit=no --suppressions="$datadir"/valgrind-python.supp --suppressions="$datadir"/valgrind.suppress --gen-suppressions=all --track-origins=yes --num-callers=30 "$bindir/$app"
