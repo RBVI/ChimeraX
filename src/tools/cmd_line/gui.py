d8c1efbb0f5c72b99090da4eb8fc94d74eeba668
Eric Pettersen <pett@cgl.ucsf.edu>
2015-04-23 17:20:37 -0700

    use combobox instead of plain text control

--- src/tools/cmd_line/gui.py
+++ src/tools/cmd_line/gui.py
@@ -15,7 +15,7 @@ class CmdLine(ToolInstance):
                                       size=self.SIZE, destroy_hides=True)
         parent = self.tool_window.ui_area
         import wx
-        self.text = wx.TextCtrl(parent, size=self.SIZE,
+        self.text = wx.ComboBox(parent, size=self.SIZE,
                                 style=wx.TE_PROCESS_ENTER | wx.TE_NOHIDESEL)
         sizer = wx.BoxSizer(wx.HORIZONTAL)
         sizer.Add(self.text, 1, wx.EXPAND)
@@ -35,44 +35,48 @@ class CmdLine(ToolInstance):
     def OnEnter(self, event):
         session = self.session
         logger = session.logger
-        text = self.text.GetLineText(0)
+        text = self.text.Value
         self.text.SelectAll()
         logger.status("")
         from chimera.core import cli
-        try:
-            cmd = cli.Command(session, text, final=True)
-            cmd.execute()
-        except SystemExit:
-            # TODO: somehow quit application
-            raise
-        except cli.UserError as err:
-            rest = cmd.current_text[cmd.amount_parsed:]
-            spaces = len(rest) - len(rest.lstrip())
-            error_at = cmd.amount_parsed + spaces
-            text = "<pre>%s<br>\n%s^<br>\n%s\n</pre>" % (
-                cmd.current_text, '.' * error_at, str(err))
-            logger.info(text, is_html=True)
-            logger.status(str(err))
-        except:
-            import traceback
-            session.logger.error(traceback.format_exc())
-        else:
-            thumb = session.main_view.image(width=100, height=100)
-            log_thumb = False
-            from time import time
-            s_t = time()
-            if thumb.getcolors(1) is None:
-                # image not just a solid background color;
-                # ensure it differs from previous thumbnail
-                thumb_data = thumb.tostring()
-                if thumb_data != self._last_thumb:
-                    self._last_thumb = thumb_data
-                    log_thumb = True
+        for cmd_text in text.split("\n"):
+            if not cmd_text:
+                continue
+            self.text.SetItems([cmd_text] + list(self.text.GetItems()[:19]))
+            try:
+                cmd = cli.Command(session, cmd_text, final=True)
+                cmd.execute()
+            except SystemExit:
+                # TODO: somehow quit application
+                raise
+            except cli.UserError as err:
+                rest = cmd.current_text[cmd.amount_parsed:]
+                spaces = len(rest) - len(rest.lstrip())
+                error_at = cmd.amount_parsed + spaces
+                text = "<pre>%s<br>\n%s^<br>\n%s\n</pre>" % (
+                    cmd.current_text, '.' * error_at, str(err))
+                logger.info(text, is_html=True)
+                logger.status(str(err))
+            except:
+                import traceback
+                session.logger.error(traceback.format_exc())
             else:
-               self._last_thumb = None
-            session.logger.info(text)
-            if log_thumb:
-                session.logger.info("graphics image", image=thumb)
+                thumb = session.main_view.image(width=100, height=100)
+                log_thumb = False
+                from time import time
+                s_t = time()
+                if thumb.getcolors(1) is None:
+                    # image not just a solid background color;
+                    # ensure it differs from previous thumbnail
+                    thumb_data = thumb.tostring()
+                    if thumb_data != self._last_thumb:
+                        self._last_thumb = thumb_data
+                        log_thumb = True
+                else:
+                   self._last_thumb = None
+                session.logger.info(cmd_text)
+                if log_thumb:
+                    session.logger.info("graphics image", image=thumb)
 
     #
     # Implement session.State methods if deriving from ToolInstance
