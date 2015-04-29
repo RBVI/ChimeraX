a0089f3031facb0ef255463fcd7ffd4d09b2803e
Tom Goddard <goddard@sonic.net>
2015-04-29 16:16:54 -0700

    Added dashed cylinders for display of pseudobonds.

--- src/core/surface/__init__.py
+++ src/core/surface/__init__.py
@@ -1,7 +1,7 @@
 # vi: set expandtab shiftwidth=4 softtabstop=4:
 from .sasa import spheres_surface_area
 from .split import split_surfaces
-from .shapes import sphere_geometry, cylinder_geometry
+from .shapes import sphere_geometry, cylinder_geometry, dashed_cylinder_geometry
 from .area import surface_area, enclosed_volume, surface_volume_and_area
 from .gridsurf import ses_surface_geometry
 from ._surface import parse_stl, subdivide_triangles, vertex_areas
