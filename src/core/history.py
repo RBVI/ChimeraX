11f2d104eb7c2ca8fc53db30418543b90ae53198
Greg Couch <gregc@cgl.ucsf.edu>
2015-05-14 13:00:25 -0700

    Get rid of warning when opening FIFOHistory for the first time.

--- src/core/history.py
+++ src/core/history.py
@@ -103,11 +103,11 @@ class FIFOHistory:
         self._history = ObjectCache(session, tag, unversioned)
         obj = self._history.load()
         if obj is None:
-            obj = ([], [])
+            obj = [[], []]
         if (not isinstance(obj, list) or len(obj) != 2 or
                 not isinstance(obj[0], list) or not isinstance(obj[1], list)):
             session.logger.warning("Corrupt %s history: cleared" % tag)
-            obj = ([], [])
+            obj = [[], []]
         self._front, self._back = obj
         while len(self._front) + len(self._back) > self._capacity:
             self.dequeue()
