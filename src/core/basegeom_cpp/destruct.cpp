7109cb293e731f55c690394eda7923ef0a71dbfb
Eric Pettersen <pett@cgl.ucsf.edu>
2015-06-02 11:46:28 -0700

    prevent notifications from going out when PDBio is just adjusting the structures (deleting empty structure, converting metal coordination bonds to pseudobonds, etc.)

--- src/core/basegeom_cpp/destruct.cpp
+++ src/core/basegeom_cpp/destruct.cpp
@@ -6,5 +6,6 @@ namespace basegeom {
 void*  DestructionCoordinator::_destruction_parent = nullptr;
 std::set<DestructionObserver*>  DestructionCoordinator::_observers;
 std::set<void*>  DestructionCoordinator::_destroyed;
+int DestructionCoordinator::_num_notifications_off = 0;
 
 }  // namespace basegeom
