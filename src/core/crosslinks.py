67058013481c610ea6f510c3ad7349775cddd74c
Tom Goddard <goddard@sonic.net>
2015-06-08 17:37:47 -0700

    Made crosslink command take specifiers for pseudobond group and molecules to position for minimizing crosslinks.

--- src/core/crosslinks.py
+++ src/core/crosslinks.py
@@ -1,68 +1,80 @@
-def minimize_crosslinks(session, pbspec = None, iterations = 5, radius = None, color = None):
-    pbg = parse_pseudobond_group_arg(pbspec, session)
-    pbonds = pbg.pseudobonds
-    mol_links = links_by_molecule(pbonds)
-    mols = list(mol_links.keys())
-    mols.sort(key = lambda m: m.name)
-    from numpy import array, float64
-    from . import align 
-    for i in range(iterations):
-        for m in mols:
-            atom_pairs = mol_links[m]
-            moving = array([a1.scene_coord for a1,a2 in atom_pairs], float64)
-            fixed = array([a2.scene_coord for a1,a2 in atom_pairs], float64)
-            tf, rms = align.align_points(moving, fixed)
-            m.position = tf * m.position
+def crosslink(session, pbgroups = None, color = None, radius = None, minimize = None, iterations = 10):
 
-    lengths = [pb.length for pb in pbonds]
-    lengths.sort(reverse = True)
-    print('%d crosslinks, lengths: %s' % (len(pbonds), ', '.join('%.1f' % d for d in lengths)))
+    if pbgroups is None:
+        from . import pbgroup
+        pbgroups = pbgroup.all_pseudobond_groups(session.models)
 
-    if radius:
-        for pb in pbonds:
-            pb.radius = radius
+    if len(pbgroups) == 0:
+        from .cli import UserError        
+        raise UserError('No pseudobond groups specified.')
+
+    from .molecule import concatenate
+    pbonds = concatenate([pbg.pseudobonds for pbg in pbgroups])
+
     if color:
         rgba = color.uint8x4()
         for pb in pbonds:
             pb.color = rgba
-    pbg.update_graphics()	# TODO: pseudobonds should update automatically when atoms move.
 
-def parse_pseudobond_group_arg(pbspec, session):
-    from .pbgroup import PseudoBondGroup
-    from .cli import UserError
-    if pbspec is None:
-        pbg_list = [m for m in session.models.list() if isinstance(m, PseudoBondGroup)]
-        if len(pbg_list) == 0:
-            raise UserError('No pseudobond group opened.')
-        elif len(pbg_list) > 1:
-            raise UserError('Multiple (%d) pseudobond groups opened, must specify one.' % len(pbg_list))
-        pbg = pbg_list[0]
-    else:
-        pbg_list = pbspec.evaluate(session).models
-        if len(pbg_list) == 0:
-            raise UserError('No pseudobond group specified by "%s".' % str(pbspec))
-        elif len(pbg_list) > 1:
-            raise UserError('Multiple (%d) pseudobond groups specifed, must specify one.' % len(pbg_list))
-        pbg = pbg_list[0]
-    return pbg
+    if radius:
+        for pb in pbonds:
+            pb.radius = radius
 
-def links_by_molecule(pblist):
+    if minimize:
+        mols = minimize
+        if len(mols) == 0:
+            from .cli import UserError        
+            raise UserError('No structures specified for minimizing crosslinks.')
+        mol_links, mol_pbonds = links_by_molecule(pbonds, mols)
+        if len(mol_links) == 0:
+            from .cli import UserError        
+            raise UserError('No pseudobonds to minimize for specified molecules.')
+        if len(mols) == 1:
+            iterations = min(1,iterations)
+        from numpy import array, float64
+        from . import align
+        for i in range(iterations):
+            for m in mols:
+                if m in mol_links:
+                    atom_pairs = mol_links[m]
+                    moving = array([a1.scene_coord for a1,a2 in atom_pairs], float64)
+                    fixed = array([a2.scene_coord for a1,a2 in atom_pairs], float64)
+                    tf, rms = align.align_points(moving, fixed)
+                    m.position = tf * m.position
+
+        lengths = [pb.length for pb in mol_pbonds]
+        lengths.sort(reverse = True)
+        lentext = ', '.join('%.1f' % d for d in lengths)
+        session.logger.info('%d crosslinks, lengths: %s' % (len(mol_pbonds), lentext))
+
+    for pbg in pbgroups:
+        pbg.update_graphics()	# TODO: pseudobonds should update automatically
+
+def links_by_molecule(pbonds, mols):
     mol_links = {}
-    for pb in pblist:
+    mol_pbonds = set()
+    mset = set(mols)
+    for pb in pbonds:
         a1, a2 = pb.atoms
         m1, m2 = a1.molecule, a2.molecule
         if m1 != m2:
-            mol_links.setdefault(m1,[]).append((a1,a2))
-            mol_links.setdefault(m2,[]).append((a2,a1))
-    return mol_links
+            if m1 in mset:
+                mol_links.setdefault(m1,[]).append((a1,a2))
+                mol_pbonds.add(pb)
+            if m2 in mset:
+                mol_links.setdefault(m2,[]).append((a2,a1))
+                mol_pbonds.add(pb)
+    return mol_links, mol_pbonds
 
 def register_crosslink_command():
     from . import cli
-    from .atomspec import AtomSpecArg
+    from .pbgroup import PseudoBondGroupsArg
     from .color import ColorArg
-    desc = cli.CmdDesc(optional = [('pbspec', AtomSpecArg)],
-                       keyword = [('iterations', cli.IntArg),
+    from .structure import AtomicStructuresArg
+    desc = cli.CmdDesc(optional = [('pbgroups', PseudoBondGroupsArg)],
+                       keyword = [('color', ColorArg),
                                   ('radius', cli.FloatArg),
-                                  ('color', ColorArg),
+                                  ('minimize', AtomicStructuresArg),
+                                  ('iterations', cli.IntArg),
                               ])
-    cli.register('crosslink', desc, minimize_crosslinks)
+    cli.register('crosslink', desc, crosslink)
