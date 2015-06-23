5be0af9d6237ee46a5761726be800652595f3a85
Eric Pettersen <pett@cgl.ucsf.edu>
2015-06-23 10:44:47 -0700

    support for opening/saving files

---
+++ src/core/open_save.py
@@ -0,0 +1,11 @@
+# vi: set expandtab shiftwidth=4 softtabstop=4:
+"""
+open_save: support for opening and saving files
+===============================================
+
+TODO
+"""
+
+import wx
+class OpenDialog(wx.FileDialog):
+    pass
