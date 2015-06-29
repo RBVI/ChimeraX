29e924da5df0b56dc64eb9730a22ab84008ff307
Conrad Huang <conrad@cgl.ucsf.edu>
2015-06-29 11:17:09 -0700

    Better SaveDialog error diagnostics

--- src/core/ui/save_dialog.py
+++ src/core/ui/save_dialog.py
@@ -15,6 +15,8 @@ class _SaveFormat:
         return self._wildcard()
 
     def window(self, parent):
+        if self._make_ui is None:
+            return None
         if self._window is None:
             self._window = self._make_ui(parent)
         return self._window
@@ -32,7 +34,7 @@ class SaveDialog:
         self._registered_formats = {}
         self._format_selector = None
         import io
-        self.register(self.DEFAULT_FORMAT, _session_wildcard, _session_ui, _session_save)
+        self.register(self.DEFAULT_FORMAT, _session_wildcard, None, _session_save)
 
     def register(self, fmt, wildcard, make_ui, save):
         self._registered_formats[fmt] = _SaveFormat(fmt, wildcard, make_ui, save)
@@ -47,37 +49,47 @@ class SaveDialog:
             self.file_dialog.SetExtraControlCreator(self._extraCreatorCB)
         if self.file_dialog.ShowModal() == wx.ID_CANCEL:
             return
-        self._registered_formats[self._format_selector.GetValue()].save(session, filename)
+        fmt = self._registered_formats[self._format_selector.GetValue()]
+        filename = self.file_dialog.GetPath()
+        fmt.save(session, filename)
 
     def _extraCreatorCB(self, parent):
         import wx
         p = wx.Panel(parent)
-        s = wx.BoxSizer(wx.VERTICAL)
-        # ComboBox at top for selecting save file type
-        self._format_selector = wx.ComboBox(p, style=wx.CB_READONLY)
-        self._format_selector.Bind(wx.EVT_COMBOBOX, self._select_format)
-        s.Add(self._format_selector, flag=wx.LEFT)
-        # Panel for displaying format-specific options goes immediately below
-        # Default is a window displaying text of "No user-settable options"
-        # For some reason, a Panel does not work but a Window does
-        self._no_options_window = wx.Window(p)
-        ns = wx.BoxSizer(wx.VERTICAL)
-        t = wx.StaticText(self._no_options_window, label="No user-settable options",
-                          style=wx.ALIGN_CENTER)
-        ns.Add(t, flag=wx.LEFT)
-        self._no_options_window.SetSizerAndFit(ns)
-        self._known_options = set([self._no_options_window])
-        self._current_option = self._no_options_window
-        s.Add(self._no_options_window, flag=wx.EXPAND|wx.ALL)
-        # Finish up layout
-        p.SetSizerAndFit(s)
-        self._options_sizer = s
-        self._options_window = p
-        # Update everything
-        self._update_format_selector()
-        self._format_selector.SetValue(self.DEFAULT_FORMAT)
-        self._select_format(self.DEFAULT_FORMAT)
-        return p
+        try:
+            s = wx.BoxSizer(wx.VERTICAL)
+            # ComboBox at top for selecting save file type
+            format_selector = wx.ComboBox(p, style=wx.CB_READONLY)
+            format_selector.Bind(wx.EVT_COMBOBOX, self._select_format)
+            s.Add(format_selector, flag=wx.LEFT)
+            # Panel for displaying format-specific options goes immediately below
+            # Default is a window displaying text of "No user-settable options"
+            # For some reason, a Panel does not work but a Window does
+            no_options_window = wx.Window(p)
+            ns = wx.BoxSizer(wx.VERTICAL)
+            t = wx.StaticText(no_options_window, label="No user-settable options",
+                              style=wx.ALIGN_CENTER)
+            ns.Add(t, flag=wx.LEFT)
+            no_options_window.SetSizerAndFit(ns)
+            s.Add(no_options_window, flag=wx.EXPAND|wx.ALL)
+            # Save references to widgets we need
+            self._options_window = p
+            self._options_sizer = s
+            self._format_selector = format_selector
+            self._no_options_window = no_options_window
+            self._known_options = set([self._no_options_window])
+            self._current_option = self._no_options_window
+            # Update everything (may require attributes set above)
+            p.SetSizerAndFit(s)
+            self._update_format_selector()
+            self._format_selector.SetValue(self.DEFAULT_FORMAT)
+            self._select_format(self.DEFAULT_FORMAT)
+            return p
+        except:
+            import traceback, sys
+            traceback.print_exc(file=sys.__stderr__)
+            p.Destroy()
+            return None
 
     def _update_format_selector(self):
         choices = list(self._registered_formats.keys())
@@ -110,15 +122,11 @@ def _session_wildcard():
     return io.wx_export_file_filter(io.SESSION)
 
 
-def _session_ui(parent):
-    return None
-
-
-def _session_save(self, session, filename):
+def _session_save(session, filename):
     import os.path
     ext = os.path.splitext(filename)[1]
     from .. import io
-    exts = io.extensions(io.category(io.SESSION))
+    exts = io.extensions("Chimera session")
     if exts and ext not in exts:
         filename += exts[0]
     # TODO: generate text command instead of calling export directly
