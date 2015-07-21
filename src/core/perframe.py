1c443c3efa37464f79c4ef04490e28e3967a174a
Greg Couch <gregc@cgl.ucsf.edu>
2015-07-20 17:23:19 -0700

    Convert to using alias mechanism.

--- src/core/perframe.py
+++ src/core/perframe.py
@@ -4,17 +4,19 @@
 # perframe  command  [ range  start,end[,step] ]... [ frames N ] [ interval K ]
 #                    [ zeroPadWidth width] [ showCommands ]
 #
+
 def register_perframe_command():
 
     from chimera.core.cli import CmdDesc, register, IntArg, StringArg, NoArg
     desc = CmdDesc(required = [('command', StringArg)],
-                   keyword = [('range', RangeArg),	# TODO: Allow multiple range arguments.
+                   keyword = [('range', RangeArg),      # TODO: Allow multiple range arguments.
                               ('frames', IntArg),
                               ('interval', IntArg),
-                              ('format', StringArg),	# TODO: Allow multiple format arguments.
+                              ('format', StringArg),    # TODO: Allow multiple format arguments.
                               ('zero_pad_width', IntArg),
                               ('show_commands', NoArg)])
     register('perframe', desc, perframe)
+    register('~perframe', CmdDesc(), stop_perframe_callbacks)
 
 def perframe(session, command, frames = None, interval = 1, format = None,
              zero_pad_width = None, range = None, show_commands = False):
@@ -23,8 +25,9 @@ def perframe(session, command, frames = None, interval = 1, format = None,
         stop_perframe_callbacks(session)
         return
 
+    from . import cli
     data = {
-        'command': command,
+        'command': cli.Alias(command),
         'frames': frames,
         'interval': interval,
         'skip': 0,
@@ -53,44 +56,42 @@ def _perframe_callback(data, session):
             d['skip'] = d['interval'] - 1
 
     frames, frame_num = d['frames'], d['frame_num']
-    cmd, stop = _expand_command(d['command'], frame_num, frames, d['ranges'],
+    args, stop = _perframe_args(frame_num, frames, d['ranges'],
                                 d['format'], d['zero_pad_width'])
+    tag = None
     if d['show_commands']:
-        session.logger.info('perframe %d: %s\n' % (frame_num, cmd))
+        tag = 'perframe %d: ' % frame_num
+    alias = d['command']
     try:
-        cmd = cli.Command(session, cmd, final=True)
-        cmd.execute()
+        alias(session, *args, echo_tag=tag)
     except:
         stop_perframe_callbacks(session, [d['callback']])
+        if alias.cmd is not None:
+            cmd_text = alias.cmd.current_text
+        else:
+            cmd_text = alias.original_text
         import sys
         session.logger.warning("Error executing per-frame command '%s': %s"
-                               % (cmd, sys.exc_info()[1]))
+                               % (cmd_text, sys.exc_info()[1]))
         return
-    if stop or (not frames is None and frame_num >= frames):
+    if stop or (frames is not None and frame_num >= frames):
         stop_perframe_callbacks(session, [d['callback']])
     else:
         d['frame_num'] = frame_num + 1
 
-def _expand_command(command, frame_num, frames, ranges, format, zero_pad_width):
-    args, stop = _perframe_args(frame_num, frames, ranges, format, zero_pad_width)
-    for i, arg in enumerate(args):
-        var = '$%d' % (i + 1)
-        command = command.replace(var, arg)
-    return command, stop
-
 def _perframe_args(frame_num, frames, ranges, format, zero_pad_width):
     args = []
     stop = False
-    if not format is None:
+    if format is not None:
         fmt = format
-    elif not zero_pad_width is None:
+    elif zero_pad_width is not None:
         fmt = ('%%0%dg' if ranges else '%%0%dd') % zero_pad_width
     else:
         fmt = '%g' if ranges else '%d'
     if ranges:
         for vr in ranges:
             r0, r1 = vr[:2]
-            explicit_rstep = not vr[2] is None
+            explicit_rstep = len(vr) > 2
             if frames is None or explicit_rstep:
                 rstep = (vr[2] if explicit_rstep
                          else (1 if r1 >= r0 else -1))
@@ -109,7 +110,7 @@ def _perframe_args(frame_num, frames, ranges, format, zero_pad_width):
 
 def stop_perframe_callbacks(session, callbacks = None):
 
-    if not hasattr(session, 'perframe_callbacks') and callbacks:
+    if not hasattr(session, 'perframe_callbacks'):
         from . import cli
         raise cli.UserError("No per-frame command active")
     pfcb = session.perframe_callbacks
@@ -123,20 +124,4 @@ def stop_perframe_callbacks(session, callbacks = None):
 # -----------------------------------------------------------------------------
 #
 from . import cli
-class RangeArg(cli.Annotation):
-    '''2 or 3 floating point values: start, end, step'''
-    name = 'range'
-
-    @staticmethod
-    def parse(text, session):
-        token, text, rest = cli.next_token(text)
-        try:
-            f = [float(x) for x in token.split(',')]
-        except ValueError:
-            f = []
-        n = len(f)
-        if n < 2 or n > 3:
-            raise cli.UserError('Range argument must be 2 or 3 comma-separated numbers, got %s' % token)
-        f0,f1 = f[:2]
-        step = f[2] if n >= 3 else None
-        return (f0,f1,step), text, rest
+RangeArg = cli.ListOf(cli.FloatArg, 2, 3, name='a range')
