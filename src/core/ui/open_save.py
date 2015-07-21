41c99ba19bef6ce44591c4828b0e167f9bb2618d
Eric Pettersen <pett@cgl.ucsf.edu>
2015-07-21 13:52:34 -0700

    add support for automatically adding suffixes to file names

--- src/core/ui/open_save.py
+++ src/core/ui/open_save.py
@@ -10,10 +10,31 @@ import wx
 class OpenDialog(wx.FileDialog):
     def __init__(self, parent, *args, **kw):
         kw['style'] = kw.get('style', 0) | wx.FD_OPEN
-        super(OpenDialog, self).__init__(parent, *args, **kw)
+        super().__init__(parent, *args, **kw)
 
 class SaveDialog(wx.FileDialog):
     def __init__(self, parent, *args, **kw):
         kw['style'] = kw.get('style', 0) | wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT
-        super(SaveDialog, self).__init__(parent, *args, **kw)
+        self.__add_suffix = kw.pop('add_suffix', None)
+        super().__init__(parent, *args, **kw)
+
+    def GetFilenames(self):
+        fns = super().GetFilename()
+        return self.__append_suffix(fns)
+
+    def GetPaths(self):
+        paths = super().GetPaths()
+        return self.__append_suffix(paths)
+
+    def __append_suffix(self, paths):
+        if self.__add_suffix is None:
+            return paths
+        appended = []
+        import os.path
+        for path in paths:
+            front, ext = os.path.splitext(path)
+            if ext:
+                appended.append(path)
+            else:
+                appended.append(path + self.__add_suffix)
 
