2cdb65738a25866420c0219ccf50acd5d78123ff
Tom Goddard <goddard@sonic.net>
2015-07-28 18:00:48 -0700

    Added code to use OSMesa for offscreen rendering in nogui mode. This is currently not enabled because OSMesa is not included in the distribution.

--- src/core/graphics/__init__.py
+++ src/core/graphics/__init__.py
@@ -15,7 +15,7 @@ from .camera import Camera, mono_camera_mode, stereo_camera_mode, CameraMode
 
 from .crossfade import CrossFade, MotionBlur
 
-from .opengl import Texture, Lighting, Material
+from .opengl import Texture, Lighting, Material, OffScreenRenderingContext
 
 from .view import View, OpenGLContext
 
