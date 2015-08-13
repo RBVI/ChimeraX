a0b7b88355f91d76c0a9f7a1f07208736e6b2d5e
Tom Goddard <goddard@sonic.net>
2015-08-12 18:05:24 -0700

    Assembly images command "aimages" for rendering mmCIF assemblies for the Protein Databank.

---
+++ src/core/commands/aimages.py
@@ -0,0 +1,90 @@
+#
+# Assembly images command for batch rendering mmCIF assembly images.
+# This is an obscure capability developed for the Protein DataBank to render images.
+#
+def render_assembly_images(session, directory = '.', subdirectories = True,
+                           width = 400, height = 400, supersample = 2,
+                           image_suffix = '.png', exclude = ['128d.cif', '1m4x.cif'],
+                           log_file = '/Users/goddard/ucsf/assemblies/log'):
+
+    s = session
+    run(s, 'set bg white')
+    run(s, 'set silhouette false')
+    run(s, 'light soft')
+    run(s, 'log warningDialog false errorDialog false')    # Avoid dialogs stopping batch rendering.
+
+    mmcifs = cif_files(directory, subdirectories, exclude, image_suffix)
+
+    log = open(log_file, 'a')
+    log.write('Rendering %d mmCIF files\n' % len(mmcifs))
+    for f in mmcifs:
+        log.write(f + '\n')
+        log.flush()
+        try:
+            save_images(f, width, height, supersample, image_suffix, s)
+        except TypeError as e:
+            log.write(str(e) + '\n')   # Handle mmcif string to long errors.
+    log.close()
+
+def cif_files(directory, subdirectories, exclude, image_suffix):
+
+    from os import listdir, path
+    files = listdir(directory)
+    has_image = set(f.rsplit('_', maxsplit=1)[0]+'.cif' for f in files if f.endswith(image_suffix))
+    mmcifs = [path.join(directory,f) for f in files
+              if f.endswith('.cif') and not f in exclude and not f in has_image]
+    # Include subdirectories
+    for f in files:
+        d = path.join(directory, f)
+        if path.isdir(d):
+            mmcifs.extend(cif_files(d, subdirectories, exclude, image_suffix))
+    return mmcifs
+
+def save_images(path, width, height, supersample, image_suffix, session):
+
+    s = session
+    m = s.models
+    mols = m.open(path)
+    m.close(mols[1:]) # Only use first model in nmr ensembles.
+    mol = mols[0]
+
+    run(s, 'ks cc')	# Color by chain
+
+    from os.path import splitext
+    image_prefix = splitext(path)[0]
+    save_assembly_images(mol, width, height, supersample,
+                         image_prefix, image_suffix, session)
+
+    run(s, 'close')
+
+def save_assembly_images(mol, width, height, supersample, image_prefix, image_suffix, session):
+    s = session
+    from . import sym
+    for assembly in sym.pdb_assemblies(mol):
+        run(s, 'sym #%s assembly %s' % (mol.id_string(), assembly.id))
+        run(s, 'window')          # Zoom to fit molecules
+        image_file = '%s_%s%s' % (image_prefix, assembly.id, image_suffix)
+        run(s, 'save "%s" width %d height %d supersample %d'
+            % (image_file, width, height, supersample))
+
+def run(session, cmd_text):
+    from . import Command
+    cmd = Command(session, cmd_text, final=True)
+    cmd.execute()
+
+def register_assembly_images_command():
+    from . import CmdDesc, StringArg, IntArg, ListOf, register
+    desc = CmdDesc(
+        optional = [('directory', StringArg)],
+        keyword = [('width', IntArg),
+                   ('height', IntArg),
+                   ('supersample', IntArg),
+                   ('image_suffix', StringArg),
+                   ('exclude', ListOf(StringArg)),
+                   ('log_file', StringArg)],
+        synopsis = 'Render mmCIF assembly images')
+    register('aimages', desc, render_assembly_images)
+
+# To make a tiled array of images with filename labels:
+# /opt/ImageMagick/bin/montage -label "%t" *.png -geometry "400x400+0+0" tiled.jpg
+
