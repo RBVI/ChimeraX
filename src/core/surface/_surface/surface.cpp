e05efdc4594e7138d489c85596e39b305c2b4663
Tom Goddard <goddard@sonic.net>
2015-08-21 12:43:10 -0700

    Added argument types to doc strings to be used by Sphinx for generating programming guide.

--- src/core/surface/_surface/surface.cpp
+++ src/core/surface/_surface/surface.cpp
@@ -21,164 +21,189 @@ static struct PyMethodDef surface_cpp_methods[] =
   /* connected.h */
   {const_cast<char*>("connected_triangles"), (PyCFunction)connected_triangles,
    METH_VARARGS|METH_KEYWORDS,
+   "connected_triangles(triangles, tindex)\n"
+   "\n"
    "Return sorted array of triangle indices of triangles connected to the\n"
    "specified triangle.  Two triangles are connected if they share a vertex.\n"
    "The surface must be oriented and at most two triangles can share an edge.\n"
    "The triangle array is triples of indices of vertices (m by 3, Numpy int32).\n"
-   "\n"
-   "Args: triangle_array, int tindex\n"
+   "Implemented in C++.\n"
   },
   {const_cast<char*>("triangle_vertices"), (PyCFunction)triangle_vertices,
    METH_VARARGS|METH_KEYWORDS,
-   "Return an array of vertex indices used the specified subset of triangles.\n"
+   "triangle_vertices(triangles, tindices) -> vertex_indices\n"
    "\n"
-   "Args: triangle_array, triangle_list (int array)\n"
+   "Return an array of vertex indices used the specified subset of triangles.\n"
   },
   {const_cast<char*>("connected_pieces"), (PyCFunction)connected_pieces,
    METH_VARARGS|METH_KEYWORDS,
+   "connected_pieces(triangles)\n"
+   "\n"
    "Return each connected piece of a surface as a separate triangle array\n"
    "and vertex array.  The return value is a tuple of pairs of vertex and\n"
    "triangle index arrays.  Vertices connected by any sequence of triangle\n"
    "edges are considered connected.\n"
-   "\n"
-   "Args: triangle_array (N by 3 int)\n"
+   "Implemented in C++.\n"
   },
 
   /* measure.h */
   {const_cast<char*>("enclosed_volume"), (PyCFunction)enclosed_volume,
    METH_VARARGS|METH_KEYWORDS,
+   "enclosed_volume(vertices, triangles) -> (volume, hole_count)\n"
+   "\n"
    "If surface has hole then returned volume is computed by capping\n"
    "boundary loops with fans centered at geometric center of loops.\n"
    "Returns volume and hole count.\n"
-   "\n"
-   "Args: vertices, triangles\n"
+   "Implemented in C++.\n"
   },
   {const_cast<char*>("surface_area"), (PyCFunction)surface_area,
    METH_VARARGS|METH_KEYWORDS,
+   "surface_area(vertices, triangles) -> area\n"
+   "\n"
    "Sum of triangle areas.\n"
-   "\n"
-   "Args: vertices, triangles\n"
-   "\n"
+   "Implemented in C++.\n"
   },
   {const_cast<char*>("vertex_areas"), (PyCFunction)vertex_areas,
    METH_VARARGS|METH_KEYWORDS,
-   "Accumulate 1/3 triangle area to each vertex.\n"
+   "vertex_areas(vertices, triangles, areas)\n"
    "\n"
-   "Args: vertices, triangles, areas\n"
+   "Accumulate 1/3 triangle area to each vertex.\n"
+   "Third parameter areas is a float array for returning vertex area values.\n"
+   "Implemented in C++.\n"
   },
   {const_cast<char*>("boundary_edges"), (PyCFunction)boundary_edges,
    METH_VARARGS|METH_KEYWORDS,
-   "Returns N by 2 array of vertex indices for directed edges.\n"
+   "boundary_edges(triangles) -> vertex index pairs\n"
    "\n"
-   "Args: triangles\n"
+   "Returns N by 2 array of vertex indices for directed edges.\n"
+   "Implemented in C++.\n"
   },
   {const_cast<char*>("boundary_loops"), (PyCFunction)boundary_loops,
    METH_VARARGS|METH_KEYWORDS,
-   "Returns tuple of arrays of vertex indices, one array for each loop.\n"
+   "boundary_loops(triangles) -> tuple of vertex index arrays\n"
    "\n"
-   "Args: triangles\n"
+   "Returns tuple of arrays of vertex indices, one array for each loop.\n"
+   "Implemented in C++.\n"
   },
 
   /* normals.h */
   {const_cast<char*>("calculate_vertex_normals"), (PyCFunction)calculate_vertex_normals,
    METH_VARARGS|METH_KEYWORDS,
-   "Args: vertex_array, triangle_array"
+   "calculate_vertex_normals(vertices, triangles)\n"
+   "\n"
+   "Calculate vertex normals by adding normals for each triangle that uses\n"
+   "a vertex, and then normalizing the sum.\n"
+   "Implemented in C++.\n"
   },
   {const_cast<char*>("invert_vertex_normals"), (PyCFunction)invert_vertex_normals,
    METH_VARARGS|METH_KEYWORDS,
-   "Args: normals_array, triangle_array"
+   "invert_vertex_normals(normals, triangles)\n"
+   "\n"
+   "Flip normals and reverse triangle vertex order.\n"
+   "Implemented in C++.\n"
   },
 
   /* parse_stl.h */
   {const_cast<char*>("parse_stl"), (PyCFunction)parse_stl,
    METH_VARARGS|METH_KEYWORDS,
-   "Read a binary STL file passed in as a byte array and return\n"
+   "parse_stl(bytes) -> (comment, vertices, normals, triangles)\n"
+   "\n"
+   "Parse a binary STL file passed in as a byte array and return\n"
    "4 values: the header comment, unique vertices, corresponding normals\n"
    "and triangles (triples of vertex indices).\n"
-   "\n"
-   "Args: binary STL as byte array\n"
+   "Implemented in C++.\n"
    },
 
   /* patches.h */
   {const_cast<char*>("sharp_edge_patches"), (PyCFunction)sharp_edge_patches,
    METH_VARARGS|METH_KEYWORDS,
+   "sharp_edge_patches(vertices, normals, triangles, vertex_to_atom_index_map, atom_positions)"
+   " -> (vertices, normals, triangles, vertex_to_atom_index_map)\n"
+   "\n"
    "Split triangles to create sharp boundaries equidistant between atoms.\n"
-   "\n"
-   "Args: vertices, normals, triangles, vertex to atom index array, atom positions.\n"
-   "\n"
-   "Returns subdivide vertices, normals, triangles, vertex to atom.\n"
+   "Implemented in C++.\n"
   },
   {const_cast<char*>("unique_vertex_map"), (PyCFunction)unique_vertex_map,
    METH_VARARGS|METH_KEYWORDS,
+   "unique_vertex_map(vertices)\n"
+   "\n"
    "Map vertex indices to unique vertex indices so vertices at the same point\n"
    "are treated as one.  This is used for connected piece calculations.\n"
+   "Implemented in C++.\n"
    "\n"
-   "Args: vertex array, n by 3 float\n"
-   "\n"
-   "Returns numpy int array, length n, mapping vertex index to unique vertex index.\n"
+   "Returns numpy int32 array, length n, mapping vertex index to unique vertex index.\n"
   },
 
   /* sasa.h */
   {const_cast<char*>("surface_area_of_spheres"), (PyCFunction)surface_area_of_spheres,
    METH_VARARGS|METH_KEYWORDS,
+   "surface_area_of_spheres(centers, radii, areas)\n"
+   "\n"
    "Compute surface area of union of solid sphere.\n"
-   "Can fail in degenerate cases returning -1 for spheres with failed area calculation.\n"
-   "\n"
-   "Args: centers, radii, areas.\n"
-   "\n"
-   "Returns: array of areas contributed by each sphere\n"
+   "Third argument areas contains areas contributed by each sphere\n"
+   "Can fail in degenerate cases giving area -1 for spheres with failed area calculation.\n"
+   "Implemented in C++.\n"
   },
   {const_cast<char*>("estimate_surface_area_of_spheres"), (PyCFunction)estimate_surface_area_of_spheres,
    METH_VARARGS|METH_KEYWORDS,
+   "estimate_surface_area_of_spheres(centers, radii, sphere_points, point_weights, areas)\n"
+   "\n"
    "Use points on sphere, count how many are inside other spheres\n"
    "to estimate surface area of union of solid spheres.\n"
-   "\n"
-   "Args: centers, radii, sphere_points, point_weights, areas\n"
-   "\n"
-   "Returns: array of areas contributed by each sphere.\n"
+   "Third argument areas contains areas contributed by each sphere\n"
+   "Implemented in C++.\n"
   },
 
   /* subdivide.h */
   {const_cast<char*>("subdivide_triangles"), (PyCFunction)subdivide_triangles,
    METH_VARARGS|METH_KEYWORDS,
+   "subdivide_triangles(vertices, triangles, normals) -> (vertices triangles, normals)\n"
+   "\n"
    "Divide each triangle into 4 triangles placing new vertices at edge midpoints.\n"
-   "\n"
-   "Args: vertices, triangles, normals\n"
-   "\n"
-   "Returns: vertices triangles, normals\n"
+   "Implemented in C++.\n"
   },
   {const_cast<char*>("subdivide_mesh"), (PyCFunction)subdivide_mesh,
    METH_VARARGS|METH_KEYWORDS,
+   "subdivide_mesh(vertices, triangles, normals, edge_length) -> (vertices triangles, normals)\n"
+   "\n"
    "Divide triangle into smaller triangles so that edges are shorter\n"
    "than the specified the maximum edge length.\n"
-   "\n"
-   "Args: vertices, triangles, normals, edge_length\n"
-   "\n"
-   "Returns: vertices triangles, normals\n"
+   "Implemented in C++.\n"
   },
 
   /* tube.h */
   {const_cast<char*>("tube_geometry"), (PyCFunction)tube_geometry,
    METH_VARARGS|METH_KEYWORDS,
+   "tube_geometry(path, tangents, cross_section, cross_section_normals)"
+   " -> (vertices, normals, triangles)\n"
+   "\n"
    "Calculates tube surface geometry from a center-line path.\n"
-   "\n"
-   "Args: path, tangents, cross_section, cross_section_normals -- all n by 3 float arrays\n"
-   "\n"
-   "Returns: vertices, normals, triangles\n"
+   "Arguments path and tangents are n by 3 float arrays,\n"
+   "and the cross section arguments are m by 3 arrays.\n"
+   "Implemented in C++.\n"
 },
   {const_cast<char*>("tube_geometry_colors"), (PyCFunction)tube_geometry_colors,
    METH_VARARGS|METH_KEYWORDS,
-   "Args: colors, segment_subdivisions, circle_subdivisions, start_divisions, end_divisions\n"
+   "tube_geometry_colors(colors, segment_subdivisions, circle_subdivisions, start_divisions, end_divisions)"
+   " -> N by 4 numpy array of RGBA colors\n"
    "\n"
-   "Returns: N by 4 numpy array of RGBA colors.\n"
+   "Computes vertex colors for a tube with geometry determined by tube_geometry()\n"
+   "Each segment can have a separate color. Colors argument is N by 4 array.\n"
+   "A segment is is the interval between segment_subdivisions+1 path points not\n"
+   "including points at ends of the path specified by start/end divisions.\n"
+   "Arguments other than colors are integers.\n"
+   "Implemented in C++.\n"
   },
   {const_cast<char*>("tube_triangle_mask"), (PyCFunction)tube_triangle_mask,
    METH_VARARGS|METH_KEYWORDS,
+   "tube_triangle_mask(segment_mask, segment_subdivisions, circle_subdivisions, start_divisions, end_divisions)"
+   " -> triangle_mask\n"
+   "\n"
    "Computes triangle mask to show only specified segments of a tube generated with tube_geometry().\n"
-   "\n"
-   "Args: segment_mask (uint8 length N array), segment_subdivisions, circle_subdivisions, start_divisions, end_divisions\n"
-   "\n"
-   "Returns: uint8 numpy array equal to number of triangles generated by tube_geometry().\n"
+   "Segments are defined in the same way as for the tube_geometry_colors() routine.\n"
+   "The input segment mask is a uint8 length N array, and output is a uint8 numpy array\n"
+   "with length equal to number of triangles generated by tube_geometry().\n"
+   "Implemented in C++.\n"
   },
 
   {NULL, NULL, 0, NULL}
