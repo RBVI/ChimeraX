42ad52cb7366053e8f49d1040545b4ad58412a22
Tom Goddard <goddard@sonic.net>
2015-09-08 19:30:27 -0700

    Added transparency command.

--- src/core/commands/commands.py
+++ src/core/commands/commands.py
@@ -7,7 +7,7 @@ def register_core_commands(session):
                'delete', 'display', 'echo', 'exit', 'export', 'help', 'lighting', 'list', 'material', 
                'move', 'open', 'pdbimages', 'perframe', 'pwd', 'roll', 'ribbon', 'run',
                'sasa', 'save', 'scolor', 'set', 'split', 'stop', 'style', 'surface', 'sym',
-               'turn', 'view', 'wait']
+               'transparency', 'turn', 'view', 'wait']
     for mod in modules:
         m = import_module('chimera.core.commands.%s' % mod)
         m.register_command(session)
