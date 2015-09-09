c9833e59ba421ec5b2ea3f336e515e3f2f05bb81
Eric Pettersen <pett@cgl.ucsf.edu>
2015-09-09 14:26:11 -0700

    add ChangeTracker.cpp

---
+++ src/core/atomic/basegeom_cpp/ChangeTracker.cpp
@@ -0,0 +1,17 @@
+// vi: set expandtab ts=4 sw=4:
+#include "ChangeTracker.h"
+
+namespace basegeom {
+
+const std::string ChangeTracker::REASON_ALT_LOC("alt_loc changed");
+const std::string ChangeTracker::REASON_ANISO_U("aniso_u changed");
+const std::string ChangeTracker::REASON_BFACTOR("bfactor changed");
+const std::string ChangeTracker::REASON_COLOR("color changed");
+const std::string ChangeTracker::REASON_COORD("coord changed");
+const std::string ChangeTracker::REASON_DISPLAY("display changed");
+const std::string ChangeTracker::REASON_HIDE("hide changed");
+const std::string ChangeTracker::REASON_IDATM_TYPE("idatm_type changed");
+const std::string ChangeTracker::REASON_OCCUPANCY("occupancy changed");
+const std::string ChangeTracker::REASON_SELECTED("selected changed");
+
+}  // namespace basegeom
