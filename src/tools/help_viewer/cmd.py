69bea80142a4bebf9d72cc9aeb7bfa3871680340
Greg Couch <gregc@cgl.ucsf.edu>
2015-09-17 00:49:31 -0700

    Add Back/Forward/Home buttons.

--- src/tools/help_viewer/cmd.py
+++ src/tools/help_viewer/cmd.py
@@ -16,33 +16,37 @@ def help(session, topic=None):
         session.logger.warning("help viewer is only available if GUI is present")
         return
 
-    import os
-    base_dir = os.path.join(session.app_data_dir, 'docs')
-
-    if topic is None:
-        topic = 'user'
-
-    tmp = os.path.join(base_dir, topic)
-    if os.path.exists(tmp):
-        help_file = tmp
+    if topic is not None and topic.startswith(('file:', 'http:')):
+        help_file = topic
     else:
-        # check if topic matches a command name
-        cmd = Command(None)
-        cmd.current_text = topic
-        cmd._find_command_name(True, no_aliases=True)
-        if cmd._ci and cmd.amount_parsed == len(cmd.current_text):
-            help_file = os.path.join(base_dir, 'user', 'commands',
-                                     '%s.html' % cmd.current_text)
+        import os
+        base_dir = os.path.join(session.app_data_dir, 'docs')
+
+        if topic is None:
+            topic = 'user'
+
+        tmp = os.path.join(base_dir, topic)
+        if os.path.exists(tmp):
+            help_file = tmp
         else:
-            session.logger.error("No help found for '%s'" % topic)
-            return
-    if os.path.isdir(help_file):
-        help_file += '/index.html'
+            # check if topic matches a command name
+            cmd = Command(None)
+            cmd.current_text = topic
+            cmd._find_command_name(True, no_aliases=True)
+            if cmd._ci and cmd.amount_parsed == len(cmd.current_text):
+                help_file = os.path.join(base_dir, 'user', 'commands',
+                                         '%s.html' % cmd.current_text)
+            else:
+                session.logger.error("No help found for '%s'" % topic)
+                return
+        if os.path.isdir(help_file):
+            help_file += '/index.html'
+        from urllib.parse import quote
+        help_file = 'file://' + quote(help_file)
 
     from .gui import get_singleton
     help_viewer = get_singleton(session)
-    from urllib.parse import quote
-    help_viewer.show('file://' + quote(help_file))
+    help_viewer.show(help_file)
 
 help_desc = CmdDesc(
     optional=[('topic', RestOfLine)],
