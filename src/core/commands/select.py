34e0eb464e452a5bbbca644c97158fae9c836ca5
Tom Goddard <goddard@sonic.net>
2015-09-18 11:37:54 -0700

    Made select command replace current selection. Added "add" and "intersect" options to select command.

--- src/core/commands/select.py
+++ src/core/commands/select.py
@@ -1,49 +1,70 @@
 # vi: set expandtab shiftwidth=4 softtabstop=4:
 
-
-def select(session, objects=None, clear=False, subtract=None):
+def select(session, objects=None, add=None, subtract=None, intersect=None, clear=False):
     '''Select specified objects.
 
     Parameters
     ----------
     objects : AtomSpecResults
       Select the specified objects (typically atoms). If no specifier is given then everything is selected.
-      Objects that are already selected remain selected.
+      The current selection is replaced.
+    add : AtomSpecResults
+      Modify the current selection by adding the specified objects.
+    subtract : AtomSpecResults
+      Modify the current selection by unselecting the specified objects.
+    intersect : AtomSpecResults
+      Modify the current selection keeping only those among the specified objects selected.
     clear : no value
       Clear the selection.
-    subtract : AtomSpecResults
-      Unselect the specified objects.
     '''
     if clear:
         session.selection.clear()
         if objects is None:
             return
 
-    if objects is None and subtract is None:
+    if objects is None and add is None and subtract is None and intersect is None:
         from . import atomspec
         objects = atomspec.everything(session).evaluate(session)
 
     if objects is not None:
-        for m, atoms in objects.atoms.by_structure:
-            m.select_atoms(atoms)
-        from ..atomic import AtomicStructure
-        for m in objects.models:
-            if not isinstance(m, AtomicStructure):
-                m.selected = True
+        session.selection.clear()
+        modify_selection(objects, 'add')
+
+    if add is not None:
+        modify_selection(add, 'add')
 
     if subtract is not None:
-        for m, atoms in subtract.atoms.by_structure:
-            m.select_atoms(atoms, selected = False)
-        from ..atomic import AtomicStructure
-        for m in subtract.models:
-            if not isinstance(m, AtomicStructure):
-                m.selected = False
-        
+        modify_selection(subtract, 'subtract')
+
+    if intersect is not None:
+        intersect_selection(intersect, session)
+
+def modify_selection(objects, mode = 'add'):
+    select = (mode == 'add')
+    for m, atoms in objects.atoms.by_structure:
+        m.select_atoms(atoms, selected = select)
+    from ..atomic import AtomicStructure
+    for m in objects.models:
+        if not isinstance(m, AtomicStructure):
+            m.selected = select
+
+def intersect_selection(objects, session):
+    from .. import atomic
+    subatoms = atomic.selected_atoms(session) - objects.atoms
+    for m, atoms in subatoms.by_structure:
+        m.select_atoms(atoms, selected = False)
+    from ..atomic import AtomicStructure
+    mkeep = set(objects.models)
+    for m in session.selection.models():
+        if m not in mkeep:
+            m.selected = False
 
 def register_command(session):
     from . import CmdDesc, register, ObjectsArg, NoArg
     desc = CmdDesc(optional=[('objects', ObjectsArg)],
-                   keyword=[('clear', NoArg),
-                            ('subtract', ObjectsArg)],
+                   keyword=[('add', ObjectsArg),
+                            ('subtract', ObjectsArg),
+                            ('intersect', ObjectsArg),
+                            ('clear', NoArg),],
                    synopsis='select specified objects')
     register('select', desc, select)
