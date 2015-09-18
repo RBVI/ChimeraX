b288feb1ded3eef21cd62722370b7e4fbb1ba65b
Greg Couch <gregc@cgl.ucsf.edu>
2015-09-17 22:34:55 -0700

    Don't allow "sethome" to be abbreviated in help command. Enable help in nogui mode (uses text-based webbrowser).

--- src/tools/help_viewer/cmd.py
+++ src/tools/help_viewer/cmd.py
@@ -12,12 +12,8 @@ def help(session, set_home=False, topic=None):
         specified then the overview is shown.  Topics that are command names
         can be abbreviated.
     '''
-    if not session.ui.is_gui:
-        session.logger.warning("help viewer is only available if GUI is present")
-        return
-
     if topic is not None and topic.startswith(('file:', 'http:')):
-        help_file = topic
+        url = topic
     else:
         import os
         base_dir = os.path.join(session.app_data_dir, 'docs')
@@ -25,31 +21,35 @@ def help(session, set_home=False, topic=None):
         if topic is None:
             topic = 'user'
 
-        tmp = os.path.join(base_dir, topic)
-        if os.path.exists(tmp):
-            help_file = tmp
+        path = os.path.join(base_dir, topic)
+        if os.path.exists(path):
+            if os.path.isdir(path):
+                path += '/index.html'
         else:
             # check if topic matches a command name
             cmd = Command(None)
             cmd.current_text = topic
             cmd._find_command_name(True, no_aliases=True)
             if cmd._ci and cmd.amount_parsed == len(cmd.current_text):
-                help_file = os.path.join(base_dir, 'user', 'commands',
-                                         '%s.html' % cmd.current_text)
+                path = os.path.join(base_dir, 'user', 'commands',
+                                    '%s.html' % cmd.current_text)
             else:
                 session.logger.error("No help found for '%s'" % topic)
                 return
-        if os.path.isdir(help_file):
-            help_file += '/index.html'
         from urllib.parse import quote
-        help_file = 'file://' + quote(help_file)
+        url = 'file://' + quote(path)
 
-    from .gui import get_singleton
-    help_viewer = get_singleton(session)
-    help_viewer.show(help_file, set_home=set_home)
+    if session.ui.is_gui:
+        from .gui import get_singleton
+        help_viewer = get_singleton(session)
+        help_viewer.show(url, set_home=set_home)
+    else:
+        import webbrowser
+        webbrowser.open(url)
 
 help_desc = CmdDesc(
-    required=[('set_home', Or(EnumOf(['sethome']), EmptyArg))],
+    required=[('set_home',
+               Or(EnumOf(['sethome'], abbreviations=False), EmptyArg))],
     optional=[('topic', RestOfLine)],
     synopsis='display help'
 )
