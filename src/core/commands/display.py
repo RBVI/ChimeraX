aa02c37935a280cd89bdeb6dbed6616917995884
Tom Goddard <goddard@sonic.net>
2015-10-16 18:19:18 -0700

    Added display command "cartoons" option for what to display.

--- src/core/commands/display.py
+++ src/core/commands/display.py
@@ -8,7 +8,7 @@ def display(session, objects=None, what=None):
     objects : AtomSpecResults or None
         Atoms, bonds or models to show.  If None then all are shown.
         Objects that are already shown remain shown.
-    what : 'atoms', 'bonds', or 'models' or None
+    what : 'atoms', 'bonds', 'cartoons', 'models' or None
         What to show.  If None then 'atoms' if any atoms specified otherwise 'models'.
     '''
     if objects is None:
@@ -26,6 +26,9 @@ def display(session, objects=None, what=None):
         atoms.inter_bonds.displays = True
         from .. import atomic
         atomic.interatom_pseudobonds(atoms, session).displays = True
+    elif what == 'cartoons':
+        res = objects.atoms.unique_residues
+        res.ribbon_displays = True
     elif what == 'models':
         for m in objects.models:
             m.display = True
@@ -37,7 +40,7 @@ def undisplay(session, objects=None, what=None):
     ----------
     objects : AtomSpecResults or None
         Atoms, bonds or models to hide. If None then all are hidden.
-    what : 'atoms', 'bonds', or 'models'
+    what : 'atoms', 'bonds', 'cartoons', 'models' or None
         What to hide.  If None then 'atoms' if any atoms specified otherwise 'models'.
     '''
     if objects is None:
@@ -54,13 +57,16 @@ def undisplay(session, objects=None, what=None):
         atoms.inter_bonds.displays = False
         from .. import atomic
         atomic.interatom_pseudobonds(atoms, session).displays = False
+    elif what == 'cartoons':
+        res = objects.atoms.unique_residues
+        res.ribbon_displays = False
     elif what == 'models':
         for m in objects.models:
             m.display = False
 
 def register_command(session):
     from . import CmdDesc, register, ObjectsArg, EnumOf, EmptyArg, Or
-    what_arg = EnumOf(('atoms', 'bonds', 'models'))
+    what_arg = EnumOf(('atoms', 'bonds', 'cartoons', 'models'))
     desc = CmdDesc(optional=[('objects', Or(ObjectsArg, EmptyArg)),
                              ('what', what_arg)],
                    synopsis='display specified objects')
