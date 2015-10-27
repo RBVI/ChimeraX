09d7cba98e4a9ae6884f57a86c545eb3baac0da4
Tom Goddard <goddard@sonic.net>
2015-10-26 18:18:51 -0700

    Renamed file alias_cmd.py to alias.py to follow convention that each command is in a file that has the same name as the command.

--- src/core/commands/commands.py
+++ src/core/commands/commands.py
@@ -7,7 +7,7 @@ def register_core_commands(session):
     # abbreviated, the one registered that matches wins, not the first
     # in alphabetical order.
     modules = [
-        'alias_cmd', 'buriedarea',
+        'alias', 'buriedarea',
         'camera', 'close', 'cofr', 'color', 'colordef', 'crossfade', 'crosslinks',
         'delete', 'display', 'echo', 'exit', 'export',
         'lighting', 'list', 'material', 'move',
