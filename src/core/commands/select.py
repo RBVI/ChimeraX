ecb2a516106891d40aacf2f42556aae9b200b522
Tom Goddard <goddard@sonic.net>
2015-11-10 15:55:07 -0800

    Make selecting a molecular surface with the select command ("select #1.2") cause the atoms for that surface to be selected.  Follow-up to bug #103.

--- src/core/commands/select.py
+++ src/core/commands/select.py
@@ -39,25 +39,57 @@ def select(session, objects=None, add=None, subtract=None, intersect=None, clear
     if intersect is not None:
         intersect_selection(intersect, session)
 
+    report_selection(session)
+
+def report_selection(session):
+    s = session.selection
+    from ..atomic import MolecularSurface, AtomicStructure    
+    mc = len([m for m in s.models() if not isinstance(m, (AtomicStructure, MolecularSurface))])
+    ac = sum([len(atoms) for atoms in s.items('atoms')], 0)
+    lines = []
+    if mc == 0 and ac == 0:
+        lines.append('Nothing')
+    if ac != 0:
+        plural = ('s' if ac > 1 else '')
+        lines.append('%d atom%s' % (ac, plural))
+    if mc != 0:
+        plural = ('s' if mc > 1 else '')
+        lines.append('%d model%s' % (mc, plural))
+    session.logger.status(', '.join(lines) + ' selected')
+
 def modify_selection(objects, mode = 'add'):
     select = (mode == 'add')
-    for m, atoms in objects.atoms.by_structure:
-        m.select_atoms(atoms, selected = select)
-    from ..atomic import AtomicStructure
-    for m in objects.models:
-        if not isinstance(m, AtomicStructure):
-            m.selected = select
+    atoms, models = _atoms_and_models(objects)
+    for m, matoms in atoms.by_structure:
+        m.select_atoms(matoms, selected = select)
+    for m in models:
+        m.selected = select
 
 def intersect_selection(objects, session):
+    atoms, models = _atoms_and_models(objects)
     from .. import atomic
-    subatoms = atomic.selected_atoms(session) - objects.atoms
-    for m, atoms in subatoms.by_structure:
-        m.select_atoms(atoms, selected = False)
-    from ..atomic import AtomicStructure
-    mkeep = set(objects.models)
-    for m in session.selection.models():
-        if m not in mkeep:
-            m.selected = False
+    subatoms = atomic.selected_atoms(session) - atoms
+    submodels = set(session.selection.models()).difference(models)
+    for m, matoms in subatoms.by_structure:
+        m.select_atoms(matoms, selected = False)
+    for m in submodels:
+        m.selected = False
+
+def _atoms_and_models(objects):
+    # Treat selecting molecular surface as selecting atoms.
+    atoms = objects.atoms
+    satoms = []
+    models = []
+    from ..atomic import MolecularSurface, AtomicStructure
+    for m in objects.models:
+        if isinstance(m, MolecularSurface):
+            satoms.append(m.atoms)
+        elif not isinstance(m, AtomicStructure):
+            models.append(m)
+    if satoms:
+        from ..atomic import molarray
+        atoms = molarray.concatenate([atoms] + satoms)
+    return atoms, models
 
 def register_command(session):
     from . import CmdDesc, register, ObjectsArg, NoArg
