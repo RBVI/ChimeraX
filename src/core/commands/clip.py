8bc8ffde030d116024901d3b163f6dd8e060f054
Tom Goddard <goddard@sonic.net>
2015-11-17 16:58:46 -0800

    Improved clip command syntax.

--- src/core/commands/clip.py
+++ src/core/commands/clip.py
@@ -1,35 +1,39 @@
 # vim: set expandtab shiftwidth=4 softtabstop=4:
 
-def clip(session, near=None, far=None, p1=None, p2=None, off=None,
-         center=None, axis=None, coordinate_system=None, cap=None):
+def clip(session, near=None, far=None, p1=None, p2=None, slab=None, list=None, off=None,
+         position=None, axis=None, coordinate_system=None, cap=None):
     '''
     Enable or disable clip planes.
 
     Parameters
     ----------
-    near, far, p1, p2 : float
+    near, far, p1, p2 : float or "off"
        Distance from center of rotation for near and far clip planes that
        remain perpendicular to view and planes p1 and p2 that rotate with models.
        For near/far, positive distances are further away, negative are closer
        than center.
-    off : no value
+    list : bool
+       Report info about the current clip planes.
+    off : bool
        Turn off clipping (all planes).
-    center : Center
-       Near far offsets are relative to this point.  If not give then center
-       of rotation is used.
+    position : Center
+       Plane offsets are relative to this point.  If not give then center
+       of bounding box of displayed models is used.
     axis : Axis
        Normal to clip plane for planes p1 and p2.  Not used for near, far planes.
     coordinate_system : Model
-       Coordinate system for axis, if none then screen coordinates are used.
+       Coordinate system for axis and position, if none then screen coordinates are used.
     cap : bool
       Option for testing display of surface caps.  Will remove this later.
     '''
 
     v = session.main_view
+    if list:
+        report_clip_info(v, session.logger)
+
     have_offset = not (near is None and far is None and p1 is None and p2 is None)
     if not have_offset and off is None:
-        if v.clip_planes:
-            report_clip_info(v, session.logger)
+        if find_plane(v.clip_planes, 'p1'):
             return
         else:
             p1 = 0
@@ -39,61 +43,93 @@ def clip(session, near=None, far=None, p1=None, p2=None, off=None,
         v.clip_planes = []
 
     if have_offset:
-        origin = plane_origin(v, center, coordinate_system)
+        origin = plane_origin(v, position, coordinate_system)
         cam = v.camera
-        adjust_near_far_planes(near, far, origin, v.clip_planes, cam)
+        planes = v.clip_planes
+        from numpy import array, float32
+        z = array((0,0,1), float32)
+        if near is not None and far is not None:
+            adjust_slab('near', near, 'far', far, origin, None, planes, cam, -z)
+        elif near is not None:
+            adjust_plane('near', near, origin, None, planes, cam, -z)
+        elif far is not None:
+            adjust_plane('far', -far, origin, None, planes, cam, z)
+
         normal = None if axis is None else axis.scene_coordinates(coordinate_system, cam)
-        adjust_scene_planes(p1, p2, origin, normal, v.clip_planes, cam)
+        if p1 is not None and p2 is not None:
+            adjust_slab('p1', p1, 'p2', p2, origin, normal, planes, cam)
+        elif p1 is not None:
+            adjust_plane('p1', p1, origin, normal, planes, cam)
+        elif p2 is not None:
+            adjust_plane('p2', p2, origin, normal, planes, cam)
 
     v.redraw_needed = True
 
     if cap:
         show_surface_caps(v)
 
-def plane_origin(view, center, origin):
-    if center is None:
+def plane_origin(view, position, coordinate_system):
+    if position is None:
         b = view.drawing_bounds()
         if b is None:
             raise UserError("Can't position clip planes relative to center "
                             " of displayed models since nothing is displayed.")
         c0 = b.center()
     else:
-        c0 = center.scene_coordinates(coordinate_system)
+        c0 = position.scene_coordinates(coordinate_system)
     return c0
 
-def adjust_near_far_planes(near, far, origin, planes, camera):
-    for name, offset, axis in (('near', near, (0,0,-1)), ('far', far, (0,0,1))):
-        if offset is None:
-            continue
-        normal = camera.position.apply_without_translation(axis)
-        plane_point = origin - offset * normal
-        p = find_plane(planes, name)
-        if p is None:
-            from ..graphics import ClipPlane
-            p = ClipPlane(name, normal, plane_point, axis)
-            planes.append(p)
-        else:
-            p.plane_point = plane_point
+def adjust_plane(name, offset, origin, normal, planes, camera, camera_normal = None):
+    if offset == 'off':
+        remove_plane(planes, name)
+        return
 
-def adjust_scene_planes(p1, p2, origin, normal, planes, camera):
-    for name, offset in (('p1', p1), ('p2', p2)):
-        if offset is None:
-            continue
-        p = find_plane(planes, name)
-        if p is None:
-            n = camera.view_direction() if normal is None else normal
-            plane_point = origin - offset * n
-            from ..graphics import ClipPlane
-            p = ClipPlane(name, n, plane_point)
-            p.name = name
-            planes.append(p)
+    if camera_normal is not None:
+        normal = camera.position.apply_without_translation(camera_normal)
+
+    p = find_plane(planes, name)
+    if p is None:
+        n = camera.view_direction() if normal is None else normal
+        plane_point = origin + offset * n
+        from ..graphics import ClipPlane
+        p = ClipPlane(name, n, plane_point, camera_normal)
+        planes.append(p)
+    else:
+        n = p.normal if normal is None else normal
+        p.plane_point = origin + offset * n
+        if normal is not None:
+            p.normal = normal
+
+def adjust_slab(name1, offset1, name2, offset2, origin, normal, planes, camera,
+                camera_normal = None):
+    if offset1 == 'off' or offset2 == 'off':
+        adjust_plane(name1, offset1, origin, normal, planes, camera, camera_normal)
+        adjust_plane(name2, offset2, origin, normal, planes, camera, camera_normal)
+        return
+
+    if normal is None and camera_normal is None:
+        # Use an existing plane normal if one exists.
+        p1, p2 = find_plane(planes, name1), find_plane(planes, name2)
+        if p1 is not None:
+            normal = p1.normal
+        elif p2 is not None:
+            normal = -p2.normal
         else:
-            n = p.normal if normal is None else normal
-            p.plane_point = origin + offset * n
+            normal = camera.view_direction()
+
+    adjust_plane(name1, offset1, origin, normal, planes, camera, camera_normal)
+    n2 = None if normal is None else -normal
+    cn2 = None if camera_normal is None else -camera_normal
+    adjust_plane(name2, -offset2, origin, n2, planes, camera, cn2)
 
 def find_plane(planes, name):
     np = [p for p in planes if p.name == name]
     return np[0] if len(np) >= 1 else None
+
+def remove_plane(planes, name):
+    rp = [p for p in planes if p.name == name]
+    for p in rp:
+        planes.remove(p)
         
 def report_clip_info(viewer, log):
     # Report current clip planes.
@@ -139,15 +175,17 @@ def show_surface_clip_caps(clip_planes, drawings, offset = 0.01):
             d.display = False
 
 def register_command(session):
-    from .cli import CmdDesc, register, FloatArg, NoArg, AxisArg, ModelArg, CenterArg
+    from .cli import CmdDesc, register, FloatArg, NoArg, AxisArg, ModelArg, CenterArg, Or, EnumOf
+    offset_arg = Or(EnumOf(['off']), FloatArg)
     desc = CmdDesc(
         optional=[],
-        keyword=[('near', FloatArg),
-                 ('far', FloatArg),
-                 ('p1', FloatArg),
-                 ('p2', FloatArg),
+        keyword=[('near', offset_arg),
+                 ('far', offset_arg),
+                 ('p1', offset_arg),
+                 ('p2', offset_arg),
+                 ('list', NoArg),
                  ('off', NoArg),
-                 ('center', CenterArg),
+                 ('position', CenterArg),
                  ('axis', AxisArg),
                  ('coordinate_system', ModelArg),
                  ('cap', NoArg)],
