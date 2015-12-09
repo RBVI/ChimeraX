98cf549ef8d9e19cea55c436da5fe8cd82966a43
Tom Goddard <goddard@sonic.net>
2015-12-08 23:50:42 -0800

    Added open command from_database option.

--- src/core/commands/open.py
+++ src/core/commands/open.py
@@ -1,7 +1,7 @@
 # vim: set expandtab shiftwidth=4 softtabstop=4:
 
 
-def open(session, filename, format=None, name=None):
+def open(session, filename, format=None, name=None, from_database=None):
     '''Open a file.
 
     Parameters
@@ -13,8 +13,34 @@ def open(session, filename, format=None, name=None):
     format : string
         Read the file using this format, instead of using the file suffix to infer the format.
     name : string
-        Not implemented.  User-supplied name (as opposed to the filename).
+        Name to use for data set.  If none specified then filename is used.
+    from_database : string
+        Database to fetch from. The filename is treated as a database identifier.
     '''
+
+    if from_database is None and format is None:
+        from os.path import splitext
+        base, ext = splitext(filename)
+        if not ext and len(filename) == 4:
+            from_database = 'pdb'
+            format = 'mmcif'
+
+    if ':' in filename:
+        prefix, fname = filename.split(':',maxsplit=1)
+        from .. import fetch
+        from_database, format = fetch.fetch_from_prefix(session, prefix)
+        if from_database is None:
+            raise UserError('Unknown database prefix "%s" must be one of %s'
+                            % (prefix, fetch.prefixes(session)))
+        filename = fname
+
+    if from_database is not None:
+        from .. import fetch
+        models = fetch.fetch_from_database(session, from_database, filename,
+                                           format=format, name=name)
+        session.models.add(models)
+        return models
+
     if format is not None:
         format = format_from_prefix(format)
     try:
@@ -36,9 +62,13 @@ def register_command(session):
         from .. import io
         prefixes = sum((tuple(io.prefixes(f)) for f in io.formats()), ())
         return prefixes
+    def db_formats():
+        from .. import fetch
+        return [f.database_name for f in fetch.fetch_databases(session).values()]
     desc = CmdDesc(required=[('filename', StringArg)],
                    keyword=[('format', DynamicEnum(formats)),
                             ('name', StringArg),
+                            ('from_database', DynamicEnum(db_formats)),
                             #('id', ModelIdArg),
                         ],
                    synopsis='read and display data')
