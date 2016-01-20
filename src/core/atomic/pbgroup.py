81c9bf92a6f0dbd39144bfc66358bb4128c0f87e
Tom Goddard <goddard@sonic.net>
2016-01-19 17:08:24 -0800

    Made PseudobondGroup render both intra-molecular and global groups.

--- src/core/atomic/pbgroup.py
+++ src/core/atomic/pbgroup.py
@@ -16,8 +16,8 @@ class PseudobondGroup(PseudobondGroupData, Model):
             session = self.structure.session
         Model.__init__(self, self.category, session)
         self._pbond_drawing = None
-
-        self._update_graphics()
+        self._dashes = 9
+        self._structure = None		# AtomicStructure if pseudobond group in intra-molecular
 
     def delete(self):
         Model.delete(self)
@@ -27,14 +27,29 @@ class PseudobondGroup(PseudobondGroupData, Model):
     def added_to_session(self, session):
         # Detect when atoms moved so pseudobonds must be redrawn.
         # TODO: Update only when atoms move or are shown hidden, not when anything shown or hidden.
+        t = session.triggers
         self.handlers = [
-            session.triggers.add_handler('graphics update',self._update_graphics_if_needed),
-            session.triggers.add_handler('shape changed', self.update_graphics)
+            t.add_handler('graphics update',self._update_graphics_if_needed),
+            t.add_handler('shape changed', self._update_graphics)
         ]
 
     def removed_from_session(self, session):
         while self.handlers:
             session.delete_handler(self.handlers.pop())
+        self.handlers = []
+
+    def _get_dashes(self):
+        return self._dashes
+    def _set_dashes(self, n):
+        self._dashes = n
+        pb = self._pbond_drawing
+        if pb:
+            owner = self._structure if self._structure else self
+            owner.remove_drawing(pb)
+            self._pbond_drawing = None
+            self._gc_shape = True
+            owner.redraw_needed(shape_changed = True)
+    dashes = property(_get_dashes, _set_dashes)
 
     def _update_graphics_if_needed(self, *_):
         c, s, se = self._gc_color, self._gc_shape, self._gc_select
@@ -43,31 +58,40 @@ class PseudobondGroup(PseudobondGroupData, Model):
             self._update_graphics()
             self.redraw_needed(shape_changed = s, selection_changed = se)
 
-    def _update_graphics(self, *_):
+    def _update_graphics(self, *_, structure = None):
+
+        if structure:
+            self._structure = structure
 
         pbonds = self.pseudobonds
         d = self._pbond_drawing
         if len(pbonds) == 0:
             if d:
-                d.delete()
+                owner = self._structure if self._structure else self
+                owner.remove_drawing(d)
                 self._pbond_drawing = None
             return
 
-        from . import structure
         if d is None:
-            self._pbond_drawing = d = self.new_drawing('pbonds')
-            va, na, ta = structure._pseudobond_geometry()
+            owner = self._structure
+            d = owner.new_drawing(self.category) if owner else self.new_drawing('pbonds')
+            self._pbond_drawing = d
+            va, na, ta = _pseudobond_geometry(self._dashes//2)
             d.vertices = va
             d.normals = na
             d.triangles = ta
 
         ba1, ba2 = bond_atoms = pbonds.atoms
-        to_pbg = self.scene_position.inverse()
-        axyz0, axyz1 = to_pbg*ba1.scene_coords, to_pbg*ba2.scene_coords
-        d.positions = structure._halfbond_cylinder_placements(axyz0, axyz1, pbonds.radii)
-        d.display_positions = structure._shown_bond_cylinders(pbonds)
+        if self._structure:
+            axyz0, axyz1 = ba1.coords, ba2.coords
+        else:
+            to_pbg = self.scene_position.inverse()
+            axyz0, axyz1 = to_pbg*ba1.scene_coords, to_pbg*ba2.scene_coords
+        from . import structure as s
+        d.positions = s._halfbond_cylinder_placements(axyz0, axyz1, pbonds.radii)
+        d.display_positions = s._shown_bond_cylinders(pbonds)
         d.colors = pbonds.half_colors
-        d.selected_positions = structure._selected_bond_cylinders(bond_atoms)
+        d.selected_positions = s._selected_bond_cylinders(bond_atoms)
 
     def first_intercept(self, mxyz1, mxyz2, exclude=None):
         if not self.display or (exclude and hasattr(self, exclude)):
@@ -107,3 +131,9 @@ def interatom_pseudobonds(atoms, session):
         if ipb:
             ipbonds |= ipb
     return ipbonds
+
+# -----------------------------------------------------------------------------
+#
+def _pseudobond_geometry(segments = 9):
+    from .. import surface
+    return surface.dashed_cylinder_geometry(segments)
