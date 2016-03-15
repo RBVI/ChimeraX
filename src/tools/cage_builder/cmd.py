47b27655bd367d68bced9eadf70761da50537576
Tom Goddard <goddard@sonic.net>
2016-03-15 14:38:12 -0700

    Added surface only and resolution options to cage command.

--- src/tools/cage_builder/cmd.py
+++ src/tools/cage_builder/cmd.py
@@ -1,6 +1,7 @@
 # vim: set expandtab ts=4 sw=4:
 
-def cage(session, cage, place_model = None, polygon_sides = 6):
+def cage(session, cage, place_model = None, polygon_sides = 6,
+         surface_only = False, resolution = None):
     '''Build polygonal cages and place molecules on faces.
 
     Parameters
@@ -11,6 +12,12 @@ def cage(session, cage, place_model = None, polygon_sides = 6):
       Place copies of model on each n-sided polygon of cage.
     polygon_sides : int
       Place on polygons with this number of sides.
+    surface_only : bool
+      Instead of showing instances of the molecule, show instances
+      of surfaces of each chain.  The chain surfaces are computed if
+      they do not already exist.
+    resolution : float
+      Resolution for computing surfaces when surface_only is true.
     '''
 
     from chimerax.core.commands import AnnotationError
@@ -25,7 +32,18 @@ def cage(session, cage, place_model = None, polygon_sides = 6):
         raise AnnotationError('Cage %s has no %d-gons.' % (cage.name, n))
     c = place_model.bounds().center()
     pc = make_closest_placement_identity(p, c)
-    place_model.positions = pc * place_model.position
+
+    # TODO: Is positioning right if cage is moved?
+    from chimerax.core.atomic import Graph
+    if surface_only and isinstance(place_model, Graph):
+        from chimerax.core.commands.surface import surface
+        surfs = surface(session, place_model.atoms, resolution = resolution)
+        mpinv = place_model.scene_position.inverse()
+        for s in surfs:
+            s.positions = mpinv * pc * s.scene_position
+    else:
+        place_model.positions = pc * place_model.scene_position
+
     session.logger.info('Placed %s at %d positions on %d-sided polygons'
                         % (place_model.name, len(pc), n))
 
@@ -38,6 +56,7 @@ def cage(session, cage, place_model = None, polygon_sides = 6):
 def make_closest_placement_identity(tflist, center):
 
     d = tflist * (0,0,0)
+    d -= center
     d2 = (d*d).sum(axis = 1)
     i = d2.argmin()
     tfinv = tflist[i].inverse()
@@ -47,9 +66,11 @@ def make_closest_placement_identity(tflist, center):
     return rtflist
 
 def register_cage_command():
-    from chimerax.core.commands import CmdDesc, register, ModelArg, IntArg
+    from chimerax.core.commands import CmdDesc, register, ModelArg, IntArg, NoArg, FloatArg
     desc = CmdDesc(required = [('cage', ModelArg)],
                    keyword = [('place_model', ModelArg),
-                              ('polygon_sides', IntArg),],
+                              ('polygon_sides', IntArg),
+                              ('surface_only', NoArg),
+                              ('resolution', FloatArg)],
                    synopsis = 'Place copies of model on polygons of cage.')
     register('cage', desc, cage)
