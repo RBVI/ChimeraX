c02983a6490dd555ae9b084108e46c4e7252d711
Greg Couch <gregc@cgl.ucsf.edu>
2016-04-28 00:39:36 -0700

    Change help to allow for filenames as topics and require that help URLs be relative to help root.

--- src/tools/help_viewer/cmd.py
+++ src/tools/help_viewer/cmd.py
@@ -35,61 +35,75 @@ def help(session, topic=None, *, option=None, is_query=False, target=None):
             # no command line?!?
             run(session, cmd)
         return
-    else:
-        path = ""
-        fragment = ""
-        if topic.startswith('help:'):
-            import os
-            base_dir = os.path.join(session.app_data_dir, 'docs')
-            from urllib.parse import urlparse
-            from urllib.request import url2pathname
-            (_, _, url_path, _, _, fragment) = urlparse(topic)
-            path = url2pathname(url_path)
-            path = os.path.expanduser(path)
-            path = os.path.join(base_dir, path)
-            if not os.path.exists(path):
-                # TODO: check if http url is within ChimeraX docs
-                # TODO: handle missing doc -- redirect to web server
-                if is_query:
-                    return False
-                session.logger.error("No help found for '%s'" % topic)
-                return
-            if is_query:
-                return True
-            if os.path.isdir(path):
-                path += '/index.html'
-            from urllib.parse import urlunparse
-            from urllib.request import pathname2url
-            url = urlunparse(('file', '', pathname2url(path), '', '', fragment))
+    elif topic.startswith('help:'):
+        # Help URLs are rooted at base_dir
+        import os
+        import sys
+        base_dir = os.path.join(session.app_data_dir, 'docs')
+        from urllib.parse import urlparse, urlunparse, quote, unquote
+        from urllib.request import url2pathname, pathname2url
+        (_, _, url_path, _, _, fragment) = urlparse(topic)
+        url_path = quote(url_path)
+        path = url2pathname(url_path)
+        # make sure path is a relative path
+        if sys.platform.startswith('win'):
+            path = os.path.relpath(path, os.path.splitdrive(path))
         else:
-            cmd_name = topic
-            while 1:
-                try:
-                    url = cli.command_url(cmd_name)
-                except ValueError:
-                    session.logger.error("No help found for '%s'" % topic)
-                    return
-                if url:
-                    if is_query:
-                        return True
-                    return help(session, url, option=option, is_query=is_query)
-                alias = cli.expand_alias(topic)
-                if not alias:
-                    break
-                if not is_query:
-                    run(session, "usage %s" % cmd_name, log=False)
-                alias_words = alias.split()
-                for i in range(len(alias_words)):
-                    try:
-                        cmd_name = ' '.join(alias_words[0:i + 1])
-                        cli.command_url(cmd_name)
-                    except ValueError:
-                        cmd_name = ' '.join(alias_words[0:i])
-                        break
+            path = os.path.relpath(path, '/')
+        path = os.path.join(base_dir, path)
+        if not os.path.exists(path):
+            # TODO: check if http url is within ChimeraX docs
+            # TODO: handle missing doc -- redirect to web server
             if is_query:
                 return False
-            run(session, "usage %s" % topic, log=False)
+            session.logger.error("No help found for '%s'" % topic)
             return
+        if is_query:
+            return True
+        if os.path.isdir(path):
+            path += '/index.html'
+        url = urlunparse(('file', '', pathname2url(path), '', '', fragment))
+    elif '/' in topic:
+        import os
+        path = os.path.expanduser(topic)
+        path = os.path.abspath(path)
+        if not os.path.exists(path):
+            if is_query:
+                return False
+            session.logger.error("Help file not found: '%s'" % path)
+            return
+        from urllib.parse import urlunparse
+        from urllib.request import pathname2url
+        url = urlunparse(('file', '', pathname2url(path), '', '', ''))
+    else:
+        cmd_name = topic
+        while 1:
+            try:
+                url = cli.command_url(cmd_name)
+            except ValueError:
+                session.logger.error("No help found for '%s'" % topic)
+                return
+            if url:
+                if is_query:
+                    return True
+                return help(session, url, option=option, is_query=is_query)
+            alias = cli.expand_alias(topic)
+            if not alias:
+                break
+            if not is_query:
+                run(session, "usage %s" % cmd_name, log=False)
+            alias_words = alias.split()
+            for i in range(len(alias_words)):
+                try:
+                    cmd_name = ' '.join(alias_words[0:i + 1])
+                    cli.command_url(cmd_name)
+                except ValueError:
+                    cmd_name = ' '.join(alias_words[0:i])
+                    break
+        if is_query:
+            return False
+        run(session, "usage %s" % topic, log=False)
+        return
 
     if session.ui.is_gui:
         new = option == 'new_viewer'
