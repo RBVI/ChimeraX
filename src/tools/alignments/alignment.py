ac9e0b1bdccedd7d0d1ce27f908cdb242f9be4dd
Eric Pettersen <pett@cgl.ucsf.edu>
2016-06-30 16:22:05 -0700

    remember all the args to the Alignment constructor

--- src/tools/alignments/alignment.py
+++ src/tools/alignments/alignment.py
@@ -3,5 +3,16 @@
 from chimerax.core.state import State
 class Alignment(State):
     """A sequence alignment"""
-    def __init__(self, session, seqs, identify_as, file_attrs=None, file_markups=None):
+    def __init__(self, session, seqs, name, file_attrs=None, file_markups=None):
         self.seqs = seqs
+        self.name = name
+        self.file_attrs = file_attrs
+        self.file_markups = file_markups
+
+    def take_snapshot(self, session, flags):
+        return { 'version': 1, 'seqs': self.seqs, 'name': self.name
+            'file_attrs': self.file_atts, 'file_markups': self.file_markups }
+
+    @staticmethod
+    def restore_snapshot(session, data):
+        return Alignment(data['seqs'], data['name'], data['file_attrs'], data['file_markups'])
