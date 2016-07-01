7bd7a3c57265e01c3a52084e40b827774dd2de78
Tom Goddard <goddard@sonic.net>
2016-07-01 13:18:35 -0700

    Ported computation of bond cylinder placements from Python to C++.

---
+++ src/core/geometry/_geometry/cylinderrot.h
@@ -0,0 +1,17 @@
+// vi: set expandtab shiftwidth=4 softtabstop=4:
+// ----------------------------------------------------------------------------
+// Routines to compute rotation of standard cylinder aligned along z axis
+// to have specified end points.  For drawing molecule bonds.
+//
+#ifndef CYLINDERROT_HEADER_INCLUDED
+#define CYLINDERROT_HEADER_INCLUDED
+
+#include <Python.h>			// use PyObject
+
+extern "C"
+{
+// cylinder_rotations(xyz0, xyz1, radii, rot44)
+PyObject *cylinder_rotations(PyObject *, PyObject *args, PyObject *keywds);
+}
+
+#endif
