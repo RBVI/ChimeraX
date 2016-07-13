967ab8b73e6b0234abb2d731bcb1842a16c8b043
Eric Pettersen <pett@cgl.ucsf.edu>
2016-07-13 14:38:17 -0700

    indent of comments was wrong

--- src/tools/alignments/__init__.py
+++ src/tools/alignments/__init__.py
@@ -1,11 +1,12 @@
 # vim: set expandtab shiftwidth=4 softtabstop=4:
 
 def finish(bundle_info, session):
-   """De-install alignments manager from existing session"""
+    """De-install alignments manager from existing session"""
     del session.alignments
 
 def initialize(bundle_info, session):
-   """Install alignments manager into existing session"""
+    """Install alignments manager into existing session"""
+    print("alignments bundle info:", type(bundle_info), bundle_info.__class__.__name__)
     from . import settings
     settings.init(session)
 
