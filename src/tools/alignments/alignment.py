8c89b34ceb64201b4c18fda357b9bfc7b326b0d8
Eric Pettersen <pett@cgl.ucsf.edu>
2016-07-14 13:50:19 -0700

    put manager in charge of launching the viewer

--- src/tools/alignments/alignment.py
+++ src/tools/alignments/alignment.py
@@ -3,11 +3,7 @@
 from chimerax.core.state import State
 class Alignment(State):
     """A sequence alignment"""
-    def __init__(self, session, seqs, name, file_attrs=None, file_markups=None, viewer=None):
-        """'viewer' is the viewer to use:  None means use preference;
-           False means no graphical viewer (accessible by command only).  If a graphical
-           viewer is used, then when the last viewer goes away the alignment is automatically
-           destroyed.  Otherwise, it must be explicitly destroyed."""
+    def __init__(self, session, seqs, name, file_attrs=None, file_markups=None):
         self.seqs = seqs
         self.name = name
         self.file_attrs = file_attrs
