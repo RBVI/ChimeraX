efe9e54b0d603e048fb423ad3b391ebd3cbed2c5
Tom Goddard <goddard@sonic.net>
2016-10-10 15:21:49 -0700

    Make volume viewer show at most 20 histograms.  Formerly was limited to 3.

--- src/bundles/volume_viewer/defaultsettings.py
+++ src/bundles/volume_viewer/defaultsettings.py
@@ -45,7 +45,7 @@ class VolumeViewerDefaultSettings:
   def factory_defaults(self):
 
     defaults = {
-        'max_histograms': 3,
+        'max_histograms': 20,
         'use_initial_colors': True,
         'initial_colors': ((.7,.7,.7,1),
                            (1,1,.7,1),
