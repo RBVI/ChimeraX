b7a9b2a7728b10d0758b45e5b2c87993b9ce8d41
Tom Goddard <goddard@sonic.net>
2016-10-21 19:02:24 -0700

    Added saving helix and strand information to mmcif save.

--- src/core/atomic/mmcif_write.py
+++ src/core/atomic/mmcif_write.py
@@ -32,15 +32,28 @@ def write_mmcif(session, path, models, **kw):
     if nm > 1:
         session.logger.info('Writing %d models to mmCIF file by appending model number to chain ids' % nm)
 
-    lines = []
+    alines = []
     entities = {}
+    sclines = []
+    srlines = []
     for mi, m in enumerate(models):
         cid_suffix = '' if nm == 1 else '%d' % (mi+1)
-        atom_site_lines(m, cid_suffix, lines, entities)
-             
-    lines.append('#')
+        alines.extend(atom_site_lines(m, len(alines), cid_suffix, entities))
+        res = m.residues
+        sclines.extend(struct_conf_lines(res, len(sclines), cid_suffix))
+        srlines.extend(struct_sheet_range_lines(res, len(sclines), cid_suffix))
 
-    atom_site_header = '''loop_
+    text = (
+        atom_site_header + '\n'.join(alines) + '\n#\n' +
+        struct_conf_header + '\n'.join(sclines) + '\n#\n' +
+        struct_sheet_range_header + '\n'.join(srlines) + '\n#\n'
+        )
+    
+    f = open(path, 'w')
+    f.write(text)
+    f.close()
+
+atom_site_header = '''loop_
 _atom_site.id 
 _atom_site.type_symbol 
 _atom_site.label_atom_id 
@@ -55,13 +68,7 @@ _atom_site.occupancy
 _atom_site.B_iso_or_equiv 
 '''
 
-    text = atom_site_header + '\n'.join(lines) + '\n'
-    
-    f = open(path, 'w')
-    f.write(text)
-    f.close()
-
-def atom_site_lines(m, cid_suffix, lines, entities):
+def atom_site_lines(m, anum_offset, cid_suffix, entities):
     
     atoms = m.atoms
     xyz = atoms.scene_coords
@@ -76,11 +83,12 @@ def atom_site_lines(m, cid_suffix, lines, entities):
     rnum = res.numbers
     eid = entity_ids(atoms, res, rname, entities)
 
-    for a in range(n):
-        line = ('%s %s %s %s %s %d %d %.3f %.3f %.3f %.2f %.2f' %
-                (a+1, elem[a], aname[a], rname[a], cid[a] + cid_suffix, eid[a], rnum[a],
-                 xyz[a,0], xyz[a,1], xyz[a,2], occ[a], bfact[a]))
-        lines.append(line)
+    lines = [('%s %s %s %s %s %d %d %.3f %.3f %.3f %.2f %.2f' %
+             (a+1+anum_offset, elem[a], aname[a], rname[a], cid[a] + cid_suffix, eid[a], rnum[a],
+              xyz[a,0], xyz[a,1], xyz[a,2], occ[a], bfact[a]))
+             for a in range(n)]
+        
+    return lines
 
 #
 # To determine entity id, use residue name if in_chain is false, use sequence if in_chain is true.
@@ -114,3 +122,58 @@ def entity_ids(atoms, residues, rname, entities):
         eids[i] = eid
 
     return eids
+
+struct_conf_header = '''loop_
+_struct_conf.conf_type_id 
+_struct_conf.id 
+_struct_conf.beg_label_comp_id 
+_struct_conf.beg_label_asym_id 
+_struct_conf.beg_label_seq_id 
+_struct_conf.end_label_comp_id 
+_struct_conf.end_label_asym_id 
+_struct_conf.end_label_seq_id 
+'''
+
+def struct_conf_lines(residues, hnum_offset, cid_suffix):
+    h = residues.is_helix
+    hse = intervals(h)	# List of (start,end) indices for helices
+    rname = residues.names
+    cid = residues.chain_ids
+    rnum = residues.numbers
+    lines = ['HELX_P %d %s %s %d %s %s %d' % (i+1+hnum_offset, rname[s], cid[s]+cid_suffix, rnum[s], rname[e], cid[e]+cid_suffix, rnum[e])
+             for i, (s,e) in enumerate(hse)]
+    return lines
+
+def intervals(mask):
+    '''Return list of intervals (start index, end index) of true values in mask array.'''
+    indices = []
+    if mask[0]:
+        indices.append(-1)
+    n = len(mask)
+    d = (mask[:n-1] != mask[1:])
+    indices.extend(d.nonzero()[0])
+    if mask[n-1]:
+        indices.append(n-1)
+    iv = [(a+1,b) for a,b in zip(indices[::2], indices[1::2])]
+    return iv
+
+struct_sheet_range_header = '''loop_
+_struct_sheet_range.sheet_id 
+_struct_sheet_range.id 
+_struct_sheet_range.beg_label_comp_id 
+_struct_sheet_range.beg_label_asym_id 
+_struct_sheet_range.beg_label_seq_id 
+_struct_sheet_range.end_label_comp_id 
+_struct_sheet_range.end_label_asym_id 
+_struct_sheet_range.end_label_seq_id 
+'''
+
+def struct_sheet_range_lines(residues, snum_offset, cid_suffix):
+    s = residues.is_strand
+    sse = intervals(s)	# List of (start,end) indices for helices
+    rname = residues.names
+    cid = residues.chain_ids
+    rnum = residues.numbers
+    lines = ['. %d %s %s %d %s %s %d' % (i+1+snum_offset, rname[s], cid[s]+cid_suffix, rnum[s], rname[e], cid[e]+cid_suffix, rnum[e])
+             for i, (s,e) in enumerate(sse)]
+    return lines
