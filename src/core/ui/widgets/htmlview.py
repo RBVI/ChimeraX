0190107c0765e26bbd41a9e97402c2ec1b7a7f28
Greg Couch <gregc@cgl.ucsf.edu>
2016-12-01 15:56:43 -0800

    Convert to using a profile per-HtmlView to keep the schemes independent.

--- src/core/ui/widgets/htmlview.py
+++ src/core/ui/widgets/htmlview.py
@@ -5,7 +5,7 @@ HtmlView is a derived class from PyQt5.QtWebEngineWidgets.QWebEngineView
 that simplifies using custom schemes and intercepting navigation requests.
 
 HtmlView may be instantiated just like QWebEngineView, but handles
-three extra keyword arguments:
+five extra keyword arguments:
 
     size_hint:   a QSize compatible value, typically (width, height),
                  specifying the preferred initial size for the view.
@@ -18,9 +18,12 @@ three extra keyword arguments:
     download:    a callback function taking one argument, an instance
                  of QWebEngineDownloadItem, invoked when download is
                  requested.
+    profile:    the QWebEngineProfile to use.  If it is given, then
+                'interceptor', 'schemes', and 'download' are ignored
+                because they are assumed to be already set.
 """
 
-from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage, QWebEngineProfile
 from PyQt5.QtWebEngineCore import QWebEngineUrlRequestInterceptor
 from PyQt5.QtWebEngineCore import QWebEngineUrlSchemeHandler
 
@@ -28,29 +31,41 @@ from PyQt5.QtWebEngineCore import QWebEngineUrlSchemeHandler
 class HtmlView(QWebEngineView):
 
     def __init__(self, *args, size_hint=None, schemes=None,
-                 interceptor=None, download=None, **kw):
+                 interceptor=None, download=None, profile=None, **kw):
         super().__init__(*args, **kw)
         self._size_hint = size_hint
-        if interceptor is None:
-            self._intercept = None
+        if profile is not None:
+            self._profile = profile
         else:
-            self._intercept = _RequestInterceptor(callback=interceptor)
-        self._download = download
-        if schemes is None:
-            self._schemes = []
-        else:
-            self._schemes = [s.encode("utf-8") for s in schemes]
-            self._scheme_handler = _SchemeHandler()
-        self._known_profiles = set()
+            from chimerax.core.fetch import html_user_agent
+            from chimerax import app_dirs
+            p = self._profile = QWebEngineProfile(self)
+            p.setHttpUserAgent('%s %s' % (p.httpUserAgent(), html_user_agent(app_dirs)))
+            if interceptor is not None:
+                self._intercept = _RequestInterceptor(callback=interceptor)
+                p.setRequestInterceptor(self._intercept)
+                if schemes:
+                    self._schemes = [s.encode("utf-8") for s in schemes]
+                    self._scheme_handler = _SchemeHandler()
+                    for scheme in self._schemes:
+                        p.installUrlSchemeHandler(scheme, self._scheme_handler)
+            if download:
+                p.downloadRequested.connect(download)
+        page = QWebEnginePage(self._profile, self)
+        self.setPage(page)
 
-    def sizeHint(self):
+    @property
+    def profile(self):
+        return self._profile
+
+    def sizeHint(self):  # noqa
         if self._size_hint:
             from PyQt5.QtCore import QSize
             return QSize(*self._size_hint)
         else:
             return super().sizeHint()
 
-    def setHtml(self, html):
+    def setHtml(self, html):  # noqa
         self.setEnabled(False)
         if len(html) < 1000000:
             super().setHtml(html)
@@ -58,10 +73,12 @@ class HtmlView(QWebEngineView):
             try:
                 tf = open(self._tf_name, "wb")
             except AttributeError:
-                import tempfile, atexit
+                import tempfile
+                import atexit
                 tf = tempfile.NamedTemporaryFile(prefix="chbp", suffix=".html",
                                                  delete=False, mode="wb")
                 self._tf_name = tf.name
+
                 def clean(filename):
                     import os
                     try:
@@ -76,22 +93,9 @@ class HtmlView(QWebEngineView):
             tf.close()
             self.load(QUrl.fromLocalFile(self._tf_name))
         self.setEnabled(True)
-        self._update_profile()
 
-    def setUrl(self, url):
+    def setUrl(self, url):  # noqa
         super().setUrl(url)
-        self._update_profile()
-
-    def _update_profile(self):
-        p = self.page().profile()
-        if p not in self._known_profiles:
-            if self._intercept:
-                p.setRequestInterceptor(self._intercept)
-                for scheme in self._schemes:
-                    p.installUrlSchemeHandler(scheme, self._scheme_handler)
-            if self._download:
-                p.downloadRequested.connect(self._download)
-            self._known_profiles.add(p)
 
 
 class _RequestInterceptor(QWebEngineUrlRequestInterceptor):
@@ -100,14 +104,15 @@ class _RequestInterceptor(QWebEngineUrlRequestInterceptor):
         super().__init__(*args, **kw)
         self._callback = callback
 
-    def interceptRequest(self, info):
+    def interceptRequest(self, info):  # noqa
         # "info" is an instance of QWebEngineUrlRequestInfo
         if self._callback:
             self._callback(info)
 
 
 class _SchemeHandler(QWebEngineUrlSchemeHandler):
-    def requestStarted(self, request):
+
+    def requestStarted(self, request):  # noqa
         # "request" is an instance of QWebEngineUrlRequestJob
         # We do nothing because caller should be intercepting
         # custom URL navigation already
