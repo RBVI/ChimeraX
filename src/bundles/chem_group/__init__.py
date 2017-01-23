568018b922bbcb3a0a84b0957d783fd427873d26
Eric Pettersen <pett@cgl.ucsf.edu>
2017-01-23 14:46:36 -0800

    import glabal names into __init__

---
+++ src/bundles/chem_group/__init__.py
@@ -0,0 +1,17 @@
+# vim: set expandtab shiftwidth=4 softtabstop=4:
+
+# === UCSF ChimeraX Copyright ===
+# Copyright 2016 Regents of the University of California.
+# All rights reserved.  This software provided pursuant to a
+# license agreement containing restrictions on its disclosure,
+# duplication and use.  For details see:
+# http://www.rbvi.ucsf.edu/chimerax/docs/licensing.html
+# This notice must be embedded in or attached to all copies,
+# including partial copies, of the software or any revisions
+# or derivations thereof.
+# === UCSF ChimeraX Copyright ===
+
+from .tool import find_group
+
+# the below in case you want to add your own custom group to group_info...
+from .tool import group_info, N, C, O, H, R, X, single_bond, heavy, non_oxygen_single_bond, RingAtom
