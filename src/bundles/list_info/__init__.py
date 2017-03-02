e7beef6417f0a31fc3bbbab1bfb4e5e600c9eaa4
Tom Goddard <goddard@sonic.net>
2017-03-01 20:32:03 -0800

    Merged info and listinfo commands into a single info command.

--- src/bundles/list_info/__init__.py
+++ src/bundles/list_info/__init__.py
@@ -2,48 +2,54 @@
 
 from chimerax.core.toolshed import BundleAPI
 
-class _MyAPI(BundleAPI):
+class _InfoAPI(BundleAPI):
 
     @staticmethod
     def register_command(command_name):
         # 'register_command' is lazily called when the command is referenced
         from . import cmd
         from chimerax.core.commands import register
-        register("listinfo models",
-                 cmd.listinfo_models_desc,
-                 cmd.listinfo_models)
-        register("listinfo chains",
-                 cmd.listinfo_chains_desc,
-                 cmd.listinfo_chains)
-        register("listinfo polymers",
-                 cmd.listinfo_polymers_desc,
-                 cmd.listinfo_polymers)
-        register("listinfo residues",
-                 cmd.listinfo_residues_desc,
-                 cmd.listinfo_residues)
-        register("listinfo atoms",
-                 cmd.listinfo_atoms_desc,
-                 cmd.listinfo_atoms)
-        register("listinfo selection",
-                 cmd.listinfo_selection_desc,
-                 cmd.listinfo_selection)
-        register("listinfo resattr",
-                 cmd.listinfo_resattr_desc,
-                 cmd.listinfo_resattr)
-        register("listinfo distmat",
-                 cmd.listinfo_distmat_desc,
-                 cmd.listinfo_distmat)
-        register("listinfo notify start",
-                 cmd.listinfo_notify_start_desc,
-                 cmd.listinfo_notify_start)
-        register("listinfo notify stop",
-                 cmd.listinfo_notify_stop_desc,
-                 cmd.listinfo_notify_stop)
-        register("listinfo notify suspend",
-                 cmd.listinfo_notify_suspend_desc,
-                 cmd.listinfo_notify_suspend)
-        register("listinfo notify resume",
-                 cmd.listinfo_notify_resume_desc,
-                 cmd.listinfo_notify_resume)
+        register('info',
+                 cmd.info_desc,
+                 cmd.info)
+        register("info bounds",
+                 cmd.info_bounds_desc,
+                 cmd.info_bounds)
+        register("info models",
+                 cmd.info_models_desc,
+                 cmd.info_models)
+        register("info chains",
+                 cmd.info_chains_desc,
+                 cmd.info_chains)
+        register("info polymers",
+                 cmd.info_polymers_desc,
+                 cmd.info_polymers)
+        register("info residues",
+                 cmd.info_residues_desc,
+                 cmd.info_residues)
+        register("info atoms",
+                 cmd.info_atoms_desc,
+                 cmd.info_atoms)
+        register("info selection",
+                 cmd.info_selection_desc,
+                 cmd.info_selection)
+        register("info resattr",
+                 cmd.info_resattr_desc,
+                 cmd.info_resattr)
+        register("info distmat",
+                 cmd.info_distmat_desc,
+                 cmd.info_distmat)
+        register("info notify start",
+                 cmd.info_notify_start_desc,
+                 cmd.info_notify_start)
+        register("info notify stop",
+                 cmd.info_notify_stop_desc,
+                 cmd.info_notify_stop)
+        register("info notify suspend",
+                 cmd.info_notify_suspend_desc,
+                 cmd.info_notify_suspend)
+        register("info notify resume",
+                 cmd.info_notify_resume_desc,
+                 cmd.info_notify_resume)
 
-bundle_api = _MyAPI()
+bundle_api = _InfoAPI()
--- src/bundles/list_info/util.py
+++ src/bundles/list_info/util.py
@@ -105,6 +105,74 @@ def report_distmat(logger, atoms, distmat):
                                               distmat[dmi]))
     logger.info('\n'.join(msgs))
 
+def model_info(m):
+    disp = 'shown' if m.display else 'hidden'
+    line = '#%s, %s, %s' % (m.id_string(), m.name, disp)
+    if m.triangles is not None:
+        line += ', %d triangles' % len(m.triangles)
+    npos = len(m.positions)
+    if npos > 1:
+        line += ', %d instances' % npos
+    spos = m.selected_positions
+    if spos is not None and spos.sum() > 0:
+        line += ', %d selected instances' % spos.sum()
+    return line
+
+def bounds_description(bounds):
+    if bounds is None:
+        bdesc = 'no bounding box'
+    else:
+        bdesc = ('bounds %.3g,%.3g,%.3g to ' % tuple(bounds.xyz_min) +
+                 '%.3g,%.3g,%.3g' % tuple(bounds.xyz_max))
+    return bdesc
+
+def structure_info(m):
+    from chimerax.core.atomic import Structure
+    if not isinstance(m, Structure):
+        return ''
+
+    line = ('\n%d atoms, %d bonds, %d residues, %d chains (%s)'
+            % (m.num_atoms, m.num_bonds, m.num_residues, m.num_chains,
+               ','.join(m.residues.unique_chain_ids)))
+    ncs = m.num_coord_sets
+    if ncs > 1:
+        line += ', %d coordsets' % ncs
+    pmap = m.pbg_map
+    if pmap:
+        line += '\n' + ', '.join('%d %s' % (pbg.num_pseudobonds, name)
+                                 for name, pbg in pmap.items())
+    return line
+
+def pseudobond_group_info(m):
+    from chimerax.core.atomic import PseudobondGroup
+    if isinstance(m, PseudobondGroup):
+        line = ', %d pseudobonds' % m.num_pseudobonds
+    else:
+        line = ''
+    return line
+
+def volume_info(m):
+
+    from chimerax.core.map import Volume
+    if not isinstance(m, Volume):
+        return ''
+
+    size = 'size %d,%d,%d' % tuple(m.data.size)
+    s0,s1,s2 = m.region[2]
+    step = ('step %d' % s0) if s1 == s0 and s2 == s0 else 'step %d,%d,%d' % (s0,s1,s2)
+    sx,sy,sz = m.data.step
+    vsize = ('voxel size %.5g' % sx) if sx == sy and sy == sz else ('voxel size %.5g,%.5g,%.5g' % (sx,sy,sz))
+    if m.representation == 'surface':
+        level = 'level ' + ', '.join(('%.4g' % l for l in m.surface_levels))
+    else:
+        level = 'level/intensity ' + ', '.join(('%.4g (%.2f)' % tuple(l) for l in m.solid_levels))
+    line = ' %s, %s, %s, %s' % (size, step, vsize, level)
+    ms = m.matrix_value_statistics()
+    line += ', value range %.5g - %.5g' % (ms.minimum, ms.maximum)
+    line += ', value type %s' % str(m.data.value_type)
+    sym = m.data.symmetries
+    line += ', %d symmetry operators' % (len(sym) if sym else 0)
+    return line
 
 # ==============================================================================
 # Code for sending REST request and waiting for response in a separate thread
