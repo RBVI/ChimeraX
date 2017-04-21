1eb0d8a4a1cd8ed98eca771413d9fc8be7a74093
Conrad Huang <conrad@cgl.ucsf.edu>
2017-04-20 22:56:57 -0700

    Add sample bundle for development against app

---
+++ src/examples/sample/src/cmd.py
@@ -0,0 +1,14 @@
+# vim: set expandtab ts=4 sw=4:
+
+from chimerax.core.commands import CmdDesc, AtomicStructuresArg
+from chimerax.core.atomic import AtomicStructure
+from . import _sample
+
+
+def sample_count(session, structures=None):
+    if structures is None:
+        structures = session.models.list(type=AtomicStructure)
+    for m in structures:
+        atoms, bonds = _sample.counts(m)
+        session.logger.info("%s: %d atoms, %d bonds" % (m, atoms, bonds))
+sample_count_desc = CmdDesc(optional=[("structures", AtomicStructuresArg)])
