feb0938f0237752626a5a18788a58d990c679d47
Eric Pettersen <pett@cgl.ucsf.edu>
2017-05-17 13:43:39 -0700

    As per Conrad, 'info polymers' should use the same polymers that ribbons do

--- src/bundles/list_info/cmd.py
+++ src/bundles/list_info/cmd.py
@@ -111,7 +111,7 @@ def info_polymers(session, atoms=None):
     from chimerax.core.atomic import AtomicStructure
     for m in results.atoms.unique_structures:
         try:
-            for p in m.polymers(AtomicStructure.PMS_NEVER_CONNECTS, False):
+            for p in m.polymers(AtomicStructure.PMS_TRACE_CONNECTS, False):
                 if p.intersects(residues):
                     polymers.append(p)
         except AttributeError:
