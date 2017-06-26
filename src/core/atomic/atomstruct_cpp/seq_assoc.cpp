09fbd3e3867e40d752707882e0c349b6bcb479d0
Eric Pettersen <pett@cgl.ucsf.edu>
2017-06-26 15:05:22 -0700

    Don't use auto for declaration since Chain has different semantics than StructureSeq (in particular if it becomes empty)

--- src/core/atomic/atomstruct_cpp/seq_assoc.cpp
+++ src/core/atomic/atomstruct_cpp/seq_assoc.cpp
@@ -450,7 +450,7 @@ try_assoc(const Sequence& align_seq, const StructureSeq& mseq,
             throw SA_AssocFailure("bad assoc");
         return retvals;
     }
-    auto no_X = mseq;
+    StructureSeq no_X = mseq;
     auto no_X_ap = AssocParams(ap.est_len, ap.segments.begin(),
         ap.segments.end(), ap.gaps.begin(), ap.gaps.end());
     while (no_X.size() > 0 && no_X.front() == 'X') {
