802fd81466993ba61f5b04ecec47a98eaf59950b
Tom Goddard <goddard@sonic.net>
2017-08-22 18:01:06 -0700

    Added pole and south texture coordinate systems to the texture command.

--- src/bundles/texture/src/texture.py
+++ src/bundles/texture/src/texture.py
@@ -21,27 +21,28 @@ def texture(session, models, image_file = None, coords = None):
     models : list of Model
         Act on these models and their child drawings.
     image_file : string
-        Path to image file, e.g. PNG or JPEG image.
-    coords : "sphere"
+        Path to image file, e.g. PNG or JPEG image.  If the value is the string "none" then
+        any texture is removed from the surface.
+    coords : "sphere", "pole", "south"
         Defines how to map the image axes onto the surface.  "sphere" assigns
         texture coordinates to each surface vertex based on longitude and lattitude.
         The image x axis is longitude (texture coordinate u), and image y axis is
-        lattitude (texture coordinate v).
+        lattitude (texture coordinate v).  "pole" has the north pole at the middle
+        of the image and the south pole on a circle touching the image edges.
+        "south" is like pole only centered on the south pole.
     '''
     
     rgba = None if image_file is None or image_file == 'none' else image_file_as_rgba(image_file)
     dlist = drawings(models)
     for d in dlist:
-        if coords == 'sphere':
-            set_sphere_texture_coordinates(d)
+        if coords is not None:
+            uv = texture_coordinates(d, coords)
+            d.texture_coordinates = uv
         if image_file == 'none':
-            d.texture = None
+            remove_texture(session, d)
         elif rgba is not None:
+            remove_texture(session, d)
             from chimerax.core.graphics import Texture
-            if d.texture is not None:
-                # TODO: Need a better mechanism to make sure discarded textures are released.
-                session.main_view.render.make_current()
-                d.texture.delete_texture()
             d.texture = Texture(rgba)
             d.opaque_texture = True
 
@@ -63,28 +64,46 @@ def image_file_as_rgba(path):
     rgba = qimage_to_numpy(qi)
     return rgba
 
-def set_sphere_texture_coordinates(drawing):
+def texture_coordinates(drawing, geometry = 'sphere'):
     d = drawing
     v = d.vertices
     nv = len(v)
-    from numpy import arctan2, sqrt, empty, float32
+    from numpy import arctan2, sqrt, empty, float32, pi, sin, cos
     uv = empty((nv,2), float32)
     x,y,z = v[:,0], v[:,1], v[:,2]
-    from math import pi
-    uv[:,0] = arctan2(y, x)	# Phi, -pi to pi
-    uv[:,0] /= 2*pi
-    uv[:,0] += 0.5
+    phi = arctan2(y, x)	# Phi, -pi to pi
     dxy = sqrt(x*x + y*y)
-    uv[:,1] = arctan2(dxy, z)	# Psi, 0 to pi
-    uv[:,1] /= pi
-    d.texture_coordinates = uv
+    psi = arctan2(dxy, z)	# Psi, 0 to pi
+    if geometry == 'sphere':
+        uv[:,0] = phi
+        uv[:,0] /= 2*pi
+        uv[:,0] += 0.5
+        uv[:,1] = psi
+        uv[:,1] /= pi
+    elif geometry == 'pole':
+        r = psi/(2*pi)
+        uv[:,0] = r*cos(phi) + 0.5
+        uv[:,1] = r*sin(phi) + 0.5
+    elif geometry == 'south':
+        r = (pi-psi)/(2*pi)
+        uv[:,0] = r*cos(phi) + 0.5
+        uv[:,1] = r*sin(phi) + 0.5
+    return uv
+
+def remove_texture(session, drawing):
+    d = drawing
+    if d.texture is not None:
+        # TODO: Need a better mechanism to make sure discarded textures are released.
+        session.main_view.render.make_current()
+        d.texture.delete_texture()
+        d.texture = None
 
 def register_texture_command(logger):
     from chimerax.core.commands import CmdDesc, register, ModelsArg, StringArg, EnumOf
     desc = CmdDesc(
         required = [('models', ModelsArg)],
         keyword = [('image_file', StringArg),
-                   ('coords', EnumOf(('sphere',)))],
+                   ('coords', EnumOf(('sphere','pole','south')))],
         synopsis = 'Color surfaces with images from files'
     )
     register('texture', desc, texture, logger=logger)
