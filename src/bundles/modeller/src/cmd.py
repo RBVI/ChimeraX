6e3569d7f1b27e2a7b535e6bfe5151d25fb5e24e
Eric Pettersen <pett@cgl.ucsf.edu>
2017-09-19 17:12:30 -0700

    in order to implement modeling command, need to implement SequenceArg

---
+++ src/bundles/modeller/src/cmd.py
@@ -0,0 +1,72 @@
+# vim: set expandtab shiftwidth=4 softtabstop=4:
+
+# === UCSF ChimeraX Copyright ===
+# Copyright 2016 Regents of the University of California.
+# All rights reserved.  This software provided pursuant to a
+# license agreement containing restrictions on its disclosure,
+# duplication and use.  For details see:
+# http://www.rbvi.ucsf.edu/chimerax/docs/licensing.html
+# This notice must be embedded in or attached to all copies,
+# including partial copies, of the software or any revisions
+# or derivations thereof.
+# === UCSF ChimeraX Copyright ===
+
+#
+def sequence_model(session, targets, combined_templates=False, custom_script=None,
+    dist_restraints_path=None, executable_location=None, fast=False, het_preserve=False,
+    hydrogens=False, license_key=None, num_models=5, temp_path=None, thorough_opt=False,
+    water_preserve=False):
+    #TODO
+    '''
+    Show chain sequence(s)
+
+    Parameters
+    ----------
+    chains : list of Chain
+        Chains to show
+    '''
+
+    if len(chains) == 1:
+        chain = chains[0]
+        ident = ".".join([str(part) for part in chain.structure.id]) + "." + chain.chain_id
+        alignment = session.alignments.new_alignment([chain], ident, seq_viewer="sv",
+            auto_associate=None, intrinsic=True)
+    else:
+        # all chains have to have the same sequence, and they will all be associated with
+        # that sequence
+        sequences = set([chain.characters for chain in chains])
+        if len(sequences) != 1:
+            from chimerax.core.errors import UserError
+            raise UserError("Chains must have same sequence")
+        chars = sequences.pop()
+        chain_ids = set([chain.chain_id for chain in chains])
+        if len(chain_ids) < len(chains) or len(chain_ids) > 10:
+            name = "%d chains" % len(chains)
+        else:
+            name = "chains %s" % ",".join(sorted(list(chain_ids)))
+        from chimerax.core.atomic import Sequence
+        seq = Sequence(name=name, characters=chars)
+        def get_numbering_start(chain):
+            for i, r in enumerate(chain.residues):
+                if r is None or r.deleted:
+                    continue
+                return r.number - i
+            return None
+        starts = set([get_numbering_start(chain) for chain in chains])
+        starts.discard(None)
+        if len(starts) == 1:
+            seq.numbering_start = starts.pop()
+        alignment = session.alignments.new_alignment([seq], None, seq_viewer="sv",
+            auto_associate=False, name=chains[0].description, intrinsic=True)
+        alignment.suspend_notify_viewers()
+        for chain in chains:
+            alignment.associate(chain, keep_intrinsic=True)
+        alignment.resume_notify_viewers()
+
+def register_sequence_model_command(logger):
+    from chimerax.core.commands import CmdDesc, register, UniqueChainsArg
+    desc = CmdDesc(
+        required = [('chains', UniqueChainsArg)],
+        synopsis = 'show structure chain sequence'
+    )
+    register('sequence model', desc, seqalign_chain, logger=logger)
