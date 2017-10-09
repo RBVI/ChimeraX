cdbc9065aee3fda1ec52f53c03b7ca03f5ebb3bc
gregcouch <gregcouch@gmail.com>
2017-10-09 11:44:58 -0700

    Add file types to browse open file dialog.

--- src/core/commands/cli.py
+++ src/core/commands/cli.py
@@ -917,6 +917,9 @@ def _browse_parse(text, session, item_kind, accept_mode, dialog_mode):
         from PyQt5.QtWidgets import QFileDialog
         dlg = QFileDialog()
         dlg.setAcceptMode(accept_mode)
+        if accept_mode == QFileDialog.AcceptOpen and dialog_mode != QFileDialog.DirectoryOnly:
+            from ..ui.open_save import open_file_filter
+            dlg.setNameFilter(open_file_filter(all=True))
         dlg.setFileMode(dialog_mode)
         if dlg.exec():
             paths = dlg.selectedFiles()
