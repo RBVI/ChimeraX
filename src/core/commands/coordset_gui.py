478e7303a31d86d5858fa230bb2824615f3a726d
Tom Goddard <goddard@sonic.net>
2017-10-24 16:10:28 -0700

    Make coordset slider show changes made with coordset command. Ticket #901.

--- src/core/commands/coordset_gui.py
+++ src/core/commands/coordset_gui.py
@@ -31,6 +31,10 @@ class CoordinateSetSlider(Slider):
                                            compute_ss = compute_ss, steady_atoms = steady_atoms)
         self.update_value(structure.active_coordset_id)
 
+        from .. import atomic
+        t = atomic.get_triggers(session)
+        self._coordset_change_handler = t.add_handler('changes', self.coordset_change_cb)
+        
         from chimerax.core.models import REMOVE_MODELS
         self._model_close_handler = session.triggers.add_handler(REMOVE_MODELS, self.models_closed_cb)
 
@@ -39,6 +43,13 @@ class CoordinateSetSlider(Slider):
 
     def valid_value(self, i):
         return i in self.coordset_ids
+
+    def coordset_change_cb(self, name, changes):
+        # If coordset changed by command, update slider
+        s = self.structure
+        if ('active_coordset changed' in changes.structure_reasons() and
+            s in changes.modified_structures()):
+            self.set_slider(s.active_coordset_id)
             
     def models_closed_cb(self, name, models):
       if self.structure in models:
@@ -46,8 +57,14 @@ class CoordinateSetSlider(Slider):
 
     # Override ToolInstance method
     def delete(self):
+        from .. import atomic
+        t = atomic.get_triggers(session)
+        t.remove_handler(self._coordset_change_handler)
+        self._coordset_change_handler = None
+
         t = self.session.triggers
         t.remove_handler(self._model_close_handler)
         self._model_close_handler = None
+
         super().delete()
         self.structure = None
