89ee1e111e8cbe35ee83a892ac489ab98214fe27
Conrad Huang <conrad@cgl.ucsf.edu>
2017-12-04 15:40:27 -0800

    More viewdockx cleaning up

--- src/bundles/viewdockx/src/io.py
+++ src/bundles/viewdockx/src/io.py
@@ -1,7 +1,6 @@
 # vim: set expandtab shiftwidth=4 softtabstop=4:
 
 
-
 def open_mol2(session, stream, name, auto_style):
     structures = []
     atoms = 0
@@ -13,38 +12,24 @@ def open_mol2(session, stream, name, auto_style):
         structures.append(s)
         atoms += s.num_atoms
         bonds += s.num_bonds
-    status = ("Opened mol2 file containing {} structures {} atoms, {} bonds".format
+    status = ("Opened mol2 file containing "
+              "{} structures {} atoms, {} bonds".format
               (len(structures), atoms, bonds))
     return structures, status
 
-def print_dict(dict):
-    # print(s)
-    for key, value in dict.items():
-        print(key, ":", value,)
-    print()
-
 
 def _read_block(session, stream, auto_style):
-    """function that calls subfunctions that each read a specific section of the mol2 file"""
-    # First section should be commented out
+    """Read all sections for a single entry and build Structure instance"""
+    # First section should be comments
     # Second section: "@<TRIPOS>MOLECULE"
     # Third section: "@<TRIPOS>ATOM"
     # Fourth section: "@<TRIPOS>BOND"
     # Fifth section: "@<TRIPOS>SUBSTRUCTURE"
-
-    from numpy import (array, float64)
-    from chimerax.core.atomic import AtomicStructure
-
-    # print("TEST1")
-
     comment_dict, molecular_dict = read_com_and_mol(session, stream)
     if not molecular_dict:
         return None
-
     atom_dict = read_atom(session, stream)
-
     bond_dict = read_bond(session, stream)
-
     subst_dict = read_subst(session, stream) #pass in # of substructures
     if not subst_dict:
         subst_dict = {}
@@ -54,29 +39,21 @@ def _read_block(session, stream, auto_style):
         for i in subst_set:
             subst_dict[i[0]] = [i[1], None, None, None, None, '****']
 
-
+    from chimerax.core.atomic import AtomicStructure
     s = AtomicStructure(session, auto_style=auto_style)
-
     csd = build_residues(s, subst_dict)
     cad = build_atoms(s, csd, atom_dict)
     build_bonds(s, cad, bond_dict)
     s.viewdock_comment = comment_dict
-
     return s
 
 
 def read_com_and_mol(session, stream):
     """Parses commented section"""
-    # import ast
-
+    # Comments section
     comment_dict = {}
-
-
-    # Comment section
-
     while True:
         comment = stream.readline()
-        # print(comment)
         if not comment: 
             break
         if not comment_dict and comment[0] == "\n": #before the comment section
@@ -94,164 +71,116 @@ def read_com_and_mol(session, stream):
         else:
             comment_dict[(parts[0])] = parts[1]
 
-    # print("CHECKPOINT1")
-
     # Molecule section
-
     if comment == "@<TRIPOS>MOLECULE":
         pass
     else:
         molecule_line = stream.readline()
-
         while "@<TRIPOS>MOLECULE" not in molecule_line:
-            if not molecule_line: #if it's not even "\n".  is true if at the end of a file
+            if not molecule_line: # Unexpected EOF
                 return None, None
             molecule_line = stream.readline()
 
-    # return comment_dict, None
-
     molecular_dict = {}
-    mol_labels = ["mol_name", ["num_atoms", "num_bonds", "num_subst", "num_feat", "num_sets"],\
-    "mol_type", "charge_type", "status_bits"]
+    mol_labels = ["mol_name", ["num_atoms", "num_bonds", "num_subst",
+                               "num_feat", "num_sets"],
+                  "mol_type", "charge_type", "status_bits"]
 
     line_num = 0
-
-
     for label in mol_labels:
-
         line_num += 1
         last_pos = stream.tell()
         molecule_line = stream.readline().strip()
-
         if "@<TRIPOS>ATOM" in molecule_line:
             stream.seek(last_pos)
             break
-
-
-
-
         if line_num == 1:
-            # molecule_line = molecule_line.strip()
+            # Molecule name
             while not molecule_line:
                 molecule_line = stream.readline().strip()
             molecular_dict[label] = molecule_line
             continue
-
-        # Only for the integers (num_atoms, num_bonds, etc)
         if line_num == 2:
+            # Number of atoms, bonds, substructures, features and sets
             molecule_line = molecule_line.split()
-
             if all(isinstance(int(item), int) for item in molecule_line):
                 molecular_dict.update(dict(zip(label, molecule_line)))
             else:
                 raise ValueError("Second line needs to be series of integers")
             continue
-            
         else:
             molecule_line = molecule_line.strip()
             molecular_dict[label] = molecule_line
-
 
     return comment_dict, molecular_dict
 
 
 def read_atom(session, stream):
     """parses atom section"""
-
     while "@<TRIPOS>ATOM" not in stream.readline():
         pass
-
     atom_dict = {}
-
     while True:
         last_pos = stream.tell()
         atom_line = stream.readline().strip()
-
         if not atom_line:
             stream.seek(last_pos)
             break
-
         if "@" in atom_line:
             stream.seek(last_pos)
             break
         if len(atom_line) == 0:
             print("error: no line found")
         parts = atom_line.split()
-        # parts wil be like, ['1', 'C1', 9.4819, 36.0139, 21.8847, 'C.3', 1, 'RIBOSE_MONOPHOSPHATE', 0.0767]
-        if len(parts) not in range(6, 11): # gives error msg if there aren't enough entries
+        if len(parts) not in range(6, 11):
             print("error: not enough or too many entries on a line")
             return None
-
         atom_dict[(parts[0])] = parts[1:]
-
-
-    # atom_dict would now be:
-    # {1 : ['C1', '9.4819', '36.0139', '21.8847', 'C.3', '1', 'RIBOSE_MONOPHOSPHATE', '0.0767'],
-    # 2 : ['C2'....] }
-
     return atom_dict
 
 def read_bond(session, stream):
     """parses bond section"""
-
     while "@<TRIPOS>BOND" not in stream.readline():
         pass
-
     bond_dict = {}
-
     while True:
         last_pos = stream.tell()
         bond_line = stream.readline()
         parts = bond_line.split()
-
-
         if not bond_line or "@" in bond_line or bond_line[0] == "#" or parts == 0:
             stream.seek(last_pos)
             break
-
         if len(parts) != 4:
             print("error: not enough entries in under bond data")
             raise ValueError
         if not isinstance(int(parts[0]), int):
             print("error: first value is needs to be an integer")
             raise ValueError
-
         bond_dict[parts[0]] = parts[1:3]
-
     return bond_dict
 
 def read_subst(session, stream):
     """parses substructure section"""
     last_pos = stream.tell()
     subst_line = stream.readline()
-
     while "@<TRIPOS>SUBSTRUCTURE" not in subst_line:
         if "#" in subst_line or not subst_line:
             stream.seek(last_pos)
             return None
 
         subst_line = stream.readline()
-
     subst_dict = {}
-
     while True:
         last_pos = stream.tell()
         subst_line = stream.readline()
         parts = subst_line.split()
-
-        # print(subst_line)
-
-# 
         if not subst_line or len(parts) == 0 or "#" in subst_line:
             stream.seek(last_pos)
             break
         if "#" in subst_line:
             stream.seek(last_pos)
             break
-
-
         subst_dict[parts[0]] = parts[1:]
-
     return subst_dict
 
 
@@ -259,22 +188,18 @@ def build_residues(s, subst_dict):
     """ create chimeraX substructure dictionary (csd) """
     csd = {}
     # csd will be something like {"1": <residue>}
-
     for s_index in subst_dict:
-
-
-        # new_residue(self, residue_name, chain_id, pos, insert=' ')
-
-        residue = s.new_residue(subst_dict[s_index][0][:4], str(subst_dict[s_index][5]), int(s_index))
+        # new_residue(residue_name, chain_id, pos)
+        residue = s.new_residue(subst_dict[s_index][0][:4],
+                                str(subst_dict[s_index][5]),
+                                int(s_index))
         csd[s_index] = residue
     return csd
 
 
 def build_atoms(s, csd, atom_dict):
-
     """ Creates chimeraX atom dictionary (cad)"""
     from numpy import array, float64
-
     cad = {}
     for key in atom_dict:
         name = atom_dict[key][0]
@@ -286,12 +211,9 @@ def build_atoms(s, csd, atom_dict):
         new_atom = s.new_atom(name, element)
         new_atom.coord = array(xyz, dtype=float64)
         # new_atom.serial_number = int(key)
-
-        # adding ne atom to subst_id 
+        # adding new atom to subst_id 
         csd[atom_dict[key][5]].add_atom(new_atom)
-
         cad[key] = new_atom
-
     return cad
 
 
--- src/bundles/viewdockx/src/viewdockx_frame.js
+++ src/bundles/viewdockx/src/viewdockx_frame.js
@@ -84,6 +84,14 @@ function init() {
 
     });
 
+    $('#graph_btn').on('click', function() {
+        window.location = "viewdockx:graph";
+    });
+    $('#histogram_btn').on('click', function() {
+        window.location = "viewdockx:histogram";
+    });
+
+/*
     // var vdx_chart=null; 
     $('#graph_btn').on('click', function() {
         if (typeof vdx_chart != 'undefined') {
@@ -102,202 +110,203 @@ function init() {
         });
     });
 
-    // $('#histogram_btn').on('click', function() {
-    //     var data = d3.range(1000).map(d3.randomBates(10));
+     $('#histogram_btn').on('click', function() {
+         var data = d3.range(1000).map(d3.randomBates(10));
 
-    //     var formatCount = d3.format(",.0f");
+         var formatCount = d3.format(",.0f");
 
-    //     var svg = d3.select("svg"),
-    //         margin = { top: 10, right: 30, bottom: 30, left: 30 },
-    //         width = +svg.attr("width") - margin.left - margin.right,
-    //         height = +svg.attr("height") - margin.top - margin.bottom,
-    //         g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+         var svg = d3.select("svg"),
+             margin = { top: 10, right: 30, bottom: 30, left: 30 },
+             width = +svg.attr("width") - margin.left - margin.right,
+             height = +svg.attr("height") - margin.top - margin.bottom,
+             g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    //     var x = d3.scaleLinear()
-    //         .rangeRound([0, width]);
+         var x = d3.scaleLinear()
+             .rangeRound([0, width]);
 
-    //     var bins = d3.histogram()
-    //         .domain(x.domain())
-    //         .thresholds(x.ticks(20))
-    //         (data);
+         var bins = d3.histogram()
+             .domain(x.domain())
+             .thresholds(x.ticks(20))
+             (data);
 
-    //     var y = d3.scaleLinear()
-    //         .domain([0, d3.max(bins, function(d) { return d.length; })])
-    //         .range([height, 0]);
+         var y = d3.scaleLinear()
+             .domain([0, d3.max(bins, function(d) { return d.length; })])
+             .range([height, 0]);
 
-    //     var bar = g.selectAll(".bar")
-    //         .data(bins)
-    //         .enter().append("g")
-    //         .attr("class", "bar")
-    //         .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
+         var bar = g.selectAll(".bar")
+             .data(bins)
+             .enter().append("g")
+             .attr("class", "bar")
+             .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
 
-    //     bar.append("rect")
-    //         .attr("x", 1)
-    //         .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
-    //         .attr("height", function(d) { return height - y(d.length); });
+         bar.append("rect")
+             .attr("x", 1)
+             .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
+             .attr("height", function(d) { return height - y(d.length); });
 
-    //     bar.append("text")
-    //         .attr("dy", ".75em")
-    //         .attr("y", 6)
-    //         .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
-    //         .attr("text-anchor", "middle")
-    //         .text(function(d) { return formatCount(d.length); });
+         bar.append("text")
+             .attr("dy", ".75em")
+             .attr("y", 6)
+             .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
+             .attr("text-anchor", "middle")
+             .text(function(d) { return formatCount(d.length); });
 
-    //     g.append("g")
-    //         .attr("class", "axis axis--x")
-    //         .attr("transform", "translate(0," + height + ")")
-    //         .call(d3.axisBottom(x));
-    // });
-
-
-//    $('#histogram_btn').on('click', function() {
-//        for (var i = 0; i < data_array.length; i++) {
-//            if(data_array[i] === "missing"){
-//                data_array[i] = 0;
-//            }
-//        }
-//
-//        if(typeof(h_data) != 'undefined'){
-//            //alert("hello"); test
-//            d3.selectAll('g').remove()
-//        }
-//
-//        h_data = data_array;
-//
-//        max = Math.max.apply(Math, h_data);
-//        min = Math.min.apply(Math, h_data);
-//
-//        var formatCount = d3.format(",.0f");
-//
-//        var svg = d3.select("svg"),
-//            margin = {top: 10, right: 30, bottom: 30, left: 30},
-//            width = +svg.attr("width") - margin.left + 10 - margin.right,
-//            height = +svg.attr("height") - margin.top - margin.bottom,
-//            g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-//
-//        var x = d3.scaleLinear(h_data)
-//            .domain([min, max]) // x axis domain, from min and max of array
-//            .rangeRound([0, width]);
-//
-//        var bins = d3.histogram()
-//            .domain(x.domain())
-//            //.thresholds()
-//            (h_data);
-//
-//        var y = d3.scaleLinear()
-//            .domain([0, d3.max(bins, function(d) { return d.length; })])
-//            .range([height, 0]);
-//
-//        var bar = g.selectAll(".bar")
-//          .data(bins)
-//          .enter().append("g")
-//            .attr("class", "bar")
-//            .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
-//
-//        bar.append("rect")
-//            .attr("x", 1)
-//            .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
-//            .attr("height", function(d) { return height - y(d.length); });
-//
-//        bar.append("text")
-//            .attr("dy", ".75em")
-//            .attr("y", 6)
-//            .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
-//            .attr("text-anchor", "middle")
-//            .text(function(d) { return formatCount(d.length); });
-//
-//        g.append("g")
-//            .attr("class", "axis axis--x")
-//            .attr("transform", "translate(0," + height + ")")
-//            .call(d3.axisBottom(x));
-//    });
+         g.append("g")
+             .attr("class", "axis axis--x")
+             .attr("transform", "translate(0," + height + ")")
+             .call(d3.axisBottom(x));
+     });
 
 
+    $('#histogram_btn').on('click', function() {
+        for (var i = 0; i < data_array.length; i++) {
+            if(data_array[i] === "missing"){
+                data_array[i] = 0;
+            }
+        }
+
+        if(typeof(h_data) != 'undefined'){
+            //alert("hello"); test
+            d3.selectAll('g').remove()
+        }
+
+        h_data = data_array;
+
+        max = Math.max.apply(Math, h_data);
+        min = Math.min.apply(Math, h_data);
+
+        var formatCount = d3.format(",.0f");
+
+        var svg = d3.select("svg"),
+            margin = {top: 10, right: 30, bottom: 30, left: 30},
+            width = +svg.attr("width") - margin.left + 10 - margin.right,
+            height = +svg.attr("height") - margin.top - margin.bottom,
+            g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var x = d3.scaleLinear(h_data)
+            .domain([min, max]) // x axis domain, from min and max of array
+            .rangeRound([0, width]);
+
+        var bins = d3.histogram()
+            .domain(x.domain())
+            //.thresholds()
+            (h_data);
+
+        var y = d3.scaleLinear()
+            .domain([0, d3.max(bins, function(d) { return d.length; })])
+            .range([height, 0]);
+
+        var bar = g.selectAll(".bar")
+          .data(bins)
+          .enter().append("g")
+            .attr("class", "bar")
+            .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
+
+        bar.append("rect")
+            .attr("x", 1)
+            .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
+            .attr("height", function(d) { return height - y(d.length); });
+
+        bar.append("text")
+            .attr("dy", ".75em")
+            .attr("y", 6)
+            .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
+            .attr("text-anchor", "middle")
+            .text(function(d) { return formatCount(d.length); });
+
+        g.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+    });
 
 
 
 
 
 
-//    var margin = {
-//        top: 30,
-//        right: 20,
-//        bottom: 30,
-//        left: 50
-//    };
-//    var width = 600 - margin.left - margin.right;
-//    var height = 270 - margin.top - margin.bottom;
-//
-//    var parseDate = d3.time.format("%d-%b-%y").parse;
-//
-//    var x = d3.time.scale().range([0, width]);
-//    var y = d3.scale.linear().range([height, 0]);
-//
-//    var xAxis = d3.svg.axis().scale(x)
-//        .orient("bottom").ticks(5);
-//
-//    var yAxis = d3.svg.axis().scale(y)
-//        .orient("left").ticks(5);
-//
-//    var valueline = d3.svg.line()
-//        .x(function (d) {
-//          return x(d.date);
-//        })
-//        .y(function (d) {
-//          return y(d.close);
-//        });
-//
-//    var svg = d3.select("body")
-//        .append("svg")
-//        .attr("width", width + margin.left + margin.right)
-//        .attr("height", height + margin.top + margin.bottom)
-//        .append("g")
-//        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-//
-//    // Get the data
-//    var data = [{
-//        date: "1-May-12",
-//        close: "58.13"
-//    }, {
-//        date: "30-Apr-12",
-//        close: "53.98"
-//    }, {
-//        date: "27-Apr-12",
-//        close: "67.00"
-//    }, {
-//        date: "26-Apr-12",
-//        close: "89.70"
-//    }, {
-//        date: "25-Apr-12",
-//        close: "99.00"
-//    }];
-//
-//    data.forEach(function (d) {
-//        d.date = parseDate(d.date);
-//        d.close = +d.close;
-//    });
-//
-//    // Scale the range of the data
-//    x.domain(d3.extent(data, function (d) {
-//        return d.date;
-//        }));
-//    y.domain([0, d3.max(data, function (d) {
-//        return d.close;
 
 
-//        })]);
-//
-//    svg.append("path") // Add the valueline path.
-//    .attr("d", valueline(data));
-//
-//    svg.append("g") // Add the X Axis
-//    .attr("class", "x axis")
-//        .attr("transform", "translate(0," + height + ")")
-//        .call(xAxis);
-//
-//    svg.append("g") // Add the Y Axis
-//    .attr("class", "y axis")
-//        .call(yAxis);
+    var margin = {
+        top: 30,
+        right: 20,
+        bottom: 30,
+        left: 50
+    };
+    var width = 600 - margin.left - margin.right;
+    var height = 270 - margin.top - margin.bottom;
+
+    var parseDate = d3.time.format("%d-%b-%y").parse;
+
+    var x = d3.time.scale().range([0, width]);
+    var y = d3.scale.linear().range([height, 0]);
+
+    var xAxis = d3.svg.axis().scale(x)
+        .orient("bottom").ticks(5);
+
+    var yAxis = d3.svg.axis().scale(y)
+        .orient("left").ticks(5);
+
+    var valueline = d3.svg.line()
+        .x(function (d) {
+          return x(d.date);
+        })
+        .y(function (d) {
+          return y(d.close);
+        });
+
+    var svg = d3.select("body")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Get the data
+    var data = [{
+        date: "1-May-12",
+        close: "58.13"
+    }, {
+        date: "30-Apr-12",
+        close: "53.98"
+    }, {
+        date: "27-Apr-12",
+        close: "67.00"
+    }, {
+        date: "26-Apr-12",
+        close: "89.70"
+    }, {
+        date: "25-Apr-12",
+        close: "99.00"
+    }];
+
+    data.forEach(function (d) {
+        d.date = parseDate(d.date);
+        d.close = +d.close;
+    });
+
+    // Scale the range of the data
+    x.domain(d3.extent(data, function (d) {
+        return d.date;
+        }));
+    y.domain([0, d3.max(data, function (d) {
+        return d.close;
+
+
+        })]);
+
+    svg.append("path") // Add the valueline path.
+    .attr("d", valueline(data));
+
+    svg.append("g") // Add the X Axis
+    .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g") // Add the Y Axis
+    .attr("class", "y axis")
+        .call(yAxis);
+*/
 
 
     
