71be0399fd52ccb1de97f8bc5d101d9e143177cd
Conrad Huang <conrad@cgl.ucsf.edu>
2018-03-10 16:53:40 -0800

    Munge "Bases" class names to be short names

--- docs/devel/class_munger.py
+++ docs/devel/class_munger.py
@@ -1,3 +1,5 @@
+# vim: set expandtab shiftwidth=4 softtabstop=4:
+
 """Sphinx extension class for munging output for autodoc subclass links.
 
 Only output the last component of the class name instead of the full path.
@@ -12,10 +14,35 @@ Replacements = [
 
 
 def setup(app):
+    munge_autodoc()
     app.connect("doctree-read", doctree_read)
     app.connect("autodoc-process-docstring", autodoc_process_docstring)
     app.connect("autodoc-skip-member", autodoc_skip_member)
     return {"version":"0.1"}
+
+
+def munge_autodoc():
+    # Munge autodoc ClassDocumenter so that base classes do
+    # not have full path
+    from sphinx.ext import autodoc
+    # Code below is copied from Sphinx 1.6.8, with the '~' inserted
+    # for non-builtin classes
+    def my_add_directive_header(self, sig):
+        if self.doc_as_attr:
+            self.directivetype = 'attribute'
+        autodoc.Documenter.add_directive_header(self, sig)
+
+        if not self.doc_as_attr and self.options.show_inheritance:
+            sourcename = self.get_sourcename()
+            self.add_line(u'', sourcename)
+            if hasattr(self.object, '__bases__') and len(self.object.__bases__):
+                bases = [b.__module__ in ('__builtin__', 'builtins') and
+                         u':class:`%s`' % b.__name__ or
+                         u':class:`~%s.%s`' % (b.__module__, b.__name__)
+                         for b in self.object.__bases__]
+                self.add_line(u'   ' + autodoc._(u'Bases: %s') % ', '.join(bases),
+                              sourcename)
+    autodoc.ClassDocumenter.add_directive_header = my_add_directive_header
 
 
 def doctree_read(app, doctree):
@@ -43,16 +70,17 @@ def autodoc_process_docstring(app, what, name, obj, options, lines):
             lines.pop(0)
         else:
             break
-    for n, line in enumerate(lines):
-        if "Supported API" in line:
-            lines[n] = line.replace("Supported API", "*Supported API*")
-            api_status = "supported"
-        elif "Experimental API" in line:
-            api_status = "experimental"
-        elif "Private API" in line:
-            api_status = "private"
-    if api_status == "unknown":
-        lines.insert(0, "*Experimental API*.")
+    if what in ["method"]:
+        for n, line in enumerate(lines):
+            if "Supported API" in line:
+                lines[n] = line.replace("Supported API", "*Supported API*")
+                api_status = "supported"
+            elif "Experimental API" in line:
+                api_status = "experimental"
+            elif "Private API" in line:
+                api_status = "private"
+        if api_status == "unknown":
+            lines.insert(0, "*Experimental API*.")
 
 
 def autodoc_skip_member(app, what, name, obj, skip, options):
