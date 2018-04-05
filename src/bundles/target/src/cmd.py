d9a6d34861e35c0f3645f42e8e39f37c0b3e2b72
Conrad Huang <conrad@cgl.ucsf.edu>
2018-04-04 21:07:37 -0700

    Make "targets" bundle into "name" command and "Basic Actions" tool

--- src/bundles/target/src/cmd.py
+++ src/bundles/target/src/cmd.py
@@ -1,52 +1,106 @@
 # vim: set expandtab ts=4 sw=4:
 
-from chimerax.core.commands import CmdDesc, StringArg, ObjectsArg, NoArg
+from chimerax.core.errors import UserError
+from chimerax.core.commands import CmdDesc, StringArg, AtomSpecArg, ObjectsArg
+from chimerax.core.commands import RestOfLine, NoArg, AnnotationError
 
 
-def target(session, name="Targets", all=False):
-    from .tool import TargetsTool
-    tool = TargetsTool(session, name, all, log_errors=True)
-    tool.setup()
-    return tool
-target_desc = CmdDesc(optional=[("name", StringArg),
-                                ("all", NoArg)])
+def name(session, name, text=None):
+    if name == "all":
+        raise UserError("\"all\" is reserved and cannot be redefined")
+    if text is None:
+        from chimerax.core.commands import get_selector
+        try:
+            sel = get_selector(name)
+        except KeyError:
+            raise UserError("\"%s\" is not defined" % name)
+        else:
+            value = _get_name_desc(sel, True)
+            session.logger.info('\t'.join([name, value]))
+    else:
+        try:
+            ast, used, unused = AtomSpecArg.parse(text, session)
+            if unused:
+                raise AnnotationError("contains extra trailing text")
+        except AnnotationError as e:
+            raise UserError("\"%s\": %s" % (text, str(e)))
+        def selector(session, models, results, spec=text):
+            objects, used, unused = ObjectsArg.parse(spec, session)
+            results.combine(objects)
+        selector.name_text = text
+        from chimerax.core.commands import register_selector
+        register_selector(name, selector, session.logger)
+name_desc = CmdDesc(required=[("name", StringArg)],
+                    optional=[("text", RestOfLine)],
+                    non_keyword=["text"])
 
 
-def target_define(session, name, objects):
+def name_frozen(session, name, objects):
+    if name == "all":
+        raise UserError("\"all\" is reserved and cannot be redefined")
     from chimerax.core.commands import register_selector
     register_selector(name, objects, session.logger)
-target_define_desc = CmdDesc(required=[("name", StringArg),
-                                       ("objects", ObjectsArg)])
+name_frozen_desc = CmdDesc(required=[("name", StringArg),
+                                     ("objects", ObjectsArg)])
 
 
-def target_undefine(session, name):
+def name_delete(session, name):
     from chimerax.core.commands import deregister_selector
-    deregister_selector(name, session.logger)
-target_undefine_desc = CmdDesc(required=[("name", StringArg)])
+    if name != "all":
+        deregister_selector(name, session.logger)
+    else:
+        from chimerax.core.commands import list_selectors, get_selector
+        for name in list_selectors():
+            sel = get_selector(name)
+            if not _is_builtin(sel):
+                deregister_selector(name, session.logger)
+name_delete_desc = CmdDesc(required=[("name", StringArg)])
 
 
-def target_list(session, all=False, log=True):
-    from chimerax.core.commands import list_selectors, get_selector
-    from chimerax.core.objects import Objects
+def name_list(session, all=False, log=True):
+    from chimerax.core.commands import list_selectors
     targets = {}
     for name in sorted(list_selectors()):
-        sel = get_selector(name)
-        if callable(sel):
-            mod_name = sel.__module__
-            if not all and (mod_name.startswith("chimerax.core") or
-                            mod_name.startswith("chimerax.chem_group")):
-                continue
-            value = "Built-in"
-        elif isinstance(sel, Objects):
-            value = ("[%d atoms, %d bonds, %d pbonds, %d models]" %
-                     (sel.num_atoms, sel.num_bonds, sel.num_pseudobonds,
-                      len(sel.models)))
-        else:
-            value = str(sel)
-        if log:
-            session.logger.info('\t'.join([name, value]))
-        targets[name] = value
+        value = _get_name_desc(name, all)
+        if value:
+            if log:
+                session.logger.info('\t'.join([name, value]))
+            targets[name] = value
     if not targets and log:
         session.logger.info("There are no user-defined targets.")
     return targets
-target_list_desc = CmdDesc(optional=[("all", NoArg)])
+name_list_desc = CmdDesc(optional=[("all", NoArg)])
+
+
+def _get_name_desc(name, builtin_okay):
+    from chimerax.core.commands import get_selector
+    from chimerax.core.objects import Objects
+    sel = get_selector(name)
+    if callable(sel):
+        if _is_builtin(sel):
+            if not builtin_okay:
+                return None
+            value = "[Built-in]"
+        else:
+            try:
+                value = sel.name_text
+            except AttributeError:
+                value = "[Function]"
+    elif isinstance(sel, Objects):
+        title = []
+        if sel.num_atoms:
+            title.append("%d atoms" % sel.num_atoms)
+        if sel.num_bonds:
+            title.append("%d bonds" % sel.num_bonds)
+        if len(sel.models) > 1:
+            title.append("%d models" % len(sel.models))
+        value = "[%s]" % ', '.join(title)
+    else:
+        value = str(sel)
+    return value
+
+
+def _is_builtin(func):
+    mod_name = func.__module__
+    return (mod_name.startswith("chimerax.core") or
+            mod_name.startswith("chimerax.chem_group"))
