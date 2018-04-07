08053437b90965b0ec0880733e858ac5c99310b7
Eric Pettersen <pett@cgl.ucsf.edu>
2018-04-06 17:28:50 -0700

    Added more informational status messages as mouse mode is used

--- src/bundles/dist_ui/src/mouse_dist.py
+++ src/bundles/dist_ui/src/mouse_dist.py
@@ -32,15 +32,16 @@ class DistMouseMode(MouseMode):
         x,y = event.position()
         from chimerax.ui.mousemodes import picked_object
         pick = picked_object(x, y, self.session.main_view)
-        message = lambda txt: self.session.logger.status(
+        warning = lambda txt: self.session.logger.status(
             "Distance mouse mode: %s" % txt, color = "red")
+        message = self.session.logger.status
         from chimerax.core.atomic import PickedAtom
         if isinstance(pick, PickedAtom):
             if self._first_atom and self._first_atom.deleted:
                 self._first_atom = None
             if self._first_atom:
                 if pick.atom == self._first_atom:
-                    message("same atom picked twice")
+                    warning("same atom picked twice")
                 else:
                     pbg = self.session.pb_manager.get_group("distances", create=False)
                     pbs = pbg.pseudobonds if pbg else []
@@ -50,14 +51,20 @@ class DistMouseMode(MouseMode):
                         or (a1 == pick.atom and self._first_atom == a2):
                             command = "~dist %s %s" % (a1.string(style="command line"),
                                 a2.string(style="command line"))
+                            message("Removing distance")
                             break
                     else:
-                        command = "dist %s %s" % (self._first_atom.string(style="command line"),
-                            pick.atom.string(style="command line"))
+                        a1, a2 = self._first_atom, pick.atom
+                        command = "dist %s %s" % (a1.string(style="command line"),
+                            a2.string(style="command line"))
+                        from chimerax.core.geometry import distance
+                        message("Distance from %s to %s is %g" % (a1, a2, distance(a1.scene_coord,
+                            a2.scene_coord)))
                     self._first_atom = None
                     from chimerax.core.commands import run
                     run(self.session, command)
             else:
                 self._first_atom = pick.atom
+                message("Distance from %s to..." % pick.atom)
         else:
-            message("no atom picked by mouse click")
+            warning("no atom picked by mouse click")
