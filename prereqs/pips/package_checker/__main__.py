ff6313286b7118dff5f08ac3ef8b95e1f9402908
Conrad Huang <conrad@cgl.ucsf.edu>
2018-04-16 10:52:39 -0700

    Fix syntax error

--- prereqs/pips/package_checker/__main__.py
+++ prereqs/pips/package_checker/__main__.py
@@ -6,7 +6,7 @@ Comments = {
     "docutils": "required by Sphinx",
     "filelock": "",
     "flake8": "used for Python lint",
-    "grako": "3.16.5 is the last version that works for ChimeraX"
+    "grako": "3.16.5 is the last version that works for ChimeraX",
     "Jinja2": "required by Sphinx",
     "mccabe": "required by flake8",
     "numpydoc": "required for Sphinx",
