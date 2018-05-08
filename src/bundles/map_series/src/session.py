8c69f21ee088a178625082d13abf0b94ee02ce93
Tom Goddard <goddard@sonic.net>
2018-05-08 15:47:40 -0700

    Remove old map series slider code.

--- src/bundles/map_series/src/session.py
+++ src/bundles/map_series/src/session.py
@@ -44,28 +44,3 @@ def state_from_series(series, session):
         'session_id': oids.object_id(series),        # Used to reference series in gui session state
     }
     return s
-    
-# -----------------------------------------------------------------------------
-# Save and restore map series gui state.
-#
-def map_series_slider_states(session):
-    from .slider import sliders
-    st = [slider_state(s,session) for s in sliders(session)]
-    return st
-
-# -----------------------------------------------------------------------------
-#
-def restore_map_series_sliders(sl_states, session, file_paths, attributes_only = False):
-    oids = session.object_ids
-    for sl_state in sl_states:
-        series = [oids.object_from_id(s) for s in sl_state['series']]
-        from . import slider
-        vss = slider.Volume_Series_Slider(series, session)
-        vss.show()
-
-# ---------------------------------------------------------------------------
-#
-def slider_state(slider, session):
-    oids = session.object_ids
-    s = { 'series': tuple(oids.object_id(s) for s in slider.series), }
-    return s
