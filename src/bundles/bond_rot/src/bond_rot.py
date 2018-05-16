34378f5ad24c6ae351d4edea3634fd4a393ba29a
Eric Pettersen <pett@cgl.ucsf.edu>
2018-05-15 17:15:45 -0700

    revamped infrastructure to allow multiple BondRotaters (given to API users) for each BondRotation

--- src/bundles/bond_rot/src/bond_rot.py
+++ src/bundles/bond_rot/src/bond_rot.py
@@ -14,14 +14,40 @@
 from chimerax.core.state import State
 #class BondRotation(State):
 class BondRotation:
-    """A bond rotation,
+    """A bond rotation, which can have multiple BondRotaters associated.
+    If any BondRotaters rotate the bond, BondRotation will update the others.
     
-    Should only be created through new_rotation method of the bond-rotation manager
+    Should only be created by the bond-rotation manager
     """
 
-    def __init__(self, session, bond, ident, moving_side, one_shot):
+    def __init__(self, session, bond):
         self.session = session
         self.bond = bond
+        self.rotaters = []
+
+    def delete_rotater(self, rotater):
+        self.rotaters.remove(rotater)
+
+    def new_rotater(self, ident, moving_side, one_shot):
+        rotater = BondRotater(self.session, self, ident, moving_side, one_shot)
+        self.rotaters.append(rotater)
+        return rotater
+
+    def _rotater_update(self, initiator, delta):
+        for r in self.rotaters:
+            if r == initiator:
+                continue
+            if r.moving_side == initiator.moving_side:
+                r._angle += delta
+            else:
+                r._angle -= delta
+
+#class BondRotater(State):
+class BondRotater:
+    # instances given to API users; works in conjunction with BondRotation
+    def __init__(self, session, rotation, ident, moving_side, one_shot):
+        self.session = session
+        self.rotation = rotation
         self.ident = ident
         self.moving_side = moving_side
         self.one_shot = one_shot
@@ -44,7 +70,7 @@ class BondRotation:
         # avoid a copy...
         update.move(coords)
         side_atoms.coords = coords
-        mgr = self.session.bond_rotations
+        self.rotation._rotater_update(self, delta)
         # manager listening on changes will fire 'modified' trigger...
 
     angle = property(get_angle, set_angle)
