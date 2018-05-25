f8fe3efa0b36d86f02eba256147fea01d767c114
gregcouch <gregcouch@gmail.com>
2018-05-25 16:56:58 -0700

    Merge bundle documentation in to user index.

--- src/bundles/help_viewer/src/cmd.py
+++ src/bundles/help_viewer/src/cmd.py
@@ -12,6 +12,7 @@
 # === UCSF ChimeraX Copyright ===
 
 from chimerax.core.commands import CmdDesc, Or, EnumOf, EmptyArg, RestOfLine, run, cli
+from collections import OrderedDict
 
 
 def help(session, topic=None, *, option=None):
@@ -40,7 +41,7 @@ def help(session, topic=None, *, option=None):
         # make sure path is a relative path
         if os.path.isabs(help_path):
             if sys.platform.startswith('win'):
-                help_path = os.path.relpath(help_path, os.path.splitdrive(path)[0])
+                help_path = os.path.relpath(help_path, os.path.splitdrive(help_path)[0])
             else:
                 help_path = os.path.relpath(help_path, '/')
         for hd in help_directories:
@@ -53,8 +54,9 @@ def help(session, topic=None, *, option=None):
             return
         if os.path.isdir(path):
             path += '/index.html'
-        # TODO: if path == 'user/index.html':
-        # TODO:     html = merged_user_index()
+        if help_path in ('user', 'user/index.html'):
+            with open(path) as f:
+                html = _generate_index(f)
         url = urlunparse(('file', '', pathname2url(path), '', '', fragment))
     else:
         cmd_name = topic
@@ -95,3 +97,131 @@ help_desc = CmdDesc(
     non_keyword=('option', 'topic'),
     synopsis='display help'
 )
+
+
+def _generate_index(source):
+    # Take contents of source, look for lists of tools and commands,
+    # and insert tools and commands from bundles that come with
+    # documentation
+    from chimerax.core import toolshed
+    ts = toolshed.get_toolshed()
+    if ts is None:
+        return None
+    # Look for <div id="foobar">
+    import lxml.html
+    html = lxml.html.parse(source)
+    for node in html.iterfind(".//div[@id]"):
+        ident = node.attrib["id"]
+        if ident == "clist":
+            _update_list(ts, node, 'commands', _update_commands)
+        elif ident == "tlist":
+            _update_list(ts, node, 'tools', _update_tools)
+    return lxml.html.tostring(html).decode(errors='replace')
+
+
+def _update_list(toolshed, node, what, callback):
+    doc_ul = None    # ul node with documented stuff
+    doc = OrderedDict()
+    undoc_ul = None  # ul node with undocumented stuff
+    undoc = OrderedDict()
+    d = None
+    for ul in node:
+        if ul.tag != 'ul':
+            continue
+        if doc_ul is None:
+            doc_ul = ul
+            d = doc
+        elif undoc_ul is None:
+            undoc_ul = ul
+            d = undoc
+        else:
+            print("unexpected ul tag in %s" % what)
+            continue
+        # <li><a href="commands/alias.html"><b>alias</b></a>
+        # - define a command alias (shortcut or composite action)</li>
+        # <li><b>texture</b> - map image onto surface</li>
+        # <li><a href="tools/basicactions.html"><b>Basic Actions</b></a></li>
+        for li in ul:
+            if li.tag != 'li':
+                print("unexpected node %r in %s on line %d" % (li, what, li.sourceline))
+                continue
+            # inspect first child for name
+            ab = li[0]
+            t = li.text
+            valid = t is None or t.strip() == ''  # should not have any text after <li>
+            if ab.tag == 'b':
+                name = ab.text.strip()
+                if d == doc_ul:
+                    print("bad <b> on line", ab.sourceline)  # DEBUG
+                    valid = False
+            elif ab.tag == 'a':
+                href = ab.attrib["href"]
+                w, name = href.split('/')
+                if w != what:
+                    print("didn't expected %s hrefs to be to %s on line %d" % (
+                        what, href, li.sourceline))
+                name = name.split('.')[0]
+                if d == undoc_ul:
+                    print("bad <a> on line", ab.sourceline)  # DEBUG
+                    valid = False
+            if not valid:
+                print("expected %s tag as first part of <li> in %s on line %d" % (
+                    "<a>" if d == doc else "<b>", what, li.sourceline))
+            d[name] = li
+    # Currently, don't do anything with undocumented things
+    callback(toolshed, doc_ul, doc)
+
+
+def _update_commands(toolshed, doc_ul, doc):
+    import lxml.etree as ET
+    missing = OrderedDict()
+    for bi in toolshed.bundle_info(None):
+        for cmd in bi.commands:
+            words = cmd.name.split(maxsplit=2)
+            name = words[0]
+            if name in doc or name in missing:
+                continue
+            synopsis = cmd.synopsis
+            if len(words) > 1:
+                # synopsis not appropriate for multiword commands
+                synopsis = bi.synopsis
+            href = bi.get_path("docs/user/commands/%s.html" % name)
+            if href:
+                missing[name] = ("help:user/commands/%s.html" % name, synopsis)
+    names = list(doc)
+    missing_names = list(missing)
+    all_names = names + missing_names
+    all_names.sort(key=str.casefold)
+    for name in missing_names:
+        i = all_names.index(name)
+        href, synopsis = missing[name]
+        if synopsis:
+            synopsis = " - " + synopsis
+        doc_ul.insert(
+            i, ET.HTML('<li><a href="%s"><b>%s</b></a>%s</li>' % (
+                href, name, synopsis)))
+
+
+def _update_tools(toolshed, doc_ul, doc):
+    import lxml.etree as ET
+    missing = OrderedDict()
+    for bi in toolshed.bundle_info(None):
+        for t in bi.tools:
+            name = t.name
+            if name in doc:
+                continue
+            href = bi.get_path("docs/user/tools/%s.html" % name.replace(' ', '_'))
+            if href:
+                missing[name] = (href, t.synopsis)
+    names = list(doc)
+    missing_names = list(missing)
+    all_names = names + missing_names
+    all_names.sort(key=str.casefold)
+    for name in missing_names:
+        i = all_names.index(name)
+        href, synopsis = missing[name]
+        if synopsis:
+            synopsis = " - " + synopsis
+        doc_ul.insert(
+            i, ET.HTML('<li><a href="%s"><b>%s</b></a>%s</li>' % (
+                href, name, synopsis)))
