75b51cc4be6818d2d6380a9233fc5490e737f7cf
Tom Goddard <goddard@sonic.net>
2018-06-05 14:38:03 -0700

    Fixed color zone auto update to work on initially empty surface.

--- src/bundles/std_commands/src/color.py
+++ src/bundles/std_commands/src/color.py
@@ -1030,11 +1030,10 @@ def color_zone(session, surfaces, near, distance=2, sharp_edges = False, update 
       Whether to update surface color when surface shape changes.  Default true.
     '''
     atoms = near
-    surfs = [s for s in surfaces if s.vertices is not None]
     bonds = None
     from chimerax.surface.colorzone import points_and_colors, color_zone, color_zone_sharp_edges
     points, colors = points_and_colors(atoms, bonds)
-    for s in surfs:
+    for s in surfaces:
         # TODO: save undo data
         spoints = s.scene_position.inverse() * points	# Transform points to surface coordinates
         color_zone(s, spoints, colors, distance, sharp_edges = sharp_edges, auto_update = update)
