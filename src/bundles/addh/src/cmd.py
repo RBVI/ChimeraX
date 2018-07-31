3379fa70e12fcd6309f77a990897c0d30d536125
Eric Pettersen <pett@cgl.ucsf.edu>
2018-07-31 11:46:24 -0700

    since coords use tinyarray now, removed some retarded numpy work-around code

--- src/bundles/addh/src/cmd.py
+++ src/bundles/addh/src/cmd.py
@@ -639,13 +639,7 @@ def find_rotamer_nearest(at_pos, idatm_type, atom, neighbor, check_dist):
     nearby = search_tree.search(center, check_dist)
     near_pos = n = near_atom = None
     for nb in nearby:
-        # using numpy.allclose() is *very* slow, so...
-        try:
-            nb_sc = list(nb._addh_coord)
-        except AttributeError:
-            print(nb.__class__, nb.__class__.__module__)
-            raise
-        if nb_sc == list(at_pos) or nb_sc == list(n_pos):
+        if nb._addh_coord in [at_pos, n_pos]:
             # exclude atoms from identical-copy structure also...
             continue
         if nb.structure != atom.structure and (
