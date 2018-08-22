76677d3b5a31dbb3993f81e386b8dd13ebc2751d
Tom Goddard <goddard@sonic.net>
2018-08-21 17:15:05 -0700

    Moved label mouse mode from ui bundle to label bundle.

--- src/bundles/ui/src/mousemodes.py
+++ src/bundles/ui/src/mousemodes.py
@@ -880,48 +880,6 @@ class AtomCenterOfRotationMode(MouseMode):
             from chimerax.std_commands import cofr
             xyz = pick.atom.scene_coord
             cofr.cofr(self.session,pivot=xyz)
-
-class LabelMode(MouseMode):
-    '''Click an atom,ribbon,pseudobond or bond to label or unlabel it with default label.'''
-    name = 'label'
-    icon_file = 'label.png'
-
-    def mouse_down(self, event):
-        MouseMode.mouse_down(self, event)
-        x,y = event.position()
-        pick = picked_object(x, y, self.session.main_view)
-        self._label_pick(pick)
-
-    def _label_pick(self, pick, color = None, orient = None):
-        if pick is None:
-            return
-
-        from chimerax import atomic
-        if isinstance(pick, atomic.PickedAtom):
-            atoms = pick.atom.residue.atoms
-        elif isinstance(pick, atomic.PickedResidue):
-            atoms = pick.residue.atoms
-        elif isinstance(pick, atomic.PickedBond):
-            atoms = atomic.Atoms(pick.bond.atoms)  # Convert tuple to Atoms
-        elif isinstance(pick, atomic.PickedPseudobond):
-            atoms = atomic.Atoms(pick.pbond.atoms)  # Convert tuple to Atoms
-        else:
-            return
-
-        from chimerax.core.objects import Objects
-        objects = Objects(atoms = atoms)
-        object_type = 'residues'
-        
-        ses = self.session
-        from chimerax.label.label3d import label, label_delete
-        if label_delete(ses, objects, object_type) == 0:
-            label(ses, objects, object_type, color=color, orient=orient)
-
-    def laser_click(self, xyz1, xyz2):
-        pick = picked_object_on_segment(xyz1, xyz2, self.view)
-        if pick:
-            from chimerax.core.colors import BuiltinColors
-            self._label_pick(pick, color = BuiltinColors['yellow'], orient = 45)
            
 class NullMouseMode(MouseMode):
     '''Used to assign no mode to a mouse button.'''
@@ -1106,6 +1064,7 @@ def standard_mouse_mode_classes():
     from chimerax import markers
     from chimerax.map import ContourLevelMouseMode, PlanesMouseMode
     from chimerax.map.series import PlaySeriesMouseMode
+    from chimerax.label import LabelMouseMode
     mode_classes = [
         SelectMouseMode,
         SelectAddMouseMode,
@@ -1120,7 +1079,7 @@ def standard_mouse_mode_classes():
         ClipMouseMode,
         ClipRotateMouseMode,
         ObjectIdMouseMode,
-        LabelMode,
+        LabelMouseMode,
         AtomCenterOfRotationMode,
         ContourLevelMouseMode,
         PlanesMouseMode,
