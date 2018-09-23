6bf7feed483ccca55c1cc98395c0b13ad1965488
Eric Pettersen <pett@cgl.ucsf.edu>
2018-09-23 08:45:01 -0700

    swizzled the export declaration around in the hope it appeases Windows

--- src/bundles/pdb/connect_cpp/connect.h
+++ src/bundles/pdb/connect_cpp/connect.h
@@ -33,22 +33,22 @@ using namespace atomstruct;
 
 namespace pdb_connect {
 
-bool PDB_CONNECT_IMEX is_standard_residue(const ResName& name);
-void PDB_CONNECT_IMEX add_standard_residue(const ResName& name);
-void PDB_CONNECT_IMEX remove_standard_residue(const ResName& name);
+PDB_CONNECT_IMEX bool is_standard_residue(const ResName& name);
+PDB_CONNECT_IMEX void add_standard_residue(const ResName& name);
+PDB_CONNECT_IMEX void remove_standard_residue(const ResName& name);
 
-void PDB_CONNECT_IMEX connect_structure(Structure* as,
+PDB_CONNECT_IMEX void connect_structure(Structure* as,
         std::vector<Residue *>* chain_starters,
         std::vector<Residue *>* chain_enders,
         std::set<Atom *>* conect_atoms, std::set<MolResId>* mod_res);
-void PDB_CONNECT_IMEX connect_residue_by_distance(Residue* r,
+PDB_CONNECT_IMEX void connect_residue_by_distance(Residue* r,
         std::set<Atom *>* conect_atoms = nullptr);
-Atom* PDB_CONNECT_IMEX find_closest(Atom* a, Residue* r, float* ret_dist_sq,
+PDB_CONNECT_IMEX Atom* find_closest(Atom* a, Residue* r, float* ret_dist_sq,
         bool nonSaturated=false);
-void PDB_CONNECT_IMEX find_nearest_pair(Residue* from, Residue* to,
+PDB_CONNECT_IMEX void find_nearest_pair(Residue* from, Residue* to,
         Atom** ret_from_atom, Atom** ret_to_atom, float* ret_dist_sq = nullptr);
-void PDB_CONNECT_IMEX find_and_add_metal_coordination_bonds(Structure* as);
-void PDB_CONNECT_IMEX find_missing_structure_bonds(Structure* as);
+PDB_CONNECT_IMEX void find_and_add_metal_coordination_bonds(Structure* as);
+PDB_CONNECT_IMEX void find_missing_structure_bonds(Structure* as);
 
 } // namespace pdb_connect
 
