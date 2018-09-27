6ad873a9bc3825b8fb4bddef7ca14a80ae37681e
Eric Pettersen <pett@cgl.ucsf.edu>
2018-09-27 14:05:46 -0700

    forget to set angle/dist slop to recommended values

--- src/bundles/hbonds/test.py
+++ src/bundles/hbonds/test.py
@@ -1,6 +1,7 @@
 from chimerax.core.commands import run
 run(session, "open 2gbp")
 from chimerax.atomic.hbonds import find_hbonds, rec_dist_slop, rec_angle_slop
-hbonds = find_hbonds(session, session.models[:1])
+hbonds = find_hbonds(session, session.models[:1],
+	dist_slop=rec_dist_slop, angle_slop=rec_angle_slop)
 if len(hbonds) != 792:
 	raise SystemExit("Exepcted to find 792 hbonds in 2gbp; actually found %d" % len(hbonds))
