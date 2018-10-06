038daba894640e661bee36ddf43b7d26d13539d9
Tom Goddard <goddard@sonic.net>
2018-10-05 17:21:46 -0700

    Fixed crossfade and motion blur broken when default texture switched from transparent to opaque.

--- src/core/graphics/crossfade.py
+++ src/core/graphics/crossfade.py
@@ -46,6 +46,7 @@ class CrossFade(Drawing):
         # Make textured square surface piece
         from .drawing import rgba_drawing
         rgba_drawing(self, self.rgba, (-1, -1), (2, 2))
+        self.opaque_texture = False
 
         v.add_overlay(self)
         session.triggers.add_handler('new frame', lambda *_, v=v: self.next_frame(v))
@@ -132,6 +133,7 @@ class MotionBlur(Drawing):
             if c == 0:
                 return False    # No change
             self.texture.reload_texture(self.rgba)
+        self.opaque_texture = False
         self.redraw_needed()
         return True
 
