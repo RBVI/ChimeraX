5dc1aaf13511bda82ebb8756f28b7fe1ad6bc60d
Eric Pettersen <pett@cgl.ucsf.edu>
2018-10-22 17:45:12 -0700

    instead of throw an error when degenerate coords detected, log an error, and return normally

--- src/bundles/atomic/atomic_cpp/atomstruct_cpp/CompSS.cpp
+++ src/bundles/atomic/atomic_cpp/atomstruct_cpp/CompSS.cpp
@@ -14,6 +14,7 @@
  */
 
 #include <algorithm>
+#include <exception>
 #include <list>
 #include <map>
 #include <string>
@@ -26,6 +27,13 @@
 #include "AtomicStructure.h"
 #include "Coord.h"
 #include "Residue.h"
+
+class bad_coords_error: public std::exception {
+public:
+	const char* what() const noexcept {
+		return "Structure has degenerate atomic coordinates; assigning all 'turn' secondary structure";
+	}
+};
 
 namespace atomstruct {
 
@@ -642,7 +650,7 @@ compute_chain(KsdsspParams& params)
 	try {
 		add_imide_hydrogens(params);
 	} catch (std::domain_error&) {
-		throw std::domain_error("Structure has degenerate atomic coordinates; assigning all 'turn'");
+		throw bad_coords_error();
 	}
     find_hbonds(params);
 
@@ -747,6 +755,13 @@ AtomicStructure::compute_secondary_structure(float energy_cutoff,
             delete crd;
         for (auto ih: params.imide_Hs)
             delete ih;
+    } catch (bad_coords_error& e) {
+		set_ss_assigned(true); // leave as all-turn; don't try again
+        for (auto crd: params.coords)
+            delete crd;
+        for (auto ih: params.imide_Hs)
+            delete ih;
+		logger::error(logger(), e.what());
     } catch (...) {
 		set_ss_assigned(true); // leave as all-turn; don't try again
         for (auto crd: params.coords)
