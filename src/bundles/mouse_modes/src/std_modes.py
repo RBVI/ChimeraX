361ff5d6a20a67aa8335f5f4f1c30904502fb682
Tom Goddard <goddard@sonic.net>
2018-11-15 12:56:32 -0800

    Made set pivot mouse mode work for clicks on residues (ribbon), bonds and pseudobonds.

--- src/bundles/mouse_modes/src/std_modes.py
+++ src/bundles/mouse_modes/src/std_modes.py
@@ -123,6 +123,7 @@ class SelectMouseMode(MouseMode):
             self._drawn_rectangle = None
 
     def laser_click(self, xyz1, xyz2):
+        from . import picked_object_on_segment
         pick = picked_object_on_segment(xyz1, xyz2, self.view)
         select_pick(self.session, pick, self.mode)
 
@@ -173,31 +174,14 @@ class SelectToggleMouseMode(SelectMouseMode):
 
 def mouse_select(event, mode, session, view):
     x,y = event.position()
+    from . import picked_object
     pick = picked_object(x, y, view)
     select_pick(session, pick, mode)
-
-def picked_object(window_x, window_y, view, max_transparent_layers = 3):
-    xyz1, xyz2 = view.clip_plane_points(window_x, window_y)
-    if xyz1 is None or xyz2 is None:
-        return None
-    p = picked_object_on_segment(xyz1, xyz2, view, max_transparent_layers = max_transparent_layers)
-    return p
-
-def picked_object_on_segment(xyz1, xyz2, view, max_transparent_layers = 3):    
-    p2 = p = view.first_intercept_on_segment(xyz1, xyz2, exclude=unpickable)
-    for i in range(max_transparent_layers):
-        if p2 and getattr(p2, 'pick_through', False) and p2.distance is not None:
-            p2 = view.first_intercept_on_segment(xyz1, xyz2, exclude=unpickable, beyond=p2.distance)
-        else:
-            break
-    return p2 if p2 else p
-
-def unpickable(drawing):
-    return not getattr(drawing, 'pickable', True)
 
 def mouse_drag_select(start_xy, event, mode, session, view):
     sx, sy = start_xy
     x,y = event.position()
+    from .mousemodes import unpickable
     pick = view.rectangle_intercept(sx,sy,x,y,exclude=unpickable)
     select_pick(session, pick, mode)
 
@@ -431,6 +415,7 @@ class ObjectIdMouseMode(MouseMode):
         if apw and ui.topLevelAt(apw.mapToGlobal(QPoint())) == ui.main_window:
             return
         x,y = position
+        from . import picked_object
         p = picked_object(x, y, self.view)
 
         # Show atom spec balloon
@@ -465,11 +450,24 @@ class AtomCenterOfRotationMode(MouseMode):
         MouseMode.mouse_down(self, event)
         x,y = event.position()
         view = self.session.main_view
+        from . import picked_object
         pick = picked_object(x, y, view)
+        from chimerax.atomic import PickedResidue, PickedBond, PickedPseudobond
         if hasattr(pick, 'atom'):
-            from chimerax.std_commands import cofr
             xyz = pick.atom.scene_coord
-            cofr.cofr(self.session,pivot=xyz)
+        elif isinstance(pick, PickedResidue):
+            r = pick.residue
+            xyz = sum([a.scene_coord for a in r.atoms]) / r.num_atoms
+        elif isinstance(pick, PickedBond):
+            b = pick.bond
+            xyz = sum([a.scene_coord for a in b.atoms]) / 2
+        elif isinstance(pick, PickedPseudobond):
+            b = pick.pbond
+            xyz = sum([a.scene_coord for a in b.atoms]) / 2
+        else:
+            return
+        from chimerax.std_commands import cofr
+        cofr.cofr(self.session, pivot=xyz)
            
 class NullMouseMode(MouseMode):
     '''Used to assign no mode to a mouse button.'''
