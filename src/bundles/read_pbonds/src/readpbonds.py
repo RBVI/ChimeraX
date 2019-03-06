aa1c861c1b61886c80cd86bfa8b8fd65a7ff90c2
Tom Goddard <goddard@sonic.net>
2019-03-06 12:25:26 -0800

    Allowed pseudobond file to have comment setting radius.

--- src/bundles/read_pbonds/src/readpbonds.py
+++ src/bundles/read_pbonds/src/readpbonds.py
@@ -25,8 +25,17 @@ def read_pseudobond_file(session, stream, file_name, *args,
 
     from chimerax.atomic import AtomsArg
     for i, line in enumerate(lines):
-        if len(line.strip()) == 0 or line.lstrip().startswith('#'):
+        if len(line.strip()) == 0:
             continue
+        
+        if line.lstrip().startswith('#'):
+            keyval = line.lstrip()[1:].split('=')
+            if len(keyval) == 2:
+                k,v = keyval
+                if k.strip() == 'radius':
+                    radius = float(v)
+            continue
+
         fields = line.split()
         aspec1, aspec2 = fields[:2]
         a1, used, rest = AtomsArg.parse(aspec1, session)
@@ -53,12 +62,16 @@ def write_pseudobond_file(session, path, models=None, selected_only=False):
         models = atomic.all_pseudobond_groups(session)
 
     lines = []
+    radius = None
     from chimerax.atomic import PseudobondGroup
     for pbg in models:
         if isinstance(pbg, PseudobondGroup):
             for pb in pbg.pseudobonds:
                 if selected_only and not pb.selected:
                     continue
+                if pb.radius != radius:
+                    lines.append('# radius = %.5g' % pb.radius)
+                    radius = pb.radius
                 a1, a2 = pb.atoms
                 color = '#%02x%02x%02x%02x' % tuple(pb.color)
                 lines.append('%s\t%s\t%s' % (a1.atomspec, a2.atomspec, color))
