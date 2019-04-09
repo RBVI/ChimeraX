602ea2cce561d8a8f581f9df33eaf3cffa179edd
Eric Pettersen <pett@cgl.ucsf.edu>
2019-04-08 17:22:04 -0700

    'fast' tool option has to set number of models to 1 "by hand"  Please enter the commit message for your changes. Lines starting

--- src/bundles/modeller/src/tool.py
+++ src/bundles/modeller/src/tool.py
@@ -66,16 +66,17 @@ class ModellerLauncher(ToolInstance):
             "will be generated from the alignment (assuming multiple chains are associated).",
             attr_name="combine_templates", settings=settings))
         max_models = 1000
-        panel.add_option("Basic", IntOption("Number of models", settings.num_models, None,
+        self._num_models_option = IntOption("Number of models", settings.num_models, None,
             attr_name="num_models", settings=settings, min=1, max=max_models, balloon=
             "Number of model structures to generate.  Must no more than %d.\n"
-            "Warning: please consider the calculation time"))
+            "Warning: please consider the calculation time")
+        panel.add_option("Basic", self._num_models_option)
         key = "" if settings.license_key is None else settings.license_key
         panel.add_option("Basic", PasswordOption("Modeller license key", key, None, attr_name="license_key",
             settings=settings, balloon=
             "Your Modeller license key.  You can obtain a license key by registering at the Modeller web site"))
-        panel.add_option("Advanced", BooleanOption("Use fast/approximate mode", settings.fast, None,
-            attr_name="fast", settings=settings, balloon=
+        panel.add_option("Advanced", BooleanOption("Use fast/approximate mode", settings.fast,
+            self._fast_option_cb, attr_name="fast", settings=settings, balloon=
             "If enabled, use a fast approximate method to generate a single model.\n"
             "Typically use to get a rough idea what the model will look like or\n"
             "to check that the alignment is reasonable."))
@@ -96,7 +97,7 @@ class ModellerLauncher(ToolInstance):
             settings.water_preserve, None, attr_name="water_preserve", settings=settings, balloon=
             "If enabled, all water molecules in the template\n"
             "structure(s) will be included in the generated models."))
-        #TODO: more options
+        self._fast_option_cb()
         from chimerax.ui.widgets import Citation
         alignments_layout.addWidget(Citation(session,
             "A. Sali and T.L. Blundell.\n"
@@ -140,6 +141,15 @@ class ModellerLauncher(ToolInstance):
             repr(settings.water_preserve).lower()
             ))
         self.delete()
+
+    def _fast_option_cb(self, *args):
+        from .settings import get_settings
+        settings = get_settings(self.session)
+        if settings.fast:
+            settings.num_models = 1
+            self._num_models_option.enabled = False
+        else:
+            self._num_models_option.enabled = True
 
     def _list_selection_cb(self):
         layout = self.targets_layout
