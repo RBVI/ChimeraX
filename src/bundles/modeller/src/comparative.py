db208251aa16ddcbee9925b7367ac00933d60948
Eric Pettersen <pett@cgl.ucsf.edu>
2019-04-17 17:07:35 -0700

    multimer templating corrected, now need to single-chain case to work in new scheme; ensured that chain info provided by outside sources (looking at you mmCIF) winds up in correct chain order

--- src/bundles/modeller/src/comparative.py
+++ src/bundles/modeller/src/comparative.py
@@ -63,6 +63,151 @@ def model(session, targets, *, block=True, multichain=True, custom_script=None,
     """
 
     from chimerax.core.errors import LimitationError
+    if multichain:
+        # So, first find structure with most associated chains and least non-associated chains.
+        # That structure is used as the multimer template.  Chains from other structures are used
+        # as "standalone" templates -- each such chain will be on its own line.  Need to allow
+        # space on the left and right of the target sequence so that the largest chains can be
+        # accomodated.
+
+        # Find the structure we will use as the multimer template
+        by_structure = {}
+        chain_info = {}
+        for alignment, orig_target in targets:
+            # Copy the target sequence, changing name to conform to Modeller limitations
+            from .common import modeller_copy
+            target = modeller_copy(orig_target)
+            for chain, aseq in alignment.associations.items():
+                if len(chain.chain_id) > 1:
+                    raise LimitationError("Modeller cannot handle templates with multi-character chain IDs")
+                by_structure.setdefault(chain.structure, []).append(chain)
+                chain_info[chain] = (aseq, target)
+        max_matched = min_unmatched = None
+        for s, match_info in by_structure.items():
+            matched = len(match_info)
+            unmatched = s.num_chains - len(match_info)
+            if max_matched is None or matched > max_matched or (matched == max_matched
+                    and (unmatched < min_unmatched)):
+                multimer_template = s
+                max_matched = matched
+                min_unmatched = unmatched
+        mm_targets = []
+        mm_chains = []
+        match_chains = []
+        for chain in multimer_template.chains:
+            mm_chains.append(chain)
+            try:
+                aseq, target = chain_info[chain]
+            except KeyError:
+                mm_targets.append(None)
+            else:
+                mm_targets.append(target)
+                match_chains.append(chain)
+        # okay, now form single-chain lines for the other structure associations, that eventually will
+        # be handled column by column in exactly the same way as the non-multichain method.
+        single_template_lines = []
+        for chain, info in chain_info.items():
+            if chain.structure == multimer_template:
+                continue
+            aseq, target = info
+            for i, mm_target in enumerate(mm_targets):
+                if mm_target != target:
+                    continue
+                template_line = [None] * len(mm_targets)
+                template_line[i] = chain
+                single_template_lines.append(template_line)
+        # AFAIK, the multimer template chain sequences need to have complete PDB sequence, so may need
+        # to prefix and suffix he corresponding alignment sequence with characters for residues
+        # outside of the alignment sequence.  For other templates/targets, affix a corresponding number
+        # of '-' characters
+        prefixes, suffixes = find_affixes(mm_chains, chain_info)
+        target_strings = []
+        for prefix, suffix, mm_target in zip(prefixes, suffixes, mm_targets):
+            if mm_target is None:
+                target_strings.append('-')
+                continue
+            target_strings.append('-' * len(prefix) + mm_target.characters + '-' * len(suffix))
+        templates_strings = []
+        templates_info = []
+        mm_template_strings = []
+        for prefix, suffix, chain in zip(prefixes, suffixes, mm_chains):
+            try:
+                aseq, target = chain_info[chain]
+            except KeyError:
+                mm_template_strings.append('-')
+                continue
+            mm_template_strings.append(prefix + regularized_seq(aseq, chain).characters + suffix)
+        templates_strings.append(mm_template_strings)
+        templates_info.append(None)
+        for template_line in single_template_lines:
+            template_strings = []
+            for prefix, suffix, chain, target in zip(prefixes, suffixes, template_line, mm_targets):
+                if target is None:
+                    template_strings.append('-')
+                elif chain is None:
+                    template_strings.append('-' * (len(prefix) + len(target) + len(suffix)))
+                else:
+                    aseq, target = chain_info[chain]
+                    template_strings.append('-' * len(prefix) + regularized_seq(aseq, chain).characters
+                        + '-' * len(suffix))
+                    templates_info.append((chain, aseq.match_maps[chain]))
+            templates_strings.append(template_strings)
+        target_name = structure_save_name(multimer_template) + "-target"
+    else:
+        if len(targets) > 1:
+            raise LimitationError("Cannot have multiple targets(/alignments) unless creating multimeric model")
+        target_name = targets[0][1].name.replace('/', ' ')
+        #TODO
+
+    from .common import write_modeller_scripts
+    script_path, config_path, temp_dir = write_modeller_scripts(license_key, num_models, het_preserve,
+        water_preserve, hydrogens, fast, None, custom_script, temp_path, thorough_opt, dist_restraints)
+
+    input_file_map = []
+
+    # form the sequences to be written out as a PIR
+    from chimerax.atomic import Sequence
+    pir_target = Sequence(name=target_name)
+    pir_target.description = "sequence:%s:.:.:.:.::::" % pir_target.name
+    pir_target.characters = '/'.join(target_strings)
+    pir_seqs = [pir_target]
+
+    structures_to_save = set()
+    for strings, info in zip(templates_strings, templates_info):
+        if info is None:
+            # multimer template
+            pir_template = Sequence(name=structure_save_name(multimer_template))
+            pir_template.description = "structure:%s:FIRST:%s::::::" % (
+                pir_template.name, multimer_template.chains[0].chain_id)
+            structures_to_save.add(multimer_template)
+        else:
+            # single-chain template
+            chain, match_map = info
+            first_assoc_pos = 0
+            while first_assoc_pos not in match_map:
+                first_assoc_pos += 1
+            first_assoc_res = match_map[first_assoc_pos]
+            pir_template = Sequence(name=chain_save_name(chain))
+            pir_template.description = "structure:%s:%d%s:%s:+%d:%s::::" % (
+                structure_save_name(chain.structure), first_assoc_res.number, first_assoc_res.insertion_code,
+                chain.chain_id, len(match_map), chain.chain_id)
+            structures_to_save.add(chain.structure)
+        pir_template.characters = '/'.join(strings)
+        pir_seqs.append(pir_template)
+    import os.path
+    pir_file = os.path.join(temp_dir.name, "alignment.ali")
+    aln = session.alignments.new_alignment(pir_seqs, False, auto_associate=False)
+    aln.save(pir_file, format_name="pir")
+    session.alignments.destroy_alignment(aln)
+    input_file_map.append(("alignment.ali", "text_file", pir_file))
+
+    # write the namelist.dat file, target seq name on first line, templates on remaining lines
+    name_file = os.path.join(temp_dir.name, "namelist.dat")
+    input_file_map.append(("namelist.dat", "text_file", name_file))
+    with open(name_file, 'w') as f:
+        for template_seq in pir_seqs:
+            print(template_seq.name, file=f)
+    """
     template_info = []
     for alignment, orig_target in targets:
         if not alignment.associations:
@@ -98,12 +243,6 @@ def model(session, targets, *, block=True, multichain=True, custom_script=None,
                 if len(chain.chain_id) > 1:
                     raise LimitationError("Modeller cannot handle templates with multi-character chain IDs")
                 target_templates.append((regularized_seq(aseq, chain), chain, aseq.match_maps[chain]))
-
-    from .common import write_modeller_scripts
-    script_path, config_path, temp_dir = write_modeller_scripts(license_key, num_models, het_preserve,
-        water_preserve, hydrogens, fast, None, custom_script, temp_path, thorough_opt, dist_restraints)
-
-    input_file_map = []
 
     # collate the template info in series of strings that can be joined with '/'
     target_strings = []
@@ -157,7 +296,6 @@ def model(session, targets, *, block=True, multichain=True, custom_script=None,
             appending.append(line_to_add)
         for insertion in insertions[i+1:]:
             insertion.append(line_to_add)
-
     # form the sequences to be written out as a PIR
     pir_seqs = []
     from chimerax.atomic import Sequence
@@ -202,6 +340,7 @@ def model(session, targets, *, block=True, multichain=True, custom_script=None,
     aln.save(pir_file, format_name="pir")
     session.alignments.destroy_alignment(aln)
     input_file_map.append(("alignment.ali", "text_file", pir_file))
+    """
 
     config_name = os.path.basename(config_path)
     input_file_map.append((config_name, "text_file", config_path))
@@ -214,7 +353,7 @@ def model(session, targets, *, block=True, multichain=True, custom_script=None,
             os.mkdir(struct_dir, mode=0o755)
         except FileExistsError:
             pass
-    from chimerax.atomic.pdb import save_pdb
+    from chimerax.atomic.pdb import save_pdb, standard_polymeric_res_names as std_res_names
     for structure in structures_to_save:
         base_name = structure_save_name(structure) + '.pdb'
         pdb_file_name = os.path.join(struct_dir, base_name)
@@ -224,6 +363,8 @@ def model(session, targets, *, block=True, multichain=True, custom_script=None,
         save_pdb(session, pdb_file_name, models=[structure], polymeric_res_names=ATOM_res_names)
         delattr(structure, 'in_seq_hets')
 
+    from chimerax.atomic import Chains
+    match_chains = Chains(match_chains)
     if executable_location is None:
         if custom_script is not None:
             raise LimitationError("Custom Modeller scripts only supported when executing locally")
@@ -231,7 +372,7 @@ def model(session, targets, *, block=True, multichain=True, custom_script=None,
             raise LimitationError("Distance restraints only supported when executing locally")
         if thorough_opt:
             session.logger.warning("Thorough optimization only supported when executing locally")
-        job_runner = ModellerWebService(session, template_info, num_models,
+        job_runner = ModellerWebService(session, match_chains, num_models,
             pir_target.name, input_file_map, config_name, targets, show_gui)
     else:
         #TODO: job_runner = ModellerLocal(...)
@@ -272,6 +413,47 @@ def regularized_seq(aseq, chain):
     rseq.characters = "".join(seq_chars)
     return rseq
 
+def find_affixes(chains, chain_info):
+    from chimerax.atomic.pdb import standard_polymeric_res_names as std_res_names
+    in_seq_hets = []
+    prefixes = []
+    suffixes = []
+    from chimerax.atomic import Sequence
+    for chain in chains:
+        try:
+            aseq, target = chain_info[chain]
+        except KeyError:
+            prefixes.append('')
+            suffixes.append('')
+            continue
+        match_map = aseq.match_maps[chain]
+        prefix = ''
+        for r in chain.existing_residues:
+            if r in match_map:
+                break
+            if r.name not in std_res_names:
+                in_seq_hets.append(r.name)
+                prefix += '.'
+            else:
+                prefix += Sequence.rname3to1(r.name)
+        prefixes.append(prefix)
+
+        suffix = ''
+        for r in reversed(chain.existing_residues):
+            if r in match_map:
+                break
+            if r.name not in std_res_names:
+                in_seq_hets.append(r.name)
+                suffix = '.' + suffix
+            else:
+                suffix = Sequence.rname3to1(r.name) + suffix
+        suffixes.append(suffix)
+    s = chain.structure
+    het_set = getattr(s, 'in_seq_hets', set())
+    het_set.update(in_seq_hets)
+    s.in_seq_hets = het_set
+    return prefixes, suffixes
+
 def structure_save_name(s):
     return s.name.replace(':', '_').replace(' ', '_') + "_" + s.id_string
 
@@ -281,10 +463,10 @@ def chain_save_name(chain):
 from .common import RunModeller
 class ModellerWebService(RunModeller):
 
-    def __init__(self, session, template_info, num_models, target_seq_name, input_file_map, config_name,
+    def __init__(self, session, match_chains, num_models, target_seq_name, input_file_map, config_name,
             targets, show_gui):
 
-        super().__init__(session, template_info, num_models, target_seq_name, targets, show_gui)
+        super().__init__(session, match_chains, num_models, target_seq_name, targets, show_gui)
         self.input_file_map = input_file_map
         self.config_name = config_name
 
@@ -316,7 +498,6 @@ class ModellerJob(OpalJob):
     def __init__(self, session, caller, command, input_file_map, block):
         super().__init__(session)
         self.caller = caller
-        print("starting job with block =", block)
         self.start(self.OPAL_SERVICE, command, input_file_map=input_file_map, blocking=block)
 
     def monitor(self):
