452b8034f116fd48538ee628874b02b2ea30a4e9
Eric Pettersen <pett@cgl.ucsf.edu>
2019-05-23 11:29:52 -0700

    Defined abstract RotamerLibrary class for use by manager/provider API

---
+++ src/bundles/rotamer_lib_mgr/src/rot_lib.py
@@ -0,0 +1,107 @@
+# vim: set expandtab shiftwidth=4 softtabstop=4:
+
+# === UCSF ChimeraX Copyright ===
+# Copyright 2016 Regents of the University of California.
+# All rights reserved.  This software provided pursuant to a
+# license agreement containing restrictions on its disclosure,
+# duplication and use.  For details see:
+# http://www.rbvi.ucsf.edu/chimerax/docs/licensing.html
+# This notice must be embedded in or attached to all copies,
+# including partial copies, of the software or any revisions
+# or derivations thereof.
+# === UCSF ChimeraX Copyright ===
+
+from abc import abstractmethod
+
+class NoResidueRotamersError(ValueError):
+    pass
+
+class UnsupportedResNameError(NoResidueRotamersError):
+    pass
+
+class RotamersParams:
+    def __init__(self, p, chis):
+        """ 'p' is the probability of this rotamer.  'chis' is a list of the chi angles. """
+        self.p = p
+        self.chis = chis
+
+class RotamerLibrary:
+    """Provide all the information needed to display/list and use a rotamer library.
+
+       Needed methods are described in their doc strings, and methods that must be
+       implemented by subclasses are marked with the 'abstractmethod' decorator.
+
+       If possible, reading the actual rotamer library itself should be done in a
+       "lazy" (i.e. on demand) manner if possible to minimize startup time.
+    """
+
+    @property
+    @abstractmethod
+    def display_name(self):
+        """Short name to display in a list of libraries, e.g. "Dunbrack 2010" or "Dynameomics".
+           Should be the same as the 'name' attribute used in your Provider tag in bundle_info.xml
+        """
+        pass
+
+    @property
+    def description(self)
+        """A somewhat longer decription than 'display_name' (though typically still one line)
+        to show in interfaces when the library has been selected, e.g.:
+
+        Dunbrack 2010 backbone-dependent rotamer library -- 5% stepdown
+        """
+        return self.display_name
+
+    @property
+    def citation(self):
+        """If your library has a reference to cite, the text of the citation.  Used as the 'cite'
+           argument to the chimerax.ui.widgets.Citation constructor.  Example:
+
+           Shapovaeov, M.S., and Dunbrack, R.L., Jr. (2011)
+           A Smoothed Backbone-Dependent Rotamer Library for Proteins
+               Derived from Adaptive Kernel Density Estimates and Regressions
+           Structure, 19, 844-858.
+        """
+        return None
+
+    @property
+    def cite_name(self):
+        """In the preamble to the citation, cite_name replaces XXX in "Publications using XXX rotamers
+           should cite:"
+        """
+        return self.display_name
+
+    @property
+    def cite_pubmed_id(self):
+        """The (integer) PubMed ID corresponding to your citation"""
+        return None
+
+    @property
+    def residue_names(self):
+        """The residue names that this rotamer library provides rotamers for.  Typically just the
+           18 standard amino acids that actually have side chains, but some libraries provide rotamers
+           for certain protonation states (e.g. CYH for non-disulphide cysteine) or conformers
+           (e.g. CPR for cis-proline).
+        """
+            return ["ARG", "ASN", "ASP", "CYS", "GLN", "GLU", "HIS", "ILE", "LEU", "LYS", "MET",
+                "PHE", "PRO", "SER", "THR", "TRP", "TYR", "VAL"]
+
+    @property
+    def res_name_mapping(self):
+        """For libraries that have non-standard residue names that correspond to certain states of
+           standard residues (see the residue_names method), this dictionary maps the non-standard
+           name to the corresponding standard name.
+        """
+            return {}
+
+    @abstractmethod
+    def rotamer_params(self, res_name, phi, psi):
+        """Return a list of RotamerParams instances corresponding to the residue name 'res_name' and
+           the backbone angle 'phi' and 'psi'.  Backbone-independent libraries will ignore phi and psi.
+           Note that phi or psi can be None for chain-terminal residues.  Backbone-dependent libraries
+           will have to use some fallback procedur for generating parameters in those cases, or throw
+           NoResidueRotamersError.  If 'res_name' does not correspond to a name supported by the library,
+           throw UnsupportedResNameError.
+        """
+        pass
+
