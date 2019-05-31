e4fa6211911f7434cbd7e2210ccd7b746e0ed4e1
Greg Couch <gregc@cgl.ucsf.edu>
2019-05-31 15:47:31 -0700

    Comment out code that causes constant redrawing, #1983.

--- src/bundles/atomic/atomic_cpp/molc.cpp
+++ src/bundles/atomic/atomic_cpp/molc.cpp
@@ -1290,6 +1290,8 @@ extern "C" EXPORT void atom_update_ribbon_visibility(void *atoms, size_t n)
                         hide = false;
                         break;
                     }
+#if 0
+                // causes constant redrawing #1983
                 if (hide) {
                     for (auto pb : atom->pseudobonds())
                         if (pb->shown()) {
@@ -1297,6 +1299,7 @@ extern "C" EXPORT void atom_update_ribbon_visibility(void *atoms, size_t n)
                             break;
                         }
                 }
+#endif
             }
             if (hide)
                 atom->set_hide_bits(Atom::HIDE_RIBBON);
