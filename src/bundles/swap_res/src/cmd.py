dd1ca0d1d3e96a7c0e777200b7f891902cdd1dea
Eric Pettersen <pett@cgl.ucsf.edu>
2019-06-24 15:18:43 -0700

    further progress on porting swapaa

--- src/bundles/swap_res/src/cmd.py
+++ src/bundles/swap_res/src/cmd.py
@@ -12,9 +12,9 @@
 # === UCSF ChimeraX Copyright ===
 
 default_criteria = "dhcp"
-def swapaa(session, residues, res_type, *, hbond_allowance=None, score_method="num",
-    threshold=None, criteria=default_crieria, density=None, angle_slop=None, dist_slop=None,
-    ignore_other_models=False, lib=None, log=True, preserve=None, relax=True, retain=False):
+def swap_aa(session, residues, res_type, *, angle_slop=None, bfactor=None, criteria=default_crieria,
+    density=None, dist_slop=None, hbond_allowance=None, ignore_other_models=False, lib=None, log=True,
+    preserve=None, relax=True, retain=False, score_method="num", threshold=None):
     '''
     Command to swap amino acid side chains
     '''
@@ -32,7 +32,7 @@ def swapaa(session, residues, res_type, *, hbond_allowance=None, score_method="n
 
     from . import swap_res
     try:
-        swap_res.swapaa(session, residues, res_type, clash_hbond_allowance=hbond_allowance,
+        swap_res.swap_aa(session, residues, res_type, bfactor=bfactor, clash_hbond_allowance=hbond_allowance,
             clash_score_method=score_method, clash_threshold=threshold,
             criteria=criteria, density=density, hbond_angle_slop=angle_slop,
             hbond_dist_slop=dist_slop, ignore_other_models=ignore_other_models, lib=lib, log=log,
@@ -48,20 +48,21 @@ def register_command(logger):
     desc = CmdDesc(
         required = [('residues', ResiduesArg), ('res_type', StringArg)],
         keyword = [
-            ('hbond_allowance', FloatArg),
-            ('score_method', EnumOf(('sum', 'num')),
-            ('threshold', FloatArg),
+            ('angle_slop', FloatArg),
+            ('bfactor', FloatArg),
             ('criteria', Or(IntArg, StringArg))),
             ('density', MapArg),
-            ('angle_slop', FloatArg),
             ('dist_slop', FloatArg),
+            ('hbond_allowance', FloatArg),
             ('ignore_other_models', BoolArg),
             ('lib', DynamicEnum(logger.session.rotamers.library_names),
             ('log', BoolArg),
             ('preserve', NonNegativeFloatArg),
-            ('retain', BoolArg),
             ('relax', BoolArg),
+            ('retain', BoolArg),
+            ('score_method', EnumOf(('sum', 'num')),
+            ('threshold', FloatArg),
         ],
         synopsis = 'Swap amino acid side chain(s)'
     )
-    register('swapaa', desc, swapaa, logger=logger)
+    register('swap_aa', desc, swapaa, logger=logger)
--- src/bundles/swap_res/src/swap_res.py
+++ src/bundles/swap_res/src/swap_res.py
@@ -11,18 +11,11 @@
 # or derivations thereof.
 # === UCSF ChimeraX Copyright ===
 
-class SwapResError(ValueError):
-    pass
-class BackboneError(SwapResError):
-    pass
-class TemplateError(SwapResError):
-    pass
-
 from chimerax.core.errors import LimitationError, UserError
 from chimerax.atomic.rotamers import NoResidueRotamersError, RotamerLibrary, NoRotamerLibraryError
 
 from .cmd import default_criteria
-def swapaa(session, residues, res_type, *, clash_hbond_allowance=None, clash_score_method="sum",
+def swap_aa(session, residues, res_type, *, bfactor=None, clash_hbond_allowance=None, clash_score_method="sum",
         clash_threshold=None, criteria=default_criteria, density=None, hbond_angle_slop=None,
         hbond_dist_slop=None, hbond_relax=True, ignore_other_models=False, lib="Dunbrack", log=True,
         preserve=None, retain=False):
@@ -35,6 +28,7 @@ def swapaa(session, residues, res_type, *, clash_hbond_allowance=None, clash_sco
         else:
             r_type = res_type.upper()
         if criteria == "manual":
+            raise LimitationError("swapaa 'manual' criteria not implemented yet")
             #TODO
             '''
             for library in libraries:
@@ -56,9 +50,9 @@ def swapaa(session, residues, res_type, *, clash_hbond_allowance=None, clash_sco
             if log:
                 replyobj.info("Swapping %s to %s\n" % (res, r_type))
             try:
-                swap(res, r_type, bfactor=None)
-            except (BackboneError, TemplateError), v:
-                raise MidasError(str(v))
+                template_swap_res(res, r_type, bfactor=bfactor)
+            except TemplateSwapError as e:
+                raise UserError(str(e))
             continue
         except NoRotamerLibraryError:
             raise MidasError("No rotamer library named '%s'" % lib)
@@ -321,6 +315,227 @@ def get_rotamers(session, res, phi=None, psi=None, cis=False, res_type=None, lib
                     todo.append(rbonded)
             done.add(a)
     return structs
+
+def template_swap_res(res, res_type, *, preserve=False, bfactor=None):
+    """change 'res' into type 'res_type'"""
+
+    #TODO
+    fixed, buds, start, end = get_res_info(res)
+    
+    if res_type == "HIS":
+        res_type = "HIP"
+    if res_type in ["A", "C", "G", "T"] and res.type in ["DA", "DC", "DT", "DG"]:
+        res_type = "D" + res_type
+    tmplRes = chimera.restmplFindResidue(res_type, start, end)
+    if not tmplRes:
+        raise TemplateError("No connectivity template for residue '%s'" % res_type)
+    # sanity check:  does the template have the bud atoms?
+    for bud in buds:
+        if not tmplRes.atomsMap.has_key(bud):
+            raise TemplateError("New residue type (%s) not compatible with"
+                " starting residue type (%s)" % (res_type, res.type))
+        
+    # if bfactor not specified, find highest bfactor in molecule
+    # and use that for swapped-in atoms
+    if bfactor is False:
+        bfactor = None
+    if bfactor is True:
+        for a in res.molecule.atoms:
+            try:
+                if bfactor is True or a.bfactor > bfactor:
+                    bfactor = a.bfactor
+            except AttributeError:
+                pass
+
+    if preserve:
+        if "CA" in fixed and res_type not in ['GLY', 'ALA']:
+            raise TemplateSwapError("'preserve' keyword not yet implemented for amino acids")
+        atomsMap = res.atomsMap
+        try:
+            a1 = atomsMap["O4'"][0]
+            a2 = atomsMap["C1'"][0]
+        except KeyError:
+            preservePos = None
+        else:
+            dihedNames = {
+                "N9": ["C4", "C8"],
+                "N1": ["C2", "C6"]
+            }
+            try:
+                a3 = atomsMap["N9"][0]
+                if a2 not in a3.bondsMap:
+                    raise KeyError()
+                preservePos = a3.coord()
+            except KeyError:
+                try:
+                    a3 = atomsMap["N1"][0]
+                    if a2 not in a3.bondsMap:
+                        raise KeyError()
+                    preservePos = a3.coord()
+                except KeyError:
+                    preservePos = None
+        if preservePos:
+            from chimera import dihedral
+            p1, p2, p3 = [a.coord() for a in (a1, a2, a3)]
+            preservedPos = False
+            try:
+                prevName, altName = dihedNames[a3.name]
+                a4 = atomsMap[prevName][0]
+                if a3 not in a4.bondsMap:
+                    raise KeyError()
+                p4 = a4.coord()
+                preserveDihed = dihedral(p1, p2, p3, p4)
+            except KeyError:
+                preserveDihed = None
+        else:
+            preserveDihed = None
+
+    # prune non-backbone atoms
+    for a in res.oslChildren():
+        if a.name not in fixed:
+            a.molecule.deleteAtom(a)
+
+    # add new sidechain
+    newAtoms = []
+    xf = None
+    while len(buds) > 0:
+        bud = buds.pop()
+        tmplBud = tmplRes.atomsMap[bud]
+        resBud = res.atomsMap[bud][0]
+
+        try:
+            info = typeInfo[tmplBud.idatmType]
+            geom = info.geometry
+            subs = info.substituents
+        except KeyError:
+            print tmplBud.idatmType
+            raise AssertionError, "Can't determine atom type" \
+                " information for atom %s of residue %s" % (
+                bud, res.oslIdent())
+
+        # use coord() rather than xformCoord():  we want to set
+        # the new atom's coord(), to which the proper xform will
+        # then be applied
+        for a, b in tmplBud.bondsMap.items():
+            if a.element.number == 1:
+                # don't add hydrogens
+                continue
+            if res.atomsMap.has_key(a.name):
+                resBonder = res.atomsMap[a.name][0]
+                if not resBud.bondsMap.has_key(resBonder):
+                    addBond(a, resBonder)
+                continue
+
+            newAtom = None
+            numBonded = len(resBud.bonds)
+            if numBonded >= subs:
+                raise AssertionError, \
+                    "Too many atoms bonded to %s of" \
+                    " residue %s" % (bud, res.oslIdent())
+            if numBonded == 0:
+                raise AssertionError, \
+                    "Atom %s of residue %s has no" \
+                    " neighbors after pruning?!?" % (
+                    bud, res.oslIdent())
+            # since fused ring systems may have distorted bond angles,
+            # always use dihedral placement
+            real1 = resBud.neighbors[0]
+            kw = {}
+            if preserve:
+                if preservePos and not preservedPos:
+                    kw['pos'] = preservePos
+                    preservedPos = True
+                    preservedName = a.name
+                elif preserveDihed is not None:
+                    prevName, altName = dihedNames[preservedName]
+                    if a.name == prevName:
+                        kw['dihed'] = preserveDihed
+                    elif a.name == altName:
+                        kw['dihed'] = preserveDihed + 180.0
+            if not kw and xf is not None:
+                kw['pos'] = xf.apply(a.coord())
+
+            newAtom = formDihedral(resBud, real1, tmplRes, a, b, **kw)
+            newAtom.drawMode = resBud.drawMode
+            if bfactor is not None and bfactor is not True:
+                newAtom.bfactor = bfactor
+            newAtoms.append(newAtom)
+
+            # TODO: need to iterate over coordSets
+            for bonded in a.bondsMap.keys():
+                if not res.atomsMap.has_key(bonded.name):
+                    continue
+                addBond(newAtom, res.atomsMap[bonded.name][0])
+            buds.append(newAtom.name)
+
+        # once we've placed 3 side chain atoms, we use superpositioning to
+        # place the remainder of the side chain, since dihedrals will
+        # likely distort ring closures if 'preserve' is true
+        if buds and not xf and len(newAtoms) >= 3:
+            placedPositions = []
+            tmplPositions = []
+            for na in newAtoms:
+                placedPositions.append(na.coord())
+                tmplPositions.append(tmplRes.atomsMap[na.name].coord())
+            import numpy
+            xf = matchPositions(numpy.array(placedPositions),
+                numpy.array(tmplPositions))[0]
+
+    from BuildStructure import changeResidueType
+    changeResidueType(res, res_type)
+
+class TemplateSwapError(ValueError):
+    pass
+class BackboneError(TemplateSwapError):
+    pass
+class TemplateError(TemplateSwapError):
+    pass
+
+amino_info = (("N", "CA", "C", "O", "OXT"), ("CA", "C", ("O", "OXT")))
+nucleic_info = (("O1P", "OP1", "O2P", "OP2", "O3P", "OP3", "P", "O5'", "C5'",
+    "C4'", "C3'", "O3'", "C2'", "O2'", "C1'", "O4'"), ("C1'", "O4'", "C4'"))
+def get_res_info(res):
+    """return a list of the fixed atoms of the residue, a list of
+       the fixed atoms that non-fixed atoms attach to, and whether
+       this residue is the start and/or end of a chain"""
+    
+    errmsg =  "Cannot identify backbone of residue %s)" % res
+    backbone = []
+    if res.find_atom("N"):
+        # putative amino acid
+        basic_info = amino_info
+        start = len([nb for nb in res.find_atom("N").neighbors if nb.element.number > 1]) != 2
+        end = res.find_atom("OXT") is not None
+    elif res.find_atom("O3'"):
+    #TODO
+        # putative nucleic acid
+        basic_info = nucleic_info
+        start = not res.atomsMap.has_key("P")
+        end = len(filter(lambda a: a.element.name == "P",
+                res.atomsMap["O3'"][0].neighbors)) == 0
+        if end and res.atomsMap.has_key("O2'"):
+            end = len(filter(lambda a: a.element.name == "P",
+                res.atomsMap["O2'"][0].neighbors)) == 0
+    else:
+        raise BackboneError(errmsg)
+    fixed, bud = basic_info
+
+    # must have the bud atoms present, (and resolve O/OXT ambiguity)
+    finalBud = []
+    for atName in bud:
+        if isinstance(atName, tuple):
+            for ambig in atName:
+                if res.atomsMap.has_key(ambig):
+                    finalBud.append(ambig)
+                    break
+            else:
+                raise BackboneError(errmsg)
+            continue
+        if res.atomsMap.has_key(atName):
+            finalBud.append(atName)
+        else:
+            raise BackboneError(errmsg)
+    return (list(fixed), finalBud, start, end)
 
 def _len_angle(new, n1, n2, template, bond_cache, angle_cache):
     from chimerax.core.geometry import distance, angle
@@ -754,5 +969,41 @@ def nukeGroup(groupName):
     group = mgr.findPseudoBondGroup(groupName)
     if group:
         chimera.openModels.close([group])
-'''
 
+from math import pi, cos, sin
+import chimera
+from chimera import angle, dihedral, cross, Coord
+from chimera.molEdit import addAtom, addDihedralAtom, addBond
+from chimera.idatm import tetrahedral, planar, linear, single, typeInfo
+from chimera.bondGeom import bondPositions
+from chimera.match import matchPositions
+
+def formDihedral(resBud, real1, tmplRes, a, b, pos=None, dihed=None):
+    res = resBud.residue
+    if pos:
+        return addAtom(a.name, a.element, res, pos, infoFrom=real1)
+    # use neighbors of resBud rather than real1 to avoid clashes with
+    # other resBud neighbors in case bond to real1 neighbor freely rotates
+    inres = [nb for nb in resBud.neighbors if nb != real1 and nb.residue == res]
+    if len(inres) < 1:
+        inres = [x for x in res.atoms if x not in [resBud, real1]]
+    if real1.residue != res or len(inres) < 1:
+        raise AssertionError, "Can't form in-residue dihedral for" \
+                " %s of residue %s" % (resBud, res.oslIdent())
+    if dihed:
+        real1 = res.atomsMap["C1'"][0]
+        real2 = res.atomsMap["O4'"][0]
+    else:
+        real2 = inres[0]
+    xyz0, xyz1, xyz2 = map(lambda a, tr=tmplRes:
+            tr.atomsMap[a.name].coord(), (resBud, real1, real2))
+
+    xyz = a.coord()
+    blen = b.length()
+    ang = angle(xyz, xyz0, xyz1)
+    if dihed is None:
+        dihed = dihedral(xyz, xyz0, xyz1, xyz2)
+    return addDihedralAtom(a.name, a.element, resBud,
+                        real1, real2, blen, ang, dihed, infoFrom=real1)
+
+'''
