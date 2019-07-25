d3ccfee756e139719fe551098b90e0358b04d5a4
gregcouch <gregcouch@gmail.com>
2019-07-25 11:10:10 -0700

    Generalize to NotABug instead of UserError.

--- src/apps/ChimeraX/ChimeraX_main.py
+++ src/apps/ChimeraX/ChimeraX_main.py
@@ -728,7 +728,7 @@ def init(argv, event_loop=True):
         try:
             from chimerax.core.commands import quote_if_necessary
             commands.run(sess, 'open %s' % quote_if_necessary(arg))
-        except (IOError, errors.UserError) as e:
+        except (IOError, errors.NotABug) as e:
             sess.logger.error(str(e))
             return os.EX_SOFTWARE
         except Exception as e:
