990bb32734b46fc6a84dc6ed20fbf8f1a2510825
Eric Pettersen <pett@cgl.ucsf.edu>
2019-08-23 15:13:52 -0700

    have missing-structure pseudobonds also do the chain-forming/coalescing check that bonds do

--- src/bundles/atomic/atomic_cpp/atomstruct_cpp/Bond.cpp
+++ src/bundles/atomic/atomic_cpp/atomstruct_cpp/Bond.cpp
@@ -35,37 +35,7 @@ Bond::Bond(Structure* as, Atom* a1, Atom* a2): UniqueConnection(a1, a2)
     if (a1->structure() != as || a2->structure() != as)
         throw std::invalid_argument("Cannot bond atoms in different molecules");
 
-    // if make_chains() has not been called and we're not still constructing the
-    // structure (no Python instance yet), then a new Bond may form new chains...
-    if (a1->structure()->_chains != nullptr && a1->structure()->py_instance(false) != Py_None) {
-        auto start_a = polymeric_start_atom();
-        if (start_a != nullptr) {
-            auto other_a = other_atom(start_a);
-            auto start_r = start_a->residue();
-            auto other_r = other_a->residue();
-            if (start_r->chain() == nullptr) {
-                if (other_r->chain() == nullptr) {
-                    // form a new chain based on start residue's chain ID
-                    auto chain = start_r->structure()->_new_chain(start_r->chain_id(),
-                        Sequence::rname_polymer_type(start_r->name()));
-                    chain->push_back(start_r);
-                    chain->push_back(other_r);
-                } else {
-                    // incorporate start_r into other_r's chain
-                    other_r->chain()->push_front(start_r);
-                }
-            } else {
-                if (other_r->chain() == nullptr) {
-                    // incorporate other_r into start_r's chain
-                    start_r->chain()->push_back(other_r);
-                } else if (start_r->chain() != other_r->chain()) {
-                    // merge other_r's chain into start_r's chain
-                    // and demote other_r's chain to a plain sequence
-                    *start_r->chain() += *other_r->chain();
-                }
-            }
-        }
-    }
+    as->_form_chain_check(a1, a2, this);
     a1->structure()->_structure_cats_dirty = true;
     change_tracker()->add_created(a1->structure(), this);
 }
