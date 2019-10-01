bc990e57efcf321c4e25a5770dc7b1b2fbef804b
Eric Pettersen <pett@cgl.ucsf.edu>
2019-09-30 19:00:22 -0700

    ported the "easy" part of 2D arrows command -- now need to actually implement drawing

--- src/bundles/label/src/__init__.py
+++ src/bundles/label/src/__init__.py
@@ -24,9 +24,10 @@ class _LabelBundle(BundleAPI):
     @staticmethod
     def register_command(command_name, logger):
         # 'register_command' is lazily called when the command is referenced
-        from . import label2d, label3d
+        from . import label2d, label3d, arrows
         label2d.register_label_command(logger)
         label3d.register_label_command(logger)
+        arrows.register_arrow_command(logger)
 
     @staticmethod
     def get_class(class_name):
