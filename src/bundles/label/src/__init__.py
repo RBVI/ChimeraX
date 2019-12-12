e3e0d36a5ec4179660da6205df05b592aa8a0152
Tom Goddard <goddard@Toms-MBP.lan>
2019-12-11 16:32:11 -0800

    Made 2d label models save directly in sessions.

--- src/bundles/label/src/__init__.py
+++ src/bundles/label/src/__init__.py
@@ -44,6 +44,9 @@ class _LabelBundle(BundleAPI):
         elif class_name == 'Labels':
             from .label2d import Labels
             return Labels
+        elif class_name == 'LabelModel':
+            from .label2d import LabelModel
+            return LabelModel
         return None
 
 bundle_api = _LabelBundle()
