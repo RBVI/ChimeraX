c485ee157b320a561f09c66348b1bfd204110b78
Eric Pettersen <pett@cgl.ucsf.edu>
2019-12-16 12:59:36 -0800

    some effort to move attr code from atomic to core, but may be abandoning the effort due to the intractible problem of generating a command for generic coloring/selecting of attrs

--- src/core/attributes.py
+++ src/core/attributes.py
@@ -12,26 +12,231 @@
 # === UCSF ChimeraX Copyright ===
 
 """
-Attributes: support for attribute discovery, and custom attibute session saving
-===============================================================================
+Attributes: support for custom attibute saving in sessions
+==========================================================
 
-This module provides two main functionalities:
-
-1) Attribute discovery -- making the attributes of classes available for inspection in user interfaces
-(e.g. selection inspector; show attribute tool).
-
-2) Custom attribute preservation -- allow classes that can have arbitrary new attributes defined to preserve
-those attributes in session files.
-
-Both capabilities are "on request": a bundle can submit information for its classes that it wants to be
-inspectable, and/or ask that custom attributes be preservable for some or all of its classes.
+This module allows classes that can have arbitrary new attributes defined to preserve those attributes
+in session files.  Such attributes can have a default value, and if provided then instances of the class
+where the attribute value has not been explicitly set will return the default value instead of raising
+AttributeError.
 """
 
-# Attribute discovery:
-# provider is per-class
-# bundle provides a list of (attr_name, attr_type, none_okay, option) for provided class
-# The attr_type/option can be None, in which case some interfaces will skip the attr
-
 # Custom attrs:
-# At class definition, need to call a method of this module to add in the registration machinery,
-# which will also have to possibly add to any attribute-discovery info and fire triggers.
+# At class definition, need to call a method of the manager to add in the registration machinery.
+
+
+from .state import State, StateManager
+
+# something that can't be a default value, yet can be saved in sessions...
+class _NoDefault(State):
+
+    def take_snapshot(self, session, flags):
+        return {'version':1}
+
+    @staticmethod
+    def restore_snapshot(session, data):
+        return NO_DEFAULT
+NO_DEFAULT = _NoDefault()
+
+# methods that the manager will insert into the managed classes (inheritance won't work)
+def _getattr_(self, attr_name, look_in_class=None):
+    if look_in_class is None:
+        look_in_class = self.__class__
+    try:
+        return look_in_class._attr_registration.get_attr(attr_name)
+    except AttributeError:
+        for base in look_in_class.__bases__:
+            if hasattr(base, "__getattr__"):
+                return base.__getattr__(self, attr_name, look_in_class=base)
+        else:
+            raise
+
+@classmethod
+def register_attr(cls, session, attr_name, registerer, *, default_value=NO_DEFAULT, attr_type=None,
+        can_return_none=False):
+    cls._attr_registration.register(session, attr_name, registerer, default_value, (attr_type, can_return_none))
+
+# used within the class to hold the registration info
+class AttrRegistration:
+    def __init__(self, class_):
+        self.reg_attr_info = {}
+        self.class_ = class_
+        self._session_attrs = {}
+        self._ses_attr_counts = {}
+
+    def get_attr(self, attr_name):
+        try:
+            registrant, default_value, type_info = self.reg_attr_info[attr_name]
+        except KeyError:
+            if attr_name in dir(self.class_):
+                raise AttributeError("Execution of '%s' object's '%s' property raised AttributeError" % (self.class_.__name__, attr_name)) from None
+            else:
+                raise AttributeError("'%s' object has no attribute '%s'" % (self.class_.__name__, attr_name)) from None
+        if default_value == NO_DEFAULT:
+            raise AttributeError("'%s' object has no attribute '%s'" % (self.class_.__name__, attr_name))
+        return default_value
+
+    def register(self, session, attr_name, registrant, default_value, type_info):
+        if attr_name in self.reg_attr_info:
+            prev_registrant, prev_default, prev_type_info = self.reg_attr_info[attr_name]
+            if prev_default == default_value and prev_type_info == type_info:
+                return
+            raise ValueError("Registration of attr '%s' with %s by %s conflicts with previous"
+                " registration by %s" % (attr_name, self.class_.__name__, registrant, prev_registrant))
+        self.reg_attr_info[attr_name] = (registrant, default_value, type_info)
+        session_attrs = self._session_attrs.setdefault(session, set())
+        if attr_name not in session_attrs:
+            session_attrs.add(attr_name)
+
+    # session functions; called from manager, not directly from session-saving mechanism,
+    # so API varies from that for State class
+    def reset_state(self, session):
+        # don't nuke existing attributes, since tools that registered attributes with default
+        # values may still depend on those attributes working
+        return
+
+    def take_snapshot(self, session, flags):
+        ses_reg_attr_info = {}
+        attr_names = self._session_attrs.get(session, [])
+        for attr_name in attr_names:
+            ses_reg_attr_info[attr_name] = self.reg_attr_info[attr_name]
+        data = {'reg_attr_info': ses_reg_attr_info, 'version': 2}
+        return data
+
+    def restore_session_data(self, session, data):
+        if data.get('version', 1) == 1:
+            reg_attr_info = {}
+            for attr_name, reg_info in data['reg_attr_info'].items():
+                registrant, default_value, attr_type = reg_info
+                reg_attr_info[attr_name] = (registrant, default_value, (attr_type, False))
+        else:
+            reg_attr_info = data['reg_attr_info']
+        for attr_name, reg_info in reg_attr_info.items():
+            self.register(session, attr_name, *reg_info)
+
+"""
+# used in session so that registered attributes get saved/restored
+from . import Atom, AtomicStructure, Bond, CoordSet, Pseudobond, \
+    PseudobondGroup, PseudobondManager, Residue, Sequence, Structure, StructureSeq
+
+# custom Chain attrs should be registered in the StructureSeq base class
+registerable_classes = [ Atom, AtomicStructure, Bond, CoordSet, Pseudobond,
+    PseudobondGroup, PseudobondManager, Residue, Sequence, Structure, StructureSeq ]
+"""
+
+# since attribute registration occurs at class-definition time, the manager may not
+# exist yet, so provide for that
+_mgr = None
+_pending_classes = {}
+def register_class(reg_class, instances_func, builtin_attr_info={}):
+    """'reg_class' is the class that wants to be able to register custom atrtributes.
+    'instances_func' is a function (taking 'session' as its only argument) that returns
+        all existing Python instances of the class.  If an instance exists only in the
+        C++ layer, the function should not create a Python instance.
+    'builtin_attr_info' should only be provided by classes that want to automatically
+        show up in some tools, such as Render By Attribute, and provides information about
+        the class's builtin (i.e non-custom) attributes.  The dictionary value should
+        be a mapping from attribute name to a list of possible types that attribute can
+        return (including None).
+    """
+    if hasattr(reg_class, '_attr_registration'):
+        return
+    reg_class._attr_registration = AttrRegistration(reg_class)
+    reg_class.__getattr__ = _getattr_
+    reg_class.register_attr = register_attr
+    if mgr:
+        mgr.class_info[reg_class] = (instances_func, builtin_attr_info)
+    else:
+        _pending_classes[reg_class] = (instances_func, builtin_attr_info)
+
+class RegAttrManager(StateManager):
+
+    def __init__(self):
+        #TODO
+        for reg_class in registerable_classes:
+            if not hasattr(reg_class, '_attr_registration'):
+                reg_class._attr_registration = AttrRegistration(reg_class)
+                reg_class.__getattr__ = _getattr_
+                reg_class.register_attr = register_attr
+        self._class_builtin_types = {}
+
+    def attributes_returning(self, class_obj, return_types, *, none_okay=False):
+        """Return list of attribute names for class 'class_obj' whose return types
+           are the same or a subset of 'return_types'.  If 'none_okay' is True,
+           then it is okay for the attribute to also possibly be None.
+        """
+        # builtin properties
+        if class_obj not in self._class_builtin_types:
+            self._class_builtin_types[class_obj] = builtin_types = {}
+            for attr_name, attr_return_types in getattr(class_obj, '_cython_property_return_info', []):
+                builtin_types[attr_name] = attr_return_types
+        matching_attr_names = []
+        for attr_name, types in self._class_builtin_types[class_obj].items():
+            matched_one = False
+            for t in types:
+                if t in return_types:
+                    matched_one = True
+                else:
+                    if not (t is None and none_okay):
+                        break
+            else:
+                if matched_one:
+                    matching_attr_names.append(attr_name)
+
+        # registered attributes
+        if hasattr(class_obj, '_attr_registration'):
+            for attr_name, attr_info in class_obj._attr_registration.reg_attr_info.items():
+                registrant, default_value, type_info = attr_info
+                attr_type, can_return_none = type_info
+                if attr_type not in return_types:
+                    # this will also catch attr_type of None
+                    continue
+                if can_return_none and not none_okay:
+                    continue
+                matching_attr_names.append(attr_name)
+        return matching_attr_names
+
+    # session functions; there is one manager per session, and is only in charge of
+    # remembering registrations from its session (atomic instances save their own
+    # attrs)
+    def reset_state(self, session):
+        for reg_class in registerable_classes:
+            reg_class._attr_registration.reset_state(session)
+
+    def take_snapshot(self, session, flags):
+        # force save of registration instance session info
+        return {
+            'version': 1,
+            'registrations': {rc.__name__: rc._attr_registration.take_snapshot(session, flags)
+                for rc in registerable_classes}
+        }
+
+    @staticmethod
+    def restore_snapshot(session, data):
+        inst = RegAttrManager()
+        from . import bundle_api
+        for class_name, registration in data['registrations'].items():
+            bundle_api.get_class(class_name)._attr_registration.restore_session_data(
+                session, registration)
+        return inst
+
+class CustomizedInstanceManager(StateManager):
+    # This manager's only job is to remember instances that have
+    # custom attibutes, so that they get saved/restored in sessions
+    # even if there are no Python-layer references to them
+
+    def reset_state(self, session):
+        pass
+
+    def take_snapshot(self, session, flags):
+        from .molobject import all_python_instances
+        # pure Sequence instances don't have 'session' attrs since they shouldn't
+        # be saved if nothing else in the Python layer wants them saved
+        return { 'instances': [inst for inst in all_python_instances()
+            if getattr(inst, 'has_custom_attrs', False) and getattr(inst, 'session', None) == session] }
+
+    @staticmethod
+    def restore_snapshot(session, data):
+        # simply having the Python instances in 'data' restored was the whole point,
+        # so mission accomplished already
+        return CustomizedInstanceManager()
