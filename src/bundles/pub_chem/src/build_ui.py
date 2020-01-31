3ed3fa1e9ab141e01e185a13d3005eb289b5ab06
Eric Pettersen <pett@cgl.ucsf.edu>
2020-01-30 17:34:08 -0800

    forgot to 'git add' a file

---
+++ src/bundles/pub_chem/src/build_ui.py
@@ -0,0 +1,44 @@
+# vim: set expandtab shiftwidth=4 softtabstop=4:
+
+# === UCSF ChimeraX Copyright ===
+# Copyright 2016 Regents of the University of California.
+# All rights reserved.  This software provided pursuant to a
+# license agreement containing restrictions on its disclosure,
+# duplication and use.  For details see:
+# http://www.rbvi.ucsf.edu/chimerax/docs/licensing.html
+# This notice must be embedded in or attached to all copies,
+# including partial copies, of the software or any revisions
+# or derivations thereof.
+# === UCSF ChimeraX Copyright ===
+
+from PyQt5.QtWidgets import QVBoxLayout, QLabel, QGridLayout, QRadioButton, QLineEdit, QWidget, QHBoxLayout
+from PyQt5.QtWidgets import QCheckBox, QSizePolicy
+from PyQt5.QtGui import QDoubleValidator, QIntValidator
+from PyQt5.QtCore import Qt
+from chimerax.core.errors import UserError
+
+def fill_widget(widget):
+    layout = QGridLayout()
+    layout.setContentsMargins(0,0,0,5)
+    layout.setSpacing(0)
+    widget.setLayout(layout)
+    layout.setRowStretch(0, 1)
+    layout.addWidget(QLabel("PubChem CID:"), 1, 0, alignment=Qt.AlignRight)
+    cid_edit = QLineEdit()
+    cid_validator = QIntValidator()
+    cid_validator.setBottom(1)
+    cid_edit.setValidator(cid_validator)
+    layout.addWidget(cid_edit, 1, 1, alignment=Qt.AlignLeft)
+    cid_edit.setObjectName("CID")
+    acknowledgement = QLabel('PubChem CID support courtesy of <a href="https://pubchemdocs.ncbi.nlm.nih.gov/power-user-gateway">PubChem Power User Gateway (PUG) web services</a>')
+    acknowledgement.setWordWrap(True)
+    acknowledgement.setOpenExternalLinks(True)
+    layout.addWidget(acknowledgement, 2, 0, 1, 2)
+    layout.setRowStretch(3, 1)
+
+def process_widget(widget):
+    cid_edit = widget.findChild(QLineEdit, "CID")
+    text = cid_edit.text().strip()
+    if not text:
+        raise UserError("No PubChem CID given")
+    return "open %s from pubchem" % text
