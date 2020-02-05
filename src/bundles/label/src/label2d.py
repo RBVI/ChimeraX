24c2d3da694ac5221bac172fe042969dc69dbf87
Eric Pettersen <pett@cgl.ucsf.edu>
2020-02-04 16:31:44 -0800

    make delete of 'all' also delete arrows

--- src/bundles/label/src/label2d.py
+++ src/bundles/label/src/label2d.py
@@ -258,7 +258,7 @@ def label_under_window_position(session, win_x, win_y):
 #
 def label_delete(session, labels = None):
     '''Delete label.'''
-    if labels is None:
+    if labels is None or labels == 'all':
         lm = session_labels(session, create=False)
         labels = lm.all_labels if lm else []
         from .arrows import session_arrows
@@ -337,7 +337,7 @@ class LabelsArrowsArg(ModelsArg):
 def register_label_command(logger):
 
     from chimerax.core.commands import CmdDesc, register, Or, BoolArg, IntArg, StringArg, FloatArg, ColorArg
-    from chimerax.core.commands import NonNegativeFloatArg
+    from chimerax.core.commands import NonNegativeFloatArg, EnumOf
     from .label3d import DefArg, NoneArg
 
     labels_arg = [('labels', Or(NamedLabelsArg, LabelsArg))]
@@ -360,7 +360,7 @@ def register_label_command(logger):
     change_desc = CmdDesc(required = labels_arg, keyword = cargs + [('frames', IntArg)],
                           synopsis = 'Change a 2d label')
     register('2dlabels change', change_desc, label_change, logger=logger)
-    delete_desc = CmdDesc(optional = [('labels', Or(NamedLabelsArg, LabelsArrowsArg))],
+    delete_desc = CmdDesc(optional = [('labels', Or(EnumOf(['all']), LabelsArrowsArg))],
                           synopsis = 'Delete a 2d label')
     register('2dlabels delete', delete_desc, label_delete, logger=logger)
     fonts_desc = CmdDesc(synopsis = 'List available fonts')
