b39631c00881c3c4301e70ff01f082eea5a38984
Eric Pettersen <pett@cgl.ucsf.edu>
2020-03-24 15:54:10 -0700

    pdbdev/IHM url changed

--- src/bundles/ihm/src/fetch_ihm.py
+++ src/bundles/ihm/src/fetch_ihm.py
@@ -15,7 +15,7 @@ def fetch_ihm(session, id, ignore_cache=False, **kw):
   '''
   Fetch IHM models from PDB-Dev.
 
-  https://pdb-dev.wwpdb.org/static/cif/PDBDEV_00000012.cif
+  https://pdb-dev.wwpdb.org/cif/PDBDEV_00000012.cif
   '''
 
   url_pattern = 'https://pdb-dev.wwpdb.org/static/cif/%s'
