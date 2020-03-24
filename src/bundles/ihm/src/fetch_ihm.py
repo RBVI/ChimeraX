60d74919ddf3be4e112061e10cb67e805cf1c86e
Eric Pettersen <pett@cgl.ucsf.edu>
2020-03-24 15:58:02 -0700

    IHM

--- src/bundles/ihm/src/fetch_ihm.py
+++ src/bundles/ihm/src/fetch_ihm.py
@@ -15,10 +15,10 @@ def fetch_ihm(session, id, ignore_cache=False, **kw):
   '''
   Fetch IHM models from PDB-Dev.
 
-  https://pdb-dev.wwpdb.org/static/cif/PDBDEV_00000012.cif
+  https://pdb-dev.wwpdb.org/cif/PDBDEV_00000012.cif
   '''
 
-  url_pattern = 'https://pdb-dev.wwpdb.org/static/cif/%s'
+  url_pattern = 'https://pdb-dev.wwpdb.org/cif/%s'
   
   if len(id) < 8:
       zero_pad = '0'*(8-len(id))
@@ -37,8 +37,8 @@ def fetch_ihm(session, id, ignore_cache=False, **kw):
                         ignore_cache=ignore_cache)
 
   log.status('Opening %s' % name)
-  from chimerax.core import io
-  models, status = io.open_data(session, filename, format = 'ihm', name = name, **kw)
+  models, status = session.open_command.open_data(filename, format = 'ihm',
+  	name = name, **kw)
     
   return models, status
 
