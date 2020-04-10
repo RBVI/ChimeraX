518ca3e44cb64cb5cd3fa612db52819819df4f05
Tom Goddard <goddard@Toms-MBP.lan>
2020-04-09 18:24:29 -0700

    Moved core/arrays_cpp into a new arrays bundle.

---
+++ src/bundles/arrays/src/__init__.py
@@ -0,0 +1,38 @@
+# vim: set expandtab ts=4 sw=4:
+
+# === UCSF ChimeraX Copyright ===
+# Copyright 2016 Regents of the University of California.
+# All rights reserved.  This software provided pursuant to a
+# license agreement containing restrictions on its disclosure,
+# duplication and use.  For details see:
+# http://www.rbvi.ucsf.edu/chimerax/docs/licensing.html
+# This notice must be embedded in or attached to all copies,
+# including partial copies, of the software or any revisions
+# or derivations thereof.
+# === UCSF ChimeraX Copyright ===
+
+from chimerax.core.toolshed import BundleAPI
+
+class _ArraysAPI(BundleAPI):
+    pass
+
+bundle_api = _ArraysAPI()
+
+_libarrays = None
+def load_libarrays():
+    '''
+    Load the libarrays C++ dynamic library.
+    This allows other C modules that link against libarrays to find it
+    without setting search paths like RPATH since it will be part of
+    the process once loaded and the runtime linker will find it.
+    Matching libraries that are part of the process are used on
+    macOS, Windows, Linux.
+    '''
+    global _libarrays
+    if _libarrays is None:
+        import sys
+        suffix = {'darwin':'.dylib', 'win32':'.dll', 'linux':'.so'}[sys.platform]
+        from os.path import dirname, join
+        path = join(dirname(__file__), 'lib', 'libarrays' + suffix)
+        from ctypes import cdll
+        _libarrays = cdll.LoadLibrary(path)
