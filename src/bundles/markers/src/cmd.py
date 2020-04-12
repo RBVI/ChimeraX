816264fde3e123194865022bf53b80a3ccb923e8
Tom Goddard <goddard@Toms-MBP.lan>
2020-04-12 13:59:25 -0700

    Added marker fromMesh command.

--- src/bundles/markers/src/cmd.py
+++ src/bundles/markers/src/cmd.py
@@ -15,10 +15,7 @@ def marker(session, marker_set, position, radius = 0.5, color = (255,255,0,255),
 
     mset = _create_marker_set(session, marker_set)
     center = position.scene_coordinates(coordinate_system)
-    from chimerax.core.colors import Color
-    rgba = color.uint8x4() if isinstance(color, Color) else color
-
-    m = mset.create_marker(center, rgba, radius)
+    m = mset.create_marker(center, color, radius)
     return m
 
 def _create_marker_set(session, marker_set):
@@ -53,21 +50,20 @@ def marker_delete(session, markers, links_only = False):
 def marker_change(session, some_markers, position = None, coordinate_system = None,
                   radius = None, color = None, markers = True, links = True):
     center = position.scene_coordinates(coordinate_system) if position else None
-    rgba8 = None if color is None else color.uint8x4()
     if markers:
         for m in some_markers:
             if center is not None:
                 m.scene_coord = center
             if radius is not None:
                 m.radius = radius
-            if rgba8 is not None:
-                m.color = rgba8
+            if color is not None:
+                m.color = color
     if links:
         for l in some_markers.intra_bonds:
             if radius is not None:
                 l.radius = radius
-            if rgba8 is not None:
-                l.color = rgba8
+            if color is not None:
+                l.color = color
 
 def marker_link(session, markers, radius = 0.5, color = (255,255,0,255)):
     if len(markers) != 2:
@@ -85,10 +81,8 @@ def marker_link(session, markers, radius = 0.5, color = (255,255,0,255)):
                         % (m1.structure.id_string, m1.residue.number,
                            m2.structure.id_string, m2.residue.number))
     
-    from chimerax.core.colors import Color
-    rgba = color.uint8x4() if isinstance(color, Color) else color
     from . import create_link
-    link = create_link(m1, m2, rgba, radius)
+    link = create_link(m1, m2, color, radius)
     return link
 
 def marker_segment(session, marker_set, position, to_position,
@@ -98,13 +92,11 @@ def marker_segment(session, marker_set, position, to_position,
     mset = _create_marker_set(session, marker_set)
     center1 = position.scene_coordinates(coordinate_system)
     center2 = to_position.scene_coordinates(coordinate_system)
-    from chimerax.core.colors import Color
-    rgba = color.uint8x4() if isinstance(color, Color) else color
 
-    m1 = mset.create_marker(center1, rgba, radius)
-    m2 = mset.create_marker(center2, rgba, radius)
+    m1 = mset.create_marker(center1, color, radius)
+    m2 = mset.create_marker(center2, color, radius)
     from . import create_link
-    link = create_link(m1, m2, rgba, radius)
+    link = create_link(m1, m2, color, radius)
 
     if label is not None:
         from chimerax.label.label3d import label as make_label
@@ -115,6 +107,72 @@ def marker_segment(session, marker_set, position, to_position,
                    text = label, color = label_color, height = label_height)
         
     return m1, m2
+
+# -----------------------------------------------------------------------------
+# Create a marker model from a surface mesh.
+#
+def markers_from_mesh(session, surfaces, edge_radius = 1, color = None,
+                      markers = None):
+
+    if len(surfaces) == 0:
+        from chimerax.core.errors import UserError
+        raise UserError('marker fromMesh: no surfaces specified')
+    
+    if markers is None:
+        from . import MarkerSet
+        markers = MarkerSet(session, 'Mesh ' + surfaces[0].name)
+        session.models.add([markers])
+
+    for s in surfaces:
+        varray = s.vertices
+        edges = _masked_edges(s)
+        if varray is None or len(edges) == 0:
+            continue
+        
+        vcolors = s.vertex_colors
+        if color is None and vcolors is None:
+            color = s.color
+
+        vmarker = {}
+        for edge in edges:
+            for v in edge:
+                if not v in vmarker:
+                    xyz = varray[v]
+                    mcolor = color if vcolors is None else vcolors[v]
+                    vmarker[v] = markers.create_marker(xyz, mcolor, edge_radius)
+
+        from . import create_link
+        ecolor = s.color if color is None else color
+        for v1,v2 in edges:
+            m1 = vmarker[v1]
+            m2 = vmarker[v2]
+            create_link(m1, m2, rgba=ecolor, radius=edge_radius)
+      
+    return markers
+
+def _masked_edges(surface):
+    tmask = surface.triangle_mask
+    emask = surface.edge_mask
+    tri = surface.triangles
+    if tri is None or tri.shape[1] == 1:
+        return []
+    if tri.shape[1] == 2:
+        return tri if tmask is None else tri[tmask]
+
+    edges = set()
+    em0 = em1 = em2 = True
+    for t, (v0,v1,v2) in enumerate(surface.triangles):
+        if tmask is None or tmask[t]:
+            if emask is not None:
+                em = emask[t]
+                em0,em1,em2 = em & 0x1, em & 0x2, em & 0x4
+                if em & 0x1 and (v1,v0) not in edges:
+                    edges.add((v0,v1))
+                if em & 0x2 and (v2,v1) not in edges:
+                    edges.add((v1,v2))
+                if em & 0x4 and (v0,v2) not in edges:
+                    edges.add((v2,v0))
+    return edges
 
 from chimerax.atomic import AtomsArg
 class MarkersArg(AtomsArg):
@@ -139,13 +197,13 @@ class MarkerSetArg(AtomSpecArg):
         return model.atomspec
     
 def register_marker_command(logger):
-    from chimerax.core.commands import CmdDesc, register, FloatArg, ColorArg, BoolArg, StringArg
-    from chimerax.core.commands import CenterArg, CoordSysArg, ModelIdArg, Or, EnumOf
+    from chimerax.core.commands import CmdDesc, register, FloatArg, Color8Arg, BoolArg, StringArg
+    from chimerax.core.commands import CenterArg, CoordSysArg, ModelIdArg, Or, EnumOf, SurfacesArg
     desc = CmdDesc(
         required = [('marker_set', Or(MarkerSetArg, ModelIdArg))],
         keyword = [('position', CenterArg),
                    ('radius', FloatArg),
-                   ('color', ColorArg),
+                   ('color', Color8Arg),
                    ('coordinate_system', CoordSysArg)],
         required_arguments = ['position'],
         synopsis = 'Place a marker'
@@ -163,7 +221,7 @@ def register_marker_command(logger):
         required = [('some_markers', MarkersArg)],
         keyword = [('position', CenterArg),
                    ('radius', FloatArg),
-                   ('color', ColorArg),
+                   ('color', Color8Arg),
                    ('coordinate_system', CoordSysArg),
                    ('markers', BoolArg),
                    ('links', BoolArg)],
@@ -174,7 +232,7 @@ def register_marker_command(logger):
     desc = CmdDesc(
         required = [('markers', MarkersArg)],
         keyword = [('radius', FloatArg),
-                   ('color', ColorArg)],
+                   ('color', Color8Arg)],
         synopsis = 'Connect two markers'
     )
     register('marker link', desc, marker_link, logger=logger)
@@ -184,12 +242,21 @@ def register_marker_command(logger):
         keyword = [('position', CenterArg),
                    ('to_position', CenterArg),
                    ('radius', FloatArg),
-                   ('color', ColorArg),
+                   ('color', Color8Arg),
                    ('coordinate_system', CoordSysArg),
                    ('label', StringArg),
                    ('label_height', FloatArg),
-                   ('label_color', Or(EnumOf(['default']),ColorArg))],
+                   ('label_color', Or(EnumOf(['default']),Color8Arg))],
         required_arguments = ['position', 'to_position'],
         synopsis = 'Create two markers and a link between them'
     )
     register('marker segment', desc, marker_segment, logger=logger)
+
+    desc = CmdDesc(
+        required = [('surfaces', SurfacesArg)],
+        keyword = [('edge_radius', FloatArg),
+                   ('color', Color8Arg),
+                   ('markers', MarkerSetArg)],
+        synopsis = 'Create markers and links for surface mesh'
+    )
+    register('marker fromMesh', desc, markers_from_mesh, logger=logger)
