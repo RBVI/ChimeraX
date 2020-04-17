a3e1f7cd1859c286bd29e0832aec96e3e5438011
Eric Pettersen <pett@cgl.ucsf.edu>
2020-04-17 09:38:41 -0700

    skeletal framework for Sequences tool

---
+++ src/bundles/sequences/src/tool.py
@@ -0,0 +1,325 @@
+# vim: set expandtab shiftwidth=4 softtabstop=4:
+
+# === UCSF ChimeraX Copyright ===
+# Copyright 2016 Regents of the University of California.
+# All rights reserved.  This software provided pursuant to a
+# license agreement containing restrictions on its disclosure,
+# duplication and use.  For details see:
+# http://www.rbvi.ucsf.edu/chimerax/docs/licensing.html
+# This notice must be embedded in or attached to all copies,
+# including partial copies, of the software or any revisions
+# or derivations thereof.
+# === UCSF ChimeraX Copyright ===
+
+from chimerax.core.tools import ToolInstance
+
+class Sequences(ToolInstance):
+
+    #help = "help:user/tools/modelpanel.html"
+
+    def __init__(self, session, tool_name):
+        ToolInstance.__init__(self, session, tool_name)
+        self.display_name = "Show Chain Sequence"
+        self.settings = SequencesSettings(session, "Sequences")
+        #TODO
+
+        from chimerax.ui import MainToolWindow
+        self.tool_window = tw = MainToolWindow(self, close_destroys=False)
+        parent = tw.ui_area
+        from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QAbstractItemView, \
+            QFrame, QPushButton, QSizePolicy
+        class SizedTreeWidget(QTreeWidget):
+            def sizeHint(self):
+                from PyQt5.QtCore import QSize
+                # side buttons will keep the vertical size reasonable
+                if getattr(self, '_first_size_hint_call', True):
+                    self._first_size_hint_call = False
+                    width = 0
+                else:
+                    width = self.header().length()
+                return QSize(width, 200)
+        self.tree = SizedTreeWidget()
+        self.tree.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.tree.keyPressEvent = session.ui.forward_keystroke
+        self.tree.expanded.connect(self._ensure_id_width)
+        layout = QHBoxLayout()
+        layout.setContentsMargins(0,0,0,0)
+        layout.setSpacing(0)
+        layout.addWidget(self.tree)
+        layout.setStretchFactor(self.tree, 1)
+        parent.setLayout(layout)
+        shown_title = "" if short_titles else "Shown"
+        sel_title = "" if short_titles else "Select"
+        self.tree.setHeaderLabels(["Name", "ID", " ", shown_title, sel_title])
+        from chimerax.ui.icons import get_qt_icon
+        self.tree.headerItem().setIcon(3, get_qt_icon("shown"))
+        self.tree.headerItem().setToolTip(3, "Shown")
+        self.tree.headerItem().setIcon(4, get_qt_icon("select"))
+        self.tree.headerItem().setToolTip(4, "Selected")
+        self.tree.setColumnWidth(self.NAME_COLUMN, 200)
+        self.tree.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.tree.setSelectionMode(QAbstractItemView.ExtendedSelection)
+        self.tree.setAnimated(True)
+        self.tree.setUniformRowHeights(True)
+        self.tree.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.tree.itemChanged.connect(self._tree_change_cb)
+        buttons_layout = QVBoxLayout()
+        layout.addLayout(buttons_layout)
+        self._items = []
+        for model_func in [close, hide, show, view]:
+            button = QPushButton(model_func.__name__.capitalize())
+            buttons_layout.addWidget(button)
+            button.clicked.connect(lambda chk, self=self, mf=model_func, ses=session:
+                mf([self.models[row] for row in [self._items.index(i)
+                    for i in self.tree.selectedItems()]] or self.models, ses))
+        self.simply_changed_models = set()
+        self.check_model_list = True
+        self.countdown = 1
+        self.self_initiated = False
+        from chimerax.core.models import ADD_MODELS, REMOVE_MODELS, \
+            MODEL_DISPLAY_CHANGED, MODEL_ID_CHANGED, MODEL_NAME_CHANGED
+        from chimerax.core.selection import SELECTION_CHANGED
+        session.triggers.add_handler(SELECTION_CHANGED,
+            lambda *args: self._initiate_fill_tree(*args, countdown=3))
+        session.triggers.add_handler(MODEL_DISPLAY_CHANGED,
+            lambda *args: self._initiate_fill_tree(*args, simple_change=True, countdown=(0,3)))
+        session.triggers.add_handler(ADD_MODELS,
+            lambda *args: self._initiate_fill_tree(*args, always_rebuild=True, countdown=(3,10)))
+        session.triggers.add_handler(REMOVE_MODELS,
+            lambda *args: self._initiate_fill_tree(*args, always_rebuild=True))
+        session.triggers.add_handler(MODEL_ID_CHANGED,
+            lambda *args: self._initiate_fill_tree(*args, always_rebuild=True, countdown=3))
+        session.triggers.add_handler(MODEL_NAME_CHANGED,
+            lambda *args: self._initiate_fill_tree(*args, simple_change=True, countdown=3))
+        from chimerax import atomic
+        atomic.get_triggers().add_handler("changes", self._changes_cb)
+        self._frame_drawn_handler = None
+        tw.manage(placement="side")
+        tw.shown_changed = self._shown_changed
+
+    NAME_COLUMN = 0
+    ID_COLUMN = 1
+    COLOR_COLUMN = 2
+    SHOWN_COLUMN = 3
+    SELECT_COLUMN = 4
+    
+    def _shown_changed(self, shown):
+        if shown:
+            # Update panel when it is shown.
+            self.check_model_list = True
+            self._initiate_fill_tree()
+
+    @classmethod
+    def get_singleton(self, session):
+        from chimerax.core import tools
+        return tools.get_singleton(session, ModelPanel, 'Model Panel', create=False)
+
+    def _changes_cb(self, trigger_name, changes):
+        reasons = changes.atom_reasons()
+        if "color changed" in reasons or 'display changed' in reasons:
+            self._initiate_fill_tree(countdown=3)
+
+    def _ensure_id_width(self, *args):
+        # ensure that the newly visible model id isn't just "..."
+        self.tree.resizeColumnToContents(self.ID_COLUMN)
+
+    def _initiate_fill_tree(self, *args, always_rebuild=False,
+            simple_change=False, countdown=1):
+        # in order to allow models to be drawn as quickly as possible,
+        # delay the update of the tree until the 'new frame' trigger fires
+        # a time of times ('countdown' -- if a tuple then it varies based on
+        # the number of models, from (lowest, highest)).  However, if no models
+        # are open, we update immediately because Rapid Access will come up and
+        # 'new frame' may not fire for awhile.
+        # We used to work off the 'frame drawn' trigger with no delay, but
+        # that is problematic if some other code is trying to do something
+        # quickly every frame (e.g. volume series playback) that causes the
+        # triggers we react to to fire.
+        if self.self_initiated:
+            self.countdown = 1
+            self.self_initiated = False
+        else:
+            if isinstance(countdown, tuple):
+                # base countdown on how expensive updating the table is
+                least, most = countdown
+                target = int(len(self.session.models) / 50)
+                countdown = max(least, min(target, most))
+            self.countdown = max(countdown, self.countdown)
+        if self.simply_changed_models is not None:
+            if simple_change and args:
+                self.simply_changed_models.add(args[-1])
+            else:
+                self.simply_changed_models = None
+        if len(self.session.models) == 0:
+            if self._frame_drawn_handler is not None:
+                self.session.triggers.remove_handler(self._frame_drawn_handler)
+            self._fill_tree(always_rebuild=always_rebuild)
+        elif self._frame_drawn_handler is None:
+            self._frame_drawn_handler = self.session.triggers.add_handler("new frame",
+                lambda *args, ft=self._fill_tree, ar=always_rebuild: ft(always_rebuild=ar))
+        elif always_rebuild:
+            self.session.triggers.remove_handler(self._frame_drawn_handler)
+            self._frame_drawn_handler = self.session.triggers.add_handler("new frame",
+                lambda *args, ft=self._fill_tree: ft(always_rebuild=True))
+
+    def _fill_tree(self, *, always_rebuild=False):
+        if not self.displayed():
+            # Don't update panel when it is hidden.
+            self._frame_drawn_handler = None
+            from chimerax.core.triggerset import DEREGISTER
+            return DEREGISTER
+
+        self.countdown -= 1
+        if self.countdown > 0:
+            return
+
+        self.tree.blockSignals(True) # particularly itemChanged
+        if self.check_model_list or always_rebuild:
+            update = self._process_models() and not always_rebuild
+            self.check_model_list = False
+        else:
+            update = not always_rebuild
+        if not update:
+            expanded_models = { i._model : i.isExpanded()
+                                for i in self._items if hasattr(i, '_model')}
+            self.tree.clear()
+            self._items = []
+        all_selected_models = self.session.selection.models(all_selected=True)
+        part_selected_models = self.session.selection.models()
+        from PyQt5.QtWidgets import QTreeWidgetItem, QPushButton
+        from PyQt5.QtCore import Qt
+        from PyQt5.QtGui import QColor
+        item_stack = [self.tree.invisibleRootItem()]
+        for model in self.models:
+            model_id, model_id_string, bg_color, display, name, selected, part_selected = \
+                self._get_info(model, all_selected_models, part_selected_models)
+            len_id = len(model_id)
+            if update:
+                if len_id == len(item_stack):
+                    # first child
+                    item = item_stack[-1].child(0)
+                    item_stack.append(item)
+                else:
+                    # sibling
+                    parent, previous_child = item_stack[len_id-1:len_id+1]
+                    item = parent.child(parent.indexOfChild(previous_child)+1)
+                    item_stack[len_id:] = [item]
+            else:
+                parent = item_stack[0] if len(item_stack) == 1 else item_stack[len_id-1]
+                item = QTreeWidgetItem(parent)
+                item._model = model
+                item_stack[len_id:] = [item]
+                self._items.append(item)
+                if bg_color is not False:
+                    from chimerax.ui.widgets import MultiColorButton
+                    but = MultiColorButton(has_alpha_channel=True, max_size=(16,16))
+                    def set_single_color(rgba, m=model):
+                        for cm in m.all_models():
+                            cm.single_color = rgba
+                    but.color_changed.connect(set_single_color)
+                    but.set_color(bg_color)
+                    self.tree.setItemWidget(item, self.COLOR_COLUMN, but)
+                
+                    
+                
+            if self.simply_changed_models and model not in self.simply_changed_models:
+                continue
+            item.setText(self.ID_COLUMN, model_id_string)
+            bg = item.background(self.ID_COLUMN)
+            if bg_color is False:
+                bg.setStyle(Qt.NoBrush)
+            else:
+                but = self.tree.itemWidget(item, self.COLOR_COLUMN)
+                if but is not None:
+                    but.set_color(bg_color)
+            item.setBackground(self.COLOR_COLUMN, bg)
+            if display is not None:
+                item.setCheckState(self.SHOWN_COLUMN, Qt.Checked if display else Qt.Unchecked)
+            if selected:
+                item.setCheckState(self.SELECT_COLUMN, Qt.Checked)
+            elif part_selected:
+                item.setCheckState(self.SELECT_COLUMN, Qt.PartiallyChecked)
+            else:
+                item.setCheckState(self.SELECT_COLUMN, Qt.Unchecked)
+            item.setText(self.NAME_COLUMN, name)
+            if not update:
+                # Expand new top-level displayed models, or if previously expanded
+                if hasattr(model, 'model_panel_show_expanded'):
+                    expand_default = model.model_panel_show_expanded
+                else:
+                    expand_default = (model.display
+                                      and len(model.id) <= 1
+                                      and len(model.child_models()) <= 10)
+                expand = expanded_models.get(model, expand_default)
+                if expand:
+                    self.tree.expandItem(item)
+        for i in range(1,self.tree.columnCount()):
+            self.tree.resizeColumnToContents(i)
+        self.tree.blockSignals(False)
+        self.simply_changed_models = set()
+
+        self._frame_drawn_handler = None
+        from chimerax.core.triggerset import DEREGISTER
+        return DEREGISTER
+
+    def _get_info(self, obj, all_selected_models, part_selected_models):
+        model_id = obj.id
+        model_id_string = obj.id_string
+        bg_color = self._model_color(obj)
+        display = obj.display
+        name = getattr(obj, "name", "(unnamed)")
+        selected = obj in all_selected_models
+        part_selected = selected or obj in part_selected_models
+        return model_id, model_id_string, bg_color, display, name, selected, part_selected
+
+    def _header_click_cb(self, index):
+        if index == 0:
+            # ID label clicked.
+            # Toggle sort order.
+            self._sort_breadth_first = not self._sort_breadth_first
+            self._fill_tree()
+
+    def _label_click(self, event):
+        if event.Col == self.ID_COLUMN:
+            # ID label clicked.
+            # Toggle sort order.
+            self._sort_breadth_first = not self._sort_breadth_first
+            self._fill_tree()
+        event.Skip()
+
+    def _model_color(self, model):
+        return model.single_color
+
+    def _process_models(self):
+        models = self.session.models.list()
+        sorted_models = sorted(models, key=lambda m: m.id)
+        final_models = list(sorted_models)
+        update = True if hasattr(self, 'models') and final_models == self.models else False
+        self.models = final_models
+        return update
+
+    def _tree_change_cb(self, item, column):
+        from PyQt5.QtCore import Qt
+        model = self.models[self._items.index(item)]
+        if column == self.SHOWN_COLUMN:
+            self.self_initiated = True
+            command_name = "show" if item.checkState(self.SHOWN_COLUMN) == Qt.Checked else "hide"
+            run(self.session, "%s #%s%s models" % (command_name,
+                "!" if len(model.all_models()) > 1 else "", model.id_string))
+        elif column == self.SELECT_COLUMN:
+            self.self_initiated = True
+            prefix = "" if item.checkState(self.SELECT_COLUMN) == Qt.Checked else "~"
+            run(self.session, prefix + "select #" + model.id_string)
+
+from chimerax.core.settings import Settings
+class SequencesSettings(Settings):
+    AUTO_SAVE = {
+        'organization': 'by contents'
+    }
+
+_seqs = None
+def sequences(session, tool_name):
+    global _seqs
+    if _seqs is None:
+        _seqs = Sequences(session, tool_name)
+    return _seqs
