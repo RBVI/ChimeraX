110d2d4b32863c88d220d2f547504a8fae6e75a6
Tom Goddard <goddard@Toms-MBP.lan>
2020-06-05 16:35:55 -0700

    Replace obsolete chimerax.core.geometry imports with chimerax.geometry.
    Removed backward compatibility for importing geometry from core. Ticket #3358

--- src/apps/ChimeraX/ChimeraX_main.py
+++ src/apps/ChimeraX/ChimeraX_main.py
@@ -472,15 +472,6 @@ def init(argv, event_loop=True):
 
     if opts.uninstall:
         return uninstall(sess)
-
-    # Put geometry in core for backwards compatibility.
-    # TODO: Remove this for ChimeraX 1.0.
-    try:
-        from chimerax import geometry
-        from sys import modules
-        modules['chimerax.core.geometry'] = geometry
-    except ImportError:
-        pass	# When building geometry may not yet exist
         
     # initialize qt
     if opts.gui:
