13c7b1f54d38199c1c810406aaa78befce702a61
Tom Goddard <goddard@cgl.ucsf.edu>
2020-07-20 18:54:19 -0700

    Allow Leap Motion to emulate 2d or 3d mouse.

--- src/bundles/leap_motion/src/leap.py
+++ src/bundles/leap_motion/src/leap.py
@@ -58,8 +58,9 @@ class LeapMotion(Model):
     def __init__(self, session):
         Model.__init__(self, 'Leap Motion', session)
 
-        self._max_delay = 1.0	# Don't use hand positions older than this (seconds).
-        self._hands = [LeapHand(session, hand = 'left'), LeapHand(session, hand = 'right')]
+        self._max_delay = 0.2	# Don't use hand positions older than this (seconds).
+        self._hands = [LeapHand(session, hand = 'left', emulate = 'mouse'),
+                       LeapHand(session, hand = 'right', emulate = 'vr')]
         self.add(self._hands)	# Add hand models as children
         
         from ._leap import leap_open
@@ -95,12 +96,14 @@ class LeapHand(Model):
     pickable = False
     SESSION_SAVE = False
     
-    def __init__(self, session, hand = 'right'):
+    def __init__(self, session, hand = 'right', emulate = 'mouse'):
+        self._emulate = emulate			# 'mouse' or 'vr' for 2d or 3d mouse mode events
         self._hand = hand			# 'left' or 'right'
-        self._hand_range = 400			# millimeters that span ChimeraX window.
+        self._hand_range = 250			# millimeters that span ChimeraX window.
         self._pinch = False			# Are thumb and index finger touching?
-        self._pinch_thresholds = (0.9, 0.5)	# Pinch strength on and off thresholds
+        self._pinch_thresholds = (0.9, 0.6)	# Pinch strength on and off thresholds
         self._last_position = None		# Used for mouse mode drag events, leap device coordinates
+        self._window_xy = None			# Window position for mouse emulation
 
         name = self._hand + ' hand'
         Model.__init__(self, name, session)
@@ -142,19 +145,43 @@ class LeapHand(Model):
         Hand position is in millimeters in Leap Motion coordinate system.
         Scale hand range in millimeters to fill camera view.
         '''
+        if self._emulate == 'mouse':
+            scene_pos = self._scene_position_2d(hand_position)
+        elif self._emulate == 'vr':
+            scene_pos = self._scene_position_3d(hand_position)
+        return scene_pos
+
+    def _scene_position_2d(self, hand_position):
+        view = self.session.main_view
+        w, h = view.window_size
+        r = self._hand_range
+        win_xy = (w/2 + w*hand_position[0]/r, w*(1.5*r - hand_position[1])/r)
+        self._window_xy = win_xy
+        xyz_min, xyz_max = view.clip_plane_points(win_xy[0], win_xy[1])
+        if xyz_min is not None and xyz_max is not None:
+            scene_pos = .9*xyz_min + .1*xyz_max
+        else:
+            scene_pos = (0,0,0)
+        rot = view.camera.position.zero_translation()
+        from chimerax.geometry import translation
+        return translation(scene_pos) * rot
+        
+    def _scene_position_3d(self, hand_position):
         # Adjust vertical origin
-        y_center = 0.5*self._hand_range	# vertical offset, millimeters
+        y_center = self._hand_range	# vertical offset, millimeters
         cpos = hand_position - (0,y_center,0)
 
         # Scale millimeters to scene units
         view = self.session.main_view
-        cam_pos = view.camera.position
-        from chimerax.geometry import inner_product
-        depth = inner_product(view.center_of_rotation - cam_pos.origin(), -cam_pos.z_axis())
-        scale = depth / self._hand_range
+        cofr = view.center_of_rotation
+        cam = view.camera
+        cam_pos = cam.position
+        scale = cam.view_width(cofr) / self._hand_range
         cpos *= scale			# millimeters to scene units
 
         # Adjust depth origin to center of rotation.
+        from chimerax.geometry import inner_product
+        depth = inner_product(cofr - cam_pos.origin(), -cam_pos.z_axis())
         cpos[2] -= depth		# Center in front of camera (-z axis).
 
         # Convert camera coordinates to scene coordinates
@@ -183,23 +210,36 @@ class LeapHand(Model):
         mode = self._mouse_mode()
         if mode is None:
             return
-        if pinch and hasattr(mode, 'vr_press'):
-            mode.vr_press(LeapPinchEvent(self.position.origin()))
-        elif not pinch and hasattr(mode, 'vr_release'):
-            mode.vr_release(LeapPinchEvent(self.position.origin()))
+        if self._emulate == 'mouse':
+            from chimerax.mouse_modes.mousemodes import MouseEvent
+            event = MouseEvent(position = self._window_xy)
+            if pinch:
+                mode.mouse_down(event)
+            else:
+                mode.mouse_up(event)
+        elif self._emulate == 'vr':
+            if pinch and hasattr(mode, 'vr_press'):
+                mode.vr_press(LeapPinchEvent(self.position.origin()))
+            elif not pinch and hasattr(mode, 'vr_release'):
+                mode.vr_release(LeapPinchEvent(self.position.origin()))
 
     def _send_motion_event(self, pos):
         mode = self._mouse_mode()
-        if mode and hasattr(mode, 'vr_motion'):
-            lpos = self._last_position
-            if lpos is not None:
-                lspos = self._scene_position(lpos).origin()
-                from chimerax.geometry import translation
-                spos = self.position.origin()
-                shift = spos - lspos
-                move = translation(shift)
-                vert = pos[1] - lpos[1]
-                mode.vr_motion(LeapMoveEvent(spos, move, vert))
+        if mode:
+            if self._emulate == 'mouse':
+                from chimerax.mouse_modes.mousemodes import MouseEvent
+                event = MouseEvent(position = self._window_xy)
+                mode.mouse_drag(event)
+            elif self._emulate == 'vr' and hasattr(mode, 'vr_motion'):
+                lpos = self._last_position
+                if lpos is not None:
+                    lspos = self._scene_position(lpos).origin()
+                    from chimerax.geometry import translation
+                    spos = self.position.origin()
+                    shift = spos - lspos
+                    move = translation(shift)
+                    vert = pos[1] - lpos[1]
+                    mode.vr_motion(LeapMoveEvent(spos, move, vert))
             
     def _mouse_mode(self):
         button = self._hand # left button for left hand, right button for right hand.
