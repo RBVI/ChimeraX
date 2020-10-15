1c2cb23ec16ed000bcf906b186c6830c3ec2428b
Eric Pettersen <pett@cgl.ucsf.edu>
2020-10-15 15:24:15 -0700

    need to import Atoms before using it

--- src/bundles/atomic/src/changes.py
+++ src/bundles/atomic/src/changes.py
@@ -168,6 +168,7 @@ class Changes:
 
 def selected_atoms(session=None):
     global _full_sel, _ordered_sel
+    from .molarray import Atoms
     return _ordered_sel if _ordered_sel is not None else (_full_sel if _full_sel is not None else Atoms())
 
 _ordered_sel = None
