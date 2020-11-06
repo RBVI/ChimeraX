6169995d67637d2612bfe0926dece660d0404bbb
Tom Goddard <goddard@sonic.net>
2020-11-05 22:58:17 -0800

    Removed cxconference command and tool.  Superceded by meeting command proxy options.

--- src/bundles/meeting/src/__init__.py
+++ src/bundles/meeting/src/__init__.py
@@ -18,19 +18,11 @@ class _MeetingAPI(BundleAPI):
     api_version = 1
 
     @staticmethod
-    def start_tool(session, bi, ti):
-        from .tool import ConferenceUI
-        return ConferenceUI(session, ti.name)
-
-    @staticmethod
     def register_command(bi, ci, logger):
         # 'register_command' is lazily called when the command is referenced
         cmd_name = ci.name
         if cmd_name.startswith('meeting'):
             from . import meeting
             meeting.register_meeting_command(cmd_name, logger)
-        elif cmd_name.startswith('conference'):
-            from . import reg_cmd
-            reg_cmd.register_command(ci, logger)
 
 bundle_api = _MeetingAPI()
