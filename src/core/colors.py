162a061658c29d8fa1fc482177ca543df96ded59
Greg Couch <gregc@cgl.ucsf.edu>
2021-03-05 21:14:31 -0800

    Not using sorted dictionaries broke color name parsing. This reverts commit 6b7663e241d57be8d4bf7a6845d660e5cbb988c6.

--- src/core/colors.py
+++ src/core/colors.py
@@ -22,6 +22,7 @@ CSS3 colors are supported with the addition of the gray() specification
 from the CSS4 color draft, https://www.w3.org/TR/css-color-4/, and CSS4
 color names.
 """
+from sortedcontainers import SortedDict
 from .state import State, StateManager
 
 # If any of the *STATE_VERSIONs change, then increase the (maximum) core session
@@ -31,17 +32,17 @@ USER_COLORS_STATE_VERSION = 1
 COLORMAP_STATE_VERSION = 1
 USER_COLORMAPS_STATE_VERSION = 1
 
-BuiltinColormaps = {}
+BuiltinColormaps = SortedDict()
 
 
-class UserColors(dict, StateManager):
+class UserColors(SortedDict, StateManager):
     """Support for per-session colors.
 
     Accessed through the session object as ``session.user_colors``.
     """
 
     def __init__(self):
-        dict.__init__(self)
+        SortedDict.__init__(self)
         self.update(BuiltinColors)
 
     def take_snapshot(self, session, flags):
@@ -216,7 +217,7 @@ class Color(State):
 # -----------------------------------------------------------------------------
 
 
-class UserColormaps(dict, StateManager):
+class UserColormaps(SortedDict, StateManager):
     """Support for per-session colormaps.
 
     Accessed through the session object as ``session.user_colormaps``.
@@ -493,7 +494,7 @@ def contrast_with_background(session):
     return contrast_with(session.main_view.background_color)
 
 # CSS4 colors + multiword color names
-BuiltinColors = {
+BuiltinColors = SortedDict({
     'aliceblue': (240, 248, 255, 255),
     'alice blue': (240, 248, 255, 255),
     'antiquewhite': (250, 235, 215, 255),
@@ -741,7 +742,7 @@ BuiltinColors = {
     'yellow': (255, 255, 0, 255),
     'yellowgreen': (154, 205, 50, 255),
     'yellow green': (154, 205, 50, 255),
-}
+})
 BuiltinColors['transparent'] = (0, 0, 0, 0)
 
 _color_names = {rgba8:name for name, rgba8 in BuiltinColors.items()}
