9e494f15143f2559418f4d16a1180714de7ca0f3
Eric Pettersen <pett@cgl.ucsf.edu>
2021-04-26 16:26:28 -0700

    hooked up Help button and made icon available for Toolbar

--- src/bundles/sel_inspector/src/tool.py
+++ src/bundles/sel_inspector/src/tool.py
@@ -15,7 +15,7 @@ from chimerax.core.tools import ToolInstance
 
 class SelInspector(ToolInstance):
 
-    #help = "help:user/tools/distances.html"
+    help = "help:user/tools/inspector.html"
 
     def __init__(self, session, tool_name):
         ToolInstance.__init__(self, session, tool_name)
@@ -54,8 +54,7 @@ class SelInspector(ToolInstance):
         bbox.accepted.connect(self.delete) # slots executed in the order they are connected
         bbox.rejected.connect(self.delete)
         from chimerax.core.commands import run
-        #bbox.helpRequested.connect(lambda *, run=run, ses=session: run(ses, "help " + self.help))
-        bbox.button(qbbox.Help).setEnabled(False)
+        bbox.helpRequested.connect(lambda *, run=run, ses=session: run(ses, "help " + self.help))
         layout.addWidget(bbox)
 
         from chimerax.dist_monitor.cmd import group_triggers
