5545e5245ba6c7746b58927e4bb13131274d56a3
Eric Pettersen <pett@cgl.ucsf.edu>
2021-05-11 17:18:33 -0700

    partial registration of command

---
+++ src/bundles/match_align/src/cmd.py
@@ -0,0 +1,33 @@
+# vim: set expandtab shiftwidth=4 softtabstop=4:
+
+# === UCSF ChimeraX Copyright ===
+# Copyright 2016 Regents of the University of California.
+# All rights reserved.  This software provided pursuant to a
+# license agreement containing restrictions on its disclosure,
+# duplication and use.  For details see:
+# http://www.rbvi.ucsf.edu/chimerax/docs/licensing.html
+# This notice must be embedded in or attached to all copies,
+# including partial copies, of the software or any revisions
+# or derivations thereof.
+# === UCSF ChimeraX Copyright ===
+
+from .settings import defaults
+
+def make_alignment(chains, dist_cutoff=defaults["dist_cutoff"], column_criteria=defaults["column_criteria"],
+        gap_char=defaults["gap_char"]
+    pass
+
+def register_command(cmd_name, logger):
+    from chimerax.core.commands import CmdDesc, register, NonNegativeFloatArg, EnumOf, CharacterArg
+    from chimerax.atomic import UniqueChainsArg
+    #from chimerax import sim_matrices
+    desc = CmdDesc(
+        required = [('chains', UniqueChainsArg)],
+        keyword = [
+            ('column_criteria', EnumOf(['any', 'all']))
+            ('dist_cutoff', NonNegativeFloatArg)
+            ('gap_char', CharacterArg)
+        ],
+        synopsis = 'Create sequence alignment from structural superposition'
+    )
+    register(cmd_name, desc, make_alignment, logger=logger)
