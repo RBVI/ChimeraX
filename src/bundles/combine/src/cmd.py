b4720fb0050bf44dc75033992ca7c0c70be13324
Eric Pettersen <pett@cgl.ucsf.edu>
2021-07-22 18:03:56 -0700

    figured out chain remapping; copied first structure

--- src/bundles/combine/src/cmd.py
+++ src/bundles/combine/src/cmd.py
@@ -12,8 +12,7 @@
 # === UCSF ChimeraX Copyright ===
 
 from chimerax.core.errors import UserError
-def combine_cmd(session, structures, *, close=False, model_id=None, name=None, new_chain_ids=True,
-        ref_model=None):
+def combine_cmd(session, structures, *, close=False, model_id=None, name=None, ref_model=None):
 
     if structures is None:
         from chimerax.atomic import Structure
@@ -21,12 +20,43 @@ def combine_cmd(session, structures, *, close=False, model_id=None, name=None, n
     if not structures:
         raise UserError("No structures specified")
 
+    # sort structures by size, so that the faster copying can be used on the largest structure
+    structures = sorted(structures, key=lambda s: 0 - s.num_atoms)
     if ref_model is None:
         ref_model = structures[0]
 
     if model_id is None:
         model_id = session.models.next_id()
-    #TODO
+
+    if name is None:
+        if len(structures) == 1:
+            name = "copy of " + ref_model.name
+        else:
+            name = "combination"
+
+    # Compute needed remapping of chain IDs
+    seen_ids = set()
+    chain_id_mapping = {}
+    for s in structures:
+        chain_ids = sorted(s.residues.unique_chain_ids)
+        for chain_id in chain_ids:
+            if chain_id in seen_ids:
+                from chimerax.atomic import next_chain_id
+                new_id = next_chain_id(chain_id)
+                while new_id in seen or new_id in chain_ids:
+                    new_id = next_chain_id(new_id)
+                session.logger.info("Remapping chain ID '%s' in %s to '%s'" % (chain_id, s, new_id))
+                chain_id_mapping[(s, chain_id)] = new_id
+                seen_ids.add(new_id)
+            else:
+                seen_ids.add(chain_id)
+
+    combination = structures[0].copy(name)
+    #TODO: adjust to ref_model
+    for s in structures[1:]:
+        #TODO
+    session.models.add([combination])
+    return combination
 
 def register_command(logger):
     from chimerax.core.commands import CmdDesc, register, Or, EmptyArg, StringArg, BoolArg, ModeIdArg, \
@@ -38,7 +68,6 @@ def register_command(logger):
             ('close', BoolArg),
             ('model_id', ModeIdArg),
             ('name', StringArg),
-            ('new_chain_ids', BoolArg),
             ('ref_model', ModelArg)),
         ],
         synopsis = 'Copy/combine structure models')
