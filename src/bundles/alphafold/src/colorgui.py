af136078e93e08b3ffb59978c69bc9932aa99422
Tom Goddard <goddard@sonic.net>
2021-08-31 15:16:38 -0700

    Made AlphaFold coloring model menu auto select first model.

--- src/bundles/alphafold/src/colorgui.py
+++ src/bundles/alphafold/src/colorgui.py
@@ -57,7 +57,8 @@ class AlphaFoldColoringGUI(ToolInstance):
     def _create_residues_menu(self, parent):
 
         from chimerax.ui.widgets import ModelMenuButton
-        sm = ModelMenuButton(self.session, filter_func = _is_alphafold_model)
+        sm = ModelMenuButton(self.session, filter_func = _is_alphafold_model,
+                             autoselect = ModelMenuButton.AUTOSELECT_FIRST_DISPLAYED)
         self._model_menu = sm
         mlist = [m for m in self.session.models.list() if _is_alphafold_model(m)]
         if mlist:
