e6e620111db6c4bb4575bf8de17e3f72fe163fdf
Greg Couch <gregc@cgl.ucsf.edu>
2021-09-03 17:26:40 -0700

    Bump cxservices version number due to additonal API.

--- prereqs/cxservices/client/cxservices/configuration.py
+++ prereqs/cxservices/client/cxservices/configuration.py
@@ -240,5 +240,5 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 0.1\n"\
-               "SDK Package Version: 1.0.1".\
+               "SDK Package Version: 1.1".\
                format(env=sys.platform, pyversion=sys.version)
