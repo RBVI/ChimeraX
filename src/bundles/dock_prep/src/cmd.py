eac2d48ea76dd9665498a02bc6c6757013142c4f
Eric Pettersen <pett@cgl.ucsf.edu>
2021-09-09 13:50:38 -0700

    baby steps

--- src/bundles/dock_prep/src/cmd.py
+++ src/bundles/dock_prep/src/cmd.py
@@ -12,7 +12,11 @@
 # === UCSF ChimeraX Copyright ===
 
 from chimerax.core.errors import UserError
-from chimerax.core.commands import CmdDesc, register, BoolArg, Or, NoneArg
+from chimerax.core.commands import CmdDesc, register, BoolArg, Or, NoneArg, EnumArg
+
+MEMORIZE_USE = "use"
+MEMORIZE_SAVE = "save"
+MEMORIZE_NONE = "none"
 
 prep_cmd_param_info = {
     'del_solvent': (BoolArg, True),
@@ -20,30 +24,61 @@ prep_cmd_param_info = {
 
 complete_cmd_param_info = {}
 # use a function to avoid importing modules of subsidary commands until needed
+_cmd_sources = None
+def cmd_sources():
+    global _cmd_sources
+    if _cmd_sources is None:
+        from .prep import prep
+        _cmd_sources = [(None, prep, prep_cmd_param_info)]
+    return _cmd_sources
+
 def get_param_info():
     if not complete_cmd_param_info:
-        for prefix, param_dict in [(None, prep_cmd_param_info)]:
+        for prefix, func, param_dict in cmd_sources():
             for arg_name, arg_info in param_dict.items():
-                complete_cmd_param_info[arg_name if prefix is None else (prefix + '_' + arg_name)] = arg_info
+                complete_cmd_param_info[arg_name
+                    if prefix is None else (prefix + '_' + arg_name)] = arg_info
     return complete_cmd_param_info
 
 def dock_prep_caller(session, structures, memorization, *, callback=None, **kw):
-    final_params = {}
-    for arg_name, arg_info in get_param_info():
-        arg_type, default = arg_info
-        final_params[arg_name if prefix is None else (prefix + '_' + arg_name)] = default
-    final_params.update(**kw)
-    #TODO: log command equivalent; handle memorization; call workhorse function; make callback
+    for prefix, func, param_dict in cmd_sources():
+        func_kw = {}
+        if memorization == MEMORIZE_NONE:
+            settings = None
+        else:
+            settings = get_settings(session, prefix) #TODO
+        #TODO: the scheme below just isn't right -- later interfaces need to be able to
+        # show GUIs and have the chosen settings memorized.  Maybe somehow use 'yield'?
+        for arg_name, arg_info in param_dict.items():
+            arg_type, default = arg_info
+            attr_name = arg_name if prefix is None else prefix + '_' _ arg_name
+            if memorization == MEMORIZE_NONE:
+                arg_val = kw.get(attr_name, default)
+            elif memorization == MEMORIZE_USE:
+                arg_val = kw.get(attr_name, getattr(settings, arg_name))
+            else:
+                arg_val = kw.get(attr_name, default)
+                setattr(settings, arg_name, arg_val)
+            func_kw[arg_name] = arg_val
+        func(session, structures, **func_kw)
+    #TODO: log command equivalents; handle memorization; call workhorse function; make callback
 
-def dock_prep_cmd(session, structures,  **kw):
-    #TODO: if structures is None:...
-    #TODO: call prerequisite command wrappers
-    return key
+def dock_prep_cmd(session, structures,  *, memorize=MEMORIZE_NONE, **kw):
+    if structures is None:
+        from chimerax.atomic import all_atomic_structures
+        structures = all_atomic_structures(session)
+    if not structures:
+        raise UserError("No atomic structures open/specified")
+    dock_prep_caller(session, structures, memorize, **kw)
+
+class MemorizationArg(EnumArg):
+    values = (MEMORIZE_USE, MEMORIZE_SAVE, MEMORIZE_NONE)
 
 def register_command(logger):
     from chimerax.atomic import AtomicStructuresArg
     cmd_desc = CmdDesc(
         required=[('structures', Or(AtomicStructuresArg, NoneArg))],
-        keyword= [(arg_name, arg_info[0]) for arg_name, arg_info in get_param_info()],
-        synopsis = 'Prepare structures for computations')
+        keyword=[('memorize', MemorizationArg)] + [(arg_name, arg_info[0])
+            for arg_name, arg_info in get_param_info()],
+        synopsis='Prepare structures for computations')
     register('dockprep', cmd_desc, dock_prep_cmd, logger=logger)
