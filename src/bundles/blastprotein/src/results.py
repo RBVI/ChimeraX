c0cf5c9fcbdd1569456ec0e98dad5587cad04794
Zach Pearson <zjp@cgl.ucsf.edu>
2021-09-14 16:27:38 -0700

    Blastprotein: Restore the table from snapshots too

--- src/bundles/blastprotein/src/results.py
+++ src/bundles/blastprotein/src/results.py
@@ -49,6 +49,7 @@ class BlastProteinResults(ToolInstance):
         self.params = kw.pop('params', None)
         self._hits = kw.pop('hits', None)
         self._sequences: Dict[int, SeqId] = kw.pop('sequences', None)
+        self._table_session_data = kw.pop('table_session_data', None)
         self._from_restore = kw.pop('from_restore', False)
         super().__init__(session, self.display_name, **kw)
         self._build_ui()
@@ -182,7 +183,10 @@ class BlastProteinResults(ToolInstance):
                 self.table.add_column(string, data_fetch=lambda x, i=oldstr: x[i], **kwdict)
             # Convert dicts to objects (they're hashable)
             self.table.data = [BlastResultsRow(item) for item in items]
-            self.table.launch()
+            if self._from_restore:
+                self.table.launch(session_info=self._table_session_data)
+            else:
+                self.table.launch()
             self.control_widget.setVisible(True)
             self._unload_progress_bar()
 
@@ -264,7 +268,7 @@ class BlastProteinResults(ToolInstance):
         sequences_dict = {}
         for (key, hit_name, sequence) in sequences:
             sequences_dict[key] = SeqId(hit_name, sequence)
-        return cls(session, data['tool_name'], hits = data['results'], sequences = sequences_dict, params = data['params'], from_restore=True)
+        return cls(session, data['tool_name'], hits = data['results'], sequences = sequences_dict, params = data['params'], table_session_data = data['table_session'], from_restore=True)
 
     @classmethod
     def restore_snapshot(cls, session, data):
@@ -273,6 +277,7 @@ class BlastProteinResults(ToolInstance):
     def take_snapshot(self, session, flags):
         data = {
             'ToolUI': ToolInstance.take_snapshot(self, session, flags),
+            'table_session': self.table.session_info(),
             'params': self.params,
             'tool_name': self.tool_name,
             'results': self._hits,
