d4d33bca72456d791300ee49b73f78cd18cab6b0
Eric Pettersen <pett@cgl.ucsf.edu>
2021-10-12 13:41:24 -0700

    untested implementation of AxisModel

--- src/bundles/axes_planes/src/cmd.py
+++ src/bundles/axes_planes/src/cmd.py
@@ -26,11 +26,13 @@ class PlaneModel(Surface, ComplexMeasurable):
         self._update_geometry()
 
     def angle(self, obj):
-        if not isinstance(obj, PlaneModel):
+        if isinstance(obj, AxisModel):
+            return _axis_plane_angle(obj, self)
+        elif not isinstance(obj, PlaneModel):
             return NotImplemented
+        val = angle(
         from chimerax.geometry import angle
-        degrees = angle(self.position.transform_vector(self.plane.normal),
-            obj.position.transform_vector(obj.plane.normal))
+        degrees = angle(self.xform_normal, obj.xform_normal)
         return degrees if degrees < 90 else 180 - degrees
 
     def distance(self, obj, *, signed=False):
@@ -54,6 +56,10 @@ class PlaneModel(Surface, ComplexMeasurable):
         # need to inverse transform to get into same coord sys as self.plane
         signed_dist = self.plane.distance(self.position.inverse() * scene_crd)
         return signed_dist if signed else abs(signed_dist)
+
+    @property
+    def normal(self):
+        return self.plane.normal
 
     def _get_radius(self):
         return self._radius
@@ -108,12 +114,176 @@ class PlaneModel(Surface, ComplexMeasurable):
         Surface.set_state_from_snapshot(inst, session, data['base data'])
         return inst
 
+    @property
+    def xform_normal(self):
+        return self.position.transform_vector(self.normal)
+
+
+class AxisModel(Surface, ComplexMeasurable):
+    def __init__(self, session, name, center, direction, extent, radius, color):
+        super().__init__(name, session)
+        self.plane = plane
+        self.color = color
+        self._center = center
+        self._direction = direction
+        self._extent = extent
+        self._radius = radius
+        self.display_style = self.Solid
+        self._update_geometry()
+
+    def angle(self, obj):
+        if isinstance(obj, PlaneModel):
+            return _axis_plane_angle(self, obj)
+        elif not isinstance(obj, AxisModel):
+            return NotImplemented
+        return quadrant_angle(angle(self.xform_direction, obj.xform_direction)
+
+    def _axis_ends_dist(self, axis):
+        return min(
+            min([self._point_distance(axis.xform_center + axis.xform_direction * f * axis.extent
+                for f in (-1, 1)]),
+            min([axis._point_distance(self.xform_center + self.xform_direction * f * self.extent
+                for f in (-1, 1)])
+        )
+
+    @property
+    def center(self):
+        return self._center
+
+    @property
+    def direction(self):
+        return self._direction
+
+    def distance(self, obj, *, signed=False):
+        if isinstance(obj, AxisModel):
+            # shortest distance between lines is perpendicular to both...
+            self_dir = self.xform_direction
+            obj_dir = obj.xform_direction
+            from chimerax.geometry import angle, cross_product, Plane
+            if angle(self_dir, obj_dir) in [0.0, 180.0]:
+                # parallel
+                return self._axis_ends_dist(obj)
+            short_dir = cross_product(self_dir, obj_dir)
+            # can use analytically shortest dist only if each axis
+            # penetrates the plane formed by the other axis and the
+            # perpendicular
+            for a1, a2 in [(obj, self), (self, obj)]:
+                normal = cross_product(a1.xform_direction, short_dir)
+                plane = Plane(a1.xform_center, normal=normal)
+                d1 = plane.distance(a2.xform_center + a2.xform_direction * a2.extent)
+                d2 = plane.distance(a2.xform_center - a2.xform_direction * a2.extent)
+                if (d1 < 0.0 and d2 < 0.0) or (d1 > 0.0 and d2 > 0.0):
+                    # both ends on same side of plane
+                    return self._axis_ends_dist(obj)
+            # distance is the difference between the planes that contain the axes and with
+            # a normal perpendicular to both
+            d1 = Plane(self.xform_center, normal=short_dir).equation()[3]
+            d2 = Plane(obj.xform_center, normal=short_dir).equation()[3]
+            return abs(d1 - d2)
+        if not isinstance(obj, SimpleMeasurable):
+            return NotImplemented
+        # put into same coord sys as axis
+        return self._point_distance(self.position.inverse() * obj.scene_coord)
+
+    @property
+    def extent(self):
+        return self._extent
+
+    def _point_distance(self, pt):
+        # pt should already be in our transformed coord sys
+        xf_center = self.xform_center
+        xf_direction = self.xform_direction
+        min_pt = xf_center - xf_direction * self.extent
+        max_pt = xf_center + xf_direction * self.extent
+        v = pt - xf_center
+        from chimerax.geometry import cross_product, Plane, inner_product, distance
+        c1 = cross_product(v, xf_direction)
+        import numpy
+        if not numpy.any(c1):
+            # colinear
+            in_plane = pt
+        else:
+            plane = Plane(xf_center, normal=xf_direction)
+            in_plane = plane.nearest(pt)
+        pt_ext = inner_product(in_plane - xf_center, xf_direction)
+        if pt_ext < -self.extent:
+            measure_pt = min_pt
+        elif pt_ext > self.extent:
+            measure_pt = max_pt
+        else:
+            measure_pt = in_plane
+        return distance(pt, measure_pt)
+
+    def _get_radius(self):
+        return self._radius
+
+    def _set_radius(self, val):
+        if self._radius == val:
+            return
+        self._radius = val
+        self._updated()
+
+    radius = property(_get_radius, _set_radius)
+
+    def take_snapshot(self, session, flags):
+        return {
+            'version': 1,
+            'base data': super().take_snapshot(session, flags),
+            'center': self.center,
+            'direction': self.direction,
+            'extent': self.extent,
+            'radius': self.radius,
+            'color': self.color
+        }
+
+    @classmethod
+    def restore_snapshot(cls, session, data):
+        inst = cls(session, None, data['center'], data['direction'], data['extent'], data['radius'],
+            data['color'])
+        Surface.set_state_from_snapshot(inst, session, data['base data'])
+        return inst
+
+    def _update_geometry(self):
+        from chimerax.shape.shape import cylinder_geometry
+        length = 2 + self.extent
+        varray, tarray = cylinder_geometry(self.radius, length,
+            max(2, int(length / 3 + 0.5)), max(40, int(20.0 * self.radius + 0.5)), True)
+        from chimerax.geometry import translation, vector_rotation
+        varray = (translation(self.center)
+            * vector_rotation((0,0,1), self.direction)).transform_points(varray)
+        from chimerax.surface import calculate_vertex_normals
+        narray = calculate_vertex_normals(varray, tarray)
+        self.set_geometry(varray, narray, tarray)
+
+    @property
+    def xform_center(self):
+        return self.position.transform_vector(self.center)
+
+    @property
+    def xform_direction(self):
+        return self.position.transform_vector(self.direction)
+
+def _axis_plane_angle(axis, plane):
+    from chimerax.geometry import angle
+    anti_angle = angle(axis.xform_direction, plane.xform_normal)
+    return 90.0 - quadrant_angle(anti_angle)
+
+def quadrant_angle(angle):
+    while angle < 0.0:
+        angle += 360.0
+    while angle > 360.0:
+        angle -= 360.0
+    if angle > 180.0:
+        angle = 360.0 - angle
+    if angle > 90.0:
+        angle = 180.0 - angle
+    return angle
 
 def cmd_define_plane(session, atoms, *, thickness=defaults["plane_thickness"], padding=0.0, color=None,
         radius=None, name="plane"):
     """Wrapper to be called by command line.
 
-       Use chimerax.axes_planes.plane for other programming applications.
+       Use chimerax.geometry.Plane for other programming applications.
     """
     from chimerax.core.errors import UserError
 
