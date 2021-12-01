93b6d234941bb6e1377630fa56e0395ca1af94c6
Tom Goddard <goddard@sonic.net>
2021-12-01 11:42:13 -0800

    Exclude new alphafold python notebook from import test since it uses IPython magic.

--- cxtestimports.py
+++ cxtestimports.py
@@ -12,6 +12,7 @@ blacklist = set([
     "chimerax.add_charge.process_lib",  # creates data.py
     "chimerax.alignment_algs.libalign_algs",  # non-importable dynamic lib
     "chimerax.alphafold.alphafold_predict_colab",  # IPython notebook with syntax magic
+    "chimerax.alphafold.alphafold21_predict_colab",  # IPython notebook with syntax magic
     "chimerax.atomic.libmolc",  # non-importable dynamic lib
     "chimerax.atomic.md_crds.dcd.MDToolsMarch97.md_tests",  # test code
     "chimerax.build_structure.process",  # processes Chimera fragment files
