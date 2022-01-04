a0c1a5a6c3025905ddd171b90949fff3c4c4d2d5
Tom Goddard <goddard@sonic.net>
2022-01-04 00:21:21 -0800

    Allow use of enumeration values without enumeration names in PyQt6.

--- prereqs/qtshim/Qt/QtGui.py
+++ prereqs/qtshim/Qt/QtGui.py
@@ -3,18 +3,11 @@ from . import using_pyqt6, using_pyqt5, using_pyside2
 if using_pyqt6:
     from PyQt6.QtGui import *
 
-    # Put some commonly used enumeration values in class namespaces.
-    enum_values = [
-        (QFont, 'Weight', ['Normal', 'Bold']),
-        (QFontDatabase, 'SystemFont', ['FixedFont']),
-        (QImage, 'Format', ['Format_ARGB32']),
-        (QPainter, 'RenderHint', ['Antialiasing']),
-        (QPalette, 'ColorRole', ['Window']),
-    ]
-    for cls, enum_name, values in enum_values:
-        enum = getattr(cls, enum_name)
-        for value in values:
-            setattr(cls, value, getattr(enum, value))
+    # Allow using enum values without enumeration name.
+    from .promote_enums import promote_enums
+    from PyQt6 import QtGui
+    promote_enums(QtGui)
+    del QtGui
 
 elif using_pyqt5:
     from PyQt5.QtGui import *
