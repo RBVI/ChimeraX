604d50ba37e9378fd291eb34a1f59cd421f3235a
Zach Pearson <zach@zjp.codes>
2022-01-04 17:09:17 -0800

    core/{session,tasks}: Move trigger addition to session

--- src/core/tasks.py
+++ src/core/tasks.py
@@ -50,6 +50,7 @@ and ``session.trigger.remove_handler``.
 """
 
 import abc
+import itertools
 from .state import State, StateManager
 
 # If any of the *STATE_VERSIONs change, then increase the (maximum) core session
@@ -60,6 +61,8 @@ ADD_TASK = 'add task'
 REMOVE_TASK = 'remove task'
 UPDATE_TASK = 'update task'
 END_TASK = 'end task'
+
+task_triggers = [ADD_TASK, REMOVE_TASK, UPDATE_TASK, END_TASK]
 
 # Possible task state
 PENDING = "pending"         # Initialized but not running
@@ -381,13 +384,7 @@ class Tasks(StateManager):
         """
         import weakref
         self._session = weakref.ref(session)
-        if first:
-            session.triggers.add_trigger(ADD_TASK)
-            session.triggers.add_trigger(REMOVE_TASK)
-            session.triggers.add_trigger(UPDATE_TASK)
-            session.triggers.add_trigger(END_TASK)
         self._tasks = {}
-        import itertools
         self._id_counter = itertools.count(1)
 
     def take_snapshot(self, session, flags):
