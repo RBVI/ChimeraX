437157d846506f3d545f11bcf4f3aaf22e0a2a08
Eric Pettersen <pett@cgl.ucsf.edu>
2022-06-10 18:22:32 -0700

    baby steps towards refactoring to allow water inspection w/o douse

---
+++ src/bundles/water_check/src/__init__.py
@@ -0,0 +1,34 @@
+# vim: set expandtab shiftwidth=4 softtabstop=4:
+
+# === UCSF ChimeraX Copyright ===
+# Copyright 2016 Regents of the University of California.
+# All rights reserved.  This software provided pursuant to a
+# license agreement containing restrictions on its disclosure,
+# duplication and use.  For details see:
+# http://www.rbvi.ucsf.edu/chimerax/docs/licensing.html
+# This notice must be embedded in or attached to all copies,
+# including partial copies, of the software or any revisions
+# or derivations thereof.
+# === UCSF ChimeraX Copyright ===
+
+from chimerax.core.toolshed import BundleAPI
+
+class _CheckWatersBundle(BundleAPI):
+
+    @staticmethod
+    def get_class(class_name):
+        from . import tool
+        return getattr(tool, class_name)
+
+    @staticmethod
+    def register_command(command_name, logger):
+        from . import cmd
+        cmd.register_command(logger)
+
+    @staticmethod
+    def start_tool(session, tool_name):
+        if tool_name == 'Check Waters':
+            from .tool import CheckWatersInputTool
+            return CheckWatersInputTool(session)
+
+bundle_api = _CheckWatersBundle()
