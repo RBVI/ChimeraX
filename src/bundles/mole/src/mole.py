7b015533c2c252fd126a6bd502315302828a5a12
Tom Goddard <goddard@sonic.net>
2022-09-12 14:42:40 -0700

    Made Mole json file reader handle pores, paths and tunnels instead of just tunnels.

--- src/bundles/mole/src/mole.py
+++ src/bundles/mole/src/mole.py
@@ -21,7 +21,7 @@ Read Mole tunnel json files and display as spheres.
 # -----------------------------------------------------------------------------
 #
 def read_mole_json(session, filename, name, transparency = 0.5):
-    """Read Mole tunnels and create a separate Structure for each to be shown as spheres.
+    """Read Mole channels and create a separate Structure for each to be shown as spheres.
 
     :param filename: either the name of a file or a file-like object
     """
@@ -34,42 +34,45 @@ def read_mole_json(session, filename, name, transparency = 0.5):
 
     import json
     j = json.load(input)
-    tunnels = j['Channels']['Tunnels']
+    channels = j['Channels']
 
-    tunnel_colors = mole_tunnel_colors()
-    num_colors = len(tunnel_colors)
+    channel_colors = mole_channel_colors()
+    num_colors = len(channel_colors)
 
     opacity = int(255 * (1-transparency))
     
     models = []
     from chimerax.atomic import Structure
-    for tnum, tunnel in enumerate(tunnels):
-        tid = tunnel['Id']
-        color = tuple(tunnel_colors[tnum % num_colors][:3]) + (opacity,)
-        s = Structure(session, name = f'Tunnel {tid}')
-        s.display = (tnum == 0)
-        res_name = 'T'
-        chain_id = 'A'
-        res_num = 1
-        res = s.new_residue(res_name, chain_id, res_num)
-        spheres = tunnel['Profile']
-        for sphere in spheres:
-            r,x,y,z = [sphere[attr] for attr in ('Radius', 'X', 'Y', 'Z')]
-            a = s.new_atom(f's{tid}', 'C')
-            a.coord = (x,y,z)
-            a.radius = r
-            a.draw_mode = a.SPHERE_STYLE
-            a.color = color
-            res.add_atom(a)
-        models.append(s)
+    for ctype in ('Pores', 'MergedPores', 'Tunnels', 'Paths'):
+        channels = j['Channels'].get(ctype,[])
+        for cnum, channel in enumerate(channels):
+            chid = channel['Id']
+            color = tuple(channel_colors[cnum % num_colors][:3]) + (opacity,)
+            s = Structure(session, name = f'{ctype[:-1]} {chid}')
+            s.display = (cnum == 0)
+            res_name = 'T'
+            chain_id = 'A'
+            res_num = 1
+            res = s.new_residue(res_name, chain_id, res_num)
+            spheres = channel['Profile']
+            for sphere in spheres:
+                r,x,y,z = [sphere[attr] for attr in ('Radius', 'X', 'Y', 'Z')]
+                a = s.new_atom(f's{chid}', 'C')
+                a.coord = (x,y,z)
+                a.radius = r
+                a.draw_mode = a.SPHERE_STYLE
+                a.color = color
+                res.add_atom(a)
+            models.append(s)
 
-    from chimerax.model_series.mseries import mseries_slider
-    mseries_slider(session, models, title = f'{len(models)} tunnels', name = 'Tunnel')
+    if len(models) > 1:
+        from chimerax.model_series.mseries import mseries_slider
+        mseries_slider(session, models, title = f'{len(models)} channels', name = 'Channel')
     
-    return models, f'Opened {len(models)} Mole tunnels' 
+    return models, f'Opened {len(models)} Mole channels' 
 
-def mole_tunnel_colors():
-    tunnel_colors = [
+def mole_channel_colors():
+    channel_colors = [
         'orange red',
         'orange',
         'yellow',
@@ -94,6 +97,6 @@ def mole_tunnel_colors():
         'red',
         ]
     from chimerax.core.colors import BuiltinColors
-    rgba8 = [BuiltinColors[cname].uint8x4() for cname in tunnel_colors]
+    rgba8 = [BuiltinColors[cname].uint8x4() for cname in channel_colors]
     return rgba8
 
