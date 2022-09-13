0a0c0628ee76616ef1f69f39973b0952cff4e8b5
Zach Pearson <zach@zjp.codes>
2022-09-13 14:10:02 -0700

    bundles/webservices: Convert to TOML for editable install

--- src/bundles/webservices/src/__init__.py
+++ src/bundles/webservices/src/__init__.py
@@ -8,6 +8,7 @@
 # including partial copies, of the software or any revisions
 # or derivations thereof.
 # === UCSF ChimeraX Copyright ===
+__version__ = "1.1.0"
 
 from chimerax.core.toolshed import BundleAPI
 from .cxservices_job import CxServicesJob
