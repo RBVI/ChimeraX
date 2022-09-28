a8fd427bfc7ebad1a662a5ba75db1dbb58b7188c
Eric Pettersen <pett@cgl.ucsf.edu>
2022-09-27 18:08:13 -0700

    Forgot to write out all files before previous commit

--- src/bundles/add_charge/src/tool.py
+++ src/bundles/add_charge/src/tool.py
@@ -49,91 +49,9 @@ class AddChargeTool(ToolInstance):
         if dock_prep_info is not None:
             self.tool_window.title = "%s for %s" % (tool_name, dock_prep_info.process_name.capitalize())
             self.structure_list.setEnabled(False)
-        """
-        self.isolation = QCheckBox("Consider each model in isolation from all others")
-        self.isolation.setChecked(True)
-        layout.addWidget(self.isolation, alignment=Qt.AlignCenter)
 
-        layout.addSpacing(10)
-
-        method_groupbox = QGroupBox("Method")
-        method_layout = QVBoxLayout()
-        method_groupbox.setLayout(method_layout)
-        layout.addWidget(method_groupbox, alignment=Qt.AlignCenter)
-        self.method_group = QButtonGroup()
-        self.steric_method = QRadioButton("steric only")
-        self.method_group.addButton(self.steric_method)
-        method_layout.addWidget(self.steric_method, alignment=Qt.AlignLeft)
-        self.hbond_method = QRadioButton("also consider H-bonds (slower)")
-        self.method_group.addButton(self.hbond_method)
-        method_layout.addWidget(self.hbond_method, alignment=Qt.AlignLeft)
-        self.hbond_method.setChecked(True)
-
-        layout.addSpacing(10)
-
-        # In Chimera, there was an option for specifying protonation states on a residue-by-residue
-        # basis in the GUI.  Not implementing that here (and in command) until a proven need exists.
-        self.options_area = QWidget()
-        layout.addWidget(self.options_area)
-        self.options_area.setHidden(True)
-        options_layout = QVBoxLayout()
-        options_layout.setContentsMargins(0, 0, 0, 0)
-        options_layout.setSpacing(0)
-        self.options_area.setLayout(options_layout)
-        protonation_res_layout = QHBoxLayout()
-        options_layout.addLayout(protonation_res_layout)
-        protonation_res_layout.addWidget(QLabel("Protonation states for:"), alignment=Qt.AlignRight)
-        self.protonation_res_button = QPushButton()
-        protonation_res_layout.addWidget(self.protonation_res_button, alignment=Qt.AlignLeft)
-        prot_menu = QMenu(self.protonation_res_button)
-        self.protonation_res_button.setMenu(prot_menu)
-        prot_menu.triggered.connect(lambda act: self._protonation_res_change(act.text()))
-        self.prot_widget_lookup = {}
-        self.prot_arg_lookup = {}
-        for res_abbr, res_name, explanation, charged in [
-            ("ASP", "aspartic acid", "ASP/ASH = negatively charged/neutral [protonated OD2]", True),
-            ("CYS", "cysteine", "CYS/CYM = unspecified/negatively charged", False),
-            ("GLU", "glutamic acid", "GLU/GLH = negatively charged/neutral [protonated OE2]", True),
-            ("HIS", "histidine", "HIS/HID/HIE/HIP = unspecified/delta/epsilon/both", False),
-            ("LYS", "lysine", "LYS/LYN = positively charged/neutral", True),
-        ]:
-            self.prot_arg_lookup[res_name] = res_abbr
-            prot_menu.addAction(res_name)
-            box = QGroupBox()
-            options_layout.addWidget(box)
-            box.setHidden(True)
-            box_layout = QVBoxLayout()
-            box.setLayout(box_layout)
-            grp = QButtonGroup()
-            self.prot_widget_lookup[res_name] = (box, grp)
-            b1 = QRadioButton(f"Residue-name-based\n({explanation})")
-            box_layout.addWidget(b1, alignment=Qt.AlignLeft)
-            grp.addButton(b1)
-            b2 = QRadioButton("Charged" if charged else "Unspecified (determined by method)")
-            box_layout.addWidget(b2, alignment=Qt.AlignLeft)
-            grp.addButton(b2)
-            b1.setChecked(True)
-        self._protonation_res_change("histidine")
-
-        from chimerax.ui.options import OptionsPanel, FloatOption, BooleanOption
-        op1 = OptionsPanel(sorting=False, scrolled=False, contents_margins=(6,0,6,0))
-        from .cmd import metal_dist_default
-        self.metal_option = FloatOption("",
-            metal_dist_default, None, min=0.0, max=99.9,
-            left_text="Do not protonate electronegative atom X within",
-            right_text="Å of metal M")
-        op1.add_option(self.metal_option)
-        options_layout.addWidget(op1)
-        options_layout.addWidget(QLabel("if X-H-M angle would be >120°"), alignment=Qt.AlignTop|Qt.AlignHCenter)
-        self.template_checkbox = QCheckBox("Use idealized template to guess "
-            "atom types in nonstandard residues")
-        self.template_checkbox.setToolTip(
-            "If a non-standard residue has an entry in the PDB Chemical Component Dictionary,\n"
-            "use the idealized coordinates from the entry for atom typing rather than the actual\n"
-            "coordinates from the structure.  This means the residue name has to correspond to the\n"
-            "component name in the dictionary.")
-        options_layout.addWidget(self.template_checkbox, alignment=Qt.AlignCenter)
-        """
+        self.standardize_button = QCheckBox('"Standardize" certain residues:')
+        self.standardize_button.setChecked(True)
 
         from Qt.QtWidgets import QDialogButtonBox as qbbox
         bbox = qbbox(qbbox.Ok | qbbox.Cancel | qbbox.Help)
