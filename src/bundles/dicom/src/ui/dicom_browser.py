3dd0cbc1b982cfde486b9c03e4f81682919c4203
Zach Pearson <zach@zjp.codes>
2022-11-21 16:30:15 -0800

    bundles/dicom: Event-driven DICOM Browser

--- src/bundles/dicom/src/ui/dicom_browser.py
+++ src/bundles/dicom/src/ui/dicom_browser.py
@@ -10,11 +10,11 @@
 # or derivations thereof.
 # === UCSF ChimeraX Copyright ===
 from itertools import chain
-
+from typing import Optional
 from Qt.QtWidgets import (
     QVBoxLayout, QHBoxLayout, QHeaderView
     , QWidget, QLabel, QAbstractItemView
-    , QPushButton
+    , QPushButton, QAction
 )
 
 from chimerax.core.models import (
@@ -27,6 +27,7 @@ from chimerax.ui import MainToolWindow
 
 from .widgets import DICOMTable
 from . import DICOMMetadata
+from ..dicom_hierarchy import Patient, Study, Series
 
 try:
     # We are inside GUI ChimeraX
@@ -50,15 +51,25 @@ finally:
         _logger = logging.getLogger()
         _logger.status = _logger.info
 
-# TODO: Make singleton
 class DICOMBrowserTool(ToolInstance):
+
+    patients = []
+
     def __init__(self, session = None, name = "DICOM Browser"):
         """Bring up a tool to explore DICOM models open in the session."""
         session = session or _session
         super().__init__(session, name)
 
+        self.patients = []
+
+        # On first startup, look and see if there are any patients that need to be
+        # shown.
+        for model in session.models:
+            if type(model) is Patient:
+                self.patients.append(model)
+
         # Construct the GUI
-        self.tool_window = MainToolWindow(self)
+        self.tool_window = MainToolWindow(self, close_destroys=False)
         self.parent = self.tool_window.ui_area
         self.main_layout = QVBoxLayout()
 
@@ -73,68 +84,120 @@ class DICOMBrowserTool(ToolInstance):
         self.load_button_container.addWidget(self.load_pt_button)
         self.load_button_container.addWidget(self.load_st_button)
         self.load_button_container.addWidget(self.load_sr_button)
-        # self.load_pt_button.clicked.connect(lambda: self.patient_metadata(self.patient_table.selected))
-        # self.load_st_button.clicked.connect(lambda: self.study_metadata(self.study_table.selected))
-        # self.load_sr_button.clicked.connect(lambda: self.series_metadata(self.series_table.selected))
+        self.load_pt_button.clicked.connect(lambda: self.patient_metadata(self.patient_table.selected))
+        self.load_st_button.clicked.connect(lambda: self.study_metadata(self.study_table.selected))
+        self.load_sr_button.clicked.connect(lambda: self.series_metadata(self.series_table.selected))
 
         self.load_buttons_widget.setLayout(self.load_button_container)
 
-        # self.patient_control_widget = QWidget(self.parent)
-        # self.study_control_widget = QWidget(self.parent)
-        # self.series_control_widget = QWidget(self.parent)
-        # self.patient_control_widget.setVisible(False)
-        # self.study_control_widget.setVisible(False)
-        # self.series_control_widget.setVisible(False)
+        self.patient_control_widget = QWidget(self.parent)
+        self.study_control_widget = QWidget(self.parent)
+        self.series_control_widget = QWidget(self.parent)
+        self.patient_control_widget.setVisible(False)
+        self.study_control_widget.setVisible(False)
+        self.series_control_widget.setVisible(False)
 
-        # self.patient_table = DICOMTable(self.patient_control_widget, None, self.parent)
-        # self.study_table = DICOMTable(self.study_control_widget, None, self.parent)
-        # self.series_table = DICOMTable(self.series_control_widget, None, self.parent)
+        self.patient_table = DICOMTable(self.patient_control_widget, None, self.parent)
+        self.study_table = DICOMTable(self.study_control_widget, None, self.parent)
+        self.series_table = DICOMTable(self.series_control_widget, None, self.parent)
 
         self.patient_label = QLabel("Patients")
         self.study_label = QLabel("Studies")
         self.series_label = QLabel("Series")
 
         # Set the selected patient to whichever happens to be first
-        # self.patient_table.data = self.models
-        # self.study_table.data = list(chain.from_iterable([x.studies for x in chain(self.models)]))
-        # self.series_table.data = list(chain.from_iterable([x.series for x in self.study_table.data]))
+        self.patient_table.data = self.patients
+        self.study_table.data = list(chain.from_iterable([x.studies for x in chain(list(self.patients))]))
+        self.series_table.data = list(chain.from_iterable([x.series for x in self.study_table.data]))
 
-        # self._set_table_columns()
+        self._set_table_columns()
 
-        # self.patient_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        # self.study_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        # self.series_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.patient_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.study_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.series_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
 
-        # self.patient_table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        #self.study_table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        #self.series_table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.patient_table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.study_table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.series_table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
-        # self.patient_table.get_selection.connect(self.on_patient_highlighted)
+        self.patient_table.selection_changed.connect(self.on_patient_highlighted)
 
         self.main_layout.addWidget(self.patient_label)
-        # self.main_layout.addWidget(self.patient_table)
+        self.main_layout.addWidget(self.patient_table)
         self.main_layout.addWidget(self.study_label)
-        # self.main_layout.addWidget(self.study_table)
+        self.main_layout.addWidget(self.study_table)
         self.main_layout.addWidget(self.series_label)
-        # self.main_layout.addWidget(self.series_table)
+        self.main_layout.addWidget(self.series_table)
 
         self.main_layout.addWidget(self.load_buttons_widget)
 
+        self.tool_window.fill_context_menu = self.fill_context_menu
         self.tool_window.ui_area.setLayout(self.main_layout)
-        # self.patient_table.launch()
-        # self.study_table.launch()
-        # self.series_table.launch()
+        self.patient_table.launch()
+        self.study_table.launch()
+        self.series_table.launch()
         session.triggers.add_handler(
-            ADD_MODELS, lambda *args: self._see_what_happened(*args)
+            ADD_MODELS, lambda *args: self._add_patient(*args)
         )
-        # session.triggers.add_handler(REMOVE_MODELS,
-        #    lambda *args: self._initiate_fill_tree(*args, always_rebuild=True))
-        # session.triggers.add_handler(MODEL_ID_CHANGED,
-        #    lambda *args: self._initiate_fill_tree(*args, always_rebuild=True, countdown=3))
-        # session.triggers.add_handler(MODEL_NAME_CHANGED,
-        #    lambda *args: self._initiate_fill_tree(*args, simple_change=True, countdown=3))
-
+        session.triggers.add_handler(
+            REMOVE_MODELS, lambda *args: self._remove_patient(*args)
+        )
         self.tool_window.manage()
+
+    def fill_context_menu(self, menu, x, y):
+        table = self._hovered_table(x, y)
+        if table:
+            load_metadata_action = QAction("Load Metadata from Highlighted Entries", menu)
+            load_metadata_action.triggered.connect(lambda: self.load_metadata(table.selected))
+            menu.addAction(load_metadata_action)
+
+    def _hovered_table(self, x, y) -> Optional[QWidget]:
+        # Not sure why but a lot of the time (or always?) the x,y is a widget inside the
+        # table and not the table itself.
+        tables = [self.patient_table, self.study_table, self.series_table]
+        widget = self.tool_window.ui_area.childAt(x, y)
+        if not widget:
+            return None
+        if widget in tables:
+            return widget
+        if widget.parent() in tables:
+            return widget.parent()
+        return None
+
+    def _remove_patient(self, _, models):
+        for model in models:
+            if type(model) is Patient:
+                if model in self.patients:
+                    self.patients.remove(model)
+        self._resync_tables()
+
+    def _add_patient(self, _, models):
+        for model in models:
+            if type(model) is Patient:
+                if model not in self.patients:
+                    self.patients.append(model)
+        self._resync_tables()
+
+    def _resync_tables(self):
+        self.patient_table.data = []
+        self.study_table.data = []
+        self.series_table.data = []
+        self.patient_table.data = self.patients
+        self.study_table.data = list(chain.from_iterable([x.studies for x in chain(list(self.patients))]))
+        self.series_table.data = list(chain.from_iterable([x.series for x in self.study_table.data]))
+
+    def load_metadata(self, data):
+        # Data will always be a uniform list of Patients, Studies, or Series
+        if not data:
+            return
+        if type(data[0]) is Patient:
+            return self.patient_metadata(data)
+        elif type(data[0]) is Study:
+            return self.study_metadata(data)
+        elif type(data[0]) is Series:
+            return self.series_metadata(data)
+        else:
+            return
 
     def patient_metadata(self, selection):
         if len(selection) > 0:
@@ -148,27 +211,18 @@ class DICOMBrowserTool(ToolInstance):
         if len(selection) > 0:
             return DICOMMetadata.from_series(self.session, selection)
 
-    def add_patient(self, patients):
-        if not patients:
-            return
-        else:
-            try:
-                for patient in patients:
-                    self.models[patient.uid] = patient
-            except TypeError:
-                self.models[patients.uid] = patients
-
     def on_patient_highlighted(self):
-        pass
+        selections = self.patient_table.selected
+        if not selections:
+            return
+        self.study_table.data = list(chain.from_iterable([x.studies for x in selections]))
+        self.series_table.data = list(chain.from_iterable([x.series for x in self.study_table.data]))
 
-    def on_series_highlighted(self):
-        pass
-
-    def _check_new_dicoms(self, _, models):
-        # DICOM models will always come in on a patient-wise basis, so only
-        # check the type of the first one
-        if type(models[0]) is Patient:
-            self.add_patient(models)
+    def on_series_highlighted(self, *args, **kwargs):
+        selections = self.study_table.selected
+        if not selections:
+            return
+        self.series_table.data = list(chain.from_iterable([x.series for x in selections]))
 
     def _set_table_columns(self):
         if not self.patient_table:
