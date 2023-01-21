f2bca36dbae5ee441714c27614ccb6c1686fb205
Zach Pearson <zach@zjp.codes>
2023-01-20 22:14:49 -0800

    wheel/tests: Move tests down to their own folders

--- wheel/tests/hbonds/test_hydrogen_bonds.py
+++ wheel/tests/hbonds/test_hydrogen_bonds.py
@@ -9,14 +9,14 @@ from chimerax.atomic import initialize_atomic
 from chimerax.dist_monitor import _DistMonitorBundleAPI
 
 @pytest.mark.dependency(
-    depends=["tests/test_open_pdb.py::test_open_pdb"]
+    depends=["tests/pdb/test_open_pdb.py::test_open_pdb"]
     , scope="session"
 )
 def test_hydrogen_bonds():
     session = Session('cx standalone')
     initialize_atomic(session)
     _DistMonitorBundleAPI.initialize(session)
-    pdb_loc = os.path.join(os.path.dirname(__file__), "data", "pdb", "1ie9.pdb")
+    pdb_loc = os.path.join(os.path.dirname(__file__), "..", "data", "pdb", "1ie9.pdb")
     pdb = open_pdb(session, pdb_loc)
     pdb_model = pdb[0][0]
     session.models.add([pdb_model])
--- wheel/tests/pdb/test_open_pdb.py
+++ wheel/tests/pdb/test_open_pdb.py
@@ -9,6 +9,6 @@ from chimerax.atomic import AtomicStructure
 @pytest.mark.dependency()
 def test_open_pdb():
     session = Session('cx standalone')
-    pdb_loc = os.path.join(os.path.dirname(__file__), "data", "pdb", "1ie9.pdb")
+    pdb_loc = os.path.join(os.path.dirname(__file__), "..", "data", "pdb", "1ie9.pdb")
     pdb = open_pdb(session, pdb_loc)
     assert pdb is not None
