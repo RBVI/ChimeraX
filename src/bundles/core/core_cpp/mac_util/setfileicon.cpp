1aa7cc4cc4d66887df0a51bcd8112691be0073be
Zach Pearson <zjp@cgl.ucsf.edu>
2023-02-13 11:33:06 -0800

    core/mac_util: PY_SSIZE_T_CLEAN is mandatory in Python >= 3.10

--- src/bundles/core/core_cpp/mac_util/setfileicon.cpp
+++ src/bundles/core/core_cpp/mac_util/setfileicon.cpp
@@ -10,7 +10,7 @@
  * or derivations thereof.
  * === UCSF ChimeraX Copyright ===
  */
-
+#define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
 #include "setfileicon_mac.h"
@@ -18,7 +18,7 @@
 extern "C" PyObject *set_file_icon(PyObject *, PyObject *args, PyObject *keywds)
 {
   const char *file_path, *image_data;
-  int image_bytes;
+  Py_ssize_t image_bytes;
   const char *kwlist[] = {"file_path", "image_data", NULL};
   if (!PyArg_ParseTupleAndKeywords(args, keywds, const_cast<char *>("sz#"),
 				   (char **)kwlist,
--- src/bundles/core/core_cpp/mac_util/setfileicon_mac.h
+++ src/bundles/core/core_cpp/mac_util/setfileicon_mac.h
@@ -15,7 +15,8 @@
 //
 #ifndef SETFILEICON_MAC_HEADER_INCLUDED
 #define SETFILEICON_MAC_HEADER_INCLUDED
-
-extern "C" int set_mac_file_icon(const char *file_path_utf8, const char *image_data, int image_bytes);
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+extern "C" int set_mac_file_icon(const char *file_path_utf8, const char *image_data, Py_ssize_t image_bytes);
 
 #endif
