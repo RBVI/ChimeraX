22728d423b925f65dbe021d785b8b641d0d0956e
Tom Goddard <goddard@sonic.net>
2023-05-15 18:03:06 -0700

    Fix energy minimization in AlphaFold predictions.  Bug #8975
    Also fixed log messages on second prediction using same Colab session not appearing.

--- src/bundles/alphafold/src/colabfold_predict.py
+++ src/bundles/alphafold/src/colabfold_predict.py
@@ -48,10 +48,12 @@ def run_prediction(sequences,
     # Write sequences, used only for inclusion in returned results.
     with open('query.fasta', 'w') as seqs_file:
         seqs_file.write(''.join(f'>{i+1}\n{seq}\n' for i,seq in enumerate(sequences)))
-                        
-    from colabfold.utils import setup_logging
+
     from pathlib import Path
-    setup_logging(Path(".").joinpath("log.txt"))
+    from colabfold import utils
+    if not hasattr(utils, 'setup_logging_done'):
+        utils.setup_logging(Path(".").joinpath("log.txt"))
+        utils.setup_logging_done = True
 
     # Avoid various FutureWarning message from deprecated jax features
     import warnings
@@ -291,14 +293,10 @@ def use_utf8_encoding():
   # https://www.rbvi.ucsf.edu/trac/ChimeraX/ticket/8313
   import locale
   if locale.getpreferredencoding() != 'UTF-8':
-      try:
-          import _locale
-          _locale.nl_langinfo_orig = _locale.nl_langinfo
-          def nl_langinfo_always_utf8(i):
-              return 'UTF-8' if i == _locale.CODESET else _locale.nl_langinfo_orig(i)
-          _locale.nl_langinfo = nl_langinfo_always_utf8
-      except:
-          pass  # Probably Python newer than 3.8 where private _locale changed.
+      locale.getpreferredencoding_orig = locale.getpreferredencoding
+      def get_preferred_encoding_utf8(do_setlocale=True):
+          return 'UTF-8'
+      locale.getpreferredencoding = get_preferred_encoding_utf8
     
 # ================================================================================================
 #
@@ -349,7 +347,7 @@ def install(use_amber = False, use_templates = False, install_log = 'install_log
     cmds = f'''
 set -e
 # We have to use "--no-warn-conflicts" because colab already has a lot preinstalled with requirements different to ours
-pip install --no-warn-conflicts "colabfold[alphafold-minus-jax] @ git+https://github.com/sokrypton/ColabFold@5b3fc193e880cd9599f91cd16fcb1fe69f7759f2"
+pip install --no-warn-conflicts "colabfold[alphafold-minus-jax] @ git+https://github.com/sokrypton/ColabFold@dc9fc3d03379d23784e796f4c7fd31d173bafaa2"
 # high risk high gain
 pip uninstall jaxlib -y
 pip install "jax[cuda11_cudnn805]==0.3.24" jaxlib==0.3.24+cuda11.cudnn805 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
@@ -384,9 +382,10 @@ touch HH_READY
     cmds = f'''  
 # setup openmm for amber refinement
 set -e
-conda install -y -q -c conda-forge openmm=7.5.1 python={python_version} pdbfixer 2>&1 1>/dev/null
+conda install -y -q -c conda-forge openmm=7.7.0 python={python_version} pdbfixer 2>&1 1>/dev/null
 # Make colab python find conda openmm and pdbfixer
 ln -s /usr/local/lib/python{python_version}/site-packages/simtk .
+ln -s /usr/local/lib/python{python_version}/site-packages/openmm .
 ln -s /usr/local/lib/python{python_version}/site-packages/pdbfixer .
 touch AMBER_READY
 '''
@@ -427,9 +426,6 @@ sequences = 'Paste a sequences separated by commas here'  #@param {type:"string"
 # Remove options from list of sequences
 seq_list = [seq.strip() for seq in sequences.split(',')]
 dont_minimize = remove_from_list(seq_list, 'dont_minimize')		# Energy minimization
-if not dont_minimize:
-    dont_minimize = True
-    print('*** Energy minimization was broken by a Google Colab update from Python 3.9 to 3.10 on April 28, 2023.  Expect fix by May 10, 2023 ***')
 use_templates = remove_from_list(seq_list, 'use_pdb_templates')
 remove_from_list(seq_list, 'prokaryote')  # Obsolete "prokaryote" flag
 
