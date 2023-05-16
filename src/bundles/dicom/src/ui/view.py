49393d6a01f8407af180dd9728b4dad9f7da3cdb
Zach Pearson <zjp@cgl.ucsf.edu>
2023-05-16 12:46:42 -0700

    DICOM: Make a FourUpView class

--- src/bundles/dicom/src/ui/view.py
+++ src/bundles/dicom/src/ui/view.py
@@ -8,72 +8,57 @@ from .orthoplanes import PlaneViewer, orthoplane_triggers, Axis
 
 medical_types = [DicomGrid, NiftiGrid, NRRDGrid]
 
-_2x2OrthoLayout = None
-_orthoplane_top_left = None
-_orthoplane_bottom_left = None
-_orthoplane_bottom_right = None
-_currentLayout = "default"
+from Qt.QtWidgets import QWidget, QGridLayout
 
-from Qt.QtWidgets import QWidget, QGridLayout, QHBoxLayout, QLabel
+
+class FourUpView(QWidget):
+    def __init__(self, session):
+        super().__init__()
+        self.session = session
+        self._graphics_area = session.ui.main_window.graphicsArea()
+
+        self._newMWLayout = QGridLayout(parent=self)
+
+        self._orthoplane_top_left = PlaneViewer(session, Axis.AXIAL, parent=self)
+        self._orthoplane_bottom_left = PlaneViewer(session, Axis.CORONAL, parent=self)
+        self._orthoplane_bottom_right = PlaneViewer(session, Axis.SAGGITAL, parent=self)
+
+        self._newMWLayout.addWidget(self._orthoplane_top_left, 0, 0)
+        self._newMWLayout.addWidget(session.ui.main_window.graphicsArea(), 0, 1)
+        self._newMWLayout.addWidget(self._orthoplane_bottom_left, 1, 0)
+        self._newMWLayout.addWidget(self._orthoplane_bottom_right, 1, 1)
+
+        self._newMWLayout.setContentsMargins(0, 0, 0, 0)
+        self._newMWLayout.setSpacing(0)
+
+        self.setLayout(self._newMWLayout)
+
+    @property
+    def graphics_area(self):
+        return self._graphics_area
+
 
 def dicom_view(session, arg, force = False):
-    global _2x2OrthoLayout, _currentLayout, _orthoplane_top_left, _orthoplane_bottom_left, _orthoplane_bottom_right
     # TODO: Enable for NIfTI and NRRD as well
     open_volumes = [v for v in session.models if type(v) is Volume]
     medical_volumes = [m for m in open_volumes if type(m.data) in medical_types]
     if not force and not medical_volumes:
         session.logger.error("No medical images open")
         return
-    if arg == "default" and _currentLayout != "default":
-        if _2x2OrthoLayout:
-            session.ui.main_window.restore_default_main_view()
-            _orthoplane_top_left.delete()
-            _orthoplane_bottom_left.delete()
-            _orthoplane_bottom_right.delete()
-            del _orthoplane_top_left
-            del _orthoplane_bottom_left
-            del _orthoplane_bottom_right
-            del _2x2OrthoLayout
-            _orthoplane_top_left = None
-            _orthoplane_bottom_left = None
-            _orthoplane_bottom_right = None
-            _2x2OrthoLayout = None
-            _currentLayout = "default"
+    if arg == "default" and session.ui.main_window.view_layout != "default":
+        session.ui.main_window.restore_default_main_view()
         for trigger in orthoplane_triggers:
             session.triggers.delete_trigger(trigger)
-    elif arg == "orthoplanes" and _currentLayout != "orthoplanes":
-        if _2x2OrthoLayout is None:
-            for trigger in orthoplane_triggers:
-                session.triggers.add_trigger(trigger)
-
-            newCentralWidget = QWidget()
-            newMWLayout = QGridLayout()
-
-            newCentralWidget.setLayout(newMWLayout)
-
-            _orthoplane_top_left = PlaneViewer(session, Axis.AXIAL, parent=newCentralWidget)
-            _orthoplane_bottom_left = PlaneViewer(session, Axis.CORONAL, parent=newCentralWidget)
-            _orthoplane_bottom_right = PlaneViewer(session, Axis.SAGGITAL, parent=newCentralWidget)
-
-            newMWLayout.addWidget(_orthoplane_top_left, 0, 0)
-            newMWLayout.addWidget(session.ui.main_window.graphicsArea(), 0, 1)
-            newMWLayout.addWidget(_orthoplane_bottom_left, 1, 0)
-            newMWLayout.addWidget(_orthoplane_bottom_right, 1, 1)
-
-            newMWLayout.setContentsMargins(0, 0, 0, 0)
-            newMWLayout.setSpacing(0)
-
-            _2x2OrthoLayout = newCentralWidget
-            session.ui.main_window.main_view = _2x2OrthoLayout
-        _currentLayout = "orthoplanes"
-
-def _reset_view(session):
-    pass
+    elif arg == "orthoplanes" and session.ui.main_window.view_layout != "fourup":
+        for trigger in orthoplane_triggers:
+            session.triggers.add_trigger(trigger)
+        session.ui.main_window.main_view = FourUpView(session)
+        session.ui.main_window.view_layout = "fourup"
 
 def _check_rapid_access(*args):
-    global _currentLayout
-    if _currentLayout != "default":
-        session = args[1][0].session
+    session = args[1][0].session
+    if session.ui.main_window.view_layout != "default":
+        # TODO: Only do this if all the models have been removed.
         dicom_view(session, "default", force=True)
 
 dicom_view_desc = CmdDesc(
