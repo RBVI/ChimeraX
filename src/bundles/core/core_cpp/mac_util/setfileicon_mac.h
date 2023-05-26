bf16e76ccb62402ffdfdeba566a33f5630dd519b
Zach Pearson <zjp@cgl.ucsf.edu>
2023-05-25 17:18:10 -0700

    Revert "core/mac_util: PY_SSIZE_T_CLEAN is mandatory in Python >= 3.10"
    This reverts commit 1aa7cc4cc4d66887df0a51bcd8112691be0073be.

--- src/bundles/core/core_cpp/mac_util/setfileicon_mac.h
+++ src/bundles/core/core_cpp/mac_util/setfileicon_mac.h
@@ -15,8 +15,7 @@
 //
 #ifndef SETFILEICON_MAC_HEADER_INCLUDED
 #define SETFILEICON_MAC_HEADER_INCLUDED
-#define PY_SSIZE_T_CLEAN
-#include <Python.h>
-extern "C" int set_mac_file_icon(const char *file_path_utf8, const char *image_data, Py_ssize_t image_bytes);
+
+extern "C" int set_mac_file_icon(const char *file_path_utf8, const char *image_data, int image_bytes);
 
 #endif
