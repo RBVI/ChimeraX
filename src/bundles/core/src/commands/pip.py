ea2f6b55e7c00be4a1957a20c3f4b9de63cb7f06
Zach Pearson <zjp@cgl.ucsf.edu>
2023-06-16 13:12:58 -0700

    Core: Move useful parts of pip.py to python_utils.py

--- src/bundles/core/src/commands/pip.py
+++ src/bundles/core/src/commands/pip.py
@@ -11,9 +11,6 @@
 # === UCSF ChimeraX Copyright ===
 import os
 import subprocess
-import sys
-import shutil
-import glob
 
 from packaging.requirements import Requirement, InvalidRequirement
 
@@ -21,8 +18,9 @@ from .cli import BoolArg, EnumOf, StringArg, CmdDesc
 
 from ..session import Session
 from ..errors import UserError
+from ..python_utils import chimerax_python_executable, chimerax_user_base, migrate_site_packages
 
-__all__ = ['pip', 'pip_desc', 'register_pip_commands']
+__all__ = ['pip', 'pip_desc', 'register_command']
 
 def pip(
     session: Session
@@ -31,103 +29,62 @@ def pip(
     , upgrade: bool = False
     , verbose: bool = False
 ):
-    from chimerax import app_dirs
-    using_chimerax = "chimerax" in sys.executable.split(os.sep)[-1].lower()
-    old_pythonuserbase = os.environ.get('PYTHONUSERBASE', None)
-    os.environ['PYTHONUSERBASE'] = app_dirs.user_data_dir
-    if not using_chimerax:
-        pip_cmd = [sys.executable]
-    else:
-        exe_parted_out = sys.executable.split(os.sep)[:-1]
-        executable_prefix = os.sep.join(["", os.path.join(*exe_parted_out)])
-        # TODO: in 3.11, glob gets a root_dir parameter that makes this unnecessary
-        old_cwd = os.getcwd()
-        os.chdir(executable_prefix)
-        cx_py_exe = glob.glob("[Pp]ython*")[0]
-        exe_parted_out.append(cx_py_exe)
-        path_to_cx_py_exe = os.sep.join([executable_prefix, cx_py_exe])
-        os.chdir(old_cwd)
-        pip_cmd = [path_to_cx_py_exe]
-    pip_cmd.extend(["-m", "pip"])
-    if action == 'install':
-        if not package:
-            raise UserError("Can't possibly install an unspecified package.")
+    with chimerax_user_base():
+        from chimerax import app_dirs
+        site_packages = os.path.join(app_dirs.user_data_dir, "site-packages")
+        if not os.path.islink(site_packages):
+        #    # #8927 -- check if there's an existing site-packages directory and, if so,
+        #    # move it to user_data_dir/lib/python3.x/site-packages, then symbolically
+        #    # link it back to its old location
+            migrate_site_packages()
+        pip_cmd = [chimerax_python_executable(), "-m", "pip"]
+        if action == 'install':
+            if not package:
+                raise UserError("Can't possibly install an unspecified package.")
+            else:
+                if not package.endswith(".tar.gz"):
+                    try:
+                        _ = Requirement(package)
+                    except InvalidRequirement:
+                        raise UserError("Can't install package: invalid requirement specified.")
+                pip_cmd.extend(["install" , "--user"])
+                if upgrade:
+                    pip_cmd.extend(["--upgrade"])
+                pip_cmd.extend(["%s" % package])
+                # If we don't add this flag then pip complains that distutils and sysconfig
+                # don't report the same location for the user's site packages directory. The
+                # error tells programmers to report the error to
+                # https://github.com/pypa/pip/issues/10151
+                pip_cmd.extend(["--no-warn-script-location"])
+        elif action == 'uninstall':
+            if not package:
+                raise UserError("Can't possibly uninstall an unspecified package.")
+            else:
+                if package.lower().startswith("chimerax"):
+                    raise UserError("Uninstalling this package could compromise ChimeraX; refusing to execute.")
+                else:
+                    pip_cmd.extend(["uninstall", "-y", "%s" % package])
+        elif action == 'list':
+            pip_cmd.extend(["list"])
+        elif action == 'check':
+            pip_cmd.extend(["check"])
+        elif action == 'show':
+            if not package:
+                raise UserError("Can't possibly show an unspecified package.")
+            pip_cmd.extend(["show", "%s" % package])
+        elif action == 'search':
+            if not package:
+                raise UserError("Can't possibly search for an unspecified package.")
+            pip_cmd.extend(["search", "%s" % package])
+        elif action == 'debug':
+           pip_cmd.extend(["debug"])
         else:
-            if not package.endswith(".tar.gz"):
-                try:
-                    req = Requirement(package)
-                except InvalidRequirement:
-                    raise UserError("Can't install package: invalid requirement specified.")
-            pip_cmd.extend(["install" , "--user"])
-            if upgrade:
-                pip_cmd.extend(["--upgrade"])
-            pip_cmd.extend(["%s" % package])
-            # If we don't add this flag then pip complains that distutils and sysconfig
-            # don't report the same location for the user's site packages directory. The
-            # error tells programmers to report the error to
-            # https://github.com/pypa/pip/issues/10151
-            pip_cmd.extend(["--no-warn-script-location"])
-            # #8927 -- check if there's an existing site-packages directory and, if so,
-            # move it to user_data_dir/lib/python3.x/site-packages, then symbolically
-            # link it back to its old location
-            user_site_packages = os.path.join(app_dirs.user_data_dir, "site-packages")
-            if sys.platform == "win32":
-                ver_info = "".join([str(x) for x in sys.version_info[:2]])
-                py_ver = "".join(["Python", ver_info])
-                real_site_dir = os.path.join(app_dirs.user_data_dir, py_ver, "site-packages")
-            else:
-                ver_info = ".".join([str(x) for x in sys.version_info[:2]])
-                py_ver = "".join(["python", ver_info])
-                real_site_dir = os.path.join(app_dirs.user_data_dir, "lib", py_ver, "site-packages")
-            if os.path.exists(user_site_packages):
-                if not os.path.islink(user_site_packages):
-                    if os.path.exists(real_site_dir):
-                        # This path should be unreachable, since a user will always either transition from
-                        # the old scheme and hit the else clause here or neither site-packages will exist and
-                        # they'll hit the else clause of the outer if
-                        ...
-                    else:
-                        os.makedirs(os.path.dirname(real_site_dir))
-                        shutil.move(user_site_packages, real_site_dir)
-                        # On Windows, we have to use a junction since a symlink requires
-                        # elevated privileges
-                        if sys.platform == "win32":
-                            subprocess.run('mklink /J "%s" "%s"' % (user_site_packages, real_site_dir), shell=True)
-                        else:
-                            os.symlink(real_site_dir, user_site_packages)
-            else:
-                os.makedirs(real_site_dir)
-                os.symlink(real_site_dir, user_site_packages)
-    elif action == 'uninstall':
-        if not package:
-            raise UserError("Can't possibly uninstall an unspecified package.")
-        else:
-            if package.lower().startswith("chimerax"):
-                raise UserError("Uninstalling this package could compromise ChimeraX; refusing to execute.")
-            else:
-                pip_cmd.extend(["uninstall", "-y", "%s" % package])
-    elif action == 'list':
-        pip_cmd.extend(["list"])
-    elif action == 'check':
-        pip_cmd.extend(["check"])
-    elif action == 'show':
-        if not package:
-            raise UserError("Can't possibly show an unspecified package.")
-        pip_cmd.extend(["show", "%s" % package])
-    elif action == 'search':
-        if not package:
-            raise UserError("Can't possibly search for an unspecified package.")
-        pip_cmd.extend(["search", "%s" % package])
-    elif action == 'debug':
-       pip_cmd.extend(["debug"])
-    else:
-        raise UserError("Unsupported action. Please use one of [install, uninstall, list, check, show, search, debug].")
-    if verbose:
-        pip_cmd.extend(["--verbose"])
-    res = subprocess.run(pip_cmd, capture_output=True)
-    os.environ['PYTHONUSERBASE'] = old_pythonuserbase or ''
-    session.logger.info(res.stdout.decode('utf-8'))
-    session.logger.info(res.stderr.decode('utf-8'))
+            raise UserError("Unsupported action. Please use one of [install, uninstall, list, check, show, search, debug].")
+        if verbose:
+            pip_cmd.extend(["--verbose"])
+        res = subprocess.run(pip_cmd, capture_output=True)
+        session.logger.info(res.stdout.decode('utf-8'))
+        session.logger.info(res.stderr.decode('utf-8'))
 
 pip_desc = CmdDesc(
     required=[
---
+++ src/bundles/core/src/python_utils.py
@@ -0,0 +1,79 @@
+# vim: set et sw=4 sts=4:
+
+import glob
+import os
+import shutil
+import subprocess
+import sys
+
+from contextlib import contextmanager
+
+"""Utilities for managing the Python executable ChimeraX depends on."""
+
+def chimerax_python_executable():
+    """Find the Python executable that comes with ChimeraX"""
+    using_chimerax = "chimerax" in sys.executable.split(os.sep)[-1].lower()
+    if not using_chimerax:
+        exe = sys.executable
+    else:
+        exe_parted_out = sys.executable.split(os.sep)[:-1]
+        exe_prefix = os.sep.join(["", os.path.join(*exe_parted_out)])
+        if sys.version_info.minor < 11:
+            old_cwd = os.getcwd()
+            os.chdir(exe_prefix)
+            cx_py_exe = glob.glob("[Pp]ython*")[0]
+            os.chdir(old_cwd)
+        else:
+            cx_py_exe = glob.glob("[Pp]ython*", root_dir = exe_prefix)[0]
+        exe = os.sep.join([exe_prefix, cx_py_exe])
+    return exe
+
+@contextmanager
+def chimerax_user_base():
+    """Make pip install packages to ChimeraX's customary PYTHONUSERBASE.
+
+    Without this context manager, Python will install packages in the traditional
+    user directory at, on macOS, ~/Library/Python/(version)/lib/python/site-packages
+    instead of our location at ~/Library/Application Support/ChimeraX/(cx_version)
+    """
+    from chimerax import app_dirs
+    old_pythonuserbase = os.environ.get('PYTHONUSERBASE', None)
+    os.environ['PYTHONUSERBASE'] = app_dirs.user_data_dir
+    yield
+    os.environ['PYTHONUSERBASE'] = old_pythonuserbase or ''
+
+def migrate_site_packages():
+    """Ensure compliance with both ChimeraX and Python site package conventions
+
+    ChimeraX's customary site-packages directory is, on macOS,
+    ~/Library/Application Support/ChimeraX/(chimerax_release_ver)/site-packages
+    where ~/Library/Application Support/ChimeraX/(chimerax_release_ver) is PYTHONUSERBASE.
+    Despite differences, we try not to fight Python too much, and Python prefers to install
+    packages to PYTHONUSERBASE/lib/python(python_ver)/site-packages.
+
+    To ensure user scripts don't break, we symlink Python's preferred location to our
+    preferred location. See Trac#8927
+    """
+    from chimerax import app_dirs
+    user_site_packages = os.path.join(app_dirs.user_data_dir, "site-packages")
+    lib = "lib"
+    if sys.platform == "win32":
+        lib = ""
+    real_site_dir = os.path.join(app_dirs.user_data_dir, lib, "python", "site-packages")
+    if os.path.exists(user_site_packages):
+        if not os.path.islink(user_site_packages):
+            if os.path.exists(real_site_dir):
+                # This path should be unreachable, since a user will always either transition
+                # from the old scheme and hit the else clause here or neither site-packages
+                # will exist and they'll hit the else clause of the outer if
+                ...
+            else:
+                os.makedirs(os.path.dirname(real_site_dir))
+                shutil.move(user_site_packages, real_site_dir)
+                if sys.platform == "win32":
+                    subprocess.run('mklink /J "%s" "%s"' % (user_site_packages, real_site_dir), shell = True)
+                else:
+                    os.symlink(real_site_dir, user_site_packages)
+    else:
+        os.makedirs(real_site_dir)
+        os.symlink(real_site_dir, user_site_packages)
