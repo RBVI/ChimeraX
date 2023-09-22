ce4d826843f3cfbe750d3daf40287a22366ac6f9
Eric Pettersen <pett@cgl.ucsf.edu>
2023-09-21 17:27:56 -0700

    Experimental session-combining code [#9258]

--- src/bundles/seqalign/src/manager.py
+++ src/bundles/seqalign/src/manager.py
@@ -277,8 +277,13 @@ class AlignmentsManager(StateManager, ProviderManager):
         }
 
     def _ses_restore(self, data):
-        for am in self._alignments.values():
-            am.close()
+        if self.session.restore_options['combine']:
+            for ident in data.keys():
+                try:
+                    existing = self._alignments[ident]
+                except KeyError:
+                    continue
+                existing._destroy()
         self._alignments = data['alignments']
 
 def _register_viewer_subcommands(logger):
