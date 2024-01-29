d56830aa8759046c6df29c61cf77cef119a4a993
Zach Pearson <zjp@cgl.ucsf.edu>
2024-01-29 10:36:04 -0800

    Format nifti with Black

--- src/bundles/nifti/src/nifti.py
+++ src/bundles/nifti/src/nifti.py
@@ -13,12 +13,11 @@
 import math
 
 import nibabel
-from numpy import zeros, uint8, transpose
 
 from chimerax.map.volume import open_grids
-from chimerax.map_data import GridData, allocate_array
+from chimerax.map_data import GridData
 from chimerax.dicom.coordinates import get_coordinate_system
-from chimerax.dicom.types import Segmentation
+
 
 class NifTI:
     def __init__(self, session, data):
@@ -39,6 +38,7 @@ class NifTI:
         models, message = open_grids(self.session, grids, name="FOO")
         return models, message
 
+
 class NiftiData:
     def __init__(self, session, data):
         self.session = session
@@ -49,14 +49,16 @@ class NiftiData:
         self.images = darray
         # TODO: Get the rotation from the NifTI file
         affine = data.affine
-        self.coordinate_system = get_coordinate_system("".join(nibabel.orientations.aff2axcodes(affine)))
+        self.coordinate_system = get_coordinate_system(
+            "".join(nibabel.orientations.aff2axcodes(affine))
+        )
         self.scale = [
-              math.sqrt(affine[0][0]**2 + affine[1][0]**2 + affine[2][0]**2)
-            , math.sqrt(affine[0][1] ** 2 + affine[1][1] ** 2 + affine[2][1] ** 2)
-            , math.sqrt(affine[0][2] ** 2 + affine[1][2] ** 2 + affine[2][2] ** 2)
+            math.sqrt(affine[0][0] ** 2 + affine[1][0] ** 2 + affine[2][0] ** 2),
+            math.sqrt(affine[0][1] ** 2 + affine[1][1] ** 2 + affine[2][1] ** 2),
+            math.sqrt(affine[0][2] ** 2 + affine[1][2] ** 2 + affine[2][2] ** 2),
         ]
         self.center = [affine[0][3], affine[1][3], affine[2][3]]
-        #self.data_rotation =[
+        # self.data_rotation =[
         #            [affine[0][0] / self.scale[0], affine[0][1] / self.scale[1], affine[0][2] / self.scale[2]]
         #        , [affine[1][0] / self.scale[0], affine[1][1] / self.scale[1], affine[1][2] / self.scale[2]]
         #        , [affine[2][0] / self.scale[0], affine[2][1] / self.scale[1], affine[2][2] / self.scale[2]]
@@ -64,11 +66,7 @@ class NiftiData:
         # We previously respected the rotation and that code is commented out above, but
         # respecting the rotation breaks the segmentation viewer and causes the 3D cursors
         # to appear in the wrong place.  So we just use the identity matrix for the rotation.
-        self.data_rotation = [
-            [1, 0, 0]
-            , [0, 1, 0]
-            , [0, 0, 1]
-        ]
+        self.data_rotation = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
         self.data_type = self.images.dtype
         self.slope, self.intercept = data.header.get_slope_inter()
         if self.slope is None:
@@ -78,29 +76,31 @@ class NiftiData:
 
 
 class NiftiGrid(GridData):
-    def __init__(self, nifti, time = None, channel = None):
+    def __init__(self, nifti, time=None, channel=None):
         self.nifti_data = nifti
         GridData.__init__(
-            self, nifti.data_size, nifti.data_type
-            , nifti.center, rotation=nifti.data_rotation, step = nifti.scale
-            # , path = ???, name = ???
-            , file_type = 'nifti' #, time, channel ???
+            self,
+            nifti.data_size,
+            nifti.data_type,
+            nifti.center,
+            rotation=nifti.data_rotation,
+            step=nifti.scale,
+            file_type="nifti",
         )
-        #self.initial_plane_display = True
 
-    def read_matrix(self, ijk_origin = (0,0,0), ijk_size = None,
-                  ijk_step = (1,1,1), progress = None):
+    def read_matrix(
+        self, ijk_origin=(0, 0, 0), ijk_size=None, ijk_step=(1, 1, 1), progress=None
+    ):
         array = self.nifti_data.images[
-            ijk_origin[2]:ijk_origin[2]+ijk_size[2]:ijk_step[2]
-            , ijk_origin[1]:ijk_origin[1]+ijk_size[1]:ijk_step[1]
-            , ijk_origin[0]:ijk_origin[0]+ijk_size[0]:ijk_step[0]
+            ijk_origin[2] : ijk_origin[2] + ijk_size[2] : ijk_step[2],
+            ijk_origin[1] : ijk_origin[1] + ijk_size[1] : ijk_step[1],
+            ijk_origin[0] : ijk_origin[0] + ijk_size[0] : ijk_step[0],
         ]
         return array
-        #if self.nifti_data.slope != 1:
+        # if self.nifti_data.slope != 1:
         #    array *= self.nifti_data.slope
-        #if self.nifti_data.intercept != 0:
+        # if self.nifti_data.intercept != 0:
         #    array += self.nifti_data.intercept
-        #return array
 
     def pixel_spacing(self) -> tuple[float, float, float]:
         return self.nifti_data.scale
