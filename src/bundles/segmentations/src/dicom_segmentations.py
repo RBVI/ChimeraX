a9d8b7d28dfd40008e031c9b3624d3a3f25fb4c0
Zach Pearson <zjp@cgl.ucsf.edu>
2024-01-31 13:05:56 -0800

    No need to import nonexistent module chimerax.map.segmentations

--- src/bundles/segmentations/src/dicom_segmentations.py
+++ src/bundles/segmentations/src/dicom_segmentations.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 import numpy as np
 
-from chimerax.map.segmentation import SegmentationStrategy
+from .segmentation import SegmentationStrategy
 
 from chimerax.dicom.types import Axis
 
