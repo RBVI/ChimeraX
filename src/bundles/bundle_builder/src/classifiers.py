9e9bd4d590c21fe2027effa44d4228eec9050df7
Zach Pearson <zjp@cgl.ucsf.edu>
2024-05-14 17:34:01 -0700

    fix(bundle_builder): In TOML bundles make managers autostart by default
    The spec says that managers' autostart keyword defaults to True, but
    before this commit the code set an omitted autostart to False.

--- src/bundles/bundle_builder/src/classifiers.py
+++ src/bundles/bundle_builder/src/classifiers.py
@@ -99,7 +99,7 @@ class Selector(ChimeraXClassifier):
 
 
 class Manager(ChimeraXClassifier):
-    default_attrs = {"gui-only": False, "autostart": False}
+    default_attrs = {"gui-only": False, "autostart": True}
 
     def __init__(self, name, attrs):
         if not attrs:
