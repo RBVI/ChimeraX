60fc54d6b005e962d6d7ad4b4e18d8716cbb5214
Zach Pearson <zjp@cgl.ucsf.edu>
2024-05-16 16:17:42 -0700

    fix(segmentations): Don't break on save if no segmentations
    Previously if you tried to click the save button with an empty
    segmentations list the tool would give a traceback. That situation
    is now handled gracefully and the user is given a warning explaining
    why the file picker dialog doesn't come up.

--- src/bundles/segmentations/src/__init__.py
+++ src/bundles/segmentations/src/__init__.py
@@ -10,7 +10,7 @@
 # including partial copies, of the software or any revisions
 # or derivations thereof.
 # === UCSF ChimeraX Copyright ===
-__version__ = "3.0.2"
+__version__ = "3.0.3"
 from chimerax.core.toolshed import BundleAPI
 
 from .segmentation import Segmentation, open_grids_as_segmentation
