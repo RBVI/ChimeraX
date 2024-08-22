956f74d0404bcdf8a33b2a7a901cd9b534955d5e
Zach Pearson <zjp@cgl.ucsf.edu>
2024-08-22 16:41:21 -0700

    test: Update conftest.py test_production_session fixture
    This fixture is intended to run a stripped down version of
    chimerax.core.__main__.init() and produce a session more or less
    identical to the NoGui session.

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-@pytest.fixture()
+@pytest.fixture(scope="function")
 def ensure_chimerax_initialized():
     _ensure_chimerax_initialized()
 
@@ -18,13 +18,52 @@ def _ensure_chimerax_initialized():
         chimerax.core.__main__.init(["dummy", "--nogui", "--safemode", "--exit"])
 
 
-@pytest.fixture()
-def test_session(ensure_chimerax_initialized):
-    from chimerax.core.session import Session
+@pytest.fixture(scope="function")
+def test_production_session():
+    # A minimal reproduction of chimerax.core.__main__.init() that sets up a
+    # valid, production-grade NoGUI session for testing.
+    from chimerax.core.__main__ import _set_app_dirs
+    from chimerax.core import version
+    from chimerax.core.session import Session, register_misc_commands
+    from chimerax.core import core_settings
+    from chimerax.core import toolshed
     from chimerax.atomic import initialize_atomic
     from chimerax.dist_monitor import _DistMonitorBundleAPI
+    from chimerax.core.session import register_misc_commands
 
-    session = Session("cx standalone", minimal=True)
-    _DistMonitorBundleAPI.initialize(session)
-    initialize_atomic(session)
-    return session
+    _set_app_dirs(version)
+    session = Session("cx standalone", minimal=False)
+    session.ui.is_gui = False
+    core_settings.init(session)
+    register_misc_commands(session)
+
+    from chimerax.core import attributes
+
+    from chimerax.core.nogui import NoGuiLog
+
+    session.logger.add_log(NoGuiLog())
+
+    attributes.RegAttrManager(session)
+
+    toolshed.init(
+        session.logger,
+        debug=session.debug,
+        check_available=False,
+        remote_url=toolshed.default_toolshed_url(),
+        session=session,
+    )
+
+    session.toolshed = toolshed.get_toolshed()
+
+    session.toolshed.bootstrap_bundles(session, safe_mode=False)
+    from chimerax.core import tools
+
+    session.tools = tools.Tools(session, first=True)
+    from chimerax.core import undo
+
+    session.undo = undo.Undo(session, first=True)
+
+    # _DistMonitorBundleAPI.initialize(session)
+    # initialize_atomic(session)
+    yield session
+    del session
