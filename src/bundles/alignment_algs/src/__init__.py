719363d5290ac4a93ea4d14c919e6f213317221e
Zach Pearson <zjp@cgl.ucsf.edu>
2024-08-27 20:04:25 -0700

    build: Convert alignment_algs to TOML

--- src/bundles/alignment_algs/src/__init__.py
+++ src/bundles/alignment_algs/src/__init__.py
@@ -21,16 +21,21 @@
 # This notice must be embedded in or attached to all copies, including partial
 # copies, of the software or any revisions or derivations thereof.
 # === UCSF ChimeraX Copyright ===
+__version__ = "2.0.2"
 import os
+
 
 def path_to_src() -> str:
     return os.path.dirname(__file__)
 
+
 def get_lib() -> str:
     return path_to_src()
 
+
 def get_include() -> str:
     return os.path.join(path_to_src(), "include")
+
 
 from . import _sw as SmithWaterman
 
