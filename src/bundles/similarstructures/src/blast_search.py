5a9e91a3fd0de17840603b043d81880afe052528
Tom Goddard <goddard@sonic.net>
2024-10-04 20:36:52 -0700

    Remove wait option from sequence blast command.

--- src/bundles/similarstructures/src/blast_search.py
+++ src/bundles/similarstructures/src/blast_search.py
@@ -25,7 +25,7 @@
 def sequence_blast(session, chain, database = 'pdb',
                    evalue_cutoff = 1e-3, max_hits = 1000,
                    trim = None, alignment_cutoff_distance = None,
-                   save_directory = None, wait = False):
+                   save_directory = None):
     '''Search PDB for similar sequences and display results in a table.'''
 
     from chimerax.blastprotein import blastprotein
