3be30a8c90f24ca1e44d05e85012ace6f4cea54c
Tom Goddard <goddard@sonic.net>
2024-10-10 18:20:43 -0700

    Added deep mutational scan scatter plot menu entries to color mutations of nearby residues.

--- src/bundles/deep_mutational_scan/src/dms_scatter_plot.py
+++ src/bundles/deep_mutational_scan/src/dms_scatter_plot.py
@@ -137,9 +137,13 @@ class ResidueScatterPlot(Graph):
             rname = name[:-1]
             self.add_menu_entry(menu, f'Mutation {name}', lambda: None)
             self.add_menu_entry(menu, f'Color mutations for {rname}',
-                                lambda self=self, rnamen=rname: self._color_residue_on_plot(rname))
+                                lambda self=self, r=r: self._color_residue_mutations(r))
+            self.add_menu_entry(menu, f'Color mutations near residue {rname}',
+                                lambda self=self, r=r: self._color_near(r))
 
+        self.add_menu_entry(menu, f'Color mutations for selected residues', self._color_selected)
         self.add_menu_entry(menu, f'Color synonymous mutations blue', self._color_synonymous)
+        self.add_menu_entry(menu, f'Clear plot colors', self._clear_colors)
 
         if item is not None:
             self.add_menu_separator(menu)
@@ -148,45 +152,84 @@ class ResidueScatterPlot(Graph):
             else:
                 rname = f'{r.one_letter_code}{r.number}'
                 self.add_menu_entry(menu, f'Structure residue {rname}', lambda: None)
-                self.add_menu_entry(menu, f'Select', lambda self=self, r=r: self._select_residue(r))
-                self.add_menu_entry(menu, f'Color green', lambda self=self, r=r: self._highlight_residue(r))
-                self.add_menu_entry(menu, f'Zoom to residue', lambda self=self, r=r: self._zoom_to_residue(r))
+                self.add_menu_entry(menu, f'Select',
+                                    lambda self=self, r=r: self._select_residue(r))
+                self.add_menu_entry(menu, f'Color green',
+                                    lambda self=self, r=r, c=(0,1,0,1): self._color_residue(r,c))
+                self.add_menu_entry(menu, f'Color to match mutation',
+                                    lambda self=self, r=r, c=item.color: self._color_residue(r,c))
+                self.add_menu_entry(menu, f'Show side chain',
+                                    lambda self=self, r=r: self._show_atoms(r))
+                self.add_menu_entry(menu, f'Zoom to residue',
+                                    lambda self=self, r=r: self._zoom_to_residue(r))
 
+        self.add_menu_separator(menu)                
         self.add_menu_entry(menu, 'Save Plot As...', self.save_plot_as)
 
     def _select_residue(self, r):
         self._run_residue_command(r, 'select %s')
-    def _highlight_residue(self, r):
-        self._run_residue_command(r, 'color %s lime')
+    def _color_residue(self, r, color):
+        from chimerax.core.colors import hex_color, rgba_to_rgba8
+        cname = hex_color(rgba_to_rgba8(color))
+        self._run_residue_command(r, f'color %s {cname}')
+        self._run_residue_command(r, f'color %s byhet')
+    def _show_atoms(self, r):
+        self._run_residue_command(r, 'show %s atoms')
     def _zoom_to_residue(self, r):
         self._run_residue_command(r, 'view %s')
-    def _color_residue_on_plot(self, r_name_num):
-        highlight_color = [r/255 for r in self._highlight_color]
-        for node in self.nodes:
-            if node.description[:-1] == r_name_num:
-                if not hasattr(node, 'original_color'):
-                    node.original_color = node.color
-                node.color = highlight_color
-            elif hasattr(node, 'original_color'):
-                node.color = node.original_color
-        # Put the colored nodes first so they are drawn on top
-        self.nodes.sort(key = lambda n: 1 if n.description[:-1] == r_name_num else 0)
-        self.graph = self._make_graph()  # Remake graph to get new node order
-        self.draw_graph()
+    def _color_residue_mutations(self, r, color = (0,1,0,1)):
+        rnodes = [node for node in self.nodes if node.residue is r]
+        self._color_and_raise_nodes(rnodes, color, tag = 'res')
+    def _color_near(self, residue, distance = 3.5):
+        cres = set(_find_close_residues(residue, residue.chain.existing_residues, distance))
+        nnodes = [node for node in self.nodes if node.residue in cres]
+        color_names = ['red', 'orange', 'yellow', 'violet', 'magenta', 'salmon', 'seagreen', 'skyblue', 'gold', 'coral']
+        from chimerax.core.colors import BuiltinColors
+        colors = [BuiltinColors[name].rgba for name in color_names]
+        n = len(colors)
+        root_color = (0,1,0,1)
+        rcolor = {r:(root_color if r is residue else colors[i%n]) for i,r in enumerate(cres)}
+        for node in nnodes:
+            node.color = rcolor[node.residue]
+            node.color_source = None
+        self._color_and_raise_nodes(nnodes, color = None, tag = 'near')
     def _color_synonymous(self, color = (0,0,1,1)):
         syn = [node for node in self.nodes if (node.description[0] == node.description[-1])]
-        for node in syn:
-            node.color = color
+        self._color_and_raise_nodes(syn, color)
+    def _color_and_raise_nodes(self, nodes, color, tag = None, uncolor = (.8,.8,.8,1)):
+        if tag is not None:
+            for node in self.nodes:
+                if getattr(node, 'color_source', None) == tag:
+                    node.color = uncolor
+                    node.color_source = None
+        for node in nodes:
+            if color is not None:
+                node.color = color
+            node.color_source = tag
         # Put the colored nodes first so they are drawn on top
-        synset = set(syn)
-        self.nodes.sort(key = lambda n: 1 if n in synset else 0)
+        nodeset = set(nodes)
+        self.nodes.sort(key = lambda n: 1 if n in nodeset else 0)
         self.graph = self._make_graph()  # Remake graph to get new node order
         self.draw_graph()
+    def _color_selected(self, color = (0,1,1,1)):
+        sel = [node for node in self.nodes if node.residue and node.residue.selected]
+        self._color_and_raise_nodes(sel, color, tag = 'sel')
+    def _clear_colors(self, clear_color = (.8,.8,.8,1)):
+        self._color_and_raise_nodes(self.nodes, clear_color)
     def _run_residue_command(self, r, command):
         self._run_command(command % r.string(style = 'command'))
     def _run_command(self, command):
         from chimerax.core.commands import run
         run(self.session, command)
+
+def _find_close_residues(residue, residues, distance):
+    rxyz = residue.atoms.coords
+    aatoms = residues.atoms
+    axyz = aatoms.coords
+    from chimerax.geometry import find_close_points
+    ri, ai = find_close_points(rxyz, axyz, distance)
+    close_res = aatoms[ai].residues.unique()
+    return close_res
 
 def register_command(logger):
     from chimerax.core.commands import CmdDesc, register, StringArg, EnumOf, BoolArg, FloatArg
