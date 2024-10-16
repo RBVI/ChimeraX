cf3d312eb433d7031f6ed81fd7c5f579a2414c17
Tom Goddard <goddard@sonic.net>
2024-10-15 17:22:47 -0700

    Made mutationscores define filtering more flexible.

--- src/bundles/mutation_scores/src/ms_define.py
+++ src/bundles/mutation_scores/src/ms_define.py
@@ -1,32 +1,47 @@
 # Define a new mutation score or residue score computed from existing mutation scores.
 def mutation_scores_define(session, score_name, from_score_name, scores_name = None,
-                           subtract_fit = None, operation = None, above = None, below = None,
+                           subtract_fit = None, aa = None, to_aa = None, synonymous = False,
+                           above = None, below = None, combine = None,
                            set_attribute = True):
     from .ms_data import mutation_scores, ScoreValues
     scores = mutation_scores(session, scores_name)
     from_score_values = scores.score_values(from_score_name)
 
-    if subtract_fit:
-        sub_score_values = scores.score_values(subtract_fit)
-        values = _subtract_fit_values(from_score_values.all_values(), sub_score_values.all_values())
-        svalues = ScoreValues(values)
+    from_aa = aa
+    if subtract_fit or from_aa or to_aa or synonymous or above is not None or below is not None:
+        values = from_score_values.all_values()
+        if subtract_fit:
+            sub_score_values = scores.score_values(subtract_fit)
+            values = _subtract_fit_values(values, sub_score_values.all_values())
+        if from_aa is not None:
+            values = [(rnum, faa, taa, value) for rnum, faa, taa, value in values if faa in from_aa]
+        if to_aa is not None:
+            values = [(rnum, faa, taa, value) for rnum, faa, taa, value in values if taa in to_aa]
+        if synonymous:
+            values = [(rnum, faa, taa, value) for rnum, faa, taa, value in values if taa == faa]
+        if above is not None:
+            values = [(rnum, faa, taa, value) for rnum, faa, taa, value in values if value >= above]
+        if below is not None:
+            values = [(rnum, faa, taa, value) for rnum, faa, taa, value in values if value <= below]
+        if len(values) == 0:
+            from chimerax.core.errors import UserError
+            raise UserError(f'No residues have score {score_name}')
+        per_residue = (synonymous or (to_aa is not None and len(to_aa) == 1) or from_score_values.per_residue)
+        svalues = ScoreValues(values, per_residue = per_residue)
     else:
         svalues = from_score_values
-
-    if operation is None:
+        
+    if combine is None:
         scores.set_computed_values(score_name, svalues)
         return svalues
 
     # Compute per-residue values from per-mutation values.
     res_values = []
-    to_aa = amino_acid_types.get(operation)
+    taa = to_aa if to_aa and len(to_aa) == 1 else None
     for res_num, aa_type in svalues.residue_numbers_and_types():
-        value = _residue_value(svalues, res_num, value_type=operation, above=above, below=below)
+        value = _combine_scores(svalues, res_num, operation=combine)
         if value is not None:
-            res_values.append((res_num, aa_type, to_aa, value))
-    if len(res_values) == 0:
-        from chimerax.core.errors import UserError
-        raise UserError(f'No residues have score {score_name}')
+            res_values.append((res_num, aa_type, taa, value))
     rvalues = ScoreValues(res_values, per_residue = True)
     scores.set_computed_values(score_name, rvalues)
 
@@ -48,58 +63,29 @@ def mutation_scores_define(session, score_name, from_score_name, scores_name = N
             session.logger.info(message)
 
     return rvalues
-
-def _attribute_name(column_name, type, above, below):
-    attr_name = f'{column_name}_{type}'
-    if above is not None:
-        attr_name += f'_ge_{"%.3g"%above}'
-    if below is not None:
-        attr_name += f'_le_{"%.3g"%below}'
-    return attr_name
     
-# Allowed value_type in _residue_value() function.
-amino_acid_types = {'ala':'A','arg':'R','asn':'N','asp':'D','cys':'C','glu':'E','gln':'Q','gly':'G',
-                    'his':'H','ile':'I','leu':'L','lys':'K','met':'M','phe':'F','pro':'P','ser':'S',
-                    'thr':'T','trp':'W','tyr':'Y','val':'V'}
-combine_operations = ('sum', 'sum_absolute', 'mean', 'stddev', 'count')
-residue_value_types = combine_operations + ('synonymous',) + tuple(amino_acid_types.keys())
+# Allowed value_type in _combine_scores() function.
+_combine_operations = ('sum', 'sum_absolute', 'mean', 'stddev', 'count')
     
-def _residue_value(score_values, residue_number, value_type='sum_absolute', above=None, below=None):
-    dms_values = score_values.mutation_values(residue_number)
-    if len(dms_values) == 0:
+def _combine_scores(score_values, residue_number, operation):
+    values = [value for from_aa, to_aa, value in score_values.mutation_values(residue_number)]
+    if len(values) == 0:
         return None
+    elif operation == 'sum':
+        value = sum(values)
+    elif operation == 'sum_absolute':
+        value = sum([abs(v) for v in values])
+    elif operation == 'mean':
+        from numpy import mean
+        value = mean(values)
+    elif operation == 'stddev':
+        from numpy import std
+        value = std(values)
+    elif operation == 'count':
+        value = len(values)
+    else:
+        value = None
 
-    value = None
-    if value_type in combine_operations:
-        values = [value for from_aa, to_aa, value in dms_values
-                  if ((above is None and below is None)
-                      or (above is not None and value >= above)
-                      or (below is not None and value <= below))]
-        if len(values) == 0:
-            value = None
-        elif value_type == 'sum':
-            value = sum(values)
-        elif value_type == 'sum_absolute':
-            value = sum([abs(v) for v in values])
-        elif value_type == 'mean':
-            from numpy import mean
-            value = mean(values)
-        elif value_type == 'stddev':
-            from numpy import std
-            value = std(values)
-        elif value_type == 'count':
-            value = len(values)
-        else:
-            value = None
-    elif value_type == 'synonymous':
-        values = [value for from_aa, to_aa, value in dms_values if to_aa == from_aa]
-        if values:
-            value = values[0]
-    elif value_type in amino_acid_types:
-        one_letter_code = amino_acid_types[value_type]
-        values = [value for from_aa, to_aa, value in dms_values if to_aa == one_letter_code]
-        if len(values) == 1:
-            value = values[0]
     return value
 
 def _subtract_fit_values(cvalues, svalues):
@@ -126,9 +112,12 @@ def register_command(logger):
         keyword = [('from_score_name', StringArg),
                    ('scores_name', StringArg),
                    ('subtract_fit', StringArg),
-                   ('operation', EnumOf(residue_value_types)),
+                   ('aa', StringArg),
+                   ('to_aa', StringArg),
+                   ('synonymous', BoolArg),
                    ('above', FloatArg),
                    ('below', FloatArg),
+                   ('combine', EnumOf(_combine_operations)),
                    ('set_attribute', BoolArg),
                    ],
         required_arguments = ['from_score_name'],
