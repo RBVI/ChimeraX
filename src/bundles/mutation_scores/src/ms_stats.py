15c777f6fba7b5ffe98bf73ed762dae9d1d58078
Tom Goddard <goddard@sonic.net>
2024-10-18 15:12:34 -0700

    Fix mutationscores statistics error if no mutation scores found.

--- src/bundles/mutation_scores/src/ms_stats.py
+++ src/bundles/mutation_scores/src/ms_stats.py
@@ -3,20 +3,29 @@ def mutation_scores_statistics(session, score_name = None, scores_name = None, t
     from .ms_data import mutation_scores
     scores = mutation_scores(session, scores_name)
     score_values = scores.score_values(score_name)
+    if score_values.count() == 0:
+        score_values = scores.computed_values(score_name)
+        if score_values is None:
+            from chimerax.core.errors import UserError
+            raise UserError(f'No mutation score named {score_name}')
     
     values = []
     for res_num, from_aa, to_aa, value in score_values.all_values():
-        if type == 'synonymous':
+        if type == 'synonymous' and not score_values.per_residue:
             if to_aa == from_aa or to_aa is None:
                 values.append(value)
         else:
             values.append(value)
 
-    import numpy
-    mean = numpy.mean(values)
-    std = numpy.std(values)
-    
-    message = f'Score {score_name}, {len(values)} {type} mutations, mean = {"%.3g"%mean}, standard deviation = {"%.3g"%std}, mean -/+ 2*SD = {"%.3g"%(mean-2*std)} to {"%.3g"%(mean+2*std)}'
+    if len(values) == 0:
+        message = f'No {score_name} scores for mutations of type {type} found'
+        mean = std = None
+    else:
+        import numpy
+        mean = numpy.mean(values)
+        std = numpy.std(values)
+        message = f'Score {score_name}, {len(values)} {type} mutations, mean = {"%.3g"%mean}, standard deviation = {"%.3g"%std}, mean -/+ 2*SD = {"%.3g"%(mean-2*std)} to {"%.3g"%(mean+2*std)}'
+
     session.logger.info(message)
 
     return mean, std
