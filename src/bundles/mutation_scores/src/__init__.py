ee0ec58624445ff9e3f2dcff6f62d7e413d420c7
Tom Goddard <goddard@sonic.net>
2024-11-12 09:25:32 -0800

    Added reading AlphaMissense and UniProt Variants mutation scores.

--- src/bundles/mutation_scores/src/__init__.py
+++ src/bundles/mutation_scores/src/__init__.py
@@ -54,13 +54,71 @@ class _MutationScoresAPI(BundleAPI):
                         from .ms_csv_file import open_mutation_scores_csv
                         ms_data, message = open_mutation_scores_csv(session, path, **kw)
                         return [], message
-
                     @property
                     def open_args(self):
                         from chimerax.atomic import ChainArg
                         return {'chain': ChainArg}
-
                 return MutationScoresInfo()
+
+            elif name == 'UniProt Variants':
+                from chimerax.open_command import OpenerInfo
+                class VariantScoresInfo(OpenerInfo):
+                    def open(self, session, path, file_name, **kw):
+                        from .uniprot_variants import open_uniprot_variant_scores
+                        ms_data, message = open_uniprot_variant_scores(session, path, **kw)
+                        return [], message
+                    @property
+                    def open_args(self):
+                        from chimerax.core.commands import StringArg
+                        from chimerax.atomic import ChainArg
+                        return {'chain': ChainArg,
+                                'identifier': StringArg}
+                return MutationScoresInfo()
+
+            elif name == 'uniprot_variants':
+                from chimerax.open_command import FetcherInfo
+                class UniProtVariantsInfo(FetcherInfo):
+                    def fetch(self, session, uniprot_id, format_name, ignore_cache, **kw):
+                        from . import uniprot_variants
+                        mset, msg = uniprot_variants.fetch_uniprot_variants(session, uniprot_id, ignore_cache = ignore_cache, **kw)
+                        return [], msg
+                    @property
+                    def fetch_args(self):
+                        from chimerax.core.commands import StringArg
+                        from chimerax.atomic import ChainArg
+                        return {'chain': ChainArg,
+                                'identifier': StringArg}
+                return UniProtVariantsInfo()
+
+            elif name == 'AlphaMissense':
+                from chimerax.open_command import OpenerInfo
+                class AlphaMissenseScoresInfo(OpenerInfo):
+                    def open(self, session, path, file_name, **kw):
+                        from .alpha_missense import open_alpha_missense_scores
+                        ms_data, message = open_alpha_missense_scores(session, path, **kw)
+                        return [], message
+                    @property
+                    def open_args(self):
+                        from chimerax.core.commands import StringArg
+                        from chimerax.atomic import ChainArg
+                        return {'chain': ChainArg,
+                                'identifier': StringArg}
+                return AlphaMissenseScoresInfo()
+
+            elif name == 'alpha_missense':
+                from chimerax.open_command import FetcherInfo
+                class AlphaMissenseInfo(FetcherInfo):
+                    def fetch(self, session, uniprot_id, format_name, ignore_cache, **kw):
+                        from .alpha_missense import fetch_alpha_missense_scores
+                        mset, msg = fetch_alpha_missense_scores(session, uniprot_id, ignore_cache = ignore_cache, **kw)
+                        return [], msg
+                    @property
+                    def fetch_args(self):
+                        from chimerax.core.commands import StringArg
+                        from chimerax.atomic import ChainArg
+                        return {'chain': ChainArg,
+                                'identifier': StringArg}
+                return AlphaMissenseInfo()
 
     # Map class name to class for session restore
     @staticmethod
