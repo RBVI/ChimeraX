e95f514214f73bde19cd5624124670f8958533f0
Eric Pettersen <pett@cgl.ucsf.edu>
2024-11-26 17:48:38 -0800

    Implemented trigger handlers

--- src/bundles/aniso/src/mgr.py
+++ src/bundles/aniso/src/mgr.py
@@ -64,6 +64,8 @@ class _StructureAnisoManager(StateManager):
     def destroy(self):
         for handler in self.handlers:
             handler.remove()
+        if not self.structure.deleted:
+            self.structure.remove_drawings(self.atom_depictions.values())
         self.structure = self.session = None
         super().destroy()
 
@@ -99,19 +101,36 @@ class _StructureAnisoManager(StateManager):
     def _add_handlers(self):
         from chimerax.core.models import REMOVE_MODELS
         self.handlers = [
-            #TODO
-            #self.structure.triggers.add_handler('changes', self._changes_cb),
-            #self.session.triggers.add_handler(REMOVE_MODELS, self._models_closed_cb)
+            self.structure.triggers.add_handler('changes', self._changes_cb),
+            self.session.triggers.add_handler(REMOVE_MODELS, self._models_closed_cb)
         ]
 
+    structure_reasons = set(["active_coordset changed", "display changed"])
+    atom_reasons = set(["alt_loc changed", "aniso_u changed", "color changed", "coord changed",
+        "display changed", "hide changed"])
+    def _changes_cb(self, trigger_name, change_info):
+        structure, changes = change_info
+        if structure.deleted:
+            self.destroy()
+            return
+        if not (set(changes.structure_reasons()).isdisjoint(self.structure_reasons)
+        and set(changes.atom_reasons()).isdisjoint(self.atom_reasons)):
+            self._create_depictions()
+        elif changes.num_deleted_atoms() > 0:
+            dead_drawings = [d for a, d in self.atom_depictions.items() if a.deleted]
+            if dead_drawings:
+                structure.remove_drawings(dead_drawings)
+                self.atom_depictions = {a:d for a,d in self.atom_depictions.items() if not a.deleted}
+
     def _create_depictions(self):
-        for drawing in self.atom_depictions.values():
-            drawing.delete()
+        if self.atom_depictions:
+            self.structure.remove_drawings(self.atom_depictions.values())
         self.atom_depictions.clear()
 
         atoms = self.structure.atoms
         atoms = atoms[atoms.has_aniso_u]
-        shown_atoms = set(self.shown_atoms)
+        displayed_atoms = self.shown_atoms.filter(self.shown_atoms.displays)
+        explicitly_depicted = set(displayed_atoms.filter(displayed_atoms.hides == 0))
         from chimerax.atomic.shapedrawing import AtomicShapeDrawing
         from chimerax.atomic import Atoms
         from numpy.linalg import svd
@@ -121,7 +140,7 @@ class _StructureAnisoManager(StateManager):
         for a in atoms:
             drawing = self.structure.new_drawing('thermal ellipsoid', subclass=AtomicShapeDrawing)
             self.atom_depictions[a] = drawing
-            drawing.display = a in shown_atoms
+            drawing.display = a in explicitly_depicted
             ignore, lengths, axes = svd(a.aniso_u)
             lengths2 = sqrt(lengths)
             lengths2 *= dp['scale']
@@ -197,161 +216,12 @@ class _StructureAnisoManager(StateManager):
                     tarray = ellipse_triangles
                     drawing.add_shape(ev, calc_normals(ev, tarray), tarray, color, atoms_arg)
 
+    def _models_closed_cb(self, trigger_name, closed_models):
+        if self.structure in closed_models:
+            self.destroy()
+            return
 
     #TODO
-    def _alt_loc_changes_cb(self, change_info, res, alt_loc):
-        alt_loc_s, changes = change_info
-        if alt_loc_s in self.expected_changes:
-            # we made the changes ourself; leave them be
-            self.expected_changes.remove(alt_loc_s)
-            return
-        for new_a in changes.created_atoms():
-            alt_loc_s.delete_atom(new_a)
-
-    def _build_alt_loc(self, res, alt_loc):
-        from chimerax.atomic import AtomicStructure, Atom
-        # _mirror_atoms code depends on structure name being the alt_loc
-        s = AtomicStructure(self.session, name=alt_loc, auto_style=False, log_info=False)
-        self.expected_changes.add(s)
-        r = s.new_residue(res.name, res.chain_id, res.number, insert=res.insertion_code)
-        self._update_alt_loc_res(alt_loc, res.atoms, s, {})
-        s.display = False
-        self.res_alt_locs.setdefault(res, {})[alt_loc] = s
-        self._add_alt_loc_changes_handler(s, res, alt_loc)
-        return s
-
-    def _build_alt_locs(self, res, main_group):
-        self.res_group[res] = self.session.models.add_group([self._build_alt_loc(res, al)
-            for al in sorted(res.alt_locs)], name=res.string(omit_structure=True), parent=main_group)
-
-    def _changes_cb(self, trigger_name, change_info):
-        structure, changes = change_info
-        if structure.deleted:
-            self.destroy()
-            return
-        del_groups = []
-        del_alt_locs = []
-        if changes.num_deleted_residues() > 0:
-            for r, group in self.res_group.items():
-                if r.deleted:
-                    del_groups.append(group)
-        if 'alt_locs changed' in changes.residue_reasons():
-            for r, group in self.res_group.items():
-                if r.deleted:
-                    continue
-                r_alt_locs = r.alt_locs
-                if not r_alt_locs:
-                    del_groups.append(group)
-                else:
-                    shown_alt_locs = set(self.res_alt_locs[r].keys())
-                    for del_al in shown_alt_locs - r_alt_locs:
-                        del_alt_locs.append(self.res_alt_locs[r][del_al])
-
-        if del_groups:
-            if len(del_groups) == len(self.res_group):
-                self.destroy()
-                return
-            # self.res_group will be cleaned up in the models-closed callback
-            self.session.models.close(del_groups)
-        if del_alt_locs:
-            self.session.models.close(del_alt_locs)
-        mod_res = set()
-        for a in changes.created_atoms():
-            if a.residue in self.res_group:
-                mod_res.add(a.residue)
-        if mod_res:
-            # defer the mirroring to the altloc model in order to allow atoms added directly to the model
-            # (e.g. from addh) to first be removed
-            from chimerax.atomic import get_triggers
-            get_triggers().add_handler('changes done',
-                lambda *args, f=self._mirror_atoms, residues=mod_res: f(residues))
-        for r in changes.created_residues():
-            if r.alt_locs:
-                self._build_alt_locs(r, self.main_group)
-
-    def _mirror_atoms(self, residues):
-        from chimerax.atomic import AtomicStructure
-        from chimerax.atomic.struct_edit import add_atom
-        for r in residues:
-            group = self.res_group[r]
-            for alt_loc_s in group.child_models():
-                if not isinstance(alt_loc_s, AtomicStructure) or len(alt_loc_s.name) != 1:
-                    continue
-                self.expected_changes.add(alt_loc_s)
-                alt_loc = alt_loc_s.name
-                name_lookup = { a.name: a for a in alt_loc_s.atoms }
-                # find newly created atoms, and correspondences between residue atoms and altloc model atoms
-                atom_map = {}
-                created = []
-                for a in r.atoms:
-                    try:
-                        atom_map[a] = name_lookup[a.name]
-                    except KeyError:
-                        created.append(a)
-                assert(len(atom_map) == alt_loc_s.num_atoms)
-                self._update_alt_loc_res(alt_loc, created, alt_loc_s, atom_map)
-        from chimerax.core.triggerset import DEREGISTER
-        return DEREGISTER
-
-    def _models_closed_cb(self, trigger_name, closed_models):
-        if self.structure in closed_models or self.main_group in closed_models:
-            self.destroy()
-            return
-
-        if not self.main_group.child_models():
-            self.destroy()
-            return
-
-        # check the altloc models:
-        closures = []
-        for r, alt_locs in list(self.res_alt_locs.items()):
-            for alt_loc, al_s in list(alt_locs.items()):
-                if al_s in closed_models:
-                    al_s._alt_loc_changes_handler.remove()
-                    del alt_locs[alt_loc]
-            if not alt_locs:
-                del self.res_alt_locs[r]
-                res_group = self.res_group[r]
-                del self.res_group[r]
-                if res_group not in closed_models:
-                    closures.append(res_group)
-        if closures:
-            self.session.models.close(closures)
-
-    def _update_alt_loc_res(self, alt_loc, source_atoms, dest_s, atom_map):
-        from chimerax.atomic.struct_edit import add_atom
-        from chimerax.atomic import Atom, Atoms, Bonds
-        new_atoms = []
-        for old_a in source_atoms:
-            use_alt_loc = alt_loc in old_a.alt_locs
-            coord = old_a.get_alt_loc_coord(alt_loc) if use_alt_loc else old_a.coord
-            new_a = add_atom(old_a.name, old_a.element, dest_s.residues[0], coord, alt_loc=alt_loc)
-            new_atoms.append(new_a)
-            new_a.draw_mode = Atom.STICK_STYLE
-            atom_map[old_a] = new_a
-            if not old_a.is_side_chain and not use_alt_loc:
-                new_a.display = False
-        new_bonds = []
-        for old_a in source_atoms:
-            for old_b in old_a.bonds:
-                a1, a2 = old_b.atoms
-                try:
-                    new1 = atom_map[a1]
-                    new2 = atom_map[a2]
-                except KeyError:
-                    continue
-                if new2 not in new1.neighbors:
-                    new_bonds.append(dest_s.new_bond(new1, new2))
-        from chimerax.core.objects import Objects
-        alt_loc_objects = Objects(atoms=Atoms(new_atoms), bonds=Bonds(new_bonds))
-        from chimerax.std_commands.color import color
-        color(self.session, alt_loc_objects, color="byelement")
-        from chimerax.std_commands.size import size
-        size(self.session, alt_loc_objects, stick_radius=0.1, verbose=False)
-
-    def reset_state(self, session):
-        self.destroy()
-
     @classmethod
     def restore_snapshot(cls, session, data):
         inst = cls(session, data['structure'], from_session=True)
