e87b29ab22628d4083e36154db1fe877a36c54d1
Zach Pearson <zjp@cgl.ucsf.edu>
2024-12-11 14:18:20 -0800

    fix(segmentations): Use the Trigger enum in the SegmentationTracker

--- src/bundles/segmentations/src/segmentation_tracker.py
+++ src/bundles/segmentations/src/segmentation_tracker.py
@@ -30,6 +30,7 @@ from chimerax.dicom import Study
 from chimerax.segmentations.segmentation import Segmentation
 
 import chimerax.segmentations.triggers
+from chimerax.segmentations.triggers import Trigger
 
 _tracker = None
 
@@ -53,7 +54,7 @@ class SegmentationTracker:
         else:
             self._unparented_segmentations.add(segmentation)
         chimerax.segmentations.triggers.activate_trigger(
-            chimerax.segmentations.triggers.SEGMENTATION_ADDED, segmentation
+            Trigger.SegmentationAdded, segmentation
         )
 
     def remove_segmentation(self, segmentation):
@@ -64,7 +65,7 @@ class SegmentationTracker:
         else:
             self._unparented_segmentations.remove(segmentation)
         chimerax.segmentations.triggers.activate_trigger(
-            chimerax.segmentations.triggers.SEGMENTATION_REMOVED, segmentation
+            Trigger.SegmentationRemoved, segmentation
         )
 
     def __delitem__(self, item):
