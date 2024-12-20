591765e240ade0a78126007cd58386ec36535a01
Eric Pettersen <pett@cgl.ucsf.edu>
2024-12-20 12:12:02 -0800

    Don't run a command (which can fire triggers) in a delete() call [#16506]

--- src/bundles/color_key/src/tool.py
+++ src/bundles/color_key/src/tool.py
@@ -215,17 +215,14 @@ class ColorKeyTool(ToolInstance):
         tw.manage(placement=None)
 
     def delete(self):
-        if self._mouse_handler:
-            button = self.mouse_button_button.text()
-            if self._prev_mouse_mode:
-                new_mode = self._prev_mouse_mode.name
-            else:
-                new_mode = 'none'
-            self._self_mm_change = True
-            run(self.session, "ui mousemode %s %s" % (button, StringArg.unparse(new_mode)))
-            self._self_mm_change = False
         for handler in self.handlers:
             handler.remove()
+        if self._mouse_handler:
+            button = self.mouse_button_button.text()
+            self._self_mm_change = True
+            # Running a command in a delete() call, which can fire triggers, is asking for trouble so...
+            self.session.ui.mouse_modes.bind_mouse_mode(button, [], self._prev_mouse_mode)
+            self._self_mm_change = False
         self.key = None
         super().delete()
 
