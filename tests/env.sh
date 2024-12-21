b22e170d8e392c0e35bcc6c4df9a453caba85761
Zach Pearson <zjp@cgl.ucsf.edu>
2024-12-20 19:29:22 -0800

    test: Update the test script to use Python 3.13

--- tests/env.sh
+++ tests/env.sh
@@ -4,11 +4,10 @@
 # runs the same whether it's called by running the ChimeraX binary or by
 # running ChimeraX.app/bin/python -I -m chimerax.core
 
-while getopts cs flag
-do
+while getopts cs flag; do
   case "${flag}" in
-    c) COVERAGE=true;;
-    s) COV_SILENT=true;;
+  c) COVERAGE=true ;;
+  s) COV_SILENT=true ;;
   esac
 done
 
@@ -26,24 +25,24 @@ CHIMERAX_BIN=
 
 case $OSTYPE in
 linux-gnu)
-	CHIMERAX_PYTHON_BIN=./ChimeraX.app/bin/python3.11
-	CHIMERAX_BIN=./ChimeraX.app/bin/ChimeraX
-	;;
+  CHIMERAX_PYTHON_BIN=./ChimeraX.app/bin/python3.13
+  CHIMERAX_BIN=./ChimeraX.app/bin/ChimeraX
+  ;;
 msys)
-	CHIMERAX_PYTHON_BIN=./ChimeraX.app/bin/python.exe
-	CHIMERAX_BIN=./ChimeraX.app/bin/ChimeraX-console.exe
-	;;
+  CHIMERAX_PYTHON_BIN=./ChimeraX.app/bin/python.exe
+  CHIMERAX_BIN=./ChimeraX.app/bin/ChimeraX-console.exe
+  ;;
 darwin*)
-	CHIMERAX_PYTHON_BIN=./ChimeraX.app/Contents/bin/python3.11
-	CHIMERAX_BIN=./ChimeraX.app/Contents/bin/ChimeraX
-	;;
+  CHIMERAX_PYTHON_BIN=./ChimeraX.app/Contents/bin/python3.13
+  CHIMERAX_BIN=./ChimeraX.app/Contents/bin/ChimeraX
+  ;;
 esac
 
 if [ ! -e "${CHIMERAX_PYTHON_BIN}" ]; then
-	echo "No ChimeraX Python binary found" && exit 1
+  echo "No ChimeraX Python binary found" && exit 1
 fi
 if [ ! -e "${CHIMERAX_BIN}" ]; then
-	echo "No ChimeraX binary found" && exit 1
+  echo "No ChimeraX binary found" && exit 1
 fi
 
 echo "Running Pytest on tests/test_env.py (ChimeraX)"
