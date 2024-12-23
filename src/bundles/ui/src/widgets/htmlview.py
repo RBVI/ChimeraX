44170b9a4549325342ac489e11ebf06c33a3cb03
Zach Pearson <zjp@cgl.ucsf.edu>
2024-12-23 11:32:17 -0800

    Revert "fix(ui): We no longer use Qt < 5.13"
    This reverts commit 5dcb09530ce747178c9ab525062f34240748b6cd.

--- src/bundles/ui/src/widgets/htmlview.py
+++ src/bundles/ui/src/widgets/htmlview.py
@@ -121,7 +121,11 @@ def delete_profile(profile):
         del profile._handlers
         del profile._scheme_handler
         del profile._schemes
-    profile.setUrlRequestInterceptor(None)
+    from Qt.QtCore import QT_VERSION
+    if QT_VERSION < 0x050d00:
+        profile.setRequestInterceptor(None)
+    else:
+        profile.setUrlRequestInterceptor(None)
 
 
 class HtmlView(QWebEngineView):
