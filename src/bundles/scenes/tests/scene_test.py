be7daf8b7af2896d90f1efc70950e517657ef49b
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2025-01-11 16:47:48 -0800

    [test] test_md_scene_implementation None Test Case.

--- src/bundles/scenes/tests/scene_test.py
+++ src/bundles/scenes/tests/scene_test.py
@@ -14,6 +14,9 @@ class MockModelC(MockModelB):
     def restore_scene(self, data):
         pass
 
+class MockModelD:
+    pass
+
 def test_md_scene_implementation():
     """
     Test that md_scene_implementation returns the most derived class that implements restore_scene.
@@ -28,6 +31,8 @@ def test_md_scene_implementation():
     assert scene.md_scene_implementation(model_b) == MockModelA
     # MockModelC implements restore_scene
     assert scene.md_scene_implementation(model_c) == MockModelC
+    # MockModelD does not implement restore_scene and neither do any of its super classes
+    assert scene.md_scene_implementation(MockModelD) is None
 
 def test_scene_super():
     model_a = MockModelA()
