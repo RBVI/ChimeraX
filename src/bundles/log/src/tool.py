97c4913c9d1ded2493128980c3a8b97d43c35f3c
Eric Pettersen <pett@cgl.ucsf.edu>
2025-02-06 10:28:39 -0800

    Remove debugging statement

--- src/bundles/log/src/tool.py
+++ src/bundles/log/src/tool.py
@@ -244,7 +244,6 @@ class Log(ToolInstance, HtmlLog):
                 from Qt.QtCore import QSize
                 return QSize(600, 300)
         self.error_dialog = BiggerErrorDialog(parent)
-        self.error_dialog.finished.connect(lambda *args: print("error dialog finished"))
         self._add_report_bug_button()
         layout = QGridLayout(parent)
         layout.setContentsMargins(0,0,0,0)
