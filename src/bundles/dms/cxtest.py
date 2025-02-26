8386e26e68a74ac656e7d382b7504987062d3bdb
alphataubio <alphataubio@gmail.com>
2025-02-26 17:23:19 -0500

    do it like eric says

--- src/bundles/dms/cxtest.py
+++
@@ -1,7 +0,0 @@
-from chimerax.core.commands import run
-run(session, "open 2gbp")
-from chimerax.hbonds import find_hbonds, rec_dist_slop, rec_angle_slop
-hbonds = find_hbonds(session, session.models[:1],
-	dist_slop=rec_dist_slop, angle_slop=rec_angle_slop)
-if len(hbonds) != 793:
-	raise SystemExit("Expected to find 793 hbonds in 2gbp; actually found %d" % len(hbonds))
