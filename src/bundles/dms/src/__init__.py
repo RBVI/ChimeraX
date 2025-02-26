4d9de6b184f552e5f072eaab18093ea0f29f3ddd
alphataubio <alphataubio@gmail.com>
2025-02-26 07:07:36 -0500

    dms bundle (first draft)

---
+++ src/bundles/dms/src/__init__.py
@@ -0,0 +1,51 @@
+# vim: set expandtab shiftwidth=4 softtabstop=4:
+
+# === UCSF ChimeraX Copyright ===
+# Copyright 2022 Regents of the University of California. All rights reserved.
+# The ChimeraX application is provided pursuant to the ChimeraX license
+# agreement, which covers academic and commercial uses. For more details, see
+# <https://www.rbvi.ucsf.edu/chimerax/docs/licensing.html>
+#
+# This particular file is part of the ChimeraX library. You can also
+# redistribute and/or modify it under the terms of the GNU Lesser General
+# Public License version 2.1 as published by the Free Software Foundation.
+# For more details, see
+# <https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html>
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+# EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. ADDITIONAL LIABILITY
+# LIMITATIONS ARE DESCRIBED IN THE GNU LESSER GENERAL PUBLIC LICENSE
+# VERSION 2.1
+#
+# This notice must be embedded in or attached to all copies, including partial
+# copies, of the software or any revisions or derivations thereof.
+# === UCSF ChimeraX Copyright ===
+
+from chimerax.core.toolshed import BundleAPI
+
+
+class _VTKBundleAPI(BundleAPI):
+
+    @staticmethod
+    def run_provider(session, name, mgr):
+        if mgr == session.open_command:
+            from chimerax.open_command import OpenerInfo
+            class VtkInfo(OpenerInfo):
+                def open(self, session, data, file_name, **kw):
+                    from . import vtk
+                    return vtk.read_vtk(session, data, file_name)
+        else:
+            from chimerax.save_command import SaverInfo
+            class VtkInfo(SaverInfo):
+                def save(self, session, path, *, models=None, **kw):
+                    from .export_vtk import write_scene_as_vtk
+                    write_scene_as_vtk(session, path, models)
+
+                @property
+                def save_args(self):
+                    from chimerax.core.commands import ModelsArg
+                    return { 'models': ModelsArg }
+        return VtkInfo()
+
+bundle_api = _VTKBundleAPI()
