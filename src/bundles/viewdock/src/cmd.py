c34887fa94497612159322402f45475e217dde91
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2025-03-01 17:22:36 -0800

    feat: init cmd.py based on viewdockx
    -Return ViewDock tool from viewdock command
    -Remove implementations for viewdock_up/down commands. Placeholder log message.

---
+++ src/bundles/viewdock/src/cmd.py
@@ -0,0 +1,40 @@
+# vim: set expandtab ts=4 sw=4:
+
+from chimerax.core.commands import CmdDesc, StringArg
+from chimerax.atomic import AtomicStructuresArg
+
+
+def viewdock(session, structures=None, name=None):
+    from .tool import ViewDockTool
+    return ViewDockTool(session, "ViewDock")
+
+viewdock_desc = CmdDesc(optional=[("structures", AtomicStructuresArg),
+                                  ("name", StringArg)])
+
+
+def viewdock_down(session, name=None):
+    session.logger.info("viewdock_down not implemented yet")
+viewdock_down_desc = CmdDesc(optional=[("name", StringArg)])
+
+
+def viewdock_up(session, name=None):
+    session.logger.info("viewdock_up not implemented yet")
+viewdock_up_desc = CmdDesc(optional=[("name", StringArg)])
+
+
+command_map = {
+    "viewdock": (viewdock, viewdock_desc),
+    "viewdock down": (viewdock_down, viewdock_down_desc),
+    "viewdock up": (viewdock_up, viewdock_up_desc),
+}
+
+
+def register_command(ci):
+    try:
+        func, desc = command_map[ci.name]
+    except KeyError:
+        raise ValueError("trying to register unknown command: %s" % ci.name)
+    if desc.synopsis is None:
+        desc.synopsis = ci.synopsis
+    from chimerax.core.commands import register
+    register(ci.name, desc, func)
