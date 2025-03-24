12f9ceb7636a723c85926b37c6317bc4ad1eeb69
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2025-03-24 16:31:07 -0700

    refactor: table_setup method and saving structures attribute
    table_setup handles all setup for the table. Structures attribute allows the tool to keep track of what structures it has for support with adding and removing structures with trigger listeners.

--- src/bundles/viewdock/src/tool.py
+++ src/bundles/viewdock/src/tool.py
@@ -15,24 +15,27 @@ class ViewDockTool(ToolInstance):
         from chimerax.ui import MainToolWindow
         self.tool_window = MainToolWindow(self)
 
-        self.setup(structures)
-
-    def setup(self, structures):
+        # Create a vertical layout for the tool
         self.main_v_layout = QVBoxLayout()
         self.tool_window.ui_area.setLayout(self.main_v_layout)
 
+        self.structures = structures
+        self.table_setup()
+        self.tool_window.manage('side')
+
+    def table_setup(self):
         self.struct_table = ItemTable(session=self.session)
         self.struct_table.add_column('Show', lambda s: s.display, data_set=self.set_visibility, format=ItemTable.COL_FORMAT_BOOLEAN)
         self.struct_table.add_column('ID', lambda s: s.id_string)
         self.struct_table.add_column('Name', lambda s: s.viewdockx_data.get('Name', ''))
-        self.struct_table.data = structures
+        self.struct_table.data = self.structures
         self.struct_table.launch()
         self.main_v_layout.addWidget(self.struct_table)
-
-        self.tool_window.manage('side')
 
     def set_visibility(self, structure, value):
         if value:
             run(self.session, f'show #{structure.id_string}')
         else:
             run(self.session, f'hide #{structure.id_string}')
+
+"""model display changed"""
