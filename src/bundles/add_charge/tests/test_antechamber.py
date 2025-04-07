b76d1cfe6b00ecb655cd20a00066ba7a643e565d
Zach Pearson <zjp@cgl.ucsf.edu>
2025-04-07 12:55:02 -0700

    test: Skip add_charge tests for now

--- src/bundles/add_charge/tests/test_antechamber.py
+++ src/bundles/add_charge/tests/test_antechamber.py
@@ -1,3 +1,6 @@
+import pytest
+
+@pytest.mark.skip(reason="Need to fix ARM ambertools to include libcifparse.so")
 def test_match_maker(test_production_session):
     from chimerax.core.commands import run
     session = test_production_session
