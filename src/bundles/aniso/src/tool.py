16a97ab70bd3c594e6a8f5759570b3e31c62c836
Eric Pettersen <pett@cgl.ucsf.edu>
2025-04-21 16:59:39 -0700

    Scale/smoothing widgets + scale calculator

--- src/bundles/aniso/src/tool.py
+++ src/bundles/aniso/src/tool.py
@@ -14,11 +14,14 @@
 from chimerax.core.tools import ToolInstance
 from chimerax.core.settings import Settings
 from Qt.QtWidgets import QVBoxLayout, QGridLayout, QHBoxLayout, QLabel, QButtonGroup, QRadioButton, QWidget
-from Qt.QtWidgets import QPushButton, QScrollArea, QMenu, QCheckBox
+from Qt.QtWidgets import QPushButton, QScrollArea, QMenu, QCheckBox, QLineEdit, QSpacerItem, QSizePolicy
+from Qt.QtGui import QDoubleValidator, QIntValidator
 from Qt.QtCore import Qt
 from chimerax.core.commands import run
 from chimerax.ui import tool_user_error
 from .cmd import builtin_presets
+
+style_attrs = list(builtin_presets["simple"].keys())
 
 class AnisoTool(ToolInstance):
 
@@ -60,7 +63,44 @@ class AnisoTool(ToolInstance):
         self.structure_button.value_changed.connect(self._update_widgets)
         preset_model_layout.addWidget(sb)
         preset_model_layout.addStretch(1)
-        self._set_preset_button_text()
+
+        from .cmd import builtin_presets
+        defaults = builtin_presets["simple"]
+
+        scale_smoothing_layout = QHBoxLayout()
+        main_layout.addLayout(scale_smoothing_layout)
+        scale_smoothing_layout.addStretch(1)
+        scale_smoothing_layout.addWidget(QLabel("Scale factor:"))
+        self.scale = sf = QLineEdit(str(defaults["scale"]))
+        sf.setAlignment(Qt.AlignCenter)
+        sf.setMaximumWidth(7 * sf.fontMetrics().averageCharWidth())
+        sf.setValidator(QDoubleValidator(0.0001, 1000000, -1))
+        scale_smoothing_layout.addWidget(sf)
+        spacer = QSpacerItem(10, 0, QSizePolicy.MinimumExpanding, QSizePolicy.Minimum)
+        scale_smoothing_layout.addSpacerItem(spacer)
+        scale_smoothing_layout.addWidget(QLabel("Smoothing level:"))
+        self.smoothing = sl = QLineEdit(str(defaults["smoothing"]))
+        sl.setAlignment(Qt.AlignCenter)
+        sl.setMaximumWidth(3 * sl.fontMetrics().averageCharWidth())
+        validator = QIntValidator()
+        validator.setBottom(1)
+        sl.setValidator(validator)
+        scale_smoothing_layout.addWidget(sl)
+        scale_smoothing_layout.addStretch(1)
+
+        set_scale_layout = QHBoxLayout()
+        main_layout.addLayout(set_scale_layout)
+        set_scale_layout.addStretch(1)
+        set_scale_layout.addWidget(QLabel("Set scale factor for probability (%):"))
+        self.set_scale = ss = QLineEdit("")
+        ss.setAlignment(Qt.AlignCenter)
+        ss.setMaximumWidth(5 * ss.fontMetrics().averageCharWidth())
+        ss.setValidator(QDoubleValidator(0.0001, 100, -1))
+        ss.editingFinished.connect(self._set_scale_cb)
+        set_scale_layout.addWidget(ss)
+        set_scale_layout.addStretch(1)
+
+        self._update_widgets()
 
         hide_show_layout = QHBoxLayout()
         main_layout.addLayout(hide_show_layout)
@@ -70,7 +110,7 @@ class AnisoTool(ToolInstance):
         hide_show_layout.addWidget(show_button)
         hide_show_layout.addWidget(QLabel("/"))
         hide_button = QPushButton("Hide")
-        hide_button.clicked.connect(lambda *args, f=self._show_hide_cb: f("hide"))
+        hide_button.clicked.connect(lambda *args, f=self._show_hide_cb: f(hide=True))
         hide_show_layout.addWidget(hide_button)
         hide_show_layout.addWidget(QLabel("depictions"))
         hide_show_layout.addStretch(1)
@@ -105,14 +145,14 @@ class AnisoTool(ToolInstance):
         s = self.structure_button.value
         if not s:
             return tool_user_error("No structure chosen")
-        self._show_hide_cb()
+        self._show_hide_cb(apply_widgets=False)
         run(self.session, "aniso preset " + s.atomspec + " " + action.text())
 
-    def _set_preset_button_text(self, *, mgr=None):
+    def _set_preset_button_text(self, mgr):
         s = self.structure_button.value
         if not s:
             self.preset_menu_button.setText(self.NO_PRESET_TEXT)
-            return
+            return None
         if mgr is None:
             from .mgr import manager_for_structure
             mgr = manager_for_structure(self.session, s)
@@ -120,20 +160,56 @@ class AnisoTool(ToolInstance):
             for name, settings in preset_dict.items():
                 if settings == mgr.drawing_params:
                     self.preset_menu_button.setText(name)
-                    return
+                    return mgr
         self.preset_menu_button.setText(self.NO_PRESET_TEXT)
+        return mgr
 
-    def _show_hide_cb(self, mod=None):
+    def _set_scale_cb(self):
+        scale_text = self.set_scale.text()
+        if not scale_text:
+            return
+        if not self.set_scale.hasAcceptableInput():
+            return tool_user_error("Probability must be a percentage")
+        prob = float(scale_text) / 100.0
+        self.scale.setText("%g" % _prob_to_scale(prob))
+
+    def _show_hide_cb(self, *, apply_widgets=True, hide=False):
         s = self.structure_button.value
         if not s:
             return tool_user_error("No structure chosen")
 
         spec = s.atomspec
 
+        if apply_widgets and not hide:
+            from chimerax.core.commands import camel_case
+            from .mgr import manager_for_structure
+            mgr = manager_for_structure(self.session, s)
+            diffs = []
+            for attr_name in style_attrs:
+                try:
+                    widget = getattr(self, attr_name)
+                except AttributeError:
+                    #TODO: remove try/accept once all attribute widgets implemented
+                    continue
+                arg_name = camel_case(attr_name)
+                if isinstance(widget, QLineEdit):
+                    str_val = widget.text()
+                    if not widget.hasAcceptableInput():
+                        return tool_user_error("Unacceptable value (%s) for '%s' argument"
+                            % (str_val, arg_name))
+                    if isinstance(widget.validator(), QDoubleValidator):
+                        val = float(str_val)
+                    else:
+                        val = int(str_val)
+                    if val != mgr.drawing_params[attr_name]:
+                        diffs.extend([arg_name, str_val])
+            if diffs:
+                run(self.session, "aniso style " + " ".join(diffs)),
+
         if self.sel_restrict_check_box.isChecked():
             spec += " & sel"
 
-        run(self.session, "aniso" + ("" if mod is None else ' ' + mod) + ' ' + spec)
+        run(self.session, "aniso" + (" hide" if hide else '') + ' ' + spec)
 
     def _style_changed_cb(self, trig_name, mgr):
         s = self.structure_button.value
@@ -142,5 +218,62 @@ class AnisoTool(ToolInstance):
         self._update_widgets(mgr=mgr)
 
     def _update_widgets(self, *, mgr=None):
-        self._set_preset_button_text(mgr=mgr)
-        #TODO: set widgets (once they exist)
+        mgr = self._set_preset_button_text(mgr)
+        if mgr is None:
+            return
+
+        for attr_name in style_attrs:
+            try:
+                widget = getattr(self, attr_name)
+            except AttributeError:
+                #TODO: remove try/accept once all attribute widgets implemented
+                continue
+            if isinstance(widget, QLineEdit):
+                widget.setText(str(mgr.drawing_params[attr_name]))
+
+def simpson(f, a, b, n=2):
+    """Approximate the definite integral of f from a to b
+       by Composite Simpson's rule, dividing the interval in n parts.
+
+       Cribbed from numerical integration wikipedia page.
+    """
+    assert n > 0
+    # forces n to be even
+    n = n + (n % 2)
+    dx = (b - a) / n
+    ans = f(a) + f(b)
+    x = a + dx
+    m = 4
+    for i in range(1, n):
+        ans += m * f(x)
+        m = 6 - m
+        x = x + dx
+
+    return dx * ans / 3
+
+from math import pi, sqrt, exp
+def integrand(val):
+    val2 = val * val
+    return val2 * exp(-val2/2.0)
+
+def integral(val, constant=sqrt(2.0/pi)):
+    return constant * simpson(integrand, 0, val, n=70)
+
+def _prob_to_scale(target_prob, low_bound=None, high_bound=None, converge=0.0001):
+    if low_bound is None and high_bound is None:
+        val = 1.0
+    elif low_bound is None:
+        val = high_bound / 2.0
+    elif high_bound is None:
+        val = low_bound * 2.0
+    else:
+        val = (low_bound + high_bound) / 2.0
+    prob = integral(val)
+    if prob < target_prob:
+        low_bound = val
+    else:
+        high_bound = val
+    if low_bound and high_bound and high_bound - low_bound < converge:
+        return (high_bound + low_bound) / 2.0
+    return _prob_to_scale(target_prob, low_bound, high_bound, converge)
+
