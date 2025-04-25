74a3bb46777e3b0a4b93999bf7435027ad4b69d9
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2025-04-25 10:45:48 -0700

    feat(viewdock): initialize tool description box on tool open
    When the ViewDock tool opens, highlight the first structure and let the `update_model_description` handle initializing the description box for the first element.

--- src/bundles/viewdock/src/tool.py
+++ src/bundles/viewdock/src/tool.py
@@ -206,6 +206,8 @@ class ViewDockTool(ToolInstance):
 
         # Add the group box to the main layout
         self.main_v_layout.addWidget(self.description_group)
+        # Select the first structure in the table to display its data in the description box
+        self.struct_table.selected = [self.structures[0]]
 
     def update_model_description(self, newly_selected):
         """
