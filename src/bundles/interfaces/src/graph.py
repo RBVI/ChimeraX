07038f8e33206f4af835481b26bbd7cbc673aa8d
Tom Goddard <goddard@sonic.net>
2025-05-09 12:42:40 -0700

    Suppress matplotlib 3.10.1 warnings about fixed data aspect in Similar Structures cluster plot.

--- src/bundles/interfaces/src/graph.py
+++ src/bundles/interfaces/src/graph.py
@@ -77,7 +77,18 @@ class Plot(ToolInstance):
         Make both axes use same scaling, pixels per plot unit.
         Without this if the window is not square, the plot squishes one axis.
         '''
+        self._suppress_matplotlib_warnings()
         self.axes.set_aspect('equal', adjustable='datalim')
+
+    def _suppress_matplotlib_warnings(self):
+        # Ignore matplotlib 3.10.1 warning that preserving equal scaling conflicts with fixed limits.
+        from logging import Filter, getLogger
+        class _AspectFilter(Filter):
+            def filter(self, record):
+                return record.msg not in (
+                    'Ignoring fixed x limits to fulfill fixed data aspect with adjustable data limits.',
+                    'Ignoring fixed y limits to fulfill fixed data aspect with adjustable data limits.')
+        getLogger('matplotlib.axes._base').addFilter(_AspectFilter())
 
     def move(self, delta_x, delta_y):
         '''Move plot objects by delta values in window pixels.'''
