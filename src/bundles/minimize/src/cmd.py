b557ffcec050218d4a0d600adef954edcb4262c9
Eric Pettersen <pett@cgl.ucsf.edu>
2025-06-27 18:25:26 -0700

    Handles missing structure by adding fake terminal atoms to the OpenMM version of the structure

--- src/bundles/minimize/src/cmd.py
+++ src/bundles/minimize/src/cmd.py
@@ -22,7 +22,7 @@
 # copies, of the software or any revisions or derivations thereof.
 # === UCSF ChimeraX Copyright ===
 
-from chimerax.core.errors import UserError
+from chimerax.core.errors import UserError, LimitationError
 from chimerax.add_charge import ChargeMethodArg
 
 def cmd_minimize(session, structure, *, dock_prep=True, **kw):
@@ -37,7 +37,7 @@ def cmd_minimize(session, structure, *, dock_prep=True, **kw):
             raise UserError("Multiple structures open")
     if dock_prep:
         from chimerax.dock_prep import dock_prep_caller
-        dock_prep_caller(session, [structure], memorize_name="minimization",
+        dock_prep_caller(session, [structure], memorize_name="minimization", nogui=True,
             callback=lambda ses=session, struct=structure: _minimize(ses, struct), **kw)
     else:
         _minimize(session, structure)
@@ -54,6 +54,28 @@ def _minimize(session, structure):
     coords = []
     reordered_atoms = []
     # OpenMM wants all atoms in a residue to be added consecutively, so loop through residues instead of atoms...
+    # Also, it can't handle missing structure, so make ends of missing structure look like terminii
+    fake_c = set()
+    fake_n = set()
+    prev_r = None
+    in_missing = False
+    for chain in structure.chains:
+        for r in chain.residues:
+            if r is None:
+                if prev_r:
+                    fake_c.add(prev_r)
+                in_missing = True
+            else:
+                if in_missing:
+                    fake_n.add(r)
+                in_missing = False
+            prev_r = r
+    n_error_template = "Don't know how to modify %s to match N-terminal template: %s"
+    n_error_template = "Don't know how to modify %s to match C-terminal template: %s"
+    from chimerax.atomic.bond_geom import bond_positions
+    from chimerax.addh import bond_with_H_length
+    NH_len = CO_len = None
+    filter = []
     for r in structure.residues:
         c = r.chain
         try:
@@ -65,9 +87,48 @@ def _minimize(session, structure):
         except KeyError:
             mm_r = residues[r] = top.addResidue(r.name, mm_c, r.number, r.insertion_code)
         for a in r.atoms:
-            atoms[a] = top.addAtom(a.name, element.Element.getBySymbol(a.element.name), mm_r, a.serial_number)
+            atoms[a] = top.addAtom(a.name, element.Element.getBySymbol(a.element.name), mm_r,
+                                    a.serial_number)
             coords.append(Quantity(vec3.Vec3(*a.coord), angstrom))
             reordered_atoms.append(a)
+            filter.append(True)
+        # add fake N/C-terminal atoms only to the OpenMM version of the molecule
+        fake_serial = max(structure.atoms.serial_numbers) + 1
+        if r in fake_n:
+            n = r.find_atom('N')
+            if not n:
+                raise LimitationError(n_error_template % (r, "can't find N atom"))
+            if NH_len is None:
+                NH_len = bond_with_H_length(n, 3)
+            bonded = n.neighbors
+            if not bonded:
+                raise LimitationError(n_error_template % (r, "N atom has no bonds"))
+            for pos in bond_positions(n.coord, 4, NH_len, [bd.coord for bd in bonded]):
+                num = 1
+                while r.find_atom("H%d" % num):
+                    num += 1
+                h = top.addAtom("H%d" % num, element.Element.getBySymbol("H"), residues[r], fake_serial)
+                coords.append(Quantity(vec3.Vec3(*pos), angstrom))
+                top.addBond(atoms[n], h)
+                fake_serial += 1
+                filter.append(False)
+        if r in fake_c:
+            c = r.find_atom("C")
+            if not c:
+                raise LimitationError(c_error_template % (r, "can't find C atom"))
+            if CO_len is None:
+                from chimerax.atomic import Element
+                CO_len = Element.bond_length("C", "O")
+            bonded = c.neighbors
+            if len(bonded) != 2:
+                raise LimitationError(c_error_template % (r, "C atom not bonded to exactly two other atoms"))
+            for pos in bond_positions(c.coord, 3, CO_len, [bd.coord for bd in bonded]):
+                o = top.addAtom("OXT", element.Element.getBySymbol("O"), residues[r], fake_serial)
+                coords.append(Quantity(vec3.Vec3(*pos), angstrom))
+                top.addBond(atoms[c], o)
+                fake_serial += 1
+                filter.append(False)
+    filter = numpy.array(filter)
 
     for b in structure.bonds:
         top.addBond(atoms[b.atoms[0]], atoms[b.atoms[1]])
@@ -91,7 +152,7 @@ def _minimize(session, structure):
                 session.logger.status("step %d: energy %.1f" % (self.step, args[0]["system energy"]), log=True)
                 crds = numpy.array(xyz)
                 crds = numpy.reshape(crds, (-1,3))
-                self.atoms.coords = crds * 10
+                self.atoms.coords = crds[filter] * 10
                 from chimerax.core.commands import run
                 run(session, "wait 1", log=False)
             return False
@@ -101,7 +162,7 @@ def _minimize(session, structure):
     final_crds = numpy.array([q.value_in_unit(angstrom)
         for q in context.getState(getPositions=True).getPositions()])
     final_crds = numpy.reshape(final_crds, (-1,3))
-    cx_atoms.coords = final_crds
+    cx_atoms.coords = final_crds[filter]
 
 def register_command(logger):
     from chimerax.core.commands import CmdDesc, register, Or, EmptyArg, EnumOf, BoolArg
