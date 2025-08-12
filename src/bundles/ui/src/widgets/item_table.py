36a8fa0496be3b8b15ba5b717eae1fdb1818a9e3
Eric Pettersen <pett@cgl.ucsf.edu>
2025-08-11 17:18:52 -0700

    Improved checkbox layout for extreme case; use case-independent sort for checkbox names

--- src/bundles/ui/src/widgets/item_table.py
+++ src/bundles/ui/src/widgets/item_table.py
@@ -835,12 +835,16 @@ class ItemTable(QTableView):
     def _arrange_col_checkboxes(self):
         while self._col_checkbox_layout.count() > 0:
             self._col_checkbox_layout.takeAt(0)
-        self._col_checkboxes.sort(key=lambda cb: cb.text())
+        self._col_checkboxes.sort(key=lambda cb: cb.text().lower())
         requested_cols = self._column_control_info[-2]
         num_buttons = len(self._col_checkboxes)
         from math import sqrt, ceil
         if requested_cols is None:
-            num_cols = int(sqrt(num_buttons)+0.5)
+            # checkboxes are much wider than tall, so to prevent the checkbox area from forcing
+            # dialogs to be wider in extreme cases (think >64 table columns, longish labels),
+            # scale number of columns based on rough estimated width
+            longest_text = max([len(cb.text()) for cb in self._col_checkboxes])
+            num_cols = int(sqrt(num_buttons/max(1, longest_text/15))+0.5)
         else:
             num_cols = requested_cols
         num_rows = int(ceil(num_buttons/num_cols))
