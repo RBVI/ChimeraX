e0b310aa389231f787a82f10ca410521c57b41e7
Tom Goddard <goddard@sonic.net>
2025-10-13 19:29:54 -0700

    Make mutationscores structure able to align to mutation data sequence instead of a specified sequence using "alignSequences true".  Also added minimum_percent_identity option (default 50) when aligning sequences.  Also made alignment use Needleman-Wunsch computed on the user's computer instead of Clustal Omega web service.

--- src/bundles/mutation_scores/src/ms_data.py
+++ src/bundles/mutation_scores/src/ms_data.py
@@ -75,6 +75,21 @@ class MutationSet(State):
     def computed_values_names(self):
         return tuple(self._computed_scores.keys())
 
+    def sequence(self, missing_code = 'X'):
+        '''
+        Returns a Sequence object starting with residue number 1.
+        Missing data will use 1-letter code "X".
+        '''
+        rnum_to_aa = self.residue_number_to_amino_acid()
+        rmax = max(rnum_to_aa.keys())
+        seq_list = [missing_code] * rmax
+        for rnum, aa in rnum_to_aa.items():
+            seq_list[rnum-1] = aa
+        seq_chars = ''.join(seq_list)
+        from chimerax.atomic import Sequence
+        seq = Sequence(characters = seq_chars)
+        return seq
+        
     def associate_chains(self, session, report = True):
         if len(self.associated_chains()) == 0:
             chains = _find_matching_chains(session, self.residue_number_to_amino_acid())
@@ -88,39 +103,55 @@ class MutationSet(State):
         self._remove_deleted_chains()
         return self._associated_chains
 
-    def set_associated_chains(self, chains, allow_mismatches = False, alignment = None):
+    def set_associated_chains(self, chains, allow_mismatches = None, minimum_identity = 0.5, pairing = None):
+        '''
+        Pairing maps chains to match list where a match list is a two tuple of mutation score
+        sequence numbers and corresponding chain ungapped positions.
+        '''
+        if allow_mismatches is None:
+            allow_mismatches = (pairing is not None)
         achains = []
         ares = []
         rnum_to_aa = self.residue_number_to_amino_acid()
-        if alignment:
-            _check_scores_sequence(alignment.seqs[0].ungapped(), rnum_to_aa)
         for chain in chains:
             cres = chain.existing_residues
-            if alignment:
-                renum = _alignment_renumbering(alignment, chain)
-                rnums = {r:rnum for r, rnum in zip(chain.residues, renum) if r is not None and rnum is not None}
-                from chimerax.atomic import Residues
-                cres = Residues([r for r in cres if r in rnums])
-                if len(cres) == 0:
+            if pairing is None:
+                cres_num = cres.numbers
+            else:
+                c2m = {crnum: mrnum for mrnum, crnum in pairing[chain]}
+                cmnums = [(cr, c2m[crnum]) for crnum,cr in enumerate(chain.residues) if cr and crnum in c2m]
+                if len(cmnums) == 0:
                     chain.structure.session.logger.warning(f'No residues of {chain} aligned to mutation score sequence')
                     continue
-                cres_num = [rnums[r] for r in cres]
-            else:
-                cres_num = cres.numbers
+                from chimerax.atomic import Residues
+                cres = Residues([cr for cr,mrnum in cmnums])
+                cres_num = [mrnum+1 for cr,mrnum in cmnums]
+
             matches, mismatches = _residue_type_matches(cres, cres_num, rnum_to_aa)
-            if matches > 0 and (len(mismatches) == 0 or allow_mismatches or alignment):
-                achains.append(chain)
-                ares.extend([(rnum,r) for r,rnum in zip(cres,cres_num) if rnum in rnum_to_aa])
-            if alignment:
-                msg = (f'Aligned {len(rnums)} residues of sequence {chain} to mutation scores {self.name} with {len(mismatches)} amino acid mismatches. {len(cres)} aligned residues have coordinates and mutation scores.')
-                chain.structure.session.logger.info(msg)
-            elif mismatches and not allow_mismatches:
+
+            if mismatches and not allow_mismatches:
                 r = mismatches[0]
                 msg = f'Did not associate chain {chain} because sequence does not match mutation set {self.name} at {len(mismatches)} positions, first mistmatch is {r.name} {r.number}.  Use the "alignSequences" or "allowMismatches" command options to associate this chain.'
                 chain.structure.session.logger.warning(msg)
+                accept = False
+            elif matches < minimum_identity * len(rnum_to_aa):
+                msg = (f'Did not associate chain {chain} with mutation scores {self.name} because only {matches} residues matched, less than {"%.0f"%(100*minimum_identity)} percent of {len(rnum_to_aa)} mutation set residues.')
+                chain.structure.session.logger.info(msg)
+                accept = False
+            elif pairing:
+                nalign = matches + len(mismatches)
+                msg = (f'Associated {nalign} residues of sequence {chain} to mutation scores {self.name} with {len(mismatches)} amino acid mismatches. {len(cres)} aligned residues have coordinates and mutation scores.')
+                chain.structure.session.logger.info(msg)
+                accept = True
             else:
                 msg = f'Associated chain {chain} to mutation scores {self.name} with {len(mismatches)} mismatches.'
                 chain.structure.session.logger.info(msg)
+                accept = True
+                
+            if accept:
+                achains.append(chain)
+                aligned_res = [(rnum,r) for r,rnum in zip(cres,cres_num) if rnum in rnum_to_aa]
+                ares.extend(aligned_res)
 
         self._associated_chains = achains
         self._associated_residues = ares
@@ -212,29 +243,13 @@ def _residue_type_matches(residues, res_nums, resnum_to_aa):
 
 def _check_scores_sequence(scores_sequence, rnum_to_aa):
     for rnum, aa in rnum_to_aa.items():
-        if rnum > len(scores_sequence) or scores_sequence[rnum-1] != aa:
+        if rnum > len(scores_sequence) or scores_sequence[rnum-1] not in (aa, 'X'):
             mseq = ['X'] * max(rnum_to_aa.keys())
             for rnum, aa in rnum_to_aa.items():
                 mseq[rnum-1] = aa
             mseq = ''.join(mseq)
             from chimerax.core.errors import UserError
             raise UserError(f'Alignment reference sequence "{scores_sequence}" does not match mutation scores sequence "{mseq}" at position {rnum}')
-
-def _alignment_renumbering(alignment, chain):
-    if chain not in alignment.associations:
-        from chimerax.core.errors import UserError
-        raise UserError(f'Chain {chain} is not present in alignment {alignment.ident}')
-
-    cseq = alignment.associations[chain]
-    rseq = alignment.seqs[0]
-    renum = [None] * len(cseq.ungapped())
-    for gi in range(len(rseq)):
-        ci = cseq.gapped_to_ungapped(gi)
-        ri = rseq.gapped_to_ungapped(gi)
-        if ci is not None and ri is not None:
-            renum[ci] = ri + 1
-
-    return renum
     
 class MutationScores(State):
     def __init__(self, residue_number, from_aa, to_aa, scores):
@@ -372,24 +387,32 @@ def mutation_scores_list(session):
     session.logger.info(f'{len(score_sets)} mutation score sets\n{sets}')
     return msm.names()
 
-def mutation_scores_structure(session, chains = None, allow_mismatches = False, align_sequences = None,
-                              mutation_set = None):
+def mutation_scores_structure(session, chains = None,
+                              allow_mismatches = None, minimum_percent_identity = 50,
+                              align_sequences = None, mutation_set = None):
     mset = mutation_scores(session, mutation_set)
     if chains is None:
         _report_associated_chains(mset, session.logger)
-    else:
-        from chimerax.atomic import Sequence
-        from chimerax.seqalign.alignment import Alignment
-        if isinstance(align_sequences, Sequence):
-            alignment = _sequence_alignment(align_sequences, chains)
-        elif isinstance(align_sequences, Alignment):
-            if len(align_sequences.seqs) == 1:
-                alignment = _sequence_alignment(align_sequences.seqs[0], chains)	# Single sequence specified
-            else:
-                alignment = align_sequences
+        return
+
+    if isinstance(align_sequences, bool) and align_sequences:
+        # Use mutation data sequence.
+        align_sequences = mset.sequence()
+    from chimerax.atomic import Sequence
+    from chimerax.seqalign.alignment import Alignment
+    if isinstance(align_sequences, Sequence):
+        pairing = _sequence_pairing(align_sequences, chains)
+    elif isinstance(align_sequences, Alignment):
+        if len(align_sequences.seqs) == 1:
+            pairing = _sequence_pairing(align_sequences.seqs[0], chains)	# Single sequence specified
         else:
-            alignment = None
-        mset.set_associated_chains(chains, allow_mismatches = allow_mismatches, alignment = alignment)
+            pairing = _sequence_pairing_from_alignment(mset, chains, align_sequences)
+    else:
+        pairing = None
+        
+    mset.set_associated_chains(chains, allow_mismatches = allow_mismatches,
+                               minimum_identity = minimum_percent_identity/100,
+                               pairing = pairing)
 
 def _report_associated_chains(mset, logger):
     chains = [chain for chain in mset.associated_chains()]
@@ -397,31 +420,30 @@ def _report_associated_chains(mset, logger):
     cspec = concise_chain_spec(chains)
     logger.status(f'Mutation set {mset.name} has {len(chains)} associated chains {cspec}.', log=True)
 
-def _sequence_alignment(scores_sequence, chains):
-    seqs = set()
-    seqs.add(scores_sequence.characters)
-    align_chains = []
+def _sequence_pairing(mseq, chains):
+    pairing = {}
+    from chimerax.alignment_algs.NeedlemanWunsch import nw
     for chain in chains:
-        if chain.characters not in seqs:
-            seqs.add(chain.characters)
-            align_chains.append(chain)
+        score, match_list = nw(mseq, chain)
+        pairing[chain] = match_list
+    return pairing
 
-    if len(align_chains) == 0:
-        if len(chains) > 0:
-            logger = chains[0].structure.session.logger
-            from chimerax.atomic import concise_chain_spec
-            cspec = concise_chain_spec(chains)
-            logger.info(f'All chains {cspec} have the same sequence as the mutation scores so no alignment is needed')
-        return None
+def _sequence_pairing_from_alignment(mset, chains, alignment):
+    rnum_to_aa = mset.residue_number_to_amino_acid()
+    # Make sure first sequence of alignment matches mutation data sequence.
+    mseq = alignment.seqs[0]
+    _check_scores_sequence(mseq.ungapped(), rnum_to_aa)
 
-    session = chains[0].structure.session
-    aseqs = ','.join(ac.characters for ac in align_chains)
-    from chimerax.core.commands import run
-    alignment = run(session, f'sequence align {scores_sequence.characters},{aseqs}')
-    alignment.seqs[0].name = 'scores'
-    for i,c in enumerate(align_chains):
-        alignment.seqs[i+1].name = f'{c.structure.name} {c.chain_id}'
-    return alignment
+    pairing = {}
+    for chain in chains:
+        if chain not in alignment.associations:
+            from chimerax.core.errors import UserError
+            raise UserError(f'Chain {chain} is not present in alignment {alignment.ident}')
+        cseq = alignment.associations[chain]
+        match_list = [(mseq.gapped_to_ungapped(gi), cseq.gapped_to_ungapped(gi)) for gi in range(len(mseq))]
+        pairing[chain] = match_list
+
+    return pairing
 
 def mutation_scores_close(session, mutation_set = None):
     msm = mutation_scores_manager(session)
@@ -441,7 +463,7 @@ def _close_plots(session, mutation_set_name):
             tool.delete()
 
 def register_commands(logger):
-    from chimerax.core.commands import CmdDesc, register, StringArg, BoolArg, Or
+    from chimerax.core.commands import CmdDesc, register, StringArg, BoolArg, FloatArg, Or
     from chimerax.atomic import UniqueChainsArg, SequenceArg
     from chimerax.seqalign import AlignmentArg
     
@@ -451,7 +473,8 @@ def register_commands(logger):
     desc = CmdDesc(
         optional = [('chains', UniqueChainsArg)],
         keyword = [('allow_mismatches', BoolArg),
-                   ('align_sequences', Or(SequenceArg, AlignmentArg)),
+                   ('minimum_percent_identity', FloatArg),
+                   ('align_sequences', Or(BoolArg, SequenceArg, AlignmentArg)),
                    ('mutation_set', StringArg)],
         synopsis = 'Associate a structure with a set of mutation scores'
     )
