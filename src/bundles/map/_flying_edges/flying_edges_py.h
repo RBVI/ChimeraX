a3737284eb7448daba8748c0c4456e31a94d3ad1
Zach Pearson <zjp@cgl.ucsf.edu>
2025-12-01 15:55:25 -0800

    feat(map): Flying Edges

---
+++ src/bundles/map/_flying_edges/flying_edges_py.h
@@ -0,0 +1,37 @@
+// vi: set expandtab shiftwidth=4 softtabstop=4:
+
+/*
+ * === UCSF ChimeraX Copyright ===
+ * Copyright 2022 Regents of the University of California. All rights reserved.
+ * The ChimeraX application is provided pursuant to the ChimeraX license
+ * agreement, which covers academic and commercial uses. For more details, see
+ * <https://www.rbvi.ucsf.edu/chimerax/docs/licensing.html>
+ *
+ * This particular file is part of the ChimeraX library. You can also
+ * redistribute and/or modify it under the terms of the GNU Lesser General
+ * Public License version 2.1 as published by the Free Software Foundation.
+ * For more details, see
+ * <https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html>
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. ADDITIONAL LIABILITY
+ * LIMITATIONS ARE DESCRIBED IN THE GNU LESSER GENERAL PUBLIC LICENSE
+ * VERSION 2.1
+ *
+ * This notice must be embedded in or attached to all copies, including partial
+ * copies, of the software or any revisions or derivations thereof.
+ * === UCSF ChimeraX Copyright ===
+ */
+
+#ifndef FLYING_EDGES_PY_HEADER_INCLUDED
+#define FLYING_EDGES_PY_HEADER_INCLUDED
+
+extern "C" {
+
+PyObject *surface_py(PyObject *s, PyObject *args, PyObject *keywds);
+PyObject *reverse_triangle_vertex_order(PyObject *, PyObject *args);
+
+}
+
+#endif
