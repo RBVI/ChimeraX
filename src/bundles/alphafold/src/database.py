6c66d6e03b2e7c91b7fb7179a15767fa13cff4f1
Tom Goddard <goddard@sonic.net>
2026-02-27 12:11:57 -0800

    Fixed AlphaFold database fetch to use v6 of database in older ChimeraX.  Ticket #19909

--- src/bundles/alphafold/src/database.py
+++ src/bundles/alphafold/src/database.py
@@ -28,12 +28,11 @@ from chimerax.core.settings import Settings
 
 class _AlphaFoldDatabaseSettings(Settings):
     EXPLICIT_SAVE = {
-        'database_url': 'https://alphafold.ebi.ac.uk/files/AF-{uniprot_id}-F1-model_v{version}.cif',
-        'database_url2': 'https://alphafold.ebi.ac.uk/files/AF-{uniprot_id}{isoform}-F{fragment}-model_v{version}.cif',
+        'database_url': 'https://alphafold.ebi.ac.uk/files/AF-{uniprot_id}{isoform}-F{fragment}-model_v{version}.cif',
         'database_version': '6',
         'last_update_time': 0.0,	# seconds since 1970 epoch
         'update_interval': 86400.0,	# seconds
-        'update_url': 'https://www.rbvi.ucsf.edu/chimerax/data/status/alphafold_database3.json',
+        'update_url': 'https://www.rbvi.ucsf.edu/chimerax/data/status/alphafold_database6.json',
     }
 
 # -----------------------------------------------------------------------------
