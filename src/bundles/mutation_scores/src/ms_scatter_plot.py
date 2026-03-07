98426ce40510cc5da58c1d9bcb5f7f6c1b94f83f
Tom Goddard <goddard@sonic.net>
2026-03-06 16:55:46 -0800

    Made mutation score scatter plot start floating instead of docked since it is large.  Also made scatterplot not clip the x-axis label.  Ticket #19165

--- src/bundles/mutation_scores/src/ms_scatter_plot.py
+++ src/bundles/mutation_scores/src/ms_scatter_plot.py
@@ -71,6 +71,7 @@ class MutationScatterPlot(Graph):
         Graph.__init__(self, session, nodes, edges,
                        tool_name = 'Mutation scores plot', title = 'Mutation scores plot',
                        hide_ticks = False, drag_select_callback = self._rectangle_selected)
+        self.figure.set_layout_engine(layout='constrained')  # Avoid clipping axis labels
 
         parent = self.tool_window.ui_area
         layout = parent.layout()
@@ -86,6 +87,8 @@ class MutationScatterPlot(Graph):
         # Add status line
         sl = self._create_status_line(parent)
         layout.addWidget(sl)
+
+        self.tool_window.manage(placement=None)	# Floating to handle large initial size
 
     def _create_axes_menus(self, parent):
         from chimerax.ui.widgets import EntriesRow
