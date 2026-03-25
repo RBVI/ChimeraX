260923138eb332f6c011f1453144eeeadd58b999
Tom Goddard <goddard@sonic.net>
2026-03-25 11:37:21 -0700

    Change OpenFold installation default branch to use chimerax_openfold_preview2.  Ticket #20065

--- src/bundles/openfold/src/download_weights.py
+++ src/bundles/openfold/src/download_weights.py
@@ -1,7 +1,6 @@
 def download_weights():
     '''Call OpenFold code to fetch the model weights.'''
-    from openfold3.entry_points.validator import _maybe_download_parameters, DEFAULT_CACHE_PATH, CHECKPOINT_NAME
-    DEFAULT_CACHE_PATH.mkdir(parents=True, exist_ok=True)
-    _maybe_download_parameters(DEFAULT_CACHE_PATH / CHECKPOINT_NAME)
+    from openfold3.setup_openfold import main
+    main(prompt = False, test = False)
 
 download_weights()
