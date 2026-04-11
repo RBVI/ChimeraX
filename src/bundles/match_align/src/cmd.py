191dc01922efb4761a1fde5b61b9c67726d375d1
Eric Pettersen <pett@cgl.ucsf.edu>
2026-04-10 18:02:13 -0700

    More processing of Match->Align results; allow associate() output to be suppress so it doesn't spam the log when forcing initial associations

--- src/bundles/match_align/src/cmd.py
+++ src/bundles/match_align/src/cmd.py
@@ -59,7 +59,6 @@ def make_alignment(session, chains, *, circular=defaults['circular'],
         iteration = 1
         if not ref_chain:
             ref_chain = aligned[0]
-        from chimerax.atomic import Atoms
         from chimerax.std_commands import align
         while True:
             ref_seq = [s for s in aligned if s.structure == ref_chain.structure][0]
@@ -83,11 +82,11 @@ def make_alignment(session, chains, *, circular=defaults['circular'],
                 break
 
             # match
-            ref_atoms = Atoms(column_atoms(ref_seq, culled))
+            ref_atoms = column_atoms(ref_seq, culled)
             for seq in aligned:
                 if seq.structure == ref_seq.structure:
                     continue
-                seq_atoms = Atoms(column_atoms(seq, culled))
+                seq_atoms = column_atoms(seq, culled)
                 session.logger.info("Matching %s onto %s" % (seq.name, ref_seq.name))
                 prev_aligned = aligned
                 aligned = [copy(chain) for chain in ordered]
@@ -105,11 +104,52 @@ def make_alignment(session, chains, *, circular=defaults['circular'],
                 break
             iteration += 1
 
+    if full_cols:
+        # show pairwise RMSD matrix in fully populated columns
+        coords = {}
+        for seq in aligned:
+            coords[seq] = column_atoms(seq, full_cols).scene_coords
+        dsq_sum = 0
+        rmsd_matrix = {}
+        from math import sqrt
+        import numpy
+        for i, s1 in enumerate(aligned):
+            rmsd_matrix[(s1, s1)] = 0.0
+            for s2 in aligned[i+1:]:
+                diff = coords[s1] - coords[s2]
+                v = sqrt(numpy.sum(diff * diff) / len(coords[s1]))
+                dsq_sum += v * v
+                rmsd_matrix[(s1,s2)] = rmsd_matrix[(s2,s1)] = v
+        overall_rmsd = sqrt(2 * dsq_sum / (len(aligned) * (len(aligned)-1)))
+        session.logger.info("Overall RMSD: %.3f" % overall_rmsd)
+        table_texts = []
+        from chimerax.core.logger import html_table_params
+        table_texts.append('<table %s>' % html_table_params)
+        table_texts.append(' <thead>')
+        table_texts.append('  <tr>')
+        table_texts.append('   <th colspan="%d">Pairwise RMSDs across all fully populated columns</th>'
+            % (len(aligned)+1))
+        table_texts.append('  </tr>')
+        table_texts.append(' </thead>')
+        table_texts.append(' <tbody>')
+        table_texts.append('  <tr>')
+        table_texts.append('   ' + ' '.join(['<td style="text-align:center">%s</td>' % item
+            for item in (["Model"]+[seq.structure.id_string for seq in aligned])]))
+        table_texts.append('  </tr>')
+        for s1 in aligned:
+            table_texts.append('  <tr>')
+            table_texts.append('   ' + ' '.join(['<td style="text-align:center">%s</td>' % item
+                for item in ([s1.structure.id_string]+["%.3f" % rmsd_matrix[(s1,s2)] for s2 in aligned])]))
+            table_texts.append('  </tr>')
+        table_texts.append(' </tbody>')
+        table_texts.append('</table>')
+        session.logger.info('\n'.join(table_texts), is_html=True)
+
     #TODO: lots
     alignment = session.alignments.new_alignment(aligned, ident,
         name="Match\N{RIGHTWARDS ARROW}Align", auto_associate=False, viewer=show_alignment)
     for orig, aligned in zip(ordered, aligned):
-        alignment.associate(orig, seq=aligned)
+        alignment.associate(orig, seq=aligned, silent=True)
     for hdr in alignment.headers:
         if hdr.ident == "rmsd":
             hdr.shown = True
@@ -126,9 +166,10 @@ def column_atoms(seq, columns):
     seq_columns = [seq.gapped_to_ungapped(i) for i in columns]
     num_residues = seq.num_residues
     residues = seq.residues
+    from chimerax.atomic import Atoms
     if seq.circular:
-        return [r.principal_atom for r in [residues[i % num_residues] for i in seq_columns]]
-    return [r.principal_atom for r in [residues[i] for i in seq_columns]]
+        return Atoms([r.principal_atom for r in [residues[i % num_residues] for i in seq_columns]])
+    return Atoms([r.principal_atom for r in [residues[i] for i in seq_columns]])
 
 def fully_populated(seqs):
     full_cols = []
