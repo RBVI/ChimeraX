3b270afd0698c298bd442b64ca39df3052bb90a6
Greg Couch <gregc@cgl.ucsf.edu>
2026-07-23 19:14:34 -0700

    Don't even try to fetch CCD for PDB's never used CCDs.

--- src/bundles/mmcif/mmcif_cpp/corecif.cpp
+++ src/bundles/mmcif/mmcif_cpp/corecif.cpp
@@ -350,7 +350,7 @@ SmallMolecule::parse_atom_site()
     string current_assembly, current_group, first_group;
     compute_cell_matrix();
     molecule = new AtomicStructure(_logger);
-    residue = molecule->new_residue("UNL", "A", 1);
+    residue = molecule->new_residue("LIG", "A", 1);
     std::vector<Atom*> alt_atoms;
     size_t alt_count = 0;
     bool alt_warned = false;
