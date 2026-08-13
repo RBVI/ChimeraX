ee01e16101677ae59c1b63c38f9bb142c6d5ee87
Greg Couch <gregc@cgl.ucsf.edu>
2026-08-13 16:20:07 -0700

    Preserve spaces in valgrind arguments

--- src/apps/valgrind/callgrind.sh
+++ src/apps/valgrind/callgrind.sh
@@ -4,4 +4,4 @@ bindir=`/usr/bin/dirname "$0"`
 app=`/usr/bin/basename "$0" .valgrind`
 datadir="$bindir/../share"
 
-exec valgrind --tool=callgrind --collect-atstart=no "$bindir/$app" $@
+exec valgrind --tool=callgrind --collect-atstart=no "$bindir/$app" "$@"
